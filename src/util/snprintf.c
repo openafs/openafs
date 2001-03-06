93f1dd5021f88e6c1e39c3e32b2f74aff91708bc
Johan Danielsson <joda@pdc.kth.se>
2001-03-06 22:20:16 +0000

    STABLE10-support-dux-51-20010305
    (cherry picked from commit bc5204d4e79198501608be68434106c7be119d67)

--- src/util/snprintf.c
+++ src/util/snprintf.c
@@ -1,7 +1,7 @@
 /* snprintf.c - Formatted, length-limited print to a string */
 
 #include <afs/param.h>
-#if defined(AFS_OSF20_ENV) || defined(AFS_AIX32_ENV)
+#if defined(AFS_OSF20_ENV) && !defined(AFS_DUX50_ENV) || defined(AFS_AIX32_ENV)
 
 #include <sys/types.h>
 #include <stdarg.h>
