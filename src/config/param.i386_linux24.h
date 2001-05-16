b9c11bc28108c3815a639314b72697016a9c4d87
Derrick Brashear <shadow@dementia.org>
2001-05-16 05:16:27 +0000

    linux24-param-cleanup-20010516
    make usr version of file not define 64 bit env
    make kernel version include modversions before afs_sysnames.h (which includes
    stds.h)

--- src/config/param.i386_linux24.h
+++ src/config/param.i386_linux24.h
@@ -20,22 +20,22 @@
 #define AFS_SYSCALL 137
 #define AFS_64BIT_IOPS_ENV  1
 #define AFS_NAMEI_ENV     1   /* User space interface to file system */
-#include <afs/afs_sysnames.h>
 
 #define AFS_USERSPACE_IP_ADDR 1
 #define RXK_LISTENER_ENV 1
 #define AFS_GCPAGS             0       /* if nonzero, garbage collect PAGs */
 
-
-/* Machine / Operating system information */
-#define SYS_NAME       "i386_linux24"
-#define SYS_NAME_ID    SYS_NAME_ID_i386_linux24
 #define AFSLITTLE_ENDIAN    1
 #define AFS_HAVE_FFS        1       /* Use system's ffs. */
 #define AFS_HAVE_STATVFS    0  /* System doesn't support statvfs */
 #define AFS_VM_RDWR_ENV            1   /* read/write implemented via VM */
 
 #if defined(__KERNEL__) && !defined(KDUMP_KERNEL)
+#if defined(MODULE) && defined(CONFIG_MODVERSIONS)
+#define MODVERSIONS
+#include <linux/modversions.h>
+#endif
+
 #include <linux/config.h>
 #ifdef CONFIG_SMP
 #undef CONFIG_SMP
@@ -52,11 +52,6 @@
 #define AFS_GLOBAL_SUNLOCK
 #endif
 
-#if defined(MODULE) && defined(CONFIG_MODVERSIONS)
-#define MODVERSIONS
-#include <linux/modversions.h>
-#endif
-
 #endif /* __KERNEL__  && !DUMP_KERNEL*/
 
 #ifdef KERNEL
@@ -68,4 +63,11 @@
 #endif
 #endif /* KERNEL */
 
+#include <afs/afs_sysnames.h>
+/* Machine / Operating system information */
+#define SYS_NAME       "i386_linux24"
+#define SYS_NAME_ID    SYS_NAME_ID_i386_linux24
+
 #endif /* _PARAM_I386_LINUX24_H_ */
+
+
