b4bc4c4c418d9f6a75697cdaf8024ee812aed1f4
Derrick Brashear <shadow@dementia.org>
2001-06-23 22:48:36 +0000

    STABLE10-winnt-redefine-snprintf-instead-of-building-one-20010429
    WINNT has _snprintf, just use that
    pull onto stable
    
    
    (cherry picked from commit 8ee4e0cec9f70f4c1cf44fad0a210f1014680a0d)

--- src/util/snprintf.c
+++ src/util/snprintf.c
@@ -1,7 +1,7 @@
 /* snprintf.c - Formatted, length-limited print to a string */
 
 #include <afs/param.h>
-#if defined(AFS_OSF20_ENV) && !defined(AFS_DUX50_ENV) || defined(AFS_AIX32_ENV) || defined(AFS_NT40_ENV)
+#if defined(AFS_OSF20_ENV) && !defined(AFS_DUX50_ENV) || defined(AFS_AIX32_ENV)
 #include <sys/types.h>
 #include <stdarg.h>
 #include <stdio.h>
