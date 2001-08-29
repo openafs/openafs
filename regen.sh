1c70ed1f9ce3a58147d38cb0ce5ac2c24456b9fa
Jeffrey Hutzelman <jhutz@cs.cmu.edu>
2001-08-29 20:17:19 +0000

    libafs-make-subtree-during-build-for-later-use-20010829
    at build time, create a subtree for use later which can be used to
    build *only* kernel modules, but only for the afs sysname you were building
    for in the first place currently.

--- regen.sh
+++ regen.sh
@@ -3,6 +3,9 @@ echo "Running aclocal"
 aclocal -I src/cf
 echo "Running autoconf"
 autoconf
+echo "Running autoconf for configure-libafs"
+autoconf configure-libafs.in > configure-libafs
+chmod +x configure-libafs
 echo "Running autoheader"
 autoheader
 #echo "Running automake"
