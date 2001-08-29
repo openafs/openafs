0eb68cde324d26b145e648075524f5e2207c9101
Derrick Brashear <shadow@dementia.org>
2001-08-29 01:46:57 +0000

    linux-call-afs-dopartialwrite-from-afs-memwrite-and-afs-ufswrite-not-afs-linux-write-20010828
    attempt to deal with case where data not being flushed from cache

--- src/afs/VNOPS/afs_vnop_write.c
+++ src/afs/VNOPS/afs_vnop_write.c
@@ -171,7 +171,7 @@ afs_MemWrite(avc, auio, aio, acred, noLock)
 	return (EFBIG);
     }
 #endif
-#ifdef	AFS_VM_RDWR_ENV
+#if (!defined(AFS_VM_RDWR_ENV)||defined(AFS_LINUX20_ENV))
     /*
      * If write is implemented via VM, afs_FakeOpen() is called from the
      * high-level write op.
@@ -280,7 +280,7 @@ afs_MemWrite(avc, auio, aio, acred, noLock)
 	if (filePos > avc->m.Length)
 	    avc->m.Length = filePos;
 #endif
-#ifndef	AFS_VM_RDWR_ENV
+#if (!defined(AFS_VM_RDWR_ENV)||defined(AFS_LINUX20_ENV))
 	/*
 	 * If write is implemented via VM, afs_DoPartialWrite() is called from
 	 * the high-level write op.
