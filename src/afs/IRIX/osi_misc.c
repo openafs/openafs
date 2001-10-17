f5d36b1c7094a8a2c74ceb292e8ef84a894236fc
Derrick Brashear <shadow@dementia.org>
2001-10-17 22:53:48 +0000

    irix-first-pass-at-ip35-support-20011017
    based on extensive feedback from Forest Godfrey and Michael Raymond
    of SGI

--- src/afs/IRIX/osi_misc.c
+++ src/afs/IRIX/osi_misc.c
@@ -51,14 +51,16 @@ int afs_init_kernel_config(int flag)
 	    inventory_t *pinv;
 	    /* test for numa arch. */
 	    /* Determine if thisis a NUMA platform. Currently, this is true
-	     * only if it's an IP27.
+	     * only if it's an IP27 or IP35.
 	     */
 	    pinv = find_inventory((inventory_t*)NULL, INV_PROCESSOR,
 				  INV_CPUBOARD, -1, -1, -1);
 	    if (!pinv)
 		code = ENODEV;
-	    else
-		afs_is_numa_arch = (pinv->inv_state == INV_IP27BOARD) ? 1 : 0;
+	    else 
+		afs_is_numa_arch = ((pinv->inv_state == INV_IP27BOARD) || 
+				    (pinv->inv_state == INV_IP35BOARD)) 
+		  ? 1 : 0;
 	}
 	else
 	    afs_is_numa_arch = flag;
@@ -85,6 +87,8 @@ int afs_ipno = 27;
 int afs_ipno = 28;
 #elif defined(IP30)
 int afs_ipno = 30;
+#elif defined(IP35)
+int afs_ipno = 35;
 #else
 int afs_ipno = -1;
 #endif
