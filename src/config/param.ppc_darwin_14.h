816b7d1aff8cce6e52855c02aa316bc486ef6cab
Chaskiel M Grundman <cg2v@andrew.cmu.edu>
2002-04-29 16:34:04 +0000

    PROTO13-darwin-use-namei-fileserver-20020429
    build namei fileserver for darwin
    
    
    (cherry picked from commit c2e075fba4cec152e772ec5514d52e8ec5b29990)

--- src/config/param.ppc_darwin_14.h
+++ src/config/param.ppc_darwin_14.h
@@ -13,6 +13,7 @@
 #define AFS_DARWIN14_ENV
 #define AFS_NONFSTRANS
 #define AFS_SYSCALL             230
+#define AFS_NAMEI_ENV 1
 
 /* File system entry (used if mount.h doesn't define MOUNT_AFS */
 #define AFS_MOUNT_AFS    "afs"
