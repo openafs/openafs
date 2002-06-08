8c0967a05551513a1209a57272a2f900148b64db
Nathan Neulinger <nneul@umr.edu>
2002-06-08 01:18:35 +0000

    STABLE12-pioctl-avoid-inadvertantly-setting-new-pags-due-to-gcc-20020516
    some versions of gcc compile the if used to decide whether to allocate a pag
    badly. make life easier.
    
    
    (cherry picked from commit 10486b5652f0fb5a1d8c1fe5ddbbf5daa2477974)

--- src/afs/afs_pioctl.c
+++ src/afs/afs_pioctl.c
@@ -1414,7 +1414,8 @@ static PGCPAGs(avc, afun, areq, ain, aout, ainSize, aoutSize, acred)
       memcpy((char *)&flag, ain, sizeof(afs_int32));		/* primary id flag */
       ain += sizeof(afs_int32);			/* skip id field */
       /* rest is cell name, look it up */
-      if (flag & 0x8000) {			/* XXX Use Constant XXX */
+      /* some versions of gcc appear to need != 0 in order to get this right */
+      if (flag & 0x8000 != 0) {			/* XXX Use Constant XXX */
 	  flag &= ~0x8000;
 	  set_parent_pag = 1;
       }
