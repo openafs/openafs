16abafbf117438054c8678fc7a7ea8b9f69eb08a
Derrick Brashear <shadow@dementia.org>
2002-06-08 21:29:49 +0000

    STABLE12-warn-of-but-allow-bogus-shutdown-20020608
    despite the danger of it, allow shutdown without umount, to prevent hanging

--- src/afs/afs_call.c
+++ src/afs/afs_call.c
@@ -544,11 +544,10 @@ long parm, parm2, parm3, parm4, parm5, parm6;
 #endif
 	afs_cold_shutdown = 0;
 	if (parm == 1) afs_cold_shutdown = 1;
-	if (afs_globalVFS != 0) {
-	    afs_warn("AFS isn't unmounted yet! Call aborted\n");
-	    code = EACCES;
-	} else
-	    afs_shutdown();
+	if (afs_globalVFS != 0) 
+	    afs_warn("AFS isn't unmounted yet!\n");
+
+	afs_shutdown();
     }
 
 #if	! defined(AFS_HPUX90_ENV) || defined(AFS_HPUX100_ENV)
