529f9c40359817cddbefe329b65c620e00ece456
Chas Williams <chas@cmf.nrl.navy.mil>
2002-07-31 23:31:10 +0000

    STABLE12-linux-alloc-drop-glock-if-we-can-20020731
    if we can drop the glock, do so. allocate up to PAGE_SIZE with kmalloc before
    switching to vmalloc
    
    
    (cherry picked from commit 4d75219bedc12833c6265b6e018e6455aa88921c)

--- src/afs/LINUX/osi_sleep.c
+++ src/afs/LINUX/osi_sleep.c
@@ -139,7 +139,7 @@ static void afs_addevent(char *event)
     
     AFS_ASSERT_GLOCK();
     hashcode = afs_evhash(event);
-    newp = osi_AllocSmallSpace(sizeof(afs_event_t));
+    newp = osi_linux_alloc(sizeof(afs_event_t), 0);
     afs_evhashcnt++;
     newp->next = afs_evhasht[hashcode];
     afs_evhasht[hashcode] = newp;
