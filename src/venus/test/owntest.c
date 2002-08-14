abc490d0fcdc35959bbe2b092e19beae9a22f492
Nathan Neulinger <nneul@umr.edu>
2002-08-14 02:21:23 +0000

    venus-owntest-cleanup-20020813
    exit with errno on failures

--- src/venus/test/owntest.c
+++ src/venus/test/owntest.c
@@ -35,11 +35,15 @@ char **argv; {
     pn = argv[1];
     printf("Starting tests on %s.\n", pn);
     code = chmod(pn, 0444);
-    if (code<0)
-	return perror("chmod to RO");
+    if (code<0) {
+	perror("chmod to RO");
+	exit(errno);
+    }
     code = chmod(pn, 0666);
-    if (code<0)
-	return perror("chmod back to RW");
+    if (code<0) {
+	perror("chmod back to RW");
+	exit(errno);
+    }
     gettimeofday(&tv[0], (char *) 0);
     gettimeofday(&tv[1], (char *) 0);
     tv[0].tv_sec -= 10000;
@@ -47,11 +51,15 @@ char **argv; {
     tv[1].tv_sec -= 20000;
     tv[1].tv_usec = 0;
     code = utimes(pn, tv);
-    if (code<0)
-	return perror("utimes");
+    if (code<0) {
+	perror("utimes");
+	exit(errno);
+    }
     code = stat(pn, &tstat);
-    if (code<0)
-	return perror("stat");
+    if (code<0) {
+	perror("stat");
+	exit(errno);
+    }
     if (tstat.st_mtime != tv[1].tv_sec) {
 	printf("modtime didn't stick\n");
 	exit(1);
