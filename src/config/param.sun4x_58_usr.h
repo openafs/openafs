acf0eeab5d31028fe679548a655e3d1ba6f7725d
Nathan Neulinger <nneul@umr.edu>
2002-08-14 02:26:50 +0000

    PROTO13-solaris8-param-cleanup-nested-comments-20020813
    get rid of nested comment
    
    
    (cherry picked from commit 8c2edd9de637404f0f3b940f81a248105376068f)

--- src/config/param.sun4x_58_usr.h
+++ src/config/param.sun4x_58_usr.h
@@ -19,7 +19,9 @@
 
 #include <afs/afs_sysnames.h>
 
-/*#define AFS_GLOBAL_SUNLOCK	1	/* For global locking */
+#if 0
+#define AFS_GLOBAL_SUNLOCK	1	/* For global locking */
+#endif
 
 #define	AFS_3DISPARES		1	/* Utilize the 3 available disk inode 'spares' */
 #define	AFS_SYSCALL		65
