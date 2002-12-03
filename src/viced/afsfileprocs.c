bb66c6098b0d20799a1ede5614b655f5f583af14
Jim Rees <rees@umich.edu>
2002-12-03 17:44:14 +0000

    remove-prototypes-20021203
    Remove prototypes from viced_prototypes.h.

--- src/viced/afsfileprocs.c
+++ src/viced/afsfileprocs.c
@@ -195,6 +195,35 @@ extern int CEs, CEBlocks;
 
 extern int HTs, HTBlocks;
 
+afs_int32
+FetchData_RXStyle(Volume *volptr, 
+		  Vnode *targetptr, 
+		  register struct rx_call *Call,
+		  afs_int32 Pos,
+		  afs_int32 Len,
+		  afs_int32 Int64Mode,
+#if FS_STATS_DETAILED
+		  afs_int32 *a_bytesToFetchP,
+		  afs_int32 *a_bytesFetchedP
+#endif /* FS_STATS_DETAILED */
+		  );
+
+afs_int32
+StoreData_RXStyle(Volume *volptr,
+		  Vnode *targetptr,
+		  struct AFSFid *Fid,
+		  struct client *client,
+		  register struct rx_call *Call,
+		  afs_uint32 Pos,
+		  afs_uint32 Length,
+		  afs_uint32 FileLength,
+		  int sync,
+#if FS_STATS_DETAILED
+		  afs_int32 *a_bytesToStoreP,
+		  afs_int32 *a_bytesStoredP
+#endif /* FS_STATS_DETAILED */
+		  );
+
 #ifdef AFS_SGI_XFS_IOPS_ENV
 #include <afs/xfsattrs.h>
 static int GetLinkCount(Volume *avp, struct stat *astat)
