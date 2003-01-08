29454d28d06594e97e53c5cf066ad0b0f472f58d
Jim Rees <rees@umich.edu>
2003-01-08 23:07:03 +0000

    openbsd-20030109
    Add AFS_64BIT_ENV. Should have been there all along.

--- src/config/param.i386_obsd31.h
+++ src/config/param.i386_obsd31.h
@@ -7,13 +7,13 @@
 
 #ifndef IGNORE_STDS_H
 #include <sys/param.h>
-#include <afs/afs_sysnames.h>
 #endif
 
 #define AFS_XBSD_ENV		1	/* {Free,Open,Net}BSD */
 #define AFS_X86_XBSD_ENV	1
 
 #define AFS_NAMEI_ENV		1	/* User space interface to file system */
+#define AFS_64BIT_ENV		1
 #define AFS_64BIT_IOPS_ENV	1	/* Needed for NAMEI */
 #define	AFS_OBSD_ENV		1
 #define AFS_NONFSTRANS		1
@@ -27,14 +27,18 @@
 #define AFS_SYSCALL		208
 #define AFS_MOUNT_AFS		"afs"
 
-#define SYS_NAME		"i386_obsd31"
-#define SYS_NAME_ID		SYS_NAME_ID_i386_obsd31
-
 #define RXK_LISTENER_ENV	1
 #define AFS_GCPAGS	        0       /* if nonzero, garbage collect PAGs */
 #define AFS_USE_GETTIMEOFDAY    1       /* use gettimeofday to implement rx clock */
 
 #define AFSLITTLE_ENDIAN	1
+
+#ifndef IGNORE_STDS_H
+#include <afs/afs_sysnames.h>
+#endif
+
+#define SYS_NAME		"i386_obsd31"
+#define SYS_NAME_ID		SYS_NAME_ID_i386_obsd31
 
 /* Extra kernel definitions (from kdefs file) */
 #ifdef _KERNEL
