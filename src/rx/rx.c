7187d07423c1f3b4db3abc57a44f28c394a9e8ee
Derrick Brashear <shadow@dementia.org>
2003-03-13 19:01:24 +0000

    rx-init-mutex-before-calling-getudpsocket-20030313
    pointed out by cg2v@andrew.cmu.edu

--- src/rx/rx.c
+++ src/rx/rx.c
@@ -398,6 +398,8 @@ int rx_Init(u_int port)
     rxi_InitializeThreadSupport();
 #endif
 
+    MUTEX_INIT(&rx_stats_mutex, "rx_stats_mutex",MUTEX_DEFAULT,0);
+
     /* Allocate and initialize a socket for client and perhaps server
      * connections. */
 
@@ -412,7 +414,6 @@ int rx_Init(u_int port)
 #ifdef RX_LOCKS_DB
     rxdb_init();
 #endif /* RX_LOCKS_DB */
-    MUTEX_INIT(&rx_stats_mutex, "rx_stats_mutex",MUTEX_DEFAULT,0);
     MUTEX_INIT(&rx_rpc_stats, "rx_rpc_stats",MUTEX_DEFAULT,0);
     MUTEX_INIT(&rx_freePktQ_lock, "rx_freePktQ_lock",MUTEX_DEFAULT,0);
     MUTEX_INIT(&freeSQEList_lock, "freeSQEList lock",MUTEX_DEFAULT,0);
