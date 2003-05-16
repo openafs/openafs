86c857fc3bf0fba19de42e1c84bc930f0062a93f
Derrick Brashear <shadow@dementia.org>
2003-05-16 18:43:21 +0000

    STABLE12-linux-try-sys-wait4-20030516
    we may have close/wait4 instead of close/chdir or open/exit
    
    
    (cherry picked from commit a8b536e7d3f45abd20fa0ea293f5f313189114d4)

--- src/afs/LINUX/osi_module.c
+++ src/afs/LINUX/osi_module.c
@@ -283,6 +283,13 @@ int init_module(void)
 	    break;
 	}
 #else
+#if defined(EXPORTED_SYS_WAIT4) && defined(EXPORTED_SYS_CLOSE)
+        if (ptr[0] == (unsigned long)&sys_close &&
+            ptr[__NR_wait4 - __NR_close] == (unsigned long)&sys_wait4) {
+            sys_call_table=ptr - __NR_close;
+            break;
+        }
+#else
 #if defined(EXPORTED_SYS_CHDIR) && defined(EXPORTED_SYS_CLOSE)
         if (ptr[0] == (unsigned long)&sys_close &&
 	    ptr[__NR_chdir - __NR_close] == (unsigned long)&sys_chdir) {
@@ -295,6 +302,7 @@ int init_module(void)
 	    sys_call_table=ptr - __NR_exit;
 	    break;
 	}
+#endif
 #endif
 #endif
     }
