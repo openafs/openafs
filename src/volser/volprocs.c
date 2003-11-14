79b67c7461097ab5ca1dac9b9c1f069dfb4d290d
Derrick Brashear <shadow@dementia.org>
2003-11-14 23:36:16 +0000

    namei-ihandle-leak-20031114
    FIXES 2518
    
    namei_dec now logs if you have open ihandles when you unlink a file
    
    ====================
    This delta was composed from multiple commits as part of the CVS->Git migration.
    The checkin message with each commit was inconsistent.
    The following are the additional commit messages.
    ====================
    FIXES 2518
    
    flush all dir buffers we have cached when rewriting a volume from a restore
    
    avoids holding an ihandle which has an open fdhandle for an unlinked file
    
    ====================
    FIXES 2518
    
    remove logging code for now. it appears to trigger on some possibly legitimate operations.

--- src/volser/volprocs.c
+++ src/volser/volprocs.c
@@ -1446,6 +1446,9 @@ VolRestore(acid, atrans, aflags, cookie)
     }
     strcpy(tt->lastProcName, "Restore");
     tt->rxCallPtr = acid;
+
+    DFlushVolume(V_parentId(tt->volume)); /* Ensure dir buffers get dropped */
+
     code = RestoreVolume(acid, tt->volume, (aflags & 1), cookie);	/* last is incrementalp */
     FSYNC_askfs(tt->volid, NULL, FSYNC_RESTOREVOLUME, 0l);	/*break call backs on the
 								 * restored volume */
