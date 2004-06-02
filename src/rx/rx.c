5d9fcb30347e97471e5604828ef433683ca7a766
Derrick Brashear <shadow@dementia.org>
2004-06-02 07:08:01 +0000

    attachserverproc-dont-dequeue-if-not-in-queue-20040602
    FIXES 4699
    
    can't see what else this can be. pile of details in the ticket.

--- src/rx/rx.c
+++ src/rx/rx.c
@@ -3993,7 +3993,8 @@ rxi_AttachServerProc(register struct rx_call *call,
 	    MUTEX_ENTER(&rx_stats_mutex);
 	    rx_nWaiting--;
 	    MUTEX_EXIT(&rx_stats_mutex);
-	    queue_Remove(call);
+	    if (queue_IsOnQueue(call))
+	        queue_Remove(call);
 	}
 	call->state = RX_STATE_ACTIVE;
 	call->mode = RX_MODE_RECEIVING;
