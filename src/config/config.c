0bc9af9ed91689ef8a9e0c631b927f1674195a35
Jeff Mahoney <jeffm@suse.com>
2004-07-29 04:57:03 +0000

    makefileproto-configer-cleanup-20040728
    make config tool for MakefileProto processing able to deal with e.g.
    "linux26" for all linux 2.6 platforms

--- src/config/config.c
+++ src/config/config.c
@@ -12,6 +12,7 @@
 #include <sys/file.h>
 #include <sys/time.h>
 #include <stdio.h>
+#include <string.h>
 #include <errno.h>
 
 #include "AFS_component_version_number.c"
@@ -24,8 +25,9 @@ main(int argc, char **argv)
 {
     register FILE *infile;
     register FILE *outfile;
-    char *alist[3];
+    char *alist[5];
     register int code;
+    char *sysname;
 
     if (argc != 4) {
 	printf
@@ -42,9 +44,20 @@ main(int argc, char **argv)
 	printf("config: output file %s not found.\n", argv[2]);
 	exit(1);
     }
+    memset (alist, 0, sizeof (alist));
     alist[0] = argv[3];
     alist[1] = "all";
-    alist[2] = NULL;
+
+    /* This allows JUST arch or JUST OS/version,
+     * Linux 2.6 uses the in-kernel build system, so 
+     * just 'linux26' is enough. */
+    sysname = strdup (alist[0]);
+    alist[2] = strchr (sysname, '_');
+    if (alist[2]) {
+        alist[3] = sysname;
+        *alist[2] = 0;
+        alist[2]++;
+    }
     code = mc_copy(infile, outfile, alist);
     if (code) {
 	printf("config: failed to correctly write makefile '%s', code %d\n",
