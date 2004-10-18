7ddf663636a29e754b5d3488f9ec1cc601b79358
Jeffrey Hutzelman <jhutz@cmu.edu>
2004-10-18 04:24:31 +0000

    STABLE12-cleanup-rx-peer-leak-20041012
    if the peer was the second in the chain we could throw away our reference to
    the first, effectively leaking it forever.
    
    
    (cherry picked from commit e106a39fe62a8d338ac7f8cf4ed857c97a9b3484)

--- src/rx/rx.c
+++ src/rx/rx.c
@@ -5681,7 +5681,7 @@ void rxi_ReapConnections()
 		    MUTEX_ENTER(&rx_stats_mutex);
 		    rx_stats.nPeerStructs--;
 		    MUTEX_EXIT(&rx_stats_mutex);
-		    if (prev == *peer_ptr) {
+		    if (peer == *peer_ptr) {
 			*peer_ptr = next;
 			prev = next;
 		    }
