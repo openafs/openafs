54e6b437f80b37b643440e9b633f647b616d6186
Joakim Fallsjö <fallsjo@sanchin.se>
2004-12-07 14:24:14 +0000

    STABLE14-sunx86-20041207
    update to provide sun x86 support
    
    
    (cherry picked from commit 1183771a422a00fcc95d47d06b71479fcc99f4b7)

--- src/lwp/lwp_elf.h
+++ src/lwp/lwp_elf.h
@@ -39,25 +39,25 @@
  */
 
 #ifndef _C_LABEL
-#if !defined(SYSV) && !defined(__ELF__) && !defined(AFS_SUN5_ENV)
+#if defined(SYSV) || defined(__ELF__) || (defined(AFS_SUN5_ENV) && defined(__sun))
+#define _C_LABEL(name)  name
+#else /* SYSV || __ELF__ || __sun */
 #ifdef __STDC__
 #define _C_LABEL(name)     _##name
 #else
 #define _C_LABEL(name)  _/**/name
 #endif
-#else /* SYSV || __ELF__ || AFS_SUN5_ENV */
-#define _C_LABEL(name)  name
 #endif
 #endif /* _C_LABEL */
 
 #ifndef ENTRY
-#if !defined(SYSV) && !defined(__ELF__) && !defined(AFS_SUN5_ENV)
+#if defined(SYSV) || defined(__ELF__) || (defined(AFS_SUN5_ENV) && defined(__sun))
+#define ENTRY(name)     name:
+#else
 #ifdef __STDC__
 #define ENTRY(name)    _##name##:
 #else
 #define ENTRY(name)     _/**/name/**/:
 #endif
-#else /* SYSV || __ELF__ || AFS_SUN5_ENV */
-#define ENTRY(name)     name:
 #endif
 #endif /* _C_LABEL */
