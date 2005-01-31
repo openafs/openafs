58ee8b895c40bd7fa3862d5fcee6ec7b4d6eb7c0
Chas Williams <chas@cmf.nrl.navy.mil>
2005-01-31 04:14:08 +0000

    STABLE14-fstrace-linux-proc-20050113
    FIXES 17240
    
    support /proc interface
    
    
    (cherry picked from commit 270f675b6f64df5cca73de2ae097a12fd56c59b9)

--- src/venus/fstrace.c
+++ src/venus/fstrace.c
@@ -2115,7 +2115,7 @@ icl_TailKernel(outFilep, logname, waitTime)
 afs_syscall(call, parm0, parm1, parm2, parm3, parm4, parm5, parm6)
      long call, parm0, parm1, parm2, parm3, parm4, parm5, parm6;
 {
-    int code;
+    int code, rval;
 #ifdef AFS_LINUX20_ENV
 #if defined AFS_LINUX_64BIT_KERNEL
     long long eparm[4];
@@ -2133,9 +2133,13 @@ afs_syscall(call, parm0, parm1, parm2, parm3, parm4, parm5, parm6)
 #endif
     /* Linux can only handle 5 arguments in the actual syscall. */
     if (call == AFSCALL_ICL) {
-	code = syscall(AFS_SYSCALL, call, parm0, parm1, parm2, eparm);
+	rval = proc_afs_syscall(call, parm0, parm1, parm2, eparm, &code);
+	if (rval)
+	    code = syscall(AFS_SYSCALL, call, parm0, parm1, parm2, eparm);
     } else {
-	code = syscall(AFS_SYSCALL, call, parm0, parm1, parm2, parm3);
+	rval = proc_afs_syscall(call, parm0, parm1, parm2, parm3, &code);
+	if (rval)
+	    code = syscall(AFS_SYSCALL, call, parm0, parm1, parm2, parm3);
     }
 #if defined(AFS_SPARC64_LINUX20_ENV) || defined(AFS_SPARC_LINUX20_ENV)
     /* on sparc this function returns none value, so do it myself */
