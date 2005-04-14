7dd07b912b62b62cc19a24abfd6ebd81abc3cca7
Chas Williams <chas@cmf.nrl.navy.mil>
2005-04-14 03:32:46 +0000

    bye-getitimer-20050413
    FIXES 18221
    
    stop using getitimer on linux

--- src/config/param.sparc_linux22.h
+++ src/config/param.sparc_linux22.h
@@ -67,6 +67,7 @@
 #define AFS_HAVE_FFS        1	/* Use system's ffs. */
 #define AFS_HAVE_STATVFS    0	/* System doesn't support statvfs */
 #define AFS_VM_RDWR_ENV	    1	/* read/write implemented via VM */
+#define AFS_USE_GETTIMEOFDAY       1	/* use gettimeofday to implement rx clock */
 
 #ifdef KERNEL
 #ifndef MIN
