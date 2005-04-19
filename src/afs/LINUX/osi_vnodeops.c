09e66c9d2d200a5fe5e5c644ed30e6eb43d07d6b
Rainer Toebbicke <rtb@pclella.cern.ch>
2005-04-19 17:03:27 +0000

    STABLE14-logging-in-linux-vnodeops-sucks-20050418
    given that we keep ending up with these weird errors, it's just not worth it.
    
    
    (cherry picked from commit 7b71eb265efc727c88de0f25c1c6569831e6cbf9)

--- src/afs/LINUX/osi_vnodeops.c
+++ src/afs/LINUX/osi_vnodeops.c
@@ -931,17 +931,6 @@ afs_linux_dentry_revalidate(struct dentry *dp)
 static void
 afs_dentry_iput(struct dentry *dp, struct inode *ip)
 {
-    int isglock;
-
-    if (ICL_SETACTIVE(afs_iclSetp)) {
-	isglock = ISAFS_GLOCK();
-	if (!isglock) AFS_GLOCK();
-	afs_Trace3(afs_iclSetp, CM_TRACE_DENTRYIPUT, ICL_TYPE_POINTER, ip,
-		   ICL_TYPE_STRING, dp->d_parent->d_name.name,
-		   ICL_TYPE_STRING, dp->d_name.name);
-	if (!isglock) AFS_GUNLOCK();
-    }
-
     osi_iput(ip);
 }
 #endif
@@ -949,16 +938,6 @@ afs_dentry_iput(struct dentry *dp, struct inode *ip)
 static int
 afs_dentry_delete(struct dentry *dp)
 {
-    int isglock;
-    if (ICL_SETACTIVE(afs_iclSetp)) {
-	isglock = ISAFS_GLOCK();
-	if (!isglock) AFS_GLOCK();
-	afs_Trace3(afs_iclSetp, CM_TRACE_DENTRYDELETE, ICL_TYPE_POINTER,
-		   dp->d_inode, ICL_TYPE_STRING, dp->d_parent->d_name.name,
-		   ICL_TYPE_STRING, dp->d_name.name);
-	if (!isglock) AFS_GUNLOCK();
-    }
-
     if (dp->d_inode && (ITOAFS(dp->d_inode)->states & CUnlinked))
 	return 1;		/* bad inode? */
 
