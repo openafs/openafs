2669e1103bc733df4e27e24438c72d7d39a4c476
Marc Horowitz <marc@mit.edu>
2005-05-08 06:55:45 +0000

    sparc64-linux26-20050508
    FIXES 18485
    
    add sparc64-linux24 port

--- src/afs/afs_pioctl.c
+++ src/afs/afs_pioctl.c
@@ -263,7 +263,9 @@ copyin_afs_ioctl(caddr_t cmarg, struct afs_ioctl *dst)
 #if defined(AFS_LINUX_64BIT_KERNEL) && !defined(AFS_ALPHA_LINUX20_ENV) && !defined(AFS_IA64_LINUX20_ENV)
     struct afs_ioctl32 dst32;
 
-#ifdef AFS_SPARC64_LINUX24_ENV
+#ifdef AFS_SPARC64_LINUX26_ENV
+    if (test_thread_flag(TIF_32BIT))
+#elif AFS_SPARC64_LINUX24_ENV
     if (current->thread.flags & SPARC_FLAG_32BIT)
 #elif defined(AFS_SPARC64_LINUX20_ENV)
     if (current->tss.flags & SPARC_FLAG_32BIT)
--- src/config/afs_sysnames.h
+++ src/config/afs_sysnames.h
@@ -151,6 +151,7 @@
 #define SYS_NAME_ID_sparc64_linux2	1800
 #define SYS_NAME_ID_sparc64_linux22	1801
 #define SYS_NAME_ID_sparc64_linux24	1802
+#define SYS_NAME_ID_sparc64_linux26	1803
 
 #define SYS_NAME_ID_s390_linux2         1900
 #define SYS_NAME_ID_s390_linux22        1901
