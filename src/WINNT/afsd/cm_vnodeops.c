a5f2ec497d2c3452d795aa1d98309414c2d8c0e7
Jeffrey Altman <jaltman@secure-endpoints.com>
2005-10-15 04:36:57 +0000

    windows-check-locks-logging-20051014
    add a log message so we can tell how long it took.

--- src/WINNT/afsd/cm_vnodeops.c
+++ src/WINNT/afsd/cm_vnodeops.c
@@ -4276,6 +4276,7 @@ void cm_CheckLocks()
     }
 
     lock_ReleaseWrite(&cm_scacheLock);
+    osi_Log1(afsd_logp, "cm_CheckLocks completes lock check cycle %d", cm_lockRefreshCycle);
 }
 
 /* NOT called with scp->mx held. */
