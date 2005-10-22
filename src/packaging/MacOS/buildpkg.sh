3491c0ffc559ac39fd861ff4c31bd1b4c0d94201
Derrick Brashear <shadow@dementia.org>
2005-10-22 07:43:09 +0000

    macos-build-dmg-20051021
    probably this script should take an argument which is the version string to use, and use it in the volume name and dmg name
    
    ====================
    This delta was composed from multiple commits as part of the CVS->Git migration.
    The checkin message with each commit was inconsistent.
    The following are the additional commit messages.
    ====================
    
    probably this script should take an argument which is the version string to use,
     and use it in the volume name and dmg name

--- src/packaging/MacOS/buildpkg.sh
+++ src/packaging/MacOS/buildpkg.sh
@@ -168,6 +168,11 @@ else
 fi
 
 rm -rf pkgroot pkgres
+mkdir dmg
+mv OpenAFS.pkg dmg
+rm -rf OpenAFS.dmg
+hdiutil create -srcfolder dmg -volname OpenAFS -anyowners OpenAFS.dmg
+rm -rf dmg
 # Unfortunately, sudo sets $USER to root, so I can't chown the 
 #.pkg dir back to myself
 #chown -R $USER OpenAFS.pkg
