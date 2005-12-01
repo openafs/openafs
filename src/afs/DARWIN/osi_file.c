49f0f7bb0ee04b6f80a4f84dbfd00dbf8f274ac2
Derrick Brashear <shadow@dementia.org>
2005-12-01 07:36:35 +0000

    macos-103-20051201
    last fixes i swear

--- src/afs/DARWIN/osi_file.c
+++ src/afs/DARWIN/osi_file.c
@@ -63,11 +63,7 @@ afs_InitDualFSCacheOps(struct vnode *vp)
 }
 
 ino_t
-#ifdef AFS_DARWIN80_ENV
 VnodeToIno(vnode_t avp)
-#else
-VnodeToIno(vnode_t * avp)
-#endif
 {
     unsigned long ret;
 
@@ -104,11 +100,7 @@ VnodeToIno(vnode_t * avp)
 
 
 dev_t
-#ifdef AFS_DARWIN80_ENV
 VnodeToDev(vnode_t avp)
-#else
-VnodeToDev(vnode_t * avp)
-#endif
 {
 #ifndef AFS_DARWIN80_ENV
     if (afs_CacheFSType == AFS_APPL_UFS_CACHE) {
