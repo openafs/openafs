94112183d2eb3b7c47c6c6d51420e59da78aaf1b
Russ Allbery <rra@debian.org>
2005-12-07 04:03:15 +0000

    * Overhaul the way kernel modules are built.     - Recommend linux-image rather than kernel-image for newer kernels.     - Always recommend the kernel package; since it's only a recommends,       there's no need to special-case the hand-built kernel case.     - Be less fragile about how the kernel version is used in prep-modules       to support other ways kernel modules are built.  (Closes: #341836)     - Add Build-Depends and Uploaders to the openafs-modules-source       control file.     - Remove unused parts of prep-modules and heavily comment it.

--- debian/genchanges.sh
+++ debian/genchanges.sh
@@ -2,21 +2,18 @@
 # genchanges.sh - generate a changes file for a deb file generated via
 #	the make-kpkg utility
 
-# KSRC KMAINT and KEMAIL are expected to be passed through the environment
+# KSRC, KVERS, KMAINT, and KEMAIL are expected to be passed through the
+# environment.
 
 set -e
 umask 022
 
-#KVERS=`cat debian/KVERS`
 MODVERS=`cat debian/VERSION | sed s/:/\+/`
 ARCH=`dpkg --print-architecture`
 
 mprefix=`grep Package: debian/control.module | cut -d' ' -f 2 | cut -d= -f 1`
-
-# the changes file's name
 chfile="$KSRC/../$mprefix${KVERS}${INT_SUBARCH}_${MODVERS}_${ARCH}.changes"
 
 dpkg-genchanges -b ${KMAINT:+-m"$KMAINT <$KEMAIL>"} -u"$KSRC/.." \
-	-cdebian/control > "$chfile"
-#pgp -fast ${KMAINT:+-u"$KMAINT"} < "$chfile.pt" > "$chfile"
-rm "$chfile.pt" || true
+    -cdebian/control > "$chfile"
+#debsign "$chfile"
