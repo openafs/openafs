e3dfba8e6c10c296c09e9b0ac0c0355658ce0be7
Russ Allbery <rra@stanford.edu>
2005-12-08 12:14:33 +0000

    man-page-conversion-20051208
    This is the initial conversion of the AFS Adminstrators Reference into POD
    for use as man pages.  The man pages are now generated via pod2man from
    regen.sh so that only those working from CVS have to have pod2man
    available.  The Makefile only installs.  The pages have also been sorted
    out into pod1, pod5, and pod8 directories, making conversion to the right
    section of man page easier without maintaining a separate list and allowing
    for names to be duplicated between pod5 and pod1 or pod8 (which will likely
    be needed in a few cases).
    
    This reconversion is done with a new script based on work by Chas Williams.
    In some cases, the output is worse than the previous POD pages, but this is
    a more comprehensive conversion.
    
    This is only the first step, and this initial conversion has various
    problems.  In addition, the file man pages that didn't have simple names
    have not been converted in this pass and will be added later.  Some of the
    man pages have syntax problems and all of them have formatting errors.  The
    next editing pass, coming shortly, will clean up most of the remaining
    mess.

--- regen.sh
+++ regen.sh
@@ -10,3 +10,24 @@ echo "Running autoheader"
 autoheader
 #echo "Running automake"
 #automake
+
+# Rebuild the man pages, to not require those building from source to have
+# pod2man available.
+echo "Building man pages"
+if test -d doc ; then
+    for f in doc/man-pages/pod1/*.pod ; do
+        pod2man -c 'AFS Command Reference' -r 'OpenAFS' -s 1 \
+            -n `basename "$f" | sed 's/\.pod$//'` "$f" \
+            > `echo "$f" | sed -e 's%pod1/%%' -e 's/\.pod$/.1/'`
+    done
+    for f in doc/man-pages/pod5/*.pod ; do
+        pod2man -c 'AFS File Reference' -r 'OpenAFS' -s 5 \
+            -n `basename "$f" | sed 's/\.pod$//'` "$f" \
+            > `echo "$f" | sed -e 's%pod5/%%' -e 's/\.pod$/.5/'`
+    done
+    for f in doc/man-pages/pod8/*.pod ; do
+        pod2man -c 'AFS Command Reference' -r 'OpenAFS' -s 8 \
+            -n `basename "$f" | sed 's/\.pod$//'` "$f" \
+            > `echo "$f" | sed -e 's%pod8/%%' -e 's/\.pod$/.8/'`
+    done
+fi
