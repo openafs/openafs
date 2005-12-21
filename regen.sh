0b21e24d0a96401033d55d3c079c954138b7c50e
Russ Allbery <rra@stanford.edu>
2005-12-21 00:37:16 +0000

    man-page-generation-20051220
    Move man page generation out into a separate script that's just invoked
    from regen.sh, so that someone can run that separate script later if they
    wish.  Make that script more robust against problems such as empty podN
    directories.  Diagnose a missing pod2man and warn about old versions of
    Pod::Man.
    
    Also, remove the old programs used to do the initial conversion from HTML.
    Enough post-conversion editing was done that they're no longer necessary
    except for historical curiosity, and for that purpose they can be pulled
    out of CVS.

--- regen.sh
+++ regen.sh
@@ -14,21 +14,6 @@ autoheader
 # Rebuild the man pages, to not require those building from source to have
 # pod2man available.
 echo "Building man pages"
-if test -d doc ; then
-    mkdir -p doc/man-pages/man1 doc/man-pages/man5 doc/man-pages/man8
-    for f in doc/man-pages/pod1/*.pod ; do
-        pod2man -c 'AFS Command Reference' -r 'OpenAFS' -s 1 \
-            -n `basename "$f" | sed 's/\.pod$//'` "$f" \
-            > `echo "$f" | sed -e 's%pod1/%man1/%' -e 's/\.pod$/.1/'`
-    done
-    for f in doc/man-pages/pod5/*.pod ; do
-        pod2man -c 'AFS File Reference' -r 'OpenAFS' -s 5 \
-            -n `basename "$f" | sed 's/\.pod$//'` "$f" \
-            > `echo "$f" | sed -e 's%pod5/%man5/%' -e 's/\.pod$/.5/'`
-    done
-    for f in doc/man-pages/pod8/*.pod ; do
-        pod2man -c 'AFS Command Reference' -r 'OpenAFS' -s 8 \
-            -n `basename "$f" | sed 's/\.pod$//'` "$f" \
-            > `echo "$f" | sed -e 's%pod8/%man8/%' -e 's/\.pod$/.8/'`
-    done
+if test -d doc/man-pages ; then
+    (cd doc/man-pages && ./generate-man)
 fi
