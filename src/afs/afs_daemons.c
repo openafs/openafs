7ce44cff31b3614f037b45c91cac8435c98c1e31
Chaskiel M Grundman <cg2v@andrew.cmu.edu>
2006-01-28 19:22:30 +0000

    STABLE14-simple-darwin-disable-bkg-20060128
    avoid potential hang by disabling bkg daemons on 10.4 for now
    
    
    (cherry picked from commit 2e76a2450e06c93d7cdbc0fe07bd9302ac085952)

--- src/afs/afs_daemons.c
+++ src/afs/afs_daemons.c
@@ -35,6 +35,9 @@ short afs_brsDaemons = 0;	/* number of daemons waiting for brs requests */
 struct brequest afs_brs[NBRS];	/* request structures */
 struct afs_osi_WaitHandle AFS_WaitHandler, AFS_CSWaitHandler;
 static int afs_brs_count = 0;	/* request counter, to service reqs in order */
+#ifdef AFS_DISABLE_BKG
+extern int disable_bkg = AFS_DISABLE_BKG;
+#endif
 
 static int rxepoch_checked = 0;
 #define afs_CheckRXEpoch() {if (rxepoch_checked == 0 && rxkad_EpochWasSet) { \
@@ -563,6 +566,10 @@ int
 afs_BBusy(void)
 {
     AFS_STATCNT(afs_BBusy);
+#ifdef AFS_DISABLE_BKG
+    if (disable_bkg)
+	return 1;
+#endif
     if (afs_brsDaemons > 0)
 	return 0;
     return 1;
