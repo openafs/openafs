d2de9e69bc08e04c00083166a59847be246a9e8c
Jeffrey Altman <jaltman@secure-endpoints.com>
2006-02-10 17:07:43 +0000

    viced-log-typo-20060210
    missing % in format string causes crash at log level 125

--- src/viced/host.c
+++ src/viced/host.c
@@ -1031,7 +1031,7 @@ removeInterfaceAddr_r(struct host *host, afs_uint32 addr, afs_uint16 port)
     assert(host);
     assert(host->interface);
 
-    ViceLog(125, ("removeInterfaceAddr : host %s:d addr %s:%d\n", 
+    ViceLog(125, ("removeInterfaceAddr : host %s:%d addr %s:%d\n", 
 		   afs_inet_ntoa_r(host->host, hoststr), ntohs(host->port), 
 		   afs_inet_ntoa_r(addr, hoststr2), ntohs(port)));
 
