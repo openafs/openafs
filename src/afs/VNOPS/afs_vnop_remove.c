b1883ef6ad619ffa1fb76caf5d3568f69c1e3136
Jim Rees <rees@umich.edu>
2006-03-13 19:18:07 +0000

    no-clid-for-bsd-20060313
    Back out rev 1.28 for BSD.
    On BSD, clid is not necessarily a pid.
    Passing it to HandleFlock results in client hangs.

--- src/afs/VNOPS/afs_vnop_remove.c
+++ src/afs/VNOPS/afs_vnop_remove.c
@@ -393,7 +393,7 @@ afs_remove(OSI_VC_ARG(adp), aname, acred)
 	code = afsrename(adp, aname, adp, unlname, acred, &treq);
 	Tnam1 = unlname;
 	if (!code) {
-	    char *oldmvid = NULL;
+	    struct VenusFid *oldmvid = NULL;
 	    if (tvc->mvid) 
 		oldmvid = tvc->mvid;
 	    tvc->mvid = (struct VenusFid *)unlname;
