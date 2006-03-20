9711c8a1ffe7f1f9ac7072234ba6540dd80b2d0a
Jeffrey Altman <jaltman@secure-endpoints.com>
2006-03-20 13:39:11 +0000

    STABLE14-another-time_t-pointer-fix-20060320
    do not pass in pointers to 32-bit values as pointers to 64-bit values
    
    
    (cherry picked from commit e6f0c0c68f6b4d415484eed556dd65bcaacc9585)

--- src/lwp/test/selsubs.c
+++ src/lwp/test/selsubs.c
@@ -150,6 +150,7 @@ Log(char *fmt, ...)
     struct timeval now;
     struct timezone tz;
     struct tm *ltime;
+    time_t tt;
     int code;
     PROCESS pid;
     extern char *program;
@@ -157,7 +158,8 @@ Log(char *fmt, ...)
     code = gettimeofday(&now, &tz);
     assert(code == 0);
 
-    ltime = localtime((time_t *) & now.tv_sec);
+    tt = now.tv_sec;
+    ltime = localtime(&tt);
 
     LWP_CurrentProcess(&pid);
     fprintf(stderr, "%s 0x%x %02d:%02d:%02d.%d: ", program ? program : "",
