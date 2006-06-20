8a9d6bfa5bb6a5c53c48f2da55eefd7591d9e9f7
Jeffrey Altman <jaltman@secure-endpoints.com>
2006-06-20 21:37:10 +0000

    DEVEL15-rxbind-for-servers-20060620
    do not call parseNetFile on Windows where the function does not exist
    
    
    (cherry picked from commit 7af6b06cb6611a8925e3fa887ddf0a03542ee4dc)

--- src/vlserver/vlserver.c
+++ src/vlserver/vlserver.c
@@ -333,6 +333,7 @@ main(argc, argv)
 
     if (rxBind) {
 	afs_int32 ccode;
+#ifndef AFS_NT40_ENV
         if (AFSDIR_SERVER_NETRESTRICT_FILEPATH || 
             AFSDIR_SERVER_NETINFO_FILEPATH) {
             char reason[1024];
@@ -340,7 +341,9 @@ main(argc, argv)
 				  ADDRSPERSITE, reason,
 				  AFSDIR_SERVER_NETINFO_FILEPATH,
 				  AFSDIR_SERVER_NETRESTRICT_FILEPATH);
-        } else {
+        } else 
+#endif	
+	{
             ccode = rx_getAllAddr(SHostAddrs, ADDRSPERSITE);
         }
         if (ccode == 1) {
