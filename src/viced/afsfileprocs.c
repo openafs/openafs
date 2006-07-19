b91baf8b8ac2170eb3962cb590aa8e39100e2046
Jeffrey Altman <jaltman@secure-endpoints.com>
2006-07-19 15:48:17 +0000

    DEVEL15-viced-checkrights-loglevel-20060719
    increase the log level on the "CheckRights: len=%u, for host=%s:%d\n"
    message to 5.
    
    
    (cherry picked from commit 14ed562c4b576bcfe6b0147b86fa5f7899d22c16)

--- src/viced/afsfileprocs.c
+++ src/viced/afsfileprocs.c
@@ -675,7 +675,7 @@ GetRights(struct client *client, struct acl_accessList *ACL,
 
     if (!client->host->hcps.prlist_len || !client->host->hcps.prlist_val) {
 	char hoststr[16];
-	ViceLog(0,
+	ViceLog(5,
 		("CheckRights: len=%u, for host=%s:%d\n",
 		 client->host->hcps.prlist_len, 
 		 afs_inet_ntoa_r(client->host->host, hoststr),
