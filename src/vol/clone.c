0fe7b3a058b6c9df06d4df4af1e0621dbd53f44d
Jeffrey Altman <jaltman@secure-endpoints.com>
2006-09-28 13:58:04 +0000

    avoid-assert-for-missing-vnodes-20060928
    specify the correct number of parameters for VForceOffline_r()

--- src/vol/clone.c
+++ src/vol/clone.c
@@ -262,7 +262,7 @@ DoCloneIndex(Volume * rwvp, Volume * clvp, VnodeClass class, int reclone)
 		    Log("IH_INC failed: %x, %s, %u errno %d\n",
 			V_linkHandle(rwvp), PrintInode(NULL, rwinode),
 			V_parentId(rwvp), errno);
-		    VForceOffline_r(rwvp);
+		    VForceOffline_r(rwvp, 0);
 		    ERROR_EXIT(EIO);
 		}
 		inodeinced = 1;
@@ -315,7 +315,7 @@ DoCloneIndex(Volume * rwvp, Volume * clvp, VnodeClass class, int reclone)
 		    Log("IH_DEC failed: %x, %s, %u errno %d\n",
 			V_linkHandle(rwvp), PrintInode(NULL, rwinode),
 			V_parentId(rwvp), errno);
-		    VForceOffline_r(rwvp);
+		    VForceOffline_r(rwvp, 0);
 		    ERROR_EXIT(EIO);
 		}
 	    }
