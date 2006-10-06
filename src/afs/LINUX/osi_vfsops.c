5f9efaeebf5b6ba355fa615f9f5a426d73157543
Chas Williams <chas@cmf.nrl.navy.mil>
2006-10-06 14:29:14 +0000

    DEVEL15-linux-2619-aio-and-read-write-changes-20061006
    changes to support 2.6.19
    
    
    (cherry picked from commit 775ea6f1b79b0e0e2b700e3894e01279e26ebbfa)

--- src/afs/LINUX/osi_vfsops.c
+++ src/afs/LINUX/osi_vfsops.c
@@ -318,8 +318,7 @@ afs_init_inodecache(void)
 void
 afs_destroy_inodecache(void)
 {
-    if (kmem_cache_destroy(afs_inode_cachep))
-	printk(KERN_INFO "afs_inode_cache: not all structures were freed\n");
+    (void) kmem_cache_destroy(afs_inode_cachep);
 }
 #else
 int
