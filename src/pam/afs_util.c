763dc42ff054dfd12c60aaf184271aedfffe589a
Derrick Brashear <shadow@dementia.org>
2006-12-05 19:37:00 +0000

    revert-aix5-curpag-20061009
    failed to commit this. commit now
    VS: ----------------------------------------------------------------------

--- src/pam/afs_util.c
+++ src/pam/afs_util.c
@@ -183,13 +183,6 @@ do_klog(const char *user, const char *password, const char *lifetime,
 static afs_int32
 curpag(void)
 {
-#if defined(AFS_AIX51_ENV)
-    afs_int32 pag;
-
-    if (get_pag(PAG_AFS, &pag) < 0 || pag == 0)
-        pag = NOPAG;
-    return pag;
-#else
     gid_t groups[NGROUPS_MAX];
     afs_uint32 g0, g1;
     afs_uint32 h, l, ret;
@@ -213,7 +206,6 @@ curpag(void)
 	    return -1;
     }
     return -1;
-#endif
 }
 
 /* Returns the AFS pag number, if any, otherwise return -1 */
