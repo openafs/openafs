f308e1e06b4695c71214c8bc17c0da3c97e7dd2b
Derrick Brashear <shadow@dementia.org>
2007-01-05 04:57:32 +0000

    DEVEL15-free-packet-avoid-issue-with-34-jumbograms-20070104
    try to deal with rainer toebbicke's ongoing issue
    
    
    (cherry picked from commit fd4a1c1c32c01a44ee09854039b81b0afa0ded32)

--- src/rx/rx_packet.c
+++ src/rx/rx_packet.c
@@ -393,12 +393,6 @@ rxi_FreePackets(int num_pkts, struct rx_queue * q)
 	    rxi_FreeDataBufsTSFPQ(c, 1, 0);
 	}
     } else {
-	for (queue_Scan(q, c, nc, rx_packet)) {
-	    rxi_FreeDataBufsTSFPQ(c, 1, 0);
-	}
-    }
-
-    if (num_pkts) {
 	RX_TS_FPQ_CHECKIN2(rx_ts_info, num_pkts, q);
     }
 
