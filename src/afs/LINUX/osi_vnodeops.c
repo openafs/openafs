4f86aa379d345c98113bde2169b9d1400dcee89c
Rainer Toebbicke <rtb@pclella.cern.ch>
2007-02-22 21:40:11 +0000

    linux-unlink-avoid-infrequent-leak-20070222
    FIXES 54689
    
    avoid minor leak

--- src/afs/LINUX/osi_vnodeops.c
+++ src/afs/LINUX/osi_vnodeops.c
@@ -1127,6 +1127,8 @@ afs_linux_unlink(struct inode *dip, struct dentry *dp)
             }
             tvc->uncred = credp;
 	    tvc->states |= CUnlinked;
+	} else {
+	    osi_FreeSmallSpace(__name);	
 	}
 	AFS_GUNLOCK();
 
