fbcafac2f6f58dfd55af8f9f54c856b2d31ecd2a
Jeffrey Altman <jaltman@secure-endpoints.com>
2007-04-03 06:40:27 +0000

    windows-vista-sdk-ntsecapi-compat-20070402
    fix inclusion of ntsecapi.h in network identity manager plug-in

--- src/WINNT/netidmgr_plugin/dynimport.h
+++ src/WINNT/netidmgr_plugin/dynimport.h
@@ -30,7 +30,23 @@
 /* Dynamic imports */
 #include<khdefs.h>
 #include<tlhelp32.h>
+
+#if defined(_WIN32_WINNT) 
+#  if (_WIN32_WINNT < 0x0501)
+#    define AFS_WIN32_WINNT _WIN32_WINNT
+#    undef _WIN32_WINNT
+#    define _WIN32_WINNT 0x0501
+#  endif
+#else 
+#  define _WIN32_WINNT 0x0501
+#endif
+
 #include<ntsecapi.h>
+#if defined(AFS_WIN32_WINNT)
+#undef _WIN32_WINNT
+#define _WIN32_WINNT AFS_WIN32_WINNT
+#undef AFS_WIN32_WINNT
+#endif
 
 #ifndef FAR
 #define FAR
