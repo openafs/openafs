00943d625ec0c536857313bfd447ea7e1b36089d
Jeffrey Altman <jaltman@secure-endpoints.com>
2007-06-14 20:05:03 +0000

    STABLE14-rx-lwp-sendmsg-20070614
    update the lwp version of rxi_sendmsg to return the same error, -1,
    returned by the pthread version.
    
    replace errno with WSAGetLastError() in the Windows blocks so that
    the correct error value is checked.
    
    
    (cherry picked from commit 46a931d020da24dd534e1064ea9fbe693acbdf65)

--- src/rx/rx_packet.c
+++ src/rx/rx_packet.c
@@ -2216,7 +2216,7 @@ rxi_SendPacket(struct rx_call *call, struct rx_connection *conn,
 	     * code.  So, when this happens let's "down" the host NOW so
 	     * we don't sit around waiting for this host to timeout later.
 	     */
-		if (call && code == -1 && errno == WSAEHOSTUNREACH)
+		if (call && code == -1 && WSAGetLastError() == WSAEHOSTUNREACH)
 			call->lastReceiveTime = 0;
 #endif
 #if defined(KERNEL) && defined(AFS_LINUX20_ENV)
