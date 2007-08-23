e5e7fc440998d8c5d92aad0076f45aa0ae6b586f
Jeffrey Altman <jaltman@secure-endpoints.com>
2007-08-23 17:49:03 +0000

    DEVEL15-windows-volstat-log-20070823
    must save the log string when it comes from a buffer on the stack
    otherwise garbage is logged in afsd.log
    
    
    (cherry picked from commit 985a2e34d60a803c11cce4729b16484847aec97b)

--- src/WINNT/afsd/cm_volume.c
+++ src/WINNT/afsd/cm_volume.c
@@ -1471,7 +1471,7 @@ void cm_VolumeStatusNotification(cm_volume_t * volp, afs_uint32 volID, enum vols
     snprintf(volstr, sizeof(volstr), "%s:%s%s", volp->cellp->name, volp->namep, ext);
 
     osi_Log4(afsd_logp, "VolumeStatusNotification: %-48s [%10u] (%s -> %s)",
-             volstr, volID, volstatus_str(old), volstatus_str(new));
+             osi_LogSaveString(afsd_logp, volstr), volID, volstatus_str(old), volstatus_str(new));
 
     cm_VolStatus_Change_Notification(volp->cellp->cellID, volID, new);
 }       
