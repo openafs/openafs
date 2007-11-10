0faeee65915ecb33f4f1f6f60e6160ba51568b8e
Dan Hyde <drh@umich.edu>
2007-11-10 00:37:50 +0000

    DEVEL15-viced-missing-lock-20071109
    FIXES 76713
    
    Add missing lock that needs to protect an in use 'host' from having
    HOSTDELETED added to the hostFlags and HWNO_INPROGRESS removed.
    
    
    (cherry picked from commit 76c886b574b7d21db2557872be8af8e6ca549f34)

--- src/viced/host.c
+++ src/viced/host.c
@@ -1575,6 +1575,7 @@ h_GetHost_r(struct rx_connection *tcon)
 		     host->interface ? uuid2 : "no_uuid"));
 
 	    /* The host in the cache is not the host for this connection */
+            h_Lock_r(host);
 	    host->hostFlags |= HOSTDELETED;
 	    host->hostFlags &= ~HWHO_INPROGRESS;
 	    h_Unlock_r(host);
