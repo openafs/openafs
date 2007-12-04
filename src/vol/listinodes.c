85352be7633d951100b98cee2e48ef0357a91454
Derrick Brashear <shadow@dementia.org>
2007-12-04 21:03:33 +0000

    DEVEL15-inode-convertro-dont-include-volser-code-in-viced-20071204
    LICENSE IPL10
    FIXES 77574
    
    viced isn't an fssync client; avoid the convert volser code there
    
    
    (cherry picked from commit 58dd384ba4020c924604ab612276948803333549)

--- src/vol/listinodes.c
+++ src/vol/listinodes.c
@@ -1642,6 +1642,7 @@ getDevName(char *pbuffer, char *wpath)
         return NULL;
 }
 
+#ifdef FSSYNC_BUILD_CLIENT
 int
 inode_ConvertROtoRWvolume(char *pname, afs_int32 volumeId)
 {
@@ -1787,4 +1788,5 @@ inode_ConvertROtoRWvolume(char *pname, afs_int32 volumeId)
     FSYNC_VolOp(h.id, pname, FSYNC_VOL_ON, 0, NULL);
     return 0;
 }
+#endif /* FSSYNC_BUILD_CLIENT */
 #endif /* AFS_NAMEI_ENV */
