f59669b16ae124a97f5fe371aa5dbfce3a09a467
Mike Meffie <mmeffie@sinenomine.net>
2007-12-11 21:30:04 +0000

    DEVEL15-afs-serveriprank-return-void-20071211
    LICENSE IPL10
    
    can't return 0 from void function
    
    
    (cherry picked from commit 319ad227d12b8ab301d464c2460be74f7a44ec4d)

--- src/afs/afs_server.c
+++ src/afs/afs_server.c
@@ -1129,7 +1129,7 @@ afsi_SetServerIPRank(struct srvAddr *sa, afs_int32 addr,
 	    sa->sa_iprank = afs_min(sa->sa_iprank, MED);
 	}
     }
-    return 0;
+    return;
 }
 #else /* AFS_USERSPACE_IP_ADDR */
 #if (! defined(AFS_SUN5_ENV)) && !defined(AFS_DARWIN60_ENV) && defined(USEIFADDR)
