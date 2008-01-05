21d6a3477334c4b6b822f5e2f1d5739bb22c9b75
Jeffrey Altman <jaltman@secure-endpoints.com>
2008-01-05 17:56:29 +0000

    DEVEL15-windows-vista-network-change-20080105
    LICENSE MIT
    
    After a network change, test to see if the desired LAN adapter to bind
    the Netbios Name to has changed.  If so, stop the Netbios Network and
    restart with the new LAN adapter.
    
    This can happen on Vista after a suspend if the service wakes up before
    the Loopback adapter re-initializes.
    
    
    (cherry picked from commit f970985bc35ade50371a9299ad580d96ad04e979)

--- src/WINNT/afsd/cm_daemon.c
+++ src/WINNT/afsd/cm_daemon.c
@@ -74,6 +74,7 @@ void cm_IpAddrDaemon(long parm)
         thrd_SetEvent(cm_IPAddrDaemon_ShutdownEvent);
         Result = NotifyAddrChange(NULL,NULL);
         if (Result == NO_ERROR && daemon_ShutdownFlag == 0) {
+            smb_LanAdapterChange();
             thrd_ResetEvent(cm_IPAddrDaemon_ShutdownEvent);
 	    Sleep(2500);
             if (daemon_ShutdownFlag == 0) {
