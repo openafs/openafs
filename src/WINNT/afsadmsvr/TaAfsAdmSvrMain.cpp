00811fa149f66017946d176d28ccbe4149b00eb6
Jeffrey Altman <jaltman@secure-endpoints.com>
2008-02-12 13:32:48 +0000

    DEVEL15-windows-no-more-rpcns4-dll-20080212
    LICENSE MIT
    
    Remove all references to rpcns4.lib and its included functions RpcNsXXX.
    These APIs are no longer supported on Vista.  Except for TaAfsAdmSvr
    they had been commented out for a long time but we still continued to
    link to the library.  Loading the library generates a no longer supported
    error on Vista.  So lets get rid of it.
    
    
    (cherry picked from commit a5e5736c67cdf3d08bce548447bc7635b65329c2)

--- src/WINNT/afsadmsvr/TaAfsAdmSvrMain.cpp
+++ src/WINNT/afsadmsvr/TaAfsAdmSvrMain.cpp
@@ -80,7 +80,9 @@ int cdecl main (int argc, char **argv)
    // Clean up any broken interface registration
    //
    RpcServerUnregisterIf (ITaAfsAdminSvr_v1_0_s_ifspec, 0, FALSE);
+#ifdef notdef
    RpcNsBindingUnexport (RPC_C_NS_SYNTAX_DEFAULT, pszENTRYNAME, ITaAfsAdminSvr_v1_0_s_ifspec, NULL);
+#endif
 
    // Register our interface
    //
@@ -118,10 +120,12 @@ int cdecl main (int argc, char **argv)
          {
          BOOL fExportedBinding = FALSE;
 
+#ifdef notdef
          if ((status = RpcNsBindingExport (RPC_C_NS_SYNTAX_DEFAULT, pszENTRYNAME, ITaAfsAdminSvr_v1_0_s_ifspec, pBindingVector, NULL)) == 0)
             fExportedBinding = TRUE;
          else
             Print (dlWARNING, TEXT("RpcNsBindingExport failed (benign); error 0x%08lX"), status);
+#endif
 
          if (!fExportedBinding && !fGotPort)
             {
@@ -157,6 +161,7 @@ int cdecl main (int argc, char **argv)
             AfsAdmSvr_Shutdown();
             }
 
+#ifdef notdef
          if (fExportedBinding)
             {
             if ((status = RpcNsBindingUnexport (RPC_C_NS_SYNTAX_DEFAULT, pszENTRYNAME, ITaAfsAdminSvr_v1_0_s_ifspec, NULL)) != 0)
@@ -164,7 +169,7 @@ int cdecl main (int argc, char **argv)
                Print (dlWARNING, TEXT("RpcNsBindingExport failed; error 0x%08lX"), status);
                }
             }
-
+#endif
          if ((status = RpcEpUnregister (ITaAfsAdminSvr_v1_0_s_ifspec, pBindingVector, NULL)) != 0)
             {
             Print (dlWARNING, TEXT("RpcEpUnregister failed; error 0x%08lX"), status);
