9bde7c71bf9c16c6108b49ff36cf464bc37a8099
Jeffrey Altman <jaltman@secure-endpoints.com>
2008-03-13 12:58:19 +0000

    DEVEL15-rx-mutex-interlocked-macros-20080312
    LICENSE IPL10
    
    fix from sxw@inf.ed.ac.uk
    
    
    (cherry picked from commit e1fb328b9ae509316ce8fff81691f6b738929ee9)

--- src/rx/rx.c
+++ src/rx/rx.c
@@ -3095,7 +3095,7 @@ rxi_ReceiveDataPacket(register struct rx_call *call,
 	MUTEX_ENTER(&rx_freePktQ_lock);
 	rxi_NeedMorePackets = TRUE;
 	MUTEX_EXIT(&rx_freePktQ_lock);
-        rx_MutexIncrement(rx_stats.nPacketBuffersOnRead, rx_stats_mutex);
+        rx_MutexIncrement(rx_stats.noPacketBuffersOnRead, rx_stats_mutex);
 	call->rprev = np->header.serial;
 	rxi_calltrace(RX_TRACE_DROP, call);
 	dpf(("packet %x dropped on receipt - quota problems", np));
