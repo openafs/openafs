a252a94da60a04e20f46ebaebe9c98851f597bae
Simon Wilkinson <sxw@inf.ed.ac.uk>
2008-05-21 16:41:22 +0000

    DEVEL153X-tweak-regen-20080521
    LICENSE IPL10
    
    make regen quieter if you don't want man pages
    
    
    (cherry picked from commit 1705bda53e4ca198cf78d70ff424fc7af69b1879)

--- regen.sh
+++ regen.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
+
+while getopts "q" flag
+do
+    case "$flag" in
+	q)
+	    skipman=1;
+	    ;;
+	*)
+	    echo "Usage ./regen.sh [-q]"
+	    echo "	-q skips man page generation"
+	    exit
+	    ;;
+    esac
+done
+
 echo "Updating configuration..."
 echo "Running aclocal"
 aclocal -I src/cf
@@ -15,9 +30,13 @@ autoheader
 echo "Deleting autom4te.cache directory"
 rm -r autom4te.cache
 
-# Rebuild the man pages, to not require those building from source to have
-# pod2man available.
-if test -d doc/man-pages ; then
-    echo "Building man pages"
-    (cd doc/man-pages && ./generate-man)
+if [ $skipman ] ; then
+    echo "Skipping man page build"
+else 
+    # Rebuild the man pages, to not require those building from source to have
+    # pod2man available.
+    if test -d doc/man-pages ; then
+        echo "Building man pages"
+        (cd doc/man-pages && ./generate-man)
+    fi
 fi
