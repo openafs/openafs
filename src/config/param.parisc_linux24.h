6cde58b78d6d7461ff13091d1b12f42fa2e7b7d7
Russ Allbery <rra@debian.org>
2008-06-30 13:32:18 -0700

    Don't try to use UCONTEXT on hppa/parisc
    glibc doesn't implement the ucontext functions on hppa/parisc, so don't
    try to use them.  This probably means that OpenAFS user space is broken
    on hppa right now.  That's not a surprise, given that we don't have a
    2.6 kernel param file either.

--- src/config/param.parisc_linux24.h
+++ src/config/param.parisc_linux24.h
@@ -70,11 +70,6 @@
 #endif
 #endif /* KERNEL */
 
-#ifdef __GLIBC__
-#if (__GLIBC__ > 2) || (__GLIBC__ == 2 && __GLIBC_MINOR__ > 3)
-#define USE_UCONTEXT
-#endif
-#endif
 #endif /* AFS_PARAM_H */
 
 #else /* !defined(UKERNEL) */
