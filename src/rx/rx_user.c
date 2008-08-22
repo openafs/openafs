e4c9c47cd1d82aac0c179b6babb0c718f8695d73
Derrick Brashear <shadow@dementia.org>
2008-08-22 05:31:48 +0000

    DEVEL15-rx-allow-jumbogram-disabling-really-20080822
    LICENSE IPL10
    
    right now, jumbograms can still be sent when -nojumbo is specified. make the
    current macro a default and override when we're supposed to.
    
    
    (cherry picked from commit 46dbd678714110002a36269beca8961e013650ac)

--- src/rx/rx_user.c
+++ src/rx/rx_user.c
@@ -729,10 +729,10 @@ rxi_InitPeerParams(struct rx_peer *pp)
     pp->natMTU = MIN((int)pp->ifMTU, OLD_MAX_PACKET_SIZE);
     pp->maxDgramPackets =
 	MIN(rxi_nDgramPackets,
-	    rxi_AdjustDgramPackets(RX_MAX_FRAGS, pp->ifMTU));
+	    rxi_AdjustDgramPackets(rxi_nSendFrags, pp->ifMTU));
     pp->ifDgramPackets =
 	MIN(rxi_nDgramPackets,
-	    rxi_AdjustDgramPackets(RX_MAX_FRAGS, pp->ifMTU));
+	    rxi_AdjustDgramPackets(rxi_nSendFrags, pp->ifMTU));
     pp->maxDgramPackets = 1;
     /* Initialize slow start parameters */
     pp->MTU = MIN(pp->natMTU, pp->maxMTU);
