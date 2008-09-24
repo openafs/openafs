5af9e7f1861bb8c3ea48b36cc89ceb4df74dc15f
Jeffrey Altman <jaltman@secure-endpoints.com>
2008-09-24 23:58:32 +0000

    DEVEL15-windows-remove-entraneous-commit-20080924
    LICENSE MIT
    
    remove extraneous commit.  maybe later.
    
    
    (cherry picked from commit f52f24d6f0027be06d444c1dd7e0d35163b16ef6)

--- src/WINNT/afsd/cm_ioctl.c
+++ src/WINNT/afsd/cm_ioctl.c
@@ -3026,8 +3026,6 @@ cm_IoctlMemoryDump(struct cm_ioctl *ioctlp, struct cm_user *userp)
     cm_DumpSCache(hLogFile, cookie, 1);
     cm_DumpBufHashTable(hLogFile, cookie, 1);
     smb_DumpVCP(hLogFile, cookie, 1);
-    rx_DumpCalls(hLogFile, cookie);
-    rx_DumpPackets(hLogFile, cookie);
 
     CloseHandle(hLogFile);                          
   
