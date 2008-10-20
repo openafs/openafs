585935fbe54ed5d9cb944aef6a1ec78225f2cad9
Derrick Brashear <shadow@dementia.org>
2008-10-20 13:38:18 +0000

    DEVEL15-linux-mmap-antirecursion-20081020
    LICENSE IPL10
    FIXES 120491
    
    avoid deadlocking ourselves due to recursion when flushing pages on an
    mmap()ed file larger than the cache
    
    
    (cherry picked from commit 4a587356a31ac5afdf17d329a8598f70cf8bc3af)

--- src/afs/afs.h
+++ src/afs/afs.h
@@ -562,7 +562,11 @@ struct SimpleLocks {
 #define CUnlinkedDel	0x00040000
 #define CVFlushed	0x00080000
 #define CCore1		0x00100000	/* osf1 core file; not same as CCore above */
+#ifdef AFS_LINUX22_ENV
+#define CPageWrite      0x00200000      /* to detect vm deadlock - linux */
+#else
 #define CWritingUFS	0x00200000	/* to detect vm deadlock - used by sgi */
+#endif
 #define CCreating	0x00400000	/* avoid needless store after open truncate */
 #define CPageHog	0x00800000	/* AIX - dumping large cores is a page hog. */
 #define CDCLock		0x02000000	/* Vnode lock held over call to GetDownD */
