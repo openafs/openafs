92c7925b12d7b99e4ff044bc053330e77336a21a
Steven Jenkins <steven@endpoint.com>
2009-01-19 18:09:06 +0000

    rename-residency-from-mrafs-to-osd-20090119
    LICENSE IPL10
    FIXES 124130
    
    rename mrafs commands into their equivalent osd versions; structures etc are the same.

--- src/afs/afs_pioctl.c
+++ src/afs/afs_pioctl.c
@@ -114,7 +114,7 @@ DECL_PIOCTL(PFlushMount);
 DECL_PIOCTL(PRxStatProc);
 DECL_PIOCTL(PRxStatPeer);
 DECL_PIOCTL(PPrefetchFromTape);
-DECL_PIOCTL(PResidencyCmd);
+DECL_PIOCTL(PFsCmd);
 DECL_PIOCTL(PCallBackAddr);
 DECL_PIOCTL(PDiscon);
 DECL_PIOCTL(PNFSNukeCreds);
@@ -219,7 +219,7 @@ static pioctlFunction VpioctlSw[] = {
 	PBogus,			/* 64 -- arla: force cache check */
 	PBogus,			/* 65 -- arla: break callback */
 	PPrefetchFromTape,	/* 66 -- MR-AFS: prefetch file from tape */
-	PResidencyCmd,		/* 67 -- MR-AFS: generic commnd interface */
+	PFsCmd,			/* 67 -- RXOSD: generic commnd interface */
 	PBogus,			/* 68 -- arla: fetch stats */
 	PGetVnodeXStatus2,	/* 69 - get caller access and some vcache status */
 };
@@ -4431,21 +4431,21 @@ DECL_PIOCTL(PPrefetchFromTape)
     return code;
 }
 
-DECL_PIOCTL(PResidencyCmd)
+DECL_PIOCTL(PFsCmd)
 {
     register afs_int32 code;
     struct conn *tc;
     struct vcache *tvc;
-    struct ResidencyCmdInputs *Inputs;
-    struct ResidencyCmdOutputs *Outputs;
+    struct FsCmdInputs *Inputs;
+    struct FsCmdOutputs *Outputs;
     struct VenusFid tfid;
     struct AFSFid *Fid;
 
-    Inputs = (struct ResidencyCmdInputs *)ain;
-    Outputs = (struct ResidencyCmdOutputs *)aout;
+    Inputs = (struct FsCmdInputs *)ain;
+    Outputs = (struct FsCmdOutputs *)aout;
     if (!avc)
 	return EINVAL;
-    if (!ain || ainSize != sizeof(struct ResidencyCmdInputs))
+    if (!ain || ainSize != sizeof(struct FsCmdInputs))
 	return EINVAL;
 
     Fid = &Inputs->fid;
@@ -4469,8 +4469,8 @@ DECL_PIOCTL(PResidencyCmd)
 	    if (tc) {
 		RX_AFS_GUNLOCK();
 		code =
-		    RXAFS_ResidencyCmd(tc->id, Fid, Inputs,
-				       (struct ResidencyCmdOutputs *)aout);
+		    RXAFS_FsCmd(tc->id, Fid, Inputs, 
+					(struct FsCmdOutputs *)aout);
 		RX_AFS_GLOCK();
 	    } else
 		code = -1;
@@ -4494,7 +4494,7 @@ DECL_PIOCTL(PResidencyCmd)
     afs_PutVCache(tvc);
 
     if (!code) {
-	*aoutSize = sizeof(struct ResidencyCmdOutputs);
+	*aoutSize = sizeof(struct FsCmdOutputs);
     }
     return code;
 }
