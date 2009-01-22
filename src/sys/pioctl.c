6784222ac1741d9da18ba5d66fdd5057343ab8b1
Matt Benjamin <matt@linuxbox.com>
2009-01-22 21:38:45 +0000

    fbsd-ignore-sigsys-20090123
    LICENSE IPL10
    FIXES 123867
    
    wrong but a step forward

--- src/sys/pioctl.c
+++ src/sys/pioctl.c
@@ -48,6 +48,11 @@ int
 lpioctl(char *path, int cmd, char *cmarg, int follow)
 {
     int errcode, rval;
+#if defined(AFS_FBSD_ENV)
+    /* As kauth/user.c says, handle smoothly the case where no AFS system call
+     *  exists (yet).  Why don't more platforms have trouble here?  Matt */
+    sig_t old = (int (*)())signal(SIGSYS, SIG_IGN);
+#endif
 
 #if defined(AFS_LINUX20_ENV)
     rval = proc_afs_syscall(AFSCALL_PIOCTL, (long)path, cmd, (long)cmarg, 
