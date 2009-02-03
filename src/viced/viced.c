1811577fca393c4433de64ea6e1cd5a8d13521a3
Simon Wilkinson <sxw@inf.ed.ac.uk>
2009-02-03 19:42:14 +0000

    rename-internal-gettimeofday-refs-20090203
    LICENSE IPL10
    FIXES 124259
    
    switch to using fasttime, instead of wrapper

--- src/viced/viced.c
+++ src/viced/viced.c
@@ -712,7 +712,7 @@ PrintCounters()
     int processSize = 0;
     char tbuffer[32];
 
-    TM_GetTimeOfDay(&tpl, 0);
+    FT_GetTimeOfDay(&tpl, 0);
     Statistics = 1;
     ViceLog(0,
 	    ("Vice was last started at %s\n",
@@ -2221,7 +2221,7 @@ main(int argc, char *argv[])
 	    (void *)&fiveminutes, "FsyncCheck", &serverPid) == LWP_SUCCESS);
 #endif /* AFS_PTHREAD_ENV */
 
-    TM_GetTimeOfDay(&tp, 0);
+    FT_GetTimeOfDay(&tp, 0);
 
 #ifndef AFS_QUIETFS_ENV
     if (console != NULL) { 
