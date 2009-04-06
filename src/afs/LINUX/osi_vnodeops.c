abb18010b26b23f8c6813997d1080e35e7733529
Simon Wilkinson <sxw@inf.ed.ac.uk>
2009-04-06 23:50:44 +0000

    STABLE14-linux-avoid-returning-invalid-pointers-on-error-20090402
    LICENSE IPL10
    FIXES 124580
    
    avoid returning a positive error as it will look like a pointer.

--- src/afs/LINUX/osi_vnodeops.c
+++ src/afs/LINUX/osi_vnodeops.c
@@ -1087,8 +1087,10 @@ afs_linux_lookup(struct inode *dip, struct dentry *dp)
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(2,2,10)
     if (code == ENOENT)
 	return ERR_PTR(0);
-    else
+    else if ((code >= 0) && (code <= MAX_ERRNO))
 	return ERR_PTR(-code);
+    else 
+	return ERR_PTR(-EIO);
 #else
     if (code == ENOENT)
 	code = 0;
@@ -1429,7 +1431,10 @@ afs_linux_follow_link(struct dentry *dp, struct dentry *basep,
 
     if (code < 0) {
 	dput(basep);
-	res = ERR_PTR(code);
+	if (code < -MAX_ERRNO)
+	    res = ERR_PTR(-EIO);
+	else
+	    res = ERR_PTR(code);
     } else {
 	name[code] = '\0';
 	res = lookup_dentry(name, basep, follow);
