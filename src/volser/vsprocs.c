e2b4719d85c280029b7ada156d9e6cd72172821b
Simon Wilkinson <sxw@inf.ed.ac.uk>
2009-06-01 00:13:59 +0000

    DEVEL15-vsprocs-log-format-bug-20090531
    LICENSE IPL10
    
    fix the logged data to avoid a format string issue
    
    
    (cherry picked from commit 8e0b8243ea8b38e6ceab8f1dc6a0e032d6622568)

--- src/volser/vsprocs.c
+++ src/volser/vsprocs.c
@@ -2361,7 +2361,7 @@ cpincr:
 		      &cookie);
     EGOTO1(mfail, code,
 	   "Failed to do the%s dump from old site to new site\n",
-	   afromvol);
+	   (flags & RV_NOCLONE) ? "" : " incremental");
     VDONE;
 
     VPRINT1("Setting volume flags on destination volume %u ...", newVol);
