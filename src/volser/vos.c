f74464f58bec024fcabc9863572c59091cd0d83e
Andrew Deason <adeason@sinenomine.net>
2009-07-07 17:34:17 -0500

    Always display vnode accesses in vos output
    Vos was not displaying the "N accesses in the past day (i.e., vnode
    references)" message when updateDate was unset. Fix that.
    
    FIXES 125064
    
    Reviewed-on: http://gerrit.openafs.org/9
    Verified-by: Andrew Deason <adeason@sinenomine.net>
    Reviewed-by: Jeffrey Altman <jaltman@openafs.org>
    Reviewed-by: Simon Wilkinson <sxw@inf.ed.ac.uk>
    Reviewed-by: Derrick Brashear <shadow@dementia.org>
    (cherry picked from commit f52b97b76cba008c9aa6ae843869c95d83fbd9f9)
    Reviewed-on: http://gerrit.openafs.org/272
    Tested-by: Derrick Brashear <shadow@dementia.org>

--- src/volser/vos.c
+++ src/volser/vos.c
@@ -532,13 +532,12 @@ DisplayFormat(pntr, server, part, totalOK, totalNotOK, totalBusy, fast,
 #endif
 	    if (!pntr->updateDate)
 		fprintf(STDOUT, "    Last Update Never\n");
-	    else {
+	    else
 		fprintf(STDOUT, "    Last Update %s",
 			vos_ctime( & pntr->updateDate));
-		fprintf(STDOUT,
-			"    %d accesses in the past day (i.e., vnode references)\n",
-			pntr->dayUse);
-	    }
+	    fprintf(STDOUT,
+		    "    %d accesses in the past day (i.e., vnode references)\n",
+		    pntr->dayUse);
 	} else if (pntr->status == VBUSY) {
 	    *totalBusy += 1;
 	    qPut(&busyHead, pntr->volid);
@@ -689,13 +688,12 @@ XDisplayFormat(a_xInfoP, a_servID, a_partID, a_totalOKP, a_totalNotOKP,
 #endif
 	    if (!a_xInfoP->updateDate)
 		fprintf(STDOUT, "    Last Update Never\n");
-	    else {
+	    else
 		fprintf(STDOUT, "    Last Update %s",
 			vos_ctime( & a_xInfoP->updateDate));
-		fprintf(STDOUT,
-			"    %d accesses in the past day (i.e., vnode references)\n",
-			a_xInfoP->dayUse);
-	    }
+	    fprintf(STDOUT,
+		    "    %d accesses in the past day (i.e., vnode references)\n",
+		    a_xInfoP->dayUse);
 
 	    /*
 	     * Print all the read/write and authorship stats.
