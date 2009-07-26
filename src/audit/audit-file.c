openafs/openafs

commit 7b0b6a0ce9b320a3145f9d600596946230d815f0

Enhance audit logs to support SysV message queues
Adds support for sysv message queues for fileserver audit logs. This
also organizes the audit log code into various 'interfaces', of which
there are two: the original 'file' interface, and the 'sysvmq' interface
that this adds. The interface is configurable at runtime with the
-audit-interface switch.

FIXES 124674

Reviewed-on: http://gerrit.openafs.org/82
Tested-by: Andrew Deason <adeason@sinenomine.net>
Tested-by: Derrick Brashear <shadow@dementia.org>
Reviewed-by: Derrick Brashear <shadow@dementia.org>

src/audit/audit-file.c
@@ -1,0 +1,89 @@
+/*
+ * Copyright 2000, International Business Machines Corporation and others.
+ * All Rights Reserved.
+ *
+ * This software has been released under the terms of the IBM Public
+ * License.  For details, see the LICENSE file in the top-level source
+ * directory or online at http://www.openafs.org/dl/license10.html
+ */
+
+#include <afsconfig.h>
+#include <afs/param.h>
+#include <afs/afsutil.h>
+
+#include <string.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <sys/param.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include "audit-api.h"
+
+static FILE *auditout;
+
+static void
+send_msg(void)
+{
+    fprintf(auditout, "\n");
+    fflush(auditout);
+}
+
+static void
+append_msg(const char *format, ...)
+{
+    va_list vaList;
+
+    va_start(vaList, format);
+    vfprintf(auditout, format, vaList);
+    va_end(vaList);
+}
+
+static int
+open_file(const char *fileName)
+{
+    int tempfd, flags;
+    char oldName[MAXPATHLEN];
+
+#ifndef AFS_NT40_ENV
+    struct stat statbuf;
+
+    if ((lstat(fileName, &statbuf) == 0)
+        && (S_ISFIFO(statbuf.st_mode))) {
+        flags = O_WRONLY | O_NONBLOCK;
+    } else
+#endif
+    {
+        strcpy(oldName, fileName);
+        strcat(oldName, ".old");
+        renamefile(fileName, oldName);
+        flags = O_WRONLY | O_TRUNC | O_CREAT;
+    }
+    tempfd = open(fileName, flags, 0666);
+    if (tempfd > -1) {
+        auditout = fdopen(tempfd, "a");
+        if (!auditout) {
+            printf("Warning: auditlog %s not writable, ignored.\n", fileName);
+            return 1;
+        }
+    } else {
+        printf("Warning: auditlog %s not writable, ignored.\n", fileName);
+        return 1;
+    }
+    return 0;
+}
+
+static void
+print_interface_stats(FILE *out)
+{
+    return;
+}
+
+const struct osi_audit_ops audit_file_ops = {
+    &send_msg,
+    &append_msg,
+    &open_file,
+    &print_interface_stats,
+};

src/audit/audit.c
@@ -36,6 +36,7 @@
 #include <rx/rx.h>
 #include <rx/rxkad.h>
 #include "audit.h"
+#include "audit-api.h"
 #include "lock.h"
 #ifdef AFS_AIX32_ENV
 #include <sys/audit.h>
@@ -52,22 +53,42 @@
 # endif
 #endif      
 
-char *bufferPtr;
-int bufferLen;
-int osi_audit_all = (-1);	/* Not determined yet */
-int osi_echo_trail = (-1);
+extern struct osi_audit_ops audit_file_ops;
+#ifdef HAVE_SYS_IPC_H
+extern struct osi_audit_ops audit_sysvmq_ops;
+#endif
 
-FILE *auditout = NULL;
+static struct {
+    const char *name;
+    const struct osi_audit_ops *ops;
+} audit_interfaces[] = {
 
-int osi_audit_check(void);
+    { "file", &audit_file_ops },
+#ifdef HAVE_SYS_IPC_H
+    { "sysvmq", &audit_sysvmq_ops },
+#endif
+};
+
+#define N_INTERFACES (sizeof(audit_interfaces) / sizeof(audit_interfaces[0]))
+
+/* default to `file' audit interface */
+static const struct osi_audit_ops *audit_ops = &audit_file_ops;
+
+static int osi_audit_all = (-1);	/* Not determined yet */
+static int osi_echo_trail = (-1);
+
+static int auditout_open = 0;
+
+static int osi_audit_check(void);
 
 #ifdef AFS_AIX32_ENV
+static char *bufferPtr;
+static int bufferLen;
+
 static void
 audmakebuf(char *audEvent, va_list vaList)
 {
-#ifdef AFS_AIX32_ENV
     int code;
-#endif
     int vaEntry;
     int vaInt;
     afs_int32 vaLong;
@@ -149,7 +170,7 @@ audmakebuf(char *audEvent, va_list vaList)
 #endif
 
 static void
-printbuf(FILE *out, int rec, char *audEvent, char *afsName, afs_int32 hostId, 
+printbuf(int rec, char *audEvent, char *afsName, afs_int32 hostId,
 	 afs_int32 errCode, va_list vaList)
 {
     int vaEntry;
@@ -170,17 +191,17 @@ printbuf(FILE *out, int rec, char *audEvent, char *afsName, afs_int32 hostId,
 	timeStamp = afs_ctime(&currenttime, tbuffer,
 			      sizeof(tbuffer));
 	timeStamp[24] = ' ';   /* ts[24] is the newline, 25 is the null */
-	fprintf(out, timeStamp);
+	audit_ops->append_msg(timeStamp);
 
 	if (num > -1)
-	    fprintf(out, "[%d] ", num);
+	    audit_ops->append_msg("[%d] ", num);
     }
     
-    fprintf(out,  "EVENT %s CODE %d ", audEvent, errCode);
+    audit_ops->append_msg("EVENT %s CODE %d ", audEvent, errCode);
 
     if (afsName) {
 	hostAddr.s_addr = hostId;
-	fprintf(out,  "NAME %s HOST %s ", afsName, inet_ntoa(hostAddr));
+	audit_ops->append_msg("NAME %s HOST %s ", afsName, inet_ntoa(hostAddr));
     }
 
     vaEntry = va_arg(vaList, int);
@@ -189,52 +210,52 @@ printbuf(FILE *out, int rec, char *audEvent, char *afsName, afs_int32 hostId,
 	case AUD_STR:		/* String */
 	    vaStr = (char *)va_arg(vaList, char *);
 	    if (vaStr)
-		fprintf(out,  "STR %s ", vaStr);
+		audit_ops->append_msg("STR %s ", vaStr);
 	    else
-		fprintf(out,  "STR <null>");
+		audit_ops->append_msg("STR <null>");
 	    break;
 	case AUD_NAME:		/* Name */
 	    vaStr = (char *)va_arg(vaList, char *);
 	    if (vaStr)
-		fprintf(out,  "NAME %s ", vaStr);
+		audit_ops->append_msg("NAME %s ", vaStr);
 	    else
-		fprintf(out,  "NAME <null>");
+		audit_ops->append_msg("NAME <null>");
 	    break;
 	case AUD_ACL:		/* ACL */
 	    vaStr = (char *)va_arg(vaList, char *);
 	    if (vaStr)
-		fprintf(out,  "ACL %s ", vaStr);
+		audit_ops->append_msg("ACL %s ", vaStr);
 	    else
-		fprintf(out,  "ACL <null>");
+		audit_ops->append_msg("ACL <null>");
 	    break;
 	case AUD_INT:		/* Integer */
 	    vaInt = va_arg(vaList, int);
-	    fprintf(out,  "INT %d ", vaInt);
+	    audit_ops->append_msg("INT %d ", vaInt);
 	    break;
 	case AUD_ID:		/* ViceId */
 	    vaInt = va_arg(vaList, int);
-	    fprintf(out,  "ID %d ", vaInt);
+	    audit_ops->append_msg("ID %d ", vaInt);
 	    break;
 	case AUD_DATE:		/* Date    */
 	    vaLong = va_arg(vaList, afs_int32);
-	    fprintf(out, "DATE %u ", vaLong);
+	    audit_ops->append_msg("DATE %u ", vaLong);
 	    break;
 	case AUD_HOST:		/* Host ID */
 	    vaLong = va_arg(vaList, afs_int32);
             hostAddr.s_addr = vaLong;
-	    fprintf(out, "HOST %s ", inet_ntoa(hostAddr));
+	    audit_ops->append_msg("HOST %s ", inet_ntoa(hostAddr));
 	    break;
 	case AUD_LONG:		/* afs_int32    */
 	    vaLong = va_arg(vaList, afs_int32);
-	    fprintf(out, "LONG %d ", vaLong);
+	    audit_ops->append_msg("LONG %d ", vaLong);
 	    break;
 	case AUD_FID:		/* AFSFid - contains 3 entries */
 	    vaFid = va_arg(vaList, struct AFSFid *);
 	    if (vaFid)
-		fprintf(out, "FID %u:%u:%u ", vaFid->Volume, vaFid->Vnode,
+		audit_ops->append_msg("FID %u:%u:%u ", vaFid->Volume, vaFid->Vnode,
 		       vaFid->Unique);
 	    else
-		fprintf(out, "FID %u:%u:%u ", 0, 0, 0);
+		audit_ops->append_msg("FID %u:%u:%u ", 0, 0, 0);
 	    break;
 	case AUD_FIDS:		/* array of Fids */
 	    vaFids = va_arg(vaList, struct AFSCBFids *);
@@ -245,24 +266,24 @@ printbuf(FILE *out, int rec, char *audEvent, char *afsName, afs_int32 hostId,
                 vaFid = vaFids->AFSCBFids_val;
                 
                 if (vaFid) {
-                    fprintf(out, "FIDS %u FID %u:%u:%u ", vaFids->AFSCBFids_len, vaFid->Volume,
+                    audit_ops->append_msg("FIDS %u FID %u:%u:%u ", vaFids->AFSCBFids_len, vaFid->Volume,
                              vaFid->Vnode, vaFid->Unique);
                     for ( i = 1; i < vaFids->AFSCBFids_len; i++, vaFid++ ) 
-                        fprintf(out, "FID %u:%u:%u ", vaFid->Volume,
+                        audit_ops->append_msg("FID %u:%u:%u ", vaFid->Volume,
                                 vaFid->Vnode, vaFid->Unique);
                 } else
-                    fprintf(out, "FIDS 0 FID 0:0:0 ");
+                    audit_ops->append_msg("FIDS 0 FID 0:0:0 ");
 
             }
 	    break;
 	default:
-	    fprintf(out, "--badval-- ");
+	    audit_ops->append_msg("--badval-- ");
 	    break;
 	}			/* end switch */
 	vaEntry = va_arg(vaList, int);
     }				/* end while */
 
-    fprintf(out, "\n");
+    audit_ops->send_msg();
 }
 
 #ifdef AFS_PTHREAD_ENV
@@ -291,7 +312,7 @@ osi_audit_init(void)
 /* ************************************************************************** */
 /* The routine that acually does the audit call.
  * ************************************************************************** */
-int
+static int
 osi_audit_internal(char *audEvent,	/* Event name (15 chars or less) */
 		   afs_int32 errCode,	/* The error code */
 		   char *afsName,
@@ -304,7 +325,6 @@ osi_audit_internal(char *audEvent,	/* Event name (15 chars or less) */
     static char BUFFER[32768];
 #endif
     int result;
-    va_list vaCopy;
 
 #ifdef AFS_PTHREAD_ENV
     /* i'm pretty sure all the server apps now call osi_audit_init(),
@@ -315,10 +335,8 @@ osi_audit_internal(char *audEvent,	/* Event name (15 chars or less) */
 
     if ((osi_audit_all < 0) || (osi_echo_trail < 0))
 	osi_audit_check();
-    if (!osi_audit_all && !auditout)
+    if (!osi_audit_all && !auditout_open)
 	return 0;
-
-    va_copy(vaCopy, vaList);
 
     switch (errCode) {
     case 0:
@@ -357,18 +375,12 @@ osi_audit_internal(char *audEvent,	/* Event name (15 chars or less) */
     audmakebuf(audEvent, vaList);
 #endif
 
-    if (osi_echo_trail) {
-	printbuf(stdout, 0, audEvent, afsName, hostId, errCode, vaList);
-    }
-    va_end(vaCopy);
-
 #ifdef AFS_AIX32_ENV
     bufferLen = (int)((afs_int32) bufferPtr - (afs_int32) & BUFFER[0]);
     code = auditlog(audEvent, result, BUFFER, bufferLen);
 #else
-    if (auditout) {
-	printbuf(auditout, 0, audEvent, afsName, hostId, errCode, vaList);
-	fflush(auditout);
+    if (auditout_open) {
+	printbuf(0, audEvent, afsName, hostId, errCode, vaList);
     }
 #endif
 #ifdef AFS_PTHREAD_ENV
@@ -386,7 +398,7 @@ osi_audit(char *audEvent,	/* Event name (15 chars or less) */
 
     if ((osi_audit_all < 0) || (osi_echo_trail < 0))
 	osi_audit_check();
-    if (!osi_audit_all && !auditout)
+    if (!osi_audit_all && !auditout_open)
 	return 0;
 
     va_start(vaList, errCode);
@@ -413,7 +425,7 @@ osi_auditU(struct rx_call *call, char *audEvent, int errCode, ...)
 
     if (osi_audit_all < 0)
 	osi_audit_check();
-    if (!osi_audit_all && !auditout)
+    if (!osi_audit_all && !auditout_open)
 	return 0;
 
     strcpy(afsName, "--Unknown--");
@@ -454,7 +466,7 @@ osi_auditU(struct rx_call *call, char *audEvent, int errCode, ...)
 			int i, lrealm_match;
 
 			if (num_lrealms == -1) {
-			    for (i=0; i<AFS_NUM_LREALMS; i++) {
+			    for (i = 0; i < AFS_NUM_LREALMS; i++) {
 				if (afs_krb_get_lrealm(local_realms[i], i) != 0 /*KSUCCESS*/)
 				    break;
 			    }
@@ -466,7 +478,7 @@ osi_auditU(struct rx_call *call, char *audEvent, int errCode, ...)
 
 			/* Check to see if the ticket cell matches one of the local realms */
 			lrealm_match = 0;
-			for ( i=0;i<num_lrealms;i++ ) {
+			for (i = 0; i < num_lrealms ; i++ ) {
 			    if (!strcasecmp(local_realms[i], tcell)) {
 				lrealm_match = 1;
 				break;
@@ -560,35 +572,31 @@ osi_audit_check(void)
 }
 
 int
-osi_audit_file(char *fileName)
+osi_audit_file(const char *fileName)
 {
-    int tempfd, flags;
-    char oldName[MAXPATHLEN];
-    
-#ifndef AFS_NT40_ENV
-    struct stat statbuf;
-    
-    if ((lstat(fileName, &statbuf) == 0)
-        && (S_ISFIFO(statbuf.st_mode))) {
-        flags = O_WRONLY | O_NONBLOCK;
-    } else 
-#endif
-    {
-        strcpy(oldName, fileName);
-        strcat(oldName, ".old");
-        renamefile(fileName, oldName);
-        flags = O_WRONLY | O_TRUNC | O_CREAT;
+    if(!audit_ops->open_file(fileName)) {
+        auditout_open = 1;
+        return 0;
     }
-    tempfd = open(fileName, flags, 0666);
-    if (tempfd > -1) {
-        auditout = fdopen(tempfd, "a");
-        if (!auditout) {
-            printf("Warning: auditlog %s not writable, ignored.\n", fileName);
-            return 1;
-        }
-    } else { 
-        printf("Warning: auditlog %s not writable, ignored.\n", fileName);
-        return 1;
+    return 1;
+}
+
+int
+osi_audit_interface(const char *interface)
+{
+    int i;
+    for (i = 0; i < N_INTERFACES; ++i) {
+	if (strcmp(interface, audit_interfaces[i].name) == 0) {
+	    audit_ops = audit_interfaces[i].ops;
+	    return 0;
+	}
     }
-    return 0;
+
+    return 1;
+}
+
+void
+audit_PrintStats(FILE *out)
+{
+    audit_ops->print_interface_stats(out);
 }

acinclude.m4
@@ -1550,7 +1550,7 @@ AC_CHECK_HEADERS(stdlib.h string.h unistd.h poll.h fcntl.h sys/time.h sys/file.h
 AC_CHECK_HEADERS(netinet/in.h netdb.h sys/fcntl.h sys/mnttab.h sys/mntent.h)
 AC_CHECK_HEADERS(mntent.h sys/vfs.h sys/param.h sys/fs_types.h sys/fstyp.h)
 AC_CHECK_HEADERS(sys/mount.h strings.h termios.h signal.h poll.h sys/pag.h)
-AC_CHECK_HEADERS(windows.h malloc.h winsock2.h direct.h io.h sys/user.h)
+AC_CHECK_HEADERS(windows.h malloc.h winsock2.h direct.h io.h sys/user.h sys/ipc.h)
 AC_CHECK_HEADERS(security/pam_modules.h siad.h usersec.h ucontext.h regex.h values.h sys/statvfs.h sys/statfs.h sys/bitypes.h)
 AC_CHECK_HEADERS(linux/errqueue.h,,,[#include <linux/types.h>])
 

doc/man-pages/pod8/bosserver.pod
@@ -8,6 +8,7 @@ bosserver - Initializes the BOS Server
 <div class="synopsis">
 
 B<bosserver> [B<-noauth>] [B<-log>] [B<-enable_peer_stats>]
+    S<<< [B<-auditlog> <I<log path>>] >>> [B<-audit-interface> (file | sysvmq)]
     [B<-enable_process_stats>] [B<-allow-dotted-principals>] [B<-help>]
 
 =for html
@@ -91,6 +92,15 @@ useful only when authorization checking is disabled on the server machine
 Records in the F</usr/afs/logs/BosLog> file the names of all users who
 successfully issue a privileged B<bos> command (one that requires being
 listed in the F</usr/afs/etc/UserList> file).
+
+=item B<-auditlog> <I<log path>>
+
+Turns on audit logging, and sets the path for the audit log.
+
+=item B<-audit-interface> (file | sysvmq)
+
+Specifies what audit interface to use. Defaults to C<file>. See
+L<fileserver(8)> for an explanation of each interface.
 
 =item B<-enable_peer_stats>
 

doc/man-pages/pod8/buserver.pod
@@ -8,6 +8,7 @@ buserver - Initializes the Backup Server
 <div class="synopsis">
 
 B<buserver> S<<< [B<-database> <I<database directory>>] >>>
+    S<<< [B<-auditlog> <I<log path>>] >>> [B<-audit-interface> (file | sysvmq)]
     S<<< [B<-cellservdb> <I<cell configuration directory>>] >>> [B<-resetdb>]
     [B<-noauth>] [B<-smallht>] [B<-servers> <I<list of ubik database servers>>+]
     [B<-enable_peer_stats>]  [B<-enable_process_stats>] [B<-rxbind>]
@@ -58,6 +59,15 @@ B<buserver> process is running.
 Specifies the pathname of an alternate directory for the Backup Database
 files, ending in a final slash (C</>). If this argument is not provided,
 the default is the F</usr/afs/db> directory.
+
+=item B<-auditlog> <I<log path>>
+
+Turns on audit logging, and sets the path for the audit log.
+
+=item B<-audit-interface> (file | sysvmq)
+
+Specifies what audit interface to use. Defaults to C<file>. See
+L<fileserver(8)> for an explanation of each interface.
 
 =item B<-cellservdb> <I<cell configuration directory>>
 

doc/man-pages/pod8/fileserver.pod
@@ -8,6 +8,7 @@ fileserver - Initializes the File Server component of the fs process
 <div class="synopsis">
 
 B<fileserver> S<<< [B<-auditlog> <I<path to log file>>] >>>
+    S<<< [B<-audit-interface> (file | sysvmq)] >>>
     S<<< [B<-d> <I<debug level>>] >>>
     S<<< [B<-p> <I<number of processes>>] >>>
     S<<< [B<-spare> <I<number of spare blocks>>] >>>
@@ -256,6 +257,17 @@ length.
 =item B<-auditlog> <I<log path>>
 
 Set and enable auditing.
+
+=item B<-audit-interface> (file | sysvmq)
+
+Specifies what audit interface to use. The C<file> interface writes audit
+messages to the file passed to B<-auditlog>. The C<sysvmq> interface
+writes audit messages to a SYSV message (see L<msgget(2)> and
+L<msgrcv(2)>). The message queue the C<sysvmq> interface writes to has the
+key C<ftok(path, 1)>, where C<path> is the path specified in the
+B<-auditlog> option.
+
+Defaults to C<file>.
 
 =item B<-d> <I<debug level>>
 

doc/man-pages/pod8/kaserver.pod
@@ -8,6 +8,7 @@ kaserver - Initializes the Authentication Server
 <div class="synopsis">
 
 B<kaserver> [B<-noAuth>] [B<-fastKeys>] [B<-database> <I<dbpath>>]
+    S<<< [B<-auditlog> <I<log path>>] >>> [B<-audit-interface> (file | sysvmq)]
     S<<< [B<-localfiles> <I<lclpath>>] >>> S<<< [B<-minhours> <I<n>>] >>>
     S<<< [B<-servers> <I<serverlist>>] >>> [B<-enable_peer_stats>]
     [B<-enable_process_stats>] [B<-help>]
@@ -90,6 +91,15 @@ files is F</usr/afs/db/kaserver>.
 Provide the B<-localfiles> argument along with this one; otherwise, the
 B<-localfiles> argument is also set to the value of this argument, which
 is probably inappropriate.
+
+=item B<-auditlog> <I<log path>>
+
+Turns on audit logging, and sets the path for the audit log.
+
+=item B<-audit-interface> (file | sysvmq)
+
+Specifies what audit interface to use. Defaults to C<file>. See
+L<fileserver(8)> for an explanation of each interface.
 
 =item B<-localfiles> <I<lclpath>>
 

doc/man-pages/pod8/ptserver.pod
@@ -13,6 +13,7 @@ B<ptserver> S<<< [B<-database> | B<-db> <I<db path>>] >>> S<<< [B<-p> <I<number 
     [B<-restricted>] [B<-enable_peer_stats>]
     [B<-enable_process_stats>] [B<-allow-dotted-principals>]
     [B<-rxbind>] S<<< [B<-auditlog> <I<file path>>] >>>
+    S<<< [B<-audit-interface> (file | sysvmq)] >>>
     S<<< [B<-syslog>[=<I<FACILITY>>]] >>> S<<< [B<-rxmaxmtu> <I<bytes>>] >>>
     [B<-help>]
 
@@ -150,9 +151,14 @@ log file.  B<-syslog>=I<FACILITY> can be used to specify to which facility
 the log message should be sent.  Logging message sent to syslog are tagged
 with the string "ptserver".
 
-=item B<-auditlog> <I<file path>>
+=item B<-auditlog> <I<log path>>
 
-Specifies the full pathname for the B<AuditLog> file.
+Turns on audit logging, and sets the path for the audit log.
+
+=item B<-audit-interface> (file | sysvmq)
+
+Specifies what audit interface to use. Defaults to C<file>. See
+L<fileserver(8)> for an explanation of each interface.
 
 =item B<-rxmaxmtu> <I<bytes>>
 

commit 96c746c75260a3cf6dc58fcf692b44f83ef65a95

Permit enhanced audit logs to work on Windows
Gerrit 82 / Git 7b0b6a0ce9b320a3145f9d600596946230d815f0
broke the Windows build.

Add audit-file.obj to file list in NTMakefile.
Remove unnecessary includes from audit.c.
Properly conditionalize includes in audit-file.c.

Reviewed-on: http://gerrit.openafs.org/219
Tested-by: Jeffrey Altman <jaltman@openafs.org>
Reviewed-by: Jeffrey Altman <jaltman@openafs.org>

src/audit/NTMakefile
@@ -20,7 +20,8 @@ INCFILES =\
 LIBFILE = $(DESTDIR)\lib\afs\afsaudit.lib
 
 LIBOBJS =\
-	$(OUT)\audit.obj
+	$(OUT)\audit.obj \
+        $(OUT)\audit-file.obj
 
 $(LIBFILE): $(LIBOBJS) 
 	$(LIBARCH)  

src/audit/audit-file.c
@@ -14,12 +14,15 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdarg.h>
-#include <sys/param.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#ifndef AFS_NT40_ENV
+#include <sys/param.h>
 #include <unistd.h>
-#include <fcntl.h>
-
+#else
+#include <io.h>
+#endif
 #include "audit-api.h"
 
 static FILE *auditout;

src/audit/audit.c
@@ -18,8 +18,6 @@
 #include <sys/stat.h>
 #ifndef AFS_NT40_ENV
 #include <unistd.h>
-#else
-#include <io.h>
 #endif
 #ifdef AFS_AIX32_ENV
 #include <sys/audit.h>
@@ -261,7 +259,7 @@ printbuf(int rec, char *audEvent, char *afsName, afs_int32 hostId,
 	    vaFids = va_arg(vaList, struct AFSCBFids *);
 
 	    if (vaFids) {
-                int i;
+                unsigned int i;
                 
                 vaFid = vaFids->AFSCBFids_val;
                 