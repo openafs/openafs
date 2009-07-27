openafs/openafs

commit 119c756d96c4a7f9e01e4ff6bb20156f1abf761b

Use -errno or WSAGetLastError() as return value from rxi_Sendmsg()
This patch causes the pthread and lwp implementations of rxi_Sendmsg()
to use -errno or WSAGetLastError() as the return value if it is
positive.  This is required in order to communicate more meaningful
error conditions to rxi_SendPacket[List], which should "down" a host
immediately when it observes ENETUNREACH.

Jeff Altman <jaltman@openafs.org> supplied the logic for the
AFS_NT40_ENV case; I was not able to test this (I do not own a
Windows license) and took him on his word.

Reviewed-on: http://gerrit.openafs.org/58
Tested-by: Derrick Brashear <shadow@dementia.org>
Reviewed-by: Adam Megacz <megacz@gmail.com>
Tested-by: Adam Megacz <megacz@gmail.com>
Reviewed-by: Derrick Brashear <shadow@dementia.org>

src/rx/rx_lwp.c
@@ -460,6 +460,13 @@ rxi_Sendmsg(osi_socket socket, struct msghdr *msg_p, int flags)
 	{
 	    (osi_Msg "rx failed to send packet: ");
 	    perror("rx_sendmsg");
+#ifndef AFS_NT40_ENV
+            if (errno > 0)
+              return -errno;
+#elif
+            if (WSAGetLastError() > 0)
+              return -WSAGetLastError();
+#endif
 	    return -1;
 	}
 	while ((err = select(socket + 1, 0, sfds, 0, 0)) != 1) {

src/rx/rx_pthread.c
@@ -440,6 +440,13 @@ rxi_Sendmsg(osi_socket socket, struct msghdr *msg_p, int flags)
 #endif
 	dpf(("rxi_sendmsg failed, error %d\n", errno));
 	fflush(stdout);
+#ifndef AFS_NT40_ENV
+        if (errno > 0)
+          return -errno;
+#elif
+            if (WSAGetLastError() > 0)
+              return -WSAGetLastError();
+#endif
 	return -1;
     }
     return 0;

commit 9a490c96a074f4c3c82a3f01c18bc74cc19ae184

Build fix - pre-processor typos in rx_lwp.c and rx_pthread.c
Commit 119c756d96c4a7f9e01e4ff6bb20156f1abf761b introduced a typo
in two places, using #elif instead of the apparently intended #else.

Reviewed-on: http://gerrit.openafs.org/231
Tested-by: Jeffrey Altman <jaltman@openafs.org>
Reviewed-by: Jeffrey Altman <jaltman@openafs.org>

src/rx/rx_lwp.c
@@ -463,7 +463,7 @@ rxi_Sendmsg(osi_socket socket, struct msghdr *msg_p, int flags)
 #ifndef AFS_NT40_ENV
             if (errno > 0)
               return -errno;
-#elif
+#else
             if (WSAGetLastError() > 0)
               return -WSAGetLastError();
 #endif

src/rx/rx_pthread.c
@@ -443,7 +443,7 @@ rxi_Sendmsg(osi_socket socket, struct msghdr *msg_p, int flags)
 #ifndef AFS_NT40_ENV
         if (errno > 0)
           return -errno;
-#elif
+#else
             if (WSAGetLastError() > 0)
               return -WSAGetLastError();
 #endif