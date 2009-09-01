5fd700d1fda856b0e16588cef0e54bc37498d375
Christof Hanke <christof.hanke@rzg.mpg.de>
2009-09-01 14:33:53 +0200

    Uploaded patchset 9. Sorry for the mess.
    Reviewed-on: http://gerrit.openafs.org/125
    Reviewed-by: Derrick Brashear <shadow@dementia.org>
    Tested-by: Derrick Brashear <shadow@dementia.org>

--- src/afs/afs_fetchstore.c
+++ src/afs/afs_fetchstore.c
@@ -612,14 +612,12 @@ afs_CacheStoreVCache(struct dcache **dcList, struct vcache *avc,
 #endif
 		code = rxfs_storeInit(avc, tc, length, bytes, base,
 					sync, &ops, &rock);
-		if ( code ) {
-		    osi_Panic(
-		    "afs_CacheStoreProc: rxfs_storeInit failed with %d", code);
-		}
+		if ( code )
+		    goto nocall;
 
 		code = afs_CacheStoreDCaches(avc, dclist, bytes, anewDV,
 			&doProcessFS, &OutStatus, nchunks, nomore, ops, rock);
-
+nocall:
 #ifdef AFS_64BIT_CLIENT
 		if (code == RXGEN_OPCODE && !afs_serverHasNo64Bit(tc)) {
 		    afs_serverSetNo64Bit(tc);
