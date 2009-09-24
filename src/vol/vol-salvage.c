openafs/openafs

commit 0ac956b3384842b3c60e72bde78a4baf58a5877f

salvager-no-chdir-20081108
LICENSE IPL10
FIXES 123577

make sure salvager cores don't get left in vice partitions

src/vol/vol-salvage.c
@@ -1204,7 +1204,8 @@ GetVolumeSummary(VolumeId singleVolumeNumber)
     struct dirent *dp;
 
     /* Get headers from volume directory */
-    if (chdir(fileSysPath) == -1 || (dirp = opendir(".")) == NULL)
+    dirp = opendir(fileSysPath);
+    if (dirp  == NULL)
 	Abort("Can't read directory %s; not salvaged\n", fileSysPath);
     if (!singleVolumeNumber) {
 	while ((dp = readdir(dirp))) {
@@ -3543,16 +3544,18 @@ ToString(char *s)
     assert(p != NULL);
     strcpy(p, s);
     return p;
-
 }
 
 /* Remove the FORCESALVAGE file */
 void
 RemoveTheForce(char *path)
 {
+    char target[1024];
+    struct afs_stat force; // so we can use afs_stat to find it
+    strcpy(target,path);
+    strcat(target,"/FORCESALVAGE");
     if (!Testing && ForceSalvage) {
-	if (chdir(path) == 0)
-	    unlink("FORCESALVAGE");
+        if (afs_stat(target,&force) == 0)  unlink(target);
     }
 }
 
@@ -3564,10 +3567,11 @@ int
 UseTheForceLuke(char *path)
 {
     struct afs_stat force;
+    char target[1024];
+    strcpy(target,path);
+    strcat(target,"/FORCESALVAGE");
 
-    assert(chdir(path) != -1);
-
-    return (afs_stat("FORCESALVAGE", &force) == 0);
+    return (afs_stat(target, &force) == 0);
 }
 #else
 /*

src/vol/vol-info.c
@@ -413,11 +413,7 @@ HandlePart(struct DiskPartition64 *partP)
     char *p = VPartitionPath(partP);
 #endif
 
-    if (chdir(p) == -1) {
-	printf("Can't chdir to partition %s; giving up\n", p);
-	exit(1);
-    }
-    if ((dirp = opendir(".")) == NULL) {
+    if ((dirp = opendir(p)) == NULL) {
 	printf("Can't read directory %s; giving up\n", p);
 	exit(1);
     }

commit 29ee678f3ab051a27cae46b02a0d42e0958a6bc8

Unlink correct files in salvager
Fallout from 0ac956b3384842b3c60e72bde78a4baf58a5877f; a couple of
unlink() calls were not updated to use the absolute path. Update them,
and log errors from unlink(), since otherwise it is difficult to tell
that anything is going wrong.

Reviewed-on: http://gerrit.openafs.org/479
Reviewed-by: Derrick Brashear <shadow@dementia.org>
Tested-by: Derrick Brashear <shadow@dementia.org>

src/vol/vol-salvage.c
@@ -874,10 +874,16 @@ SalvageFileSys1(struct DiskPartition64 *partP, VolumeId singleVolumeNumber)
 void
 DeleteExtraVolumeHeaderFile(register struct VolumeSummary *vsp)
 {
+    char path[64];
+    sprintf(path, "%s/%s", fileSysPath, vsp->fileName);
+
     if (!Showmode)
-	Log("The volume header file %s is not associated with any actual data (%sdeleted)\n", vsp->fileName, (Testing ? "would have been " : ""));
-    if (!Testing)
-	unlink(vsp->fileName);
+	Log("The volume header file %s is not associated with any actual data (%sdeleted)\n", path, (Testing ? "would have been " : ""));
+    if (!Testing) {
+	if (unlink(path)) {
+	    Log("Unable to unlink %s (errno = %d)\n", path, errno);
+	}
+    }
     vsp->fileName = 0;
 }
 
@@ -3197,8 +3203,13 @@ MaybeZapVolume(register struct InodeSummary *isp, char *message, int deleteMe,
 		if (!Showmode)
 		    Log("it will be deleted instead.  It should be recloned.\n");
 	    }
-	    if (!Testing)
-		unlink(isp->volSummary->fileName);
+	    if (!Testing) {
+		char path[64];
+		sprintf(path, "%s/%s", fileSysPath, isp->volSummary->fileName);
+		if (unlink(path)) {
+		    Log("Unable to unlink %s (errno = %d)\n", path, errno);
+		}
+	    }
 	}
     } else if (!check) {
 	Log("%s salvage was unsuccessful: read-write volume %u\n", message,