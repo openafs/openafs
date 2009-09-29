ca8890245b44cc3abf311797ae62d2f0aaca5429
Derrick Brashear <shadow@dementia.org>
2009-09-29 10:26:23 -0400

    intptr fallout
    some kernels don't have e.g. uintptr_t; revert this hunk for now.
    also, autoconf as recent as what's in rhel5 has no macros. provide
    some.
    
    Reviewed-on: http://gerrit.openafs.org/553
    Tested-by: Derrick Brashear <shadow@dementia.org>
    Reviewed-by: Derrick Brashear <shadow@dementia.org>

--- src/afs/afs_lock.c
+++ src/afs/afs_lock.c
@@ -279,7 +279,7 @@ afs_BozonLock(struct afs_bozoLock *alock, struct vcache *avc)
 	    /* To shut up SGI compiler on remark(1413) warnings. */
 	    alock->proc = (char *)(long)MyPidxx;
 #else /* AFS_64BITPOINTER_ENV */
-	    alock->proc = (char *)(intptr_t)MyPidxx;
+	    alock->proc = (char *)MyPidxx;
 #endif /* AFS_64BITPOINTER_ENV */
 #endif
 	    alock->count = 1;
@@ -291,7 +291,7 @@ afs_BozonLock(struct afs_bozoLock *alock, struct vcache *avc)
 	    /* To shut up SGI compiler on remark(1413) warnings. */
 	} else if (alock->proc == (char *)(long)MyPidxx) {
 #else /* AFS_64BITPOINTER_ENV */
-	} else if (alock->proc == (char *)(intptr_t)MyPidxx) {
+	} else if (alock->proc == (char *)MyPidxx) {
 #endif /* AFS_64BITPOINTER_ENV */
 #endif
 	    /* lock is held, but by us, so we win anyway */
@@ -350,7 +350,7 @@ afs_CheckBozonLockBlocking(struct afs_bozoLock *alock)
 	/* To shut up SGI compiler on remark(1413) warnings. */
 	if (alock->proc != (char *)(long)MyPidxx)
 #else /* AFS_64BITPOINTER_ENV */
-	if (alock->proc != (char *)(intptr_t)MyPidxx)
+	if (alock->proc != (char *)MyPidxx)
 #endif /* AFS_64BITPOINTER_ENV */
 #endif
 	    return 1;
