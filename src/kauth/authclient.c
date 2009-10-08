7f7b4c9aad5deb5be62825ed6b88ca17093c0da6
Marc Dionne <marc.c.dionne@gmail.com>
2009-10-08 08:12:28 -0400

    authclient.c: fix 64-bit specific warnings
    Commit 36be36e034141ded8fff5ff2edab3223b23c9fcf introduced some casts
    from an integer type to void *.  This generates warnings on a 64-bit
    platform.
    Use uintptr_t casts to make the compiler happy.
    
    Reviewed-on: http://gerrit.openafs.org/605
    Tested-by: Derrick Brashear <shadow@dementia.org>
    Reviewed-by: Derrick Brashear <shadow@dementia.org>

--- src/kauth/authclient.c
+++ src/kauth/authclient.c
@@ -564,7 +564,7 @@ ka_Authenticate(char *name, char *instance, char *cell, struct ubik_client * con
     version = 2;
     code =
 	kawrap_ubik_Call(KAA_AuthenticateV2, conn, 0, name, instance,
-			 (void*)start, (void*)end, &arequest, &oanswer, 0, 0);
+			 (void*)(uintptr_t)start, (void*)(uintptr_t)end, &arequest, &oanswer, 0, 0);
     if (code == RXGEN_OPCODE) {
 	oanswer.MaxSeqLen = sizeof(answer);
 	oanswer.SeqBody = (char *)&answer;
