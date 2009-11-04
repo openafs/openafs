812dcc2c1b4e0f333dd0a85f46066c8018aad79b
Evan Broder <broder@mit.edu>
2009-11-03 23:50:39 -0500

    Increase the maximum number of sysnames
    Change-Id: I8438ca19649730260370d8b00fb0157e808abe31
    Reviewed-on: http://gerrit.openafs.org/778
    Reviewed-by: Jeffrey Altman <jaltman@openafs.org>
    Reviewed-by: Derrick Brashear <shadow@dementia.org>
    Tested-by: Derrick Brashear <shadow@dementia.org>
    (cherry picked from commit b772bd7e3c1305710eb3c18b2fe4108e467bd383)
    Reviewed-on: http://gerrit.openafs.org/1650

--- src/afs/afs.h
+++ src/afs/afs.h
@@ -75,7 +75,7 @@ extern int afs_shuttingdown;
 #define	PIGGYSIZE	1350	/* max piggyback size */
 #define	MAXVOLS		128	/* max vols we can store */
 #define	MAXSYSNAME	128	/* max sysname (i.e. @sys) size */
-#define MAXNUMSYSNAMES	16	/* max that current constants allow */
+#define MAXNUMSYSNAMES	32	/* max that current constants allow */
 #define	NOTOKTIMEOUT	(2*3600)	/* time after which to timeout conns sans tokens */
 #define	NOPAG		0xffffffff
 #define AFS_NCBRS	300	/* max # of call back return entries */
