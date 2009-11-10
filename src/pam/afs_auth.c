2d0824245c030b08f90972ea0756fccf981263c0
Marc Dionne <marc.c.dionne@gmail.com>
2009-11-10 18:16:45 -0500

    src/pam/afs_auth.c warning fix
    ka_UserAuthenticateGeneral expects an afs_int32 pointer for the
    password_expires argument.  A (long *) was used in afs_auth.c,
    generating a few warnings.
    
    Change-Id: Iafc92e72022644ff23c642e801d51bd4387afa88
    Reviewed-on: http://gerrit.openafs.org/803
    Reviewed-by: Russ Allbery <rra@stanford.edu>
    Reviewed-by: Simon Wilkinson <sxw@inf.ed.ac.uk>
    Tested-by: Derrick Brashear <shadow@dementia.org>
    Reviewed-by: Derrick Brashear <shadow@dementia.org>

--- src/pam/afs_auth.c
+++ src/pam/afs_auth.c
@@ -60,7 +60,7 @@ pam_sm_authenticate(pam_handle_t * pamh, int flags, int argc,
     int set_expires = 0;	/* This option is only used in pam_set_cred() */
     int got_authtok = 0;	/* got PAM_AUTHTOK upon entry */
     char *user = NULL, *password = NULL;
-    long password_expires = -1;
+    afs_int32 password_expires = -1;
     int torch_password = 1;
     int i;
     struct pam_conv *pam_convp = NULL;
