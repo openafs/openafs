5afa2965b2c4eab2a32dbd4ff036a835eb903e75
Derrick Brashear <shadow@dementia.org>
2010-04-24 17:05:47 -0400

    cachemgr store file too big error exit via usual path
    have just one path for exiting on errors so we do everything
    
    this means we will EndCall, instead of not, in the EFBIG case
    
    Change-Id: I2df025eee4bd5173ed4f80a73f9a9a347e7257d2
    Reviewed-on: http://gerrit.openafs.org/1827
    Reviewed-by: Derrick Brashear <shadow@dementia.org>
    Tested-by: Derrick Brashear <shadow@dementia.org>

--- src/afs/afs_segments.c
+++ src/afs/afs_segments.c
@@ -95,8 +95,8 @@ afs_StoreMini(register struct vcache *avc, struct vrequest *areq)
 		if ((avc->f.m.Length > 0x7fffffff) ||
 		    (tlen > 0x7fffffff) ||
 		    ((0x7fffffff - tlen) < avc->f.m.Length)) {
-		    RX_AFS_GLOCK();
-		    return EFBIG;
+		    code = EFBIG;
+		    goto error;
 		}
 		code =
 		    StartRXAFS_StoreData(tcall,
@@ -111,6 +111,9 @@ afs_StoreMini(register struct vcache *avc, struct vrequest *areq)
 	    if (code == 0) {
 		code = EndRXAFS_StoreData(tcall, &OutStatus, &tsync);
 	    }
+#ifdef AFS_64BIT_CLIENT
+	error:
+#endif
 	    code = rx_EndCall(tcall, code);
 	    RX_AFS_GLOCK();
 	    XSTATS_END_TIME;
