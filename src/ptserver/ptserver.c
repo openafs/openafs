openafs/openafs

commit 1f22cab96abd1ec83ea5c0d9c3fc1a405385b8ea

Remove -rebuildDB flag to ptserver
The -rebuildDB flag was documented to rebuild the Protection Database at
startup, but it was accepted and ignored in the ptserver source, doing
nothing.  Remove the documentation and the option recognition in ptserver.

Change-Id: I36f30f38464b602cb4739a958663a6feb5fe27bf
Reviewed-on: http://gerrit.openafs.org/2029
Reviewed-by: Derrick Brashear <shadow@dementia.org>
Tested-by: Derrick Brashear <shadow@dementia.org>

src/ptserver/ptserver.c
@@ -293,10 +293,8 @@ main(int argc, char **argv)
 		       lwps, 3);
 		lwps = 3;
 	    }
-	} else if (strncmp(arg, "-rebuild", alen) == 0)	/* rebuildDB++ */
-	    ;
 #if defined(SUPERGROUPS)
-	else if ((strncmp(arg, "-groupdepth", alen) == 0)
+	} else if ((strncmp(arg, "-groupdepth", alen) == 0)
 		 || (strncmp(arg, "-depth", alen) == 0)) {
 	    depthsg = atoi(argv[++a]);	/* Max search depth for supergroups */
 	}

doc/man-pages/pod8/ptserver.pod
@@ -8,7 +8,7 @@ ptserver - Initializes the Protection Server
 <div class="synopsis">
 
 B<ptserver> S<<< [B<-database> | B<-db> <I<db path>>] >>> S<<< [B<-p> <I<number of threads>>] >>> S<<< [B<-d> <I<debug level>>] >>>
-    [B<-rebuildDB>] S<<< [B<-groupdepth> <I<# of nested groups>>] >>>
+    S<<< [B<-groupdepth> <I<# of nested groups>>] >>>
     S<<< [B<-default_access> <I<user access mask>> <I<group access mask>>] >>>
     [B<-restricted>] [B<-enable_peer_stats>]
     [B<-enable_process_stats>] [B<-allow-dotted-principals>]
@@ -89,12 +89,6 @@ F</usr/afs/db/prdb>.
 Sets the number of server lightweight processes (LWPs or pthreads) to run.
 Provide a positive integer from the range C<3> to C<16>. The default
 value is C<3>.
-
-=item B<-rebuildDB>
-
-Rebuilds the Protection Database at the beginning of Protection Server
-initialization. Use this argument only in consultation with AFS
-Development or Product Support.
 
 =item B<-groupdepth> <I<# of nested groups>>, B<-depth> <I<# of nested groups>>
 

commit 5698becffffb49ff49f69ce202032ae0bd9e9f7a

Fix syntax error when supergroups are not enabled
1f22cab broke non-supergroups builds due to incorrect brace placement.
Move the correct closing brace outside of the #ifdef.

Change-Id: I2ed3f8f57b35f0030e26306f7e791ea6b5158c9a
Reviewed-on: http://gerrit.openafs.org/2038
Reviewed-by: Russ Allbery <rra@stanford.edu>
Tested-by: Russ Allbery <rra@stanford.edu>

src/ptserver/ptserver.c
@@ -297,9 +297,8 @@ main(int argc, char **argv)
 	} else if ((strncmp(arg, "-groupdepth", alen) == 0)
 		 || (strncmp(arg, "-depth", alen) == 0)) {
 	    depthsg = atoi(argv[++a]);	/* Max search depth for supergroups */
-	}
 #endif
-	else if (strncmp(arg, "-default_access", alen) == 0) {
+	} else if (strncmp(arg, "-default_access", alen) == 0) {
 	    prp_user_default = prp_access_mask(argv[++a]);
 	    prp_group_default = prp_access_mask(argv[++a]);
 	}