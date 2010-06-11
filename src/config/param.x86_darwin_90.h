09e17c245220203b7fc14cb5908f8475d79a0707
Simon Wilkinson <sxw@inf.ed.ac.uk>
2010-06-11 22:25:35 +0100

    Changes to build with clang on Mac OS 10.5
    clang defines __x86_64__ and doesn't define __i386__ when building
    on a 64bit Leopard machine. Change the defines in param.h so we
    can build on this platform.
    
    Change-Id: Icfc16ca83162e8148d4261207c05d90d12cbabc2
    Reviewed-on: http://gerrit.openafs.org/2132
    Reviewed-by: Russ Allbery <rra@stanford.edu>
    Reviewed-by: Derrick Brashear <shadow@dementia.org>
    Tested-by: Derrick Brashear <shadow@dementia.org>

--- src/config/param.x86_darwin_90.h
+++ src/config/param.x86_darwin_90.h
@@ -10,7 +10,7 @@
 #define AFS_64BIT_IOPS_ENV	1
 #if defined(__ppc__)
 #define AFS_PPC_ENV		1
-#elif defined(__i386__)
+#elif defined(__i386__) || defined(__x86_64__)
 #define AFS_X86_ENV		1
 #else
 #error Unsupported architecture
@@ -46,7 +46,7 @@
 #define SYS_NAME		"ppc_darwin_90"
 #define SYS_NAME_ID		SYS_NAME_ID_ppc_darwin_90
 #define AFSBIG_ENDIAN		1
-#elif defined(__i386__)
+#elif defined(__i386__) || defined(__x86_64__)
 #define sys_x86_darwin_12	1
 #define sys_x86_darwin_13	1
 #define sys_x86_darwin_14	1
@@ -106,7 +106,7 @@
 #define AFS_64BIT_ENV           1	/* Defines afs_int32 as int, not long. */
 #if defined(__ppc__)
 #define AFS_PPC_ENV		1
-#elif defined(__i386__)
+#elif defined(__i386__) || defined(__x86_64__)
 #define AFS_X86_ENV		1
 #else
 #error Unsupported architecture
@@ -139,7 +139,7 @@
 #define SYS_NAME		"ppc_darwin_90"
 #define SYS_NAME_ID		SYS_NAME_ID_ppc_darwin_90
 #define AFSBIG_ENDIAN		1
-#elif defined(__i386__)
+#elif defined(__i386__) || defined(__x86_64__)
 #define sys_x86_darwin_12	1
 #define sys_x86_darwin_13	1
 #define sys_x86_darwin_14	1
