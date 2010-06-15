0b958626014b9fabb7bd4c4b925838462dc09d82
Jeffrey Altman <jaltman@your-file-system.com>
2010-06-15 07:32:11 -0400

    mkvers.c - remove afsconfig.h, afs/param.h and assert.h
    assert() is not used within mkvers.c.
    
    afsconfig.h and afs/param.h have dependency problems in
    src/config/
    
    Change-Id: I2012a01c87cbd993a70df76ee3384b43ff037b29
    Reviewed-on: http://gerrit.openafs.org/2203
    Reviewed-by: Simon Wilkinson <sxw@inf.ed.ac.uk>
    Tested-by: Simon Wilkinson <sxw@inf.ed.ac.uk>
    Reviewed-by: Jeffrey Altman <jaltman@openafs.org>
    Tested-by: Jeffrey Altman <jaltman@openafs.org>

--- src/config/mkvers.c
+++ src/config/mkvers.c
@@ -12,15 +12,11 @@
  *
  */
 
-#include <afsconfig.h>
-#include <afs/param.h>
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <errno.h>
-#include <assert.h>
 #include <string.h>
 
 
