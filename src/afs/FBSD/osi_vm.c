3688073993b57c4cd13f257294acd192e82012cf
Ben Kaduk <kaduk@mit.edu>
2010-06-30 00:12:59 -0400

    Correct whitespace in FBSD/osi_vm.c
    I'm about to touch this, and the whitespace was bothering me.
    Change the six-space indentations to match the rest of the code.
    
    Change-Id: I35a12c82f8b2b4644e58cf1f19f021282e43dc77
    Reviewed-on: http://gerrit.openafs.org/2293
    Reviewed-by: Derrick Brashear <shadow@dementia.org>
    Tested-by: Derrick Brashear <shadow@dementia.org>

--- src/afs/FBSD/osi_vm.c
+++ src/afs/FBSD/osi_vm.c
@@ -191,35 +191,35 @@ osi_VM_TryToSmush(struct vcache *avc, afs_ucred_t *acred, int sync)
     vp = AFSTOV(avc);
 
     if (vp->v_iflag & VI_DOOMED) {
-      USERPRI;
-      return;
+	USERPRI;
+	return;
     }
 
     if (vp->v_bufobj.bo_object != NULL) {
-      VM_OBJECT_LOCK(vp->v_bufobj.bo_object);
-      /*
-       * Do we really want OBJPC_SYNC?  OBJPC_INVAL would be
-       * faster, if invalidation is really what we are being
-       * asked to do.  (It would make more sense, too, since
-       * otherwise this function is practically identical to
-       * osi_VM_StoreAllSegments().)  -GAW
-       */
+	VM_OBJECT_LOCK(vp->v_bufobj.bo_object);
+	/*
+	 * Do we really want OBJPC_SYNC?  OBJPC_INVAL would be
+	 * faster, if invalidation is really what we are being
+	 * asked to do.  (It would make more sense, too, since
+	 * otherwise this function is practically identical to
+	 * osi_VM_StoreAllSegments().)  -GAW
+	 */
 
-      /*
-       * Dunno.  We no longer resemble osi_VM_StoreAllSegments,
-       * though maybe that's wrong, now.  And OBJPC_SYNC is the
-       * common thing in 70 file systems, it seems.  Matt.
-       */
+	/*
+	 * Dunno.  We no longer resemble osi_VM_StoreAllSegments,
+	 * though maybe that's wrong, now.  And OBJPC_SYNC is the
+	 * common thing in 70 file systems, it seems.  Matt.
+	 */
 
-      vm_object_page_clean(vp->v_bufobj.bo_object, 0, 0, OBJPC_SYNC);
-      VM_OBJECT_UNLOCK(vp->v_bufobj.bo_object);
+	vm_object_page_clean(vp->v_bufobj.bo_object, 0, 0, OBJPC_SYNC);
+	VM_OBJECT_UNLOCK(vp->v_bufobj.bo_object);
     }
 
     tries = 5;
     code = osi_vinvalbuf(vp, V_SAVE, PCATCH, 0);
     while (code && (tries > 0)) {
-      code = osi_vinvalbuf(vp, V_SAVE, PCATCH, 0);
-      --tries;
+	code = osi_vinvalbuf(vp, V_SAVE, PCATCH, 0);
+	--tries;
     }
     USERPRI;
 }
