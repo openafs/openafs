c77ac5ce976b6f3e5a13a3f739ddbb6e32a177b1
Phillip Moore <w.phillip.moore@gmail.com>
2010-10-19 11:31:47 -0400

    fs getfid defaults to '.', like other path-related commands
    I noticed that all of the other commands that accept a list of paths
    use the SetDotDefault() function to default to ".", when no arguments
    are given.  This patch adds that call to getfid, making it more
    consistent with similar commands.
    
    Change-Id: Ia50c6805996d338cbd8d006107caa4ea56178c8e
    Reviewed-on: http://gerrit.openafs.org/3008
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Jeffrey Altman <jaltman@openafs.org>
    Reviewed-by: Derrick Brashear <shadow@dementia.org>

--- src/venus/fs.c
+++ src/venus/fs.c
@@ -4256,6 +4256,7 @@ GetFidCmd(struct cmd_syndesc *as, void *arock)
     int error = 0;
     char cell[MAXCELLCHARS];
 
+    SetDotDefault(&as->parms[0].items);
     for (ti = as->parms[0].items; ti; ti = ti->next) {
         struct VenusFid vfid;
 
