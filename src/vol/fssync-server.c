ab6f0b9630887d634103f425336b2bb38a063580
Jeffrey Altman <jaltman@your-file-system.com>
2010-10-28 00:43:26 -0400

    vol: Use OSI_NULLSOCKET and not -1 to indicate invalid fssync fd
    The FSync file descriptor is an osi_socket which has an invalid
    value of OSI_NULLSOCKET which is not necessarily -1.  Be sure to
    compare against OSI_NULLSOCKET and not -1 when checking an invalid
    value.
    
    Reviewed-on: http://gerrit.openafs.org/3179
    Reviewed-by: Derrick Brashear <shadow@dementia.org>
    Reviewed-by: Andrew Deason <adeason@sinenomine.net>
    Tested-by: Derrick Brashear <shadow@dementia.org>
    (cherry picked from commit e8991ff8286f151d109bb4f98d885a583e198f83)
    
    Change-Id: I1627ce0b78d10626b7d375546b400459ff1724ca
    Reviewed-on: http://gerrit.openafs.org/3885
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Derrick Brashear <shadow@dementia.org>

--- src/vol/fssync-server.c
+++ src/vol/fssync-server.c
@@ -114,7 +114,7 @@ static struct offlineInfo OfflineVolumes[MAXHANDLERS][MAXOFFLINEVOLUMES];
  * fssync server socket handle.
  */
 static SYNC_server_state_t fssync_server_state =
-    { -1,                       /* file descriptor */
+    { OSI_NULLSOCKET,                       /* file descriptor */
       FSSYNC_ENDPOINT_DECL,     /* server endpoint */
       FSYNC_PROTO_VERSION,      /* protocol version */
       5,                        /* bind() retry limit */
@@ -435,7 +435,7 @@ FSYNC_newconnection(osi_socket afd)
     socklen_t junk;
     junk = sizeof(other);
     fd = accept(afd, (struct sockaddr *)&other, &junk);
-    if (fd == -1) {
+    if (fd == OSI_NULLSOCKET) {
 	Log("FSYNC_newconnection:  accept failed, errno==%d\n", errno);
 	osi_Assert(1 == 2);
     } else if (!AddHandler(fd, FSYNC_com)) {
@@ -1974,7 +1974,7 @@ InitHandler(void)
     int i;
     ObtainWriteLock(&FSYNC_handler_lock);
     for (i = 0; i < MAXHANDLERS; i++) {
-	HandlerFD[i] = -1;
+	HandlerFD[i] = OSI_NULLSOCKET;
 	HandlerProc[i] = 0;
     }
     ReleaseWriteLock(&FSYNC_handler_lock);
@@ -2020,7 +2020,7 @@ AddHandler(osi_socket afd, void (*aproc) (osi_socket))
     int i;
     ObtainWriteLock(&FSYNC_handler_lock);
     for (i = 0; i < MAXHANDLERS; i++)
-	if (HandlerFD[i] == -1)
+	if (HandlerFD[i] == OSI_NULLSOCKET)
 	    break;
     if (i >= MAXHANDLERS) {
 	ReleaseWriteLock(&FSYNC_handler_lock);
@@ -2063,7 +2063,7 @@ static int
 RemoveHandler(osi_socket afd)
 {
     ObtainWriteLock(&FSYNC_handler_lock);
-    HandlerFD[FindHandler_r(afd)] = -1;
+    HandlerFD[FindHandler_r(afd)] = OSI_NULLSOCKET;
     ReleaseWriteLock(&FSYNC_handler_lock);
     return 1;
 }
@@ -2076,7 +2076,7 @@ GetHandler(struct pollfd *fds, int maxfds, int events, int *nfds)
     int fdi = 0;
     ObtainReadLock(&FSYNC_handler_lock);
     for (i = 0; i < MAXHANDLERS; i++)
-	if (HandlerFD[i] != -1) {
+	if (HandlerFD[i] != OSI_NULLSOCKET) {
 	    osi_Assert(fdi<maxfds);
 	    fds[fdi].fd = HandlerFD[i];
 	    fds[fdi].events = events;
@@ -2095,7 +2095,7 @@ GetHandler(fd_set * fdsetp, int *maxfdp)
     FD_ZERO(fdsetp);
     ObtainReadLock(&FSYNC_handler_lock);	/* just in case */
     for (i = 0; i < MAXHANDLERS; i++)
-	if (HandlerFD[i] != -1) {
+	if (HandlerFD[i] != OSI_NULLSOCKET) {
 	    FD_SET(HandlerFD[i], fdsetp);
 #ifndef AFS_NT40_ENV
             /* On Windows the nfds parameter to select() is ignored */
