fa3669ee0bcd8005d684e9077bf7ed1b4ff43e07
Heimdal Developers <heimdal-discuss@sics.se>
2010-11-24 08:14:40 +0000

    Import of code from heimdal
    This commit updates the code imported from heimdal to
    0cc7b890e2632a7cb26517825deb802f79e838f8 (switch-from-svn-to-git-1702-g0cc7b89)
    
    Upstream changes are:
    
    Asanka C. Herath (3):
          Annotate exported variables in getopt.c
          Windows: Support building roken as a DLL
          Declarations for getopt externs
    
    Simon Wilkinson (1):
          roken: getopt should include roken.h
    
    Change-Id: I5ef850bcac7dace3a8206958783ed6fd5830492e
    Reviewed-on: http://gerrit.openafs.org/3378
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Jeffrey Altman <jaltman@openafs.org>
    Tested-by: Jeffrey Altman <jaltman@openafs.org>

--- src/external/heimdal/roken/getopt.c
+++ src/external/heimdal/roken/getopt.c
@@ -34,6 +34,9 @@ static char sccsid[] = "@(#)getopt.c	8.1 (Berkeley) 6/4/93";
 #ifndef __STDC__
 #define const
 #endif
+#include <config.h>
+#include "roken.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -41,11 +44,11 @@ static char sccsid[] = "@(#)getopt.c	8.1 (Berkeley) 6/4/93";
 /*
  * get option letter from argument vector
  */
-int	opterr = 1,		/* if error message should be printed */
-	optind = 1,		/* index into parent argv vector */
-	optopt,			/* character checked for validity */
-	optreset;		/* reset getopt */
-char	*optarg;		/* argument associated with option */
+ROKEN_LIB_VARIABLE int opterr = 1; /* if error message should be printed */
+ROKEN_LIB_VARIABLE int optind = 1; /* index into parent argv vector */
+ROKEN_LIB_VARIABLE int optopt;   /* character checked for validity */
+int	optreset;                /* reset getopt */
+ROKEN_LIB_VARIABLE char	*optarg; /* argument associated with option */
 
 #define	BADCH	(int)'?'
 #define	BADARG	(int)':'
