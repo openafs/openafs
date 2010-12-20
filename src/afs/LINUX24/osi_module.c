87a98b3109d389eac0e4a9bb4862ca9e320e21fc
Andrew Deason <adeason@sinenomine.net>
2010-12-20 17:29:37 -0500

    Cache bypass: remove ifdefs under src/afs/LINUX24
    Same as commit 9657695d34badaac654227be8c731a1512f2106b, but for
    LINUX24. Remove the AFS_CACHE_BYPASS references in LINUX24, since it
    is always enabled. Also remove messages about a "cache bypass patched
    module".
    
    Reviewed-on: http://gerrit.openafs.org/3568
    Tested-by: Andrew Deason <adeason@sinenomine.net>
    Reviewed-by: Derrick Brashear <shadow@dementia.org>
    Tested-by: Derrick Brashear <shadow@dementia.org>
    (cherry picked from commit 64e564b29e95898a6eb85dc527d046aab241fb8f)
    
    Change-Id: Ia1165e1ff37c18417deef56e79ed791ca77d7a53
    Reviewed-on: http://gerrit.openafs.org/3577
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Derrick Brashear <shadow@dementia.org>

--- src/afs/LINUX24/osi_module.c
+++ src/afs/LINUX24/osi_module.c
@@ -110,9 +110,6 @@ init_module(void)
     osi_proc_init();
     osi_ioctl_init();
 #endif
-#if defined(AFS_CACHE_BYPASS)
-    afs_warn("Cache bypass patched libafs module init.\n");
-#endif
 
     return 0;
 }
@@ -125,10 +122,6 @@ void
 cleanup_module(void)
 #endif
 {
-#if defined(AFS_CACHE_BYPASS)
-    afs_warn("Cache bypass patched libafs module cleaning up.\n");
-#endif
-
 #ifdef LINUX_KEYRING_SUPPORT
     osi_keyring_shutdown();
 #endif
