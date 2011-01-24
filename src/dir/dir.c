0a7de3d7ad239a5d89aa4cb3392b5f9d1c6163c8
Antoine Verheijen <antoine@ualberta.ca>
2011-01-24 00:34:56 -0700

    OpenBSD: Remove user.h from dir.c for OpenBSD 4.8
    OpenBSD 4.8 no longer needs to include h/user.h in dir.c
    and it's presence now gets in the way of the compile so
    remove it.
    
    Reviewed-on: http://gerrit.openafs.org/3750
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Derrick Brashear <shadow@dementia.org>
    (cherry picked from commit e1b0a95352e2e6aa312a526ffcc8a2f77a8fb7df)
    
    Change-Id: Ic8298fbfe573563e36aee7377bb0194bcaaca610
    Reviewed-on: http://gerrit.openafs.org/4358
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Derrick Brashear <shadow@dementia.org>

--- src/dir/dir.c
+++ src/dir/dir.c
@@ -33,7 +33,7 @@
 #if	defined(AFS_SUN56_ENV) || defined(AFS_HPUX_ENV) || defined(AFS_FBSD_ENV) || defined(AFS_DARWIN80_ENV)
 #include "afs/sysincludes.h"
 #endif
-#if !defined(AFS_SGI64_ENV) && !defined(AFS_DARWIN60_ENV)
+#if !defined(AFS_SGI64_ENV) && !defined(AFS_DARWIN60_ENV) && !defined(AFS_OBSD48_ENV)
 #include "h/user.h"
 #endif /* AFS_SGI64_ENV */
 #include "h/uio.h"
