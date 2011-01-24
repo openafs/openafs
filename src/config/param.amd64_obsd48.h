85eb528a680b3611ab2ee53e6097a68f91b09429
Antoine Verheijen <antoine@ualberta.ca>
2011-01-24 00:16:02 -0700

    OpenBSD: Add support for OpenBSD 4.8
    Add param headers and other config info for OpenBSD 4.8
    support.
    
    Reviewed-on: http://gerrit.openafs.org/3748
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Derrick Brashear <shadow@dementia.org>
    Tested-by: Derrick Brashear <shadow@dementia.org>
    (cherry picked from commit be0e9fb703d9ca0ae17f1117cd53796f82d7fb30)
    
    Change-Id: I03d4474d66d015126654d6d031a1870705733a41
    Reviewed-on: http://gerrit.openafs.org/3922
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Derrick Brashear <shadow@dementia.org>

---
+++ src/config/param.amd64_obsd48.h
@@ -0,0 +1,19 @@
+/*
+ * Thanks to Jim Rees and University of Michigan CITI, for the initial
+ * OpenBSD porting work.
+ */
+
+#ifndef	AFS_AMD64_PARAM_H
+#define	AFS_AMD64_PARAM_H
+
+#define SYS_NAME		"amd64_obsd48"
+#define SYS_NAME_ID		SYS_NAME_ID_amd64_obsd48
+
+#define AFS_XBSD_ENV		1	/* {Free,Open,Net}BSD */
+#define AFS_X86_XBSD_ENV	1
+#define AFS_X86_ENV	        1
+#define AFS_64BITPOINTER_ENV    1
+#define AFS_64BITUSERPOINTER_ENV	1
+#define AFSLITTLE_ENDIAN	1
+
+#endif /* AFS_AMD64_PARAM_H */
