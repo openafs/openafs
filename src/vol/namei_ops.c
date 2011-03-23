d69d96588ba1068b9ace1156f00e6c26a145e089
Andrew Deason <adeason@sinenomine.net>
2011-03-23 15:19:03 -0500

    namei: Log ListViceInodes write failures
    If we fail to write to the inodeFile given by ListViceInodes, we
    should log the failure. Otherwise, no indication is given as to what
    failed, when a salvage gathers the inode list.
    
    Reviewed-on: http://gerrit.openafs.org/4294
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Jeffrey Altman <jaltman@openafs.org>
    Reviewed-by: Derrick Brashear <shadow@dementia.org>
    (cherry picked from commit 22149d82d465f5defbe329b3f9b3c5040a6293e9)
    
    Change-Id: I7fb56f0af99bb040e64cf54bf05e01306a609326
    Reviewed-on: http://gerrit.openafs.org/6275
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Derrick Brashear <shadow@dementix.org>

--- src/vol/namei_ops.c
+++ src/vol/namei_ops.c
@@ -1908,9 +1908,12 @@ _namei_examine_special(char * path1,
 
     if (!judgeFun ||
 	(*judgeFun) (&info, singleVolumeNumber, rock)) {
-	ret = 1;
-        if ((*writeFun) (fp, &info, path1, dname) < 0) {
+	ret = (*writeFun) (fp, &info, path1, dname);
+	if (ret < 0) {
+	    Log("_namei_examine_special: writeFun returned %d\n", ret);
 	    ret = -1;
+	} else {
+	    ret = 1;
 	}
     }
 
@@ -1990,9 +1993,12 @@ _namei_examine_reg(char * path3,
 
     if (!judgeFun ||
 	(*judgeFun) (&info, singleVolumeNumber, rock)) {
-	ret = 1;
-        if ((*writeFun) (fp, &info, path3, dname) < 0) {
+	ret = (*writeFun) (fp, &info, path3, dname);
+	if (ret < 0) {
+	    Log("_namei_examine_reg: writeFun returned %d\n", ret);
 	    ret = -1;
+	} else {
+	    ret = 1;
 	}
     }
 
