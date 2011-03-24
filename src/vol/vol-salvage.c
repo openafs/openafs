c5be1f72875bcb083a9ddef4621efecf62aa880e
Andrew Deason <adeason@sinenomine.net>
2011-03-24 17:57:32 -0500

    salvager: Stop asking for DAFSness on SYNC_FAILED
    Break out of the loop so we stop asking the same thing repeatedly.
    
    Change-Id: Ifcb2cd62589867927ab9f8be03bab7b981e5f761
    Reviewed-on: http://gerrit.openafs.org/4315
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Derrick Brashear <shadow@dementia.org>

--- src/vol/vol-salvage.c
+++ src/vol/vol-salvage.c
@@ -4405,6 +4405,7 @@ AskDAFS(void)
 		ret = 1;
 	    else
 		ret = 0;
+	    break;
 	} else if (i < 2) {
 	    /* try it again */
 	    Log("AskDAFS:  request to query fileserver failed; trying again...\n");
