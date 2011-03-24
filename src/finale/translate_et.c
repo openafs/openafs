4e6aff0bcb69b13ae5ca1d983f9b54c3245b0cc2
Simon Wilkinson <sxw@your-file-system.com>
2011-03-24 20:58:46 +0000

    finale: Tidy header includes
    Since we adopted libroken, we get a lot of header includes automatically
    from roken.h. Use these, instead of including lots of things multiple
    times.
    
    Also, tidy up translate_et_nt.c by removing the AIX specific code which
    will never be built, as this file is only compiled on Windows!
    
    Change-Id: Iec7a811f02eaf1a2ad573bbadc30d782426231d0
    Reviewed-on: http://gerrit.openafs.org/4327
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Jeffrey Altman <jaltman@openafs.org>

--- src/finale/translate_et.c
+++ src/finale/translate_et.c
@@ -10,6 +10,7 @@
 #include <afsconfig.h>
 #include <afs/param.h>
 
+#include <roken.h>
 
 #include <afs/com_err.h>
 #include <rx/rxkad.h>
@@ -21,10 +22,6 @@
 #include <afs/bnode.h>
 #include <afs/volser.h>
 #include <ubik.h>
-#ifdef	AFS_AIX32_ENV
-#include <signal.h>
-#endif
-
 
 #define ERRCODE_RANGE 8		/* from error_table.h */
 
