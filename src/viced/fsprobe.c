ef105dcc4932da804750dea0c922d5814fc5bf15
Marc Dionne <marc.c.dionne@gmail.com>
2011-04-02 08:40:15 -0400

    fsprobe: call RXAFS_GetTime directly
    Call RXAFS_GetTime directly instead of using ubik_Call which is
    not prototyped and has logic that we don't need/want here since
    we're targeting a specific server.
    
    Change-Id: Ia71d1cb164d3fe71f01c88fc720837c69566baf5
    Reviewed-on: http://gerrit.openafs.org/4397
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Derrick Brashear <shadow@dementia.org>

--- src/viced/fsprobe.c
+++ src/viced/fsprobe.c
@@ -95,7 +95,7 @@ main(int argc, char **argv)
 	exit(1);
     }
 
-    code = ubik_Call(RXAFS_GetTime, cstruct, 0, &tv.tv_sec, &tv.tv_usec);
+    code = RXAFS_GetTime(cstruct->conns[0], (afs_uint32 *)&tv.tv_sec, (afs_uint32 *)&tv.tv_usec);
     if (!code)
 	printf("AFS_GetTime on %s sec=%ld, usec=%ld\n", av[0], tv.tv_sec,
 	       (long int)tv.tv_usec);
