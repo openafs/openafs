fbb4c6115b9af9c52ee06fa9c979a3f4195ad342
Andrew Deason <adeason@sinenomine.net>
2011-04-13 11:10:52 -0500

    pam: Check for null upwd from getpwnam_r
    The POSIX getpwnam_r can yield a NULL struct passwd pointer even when
    the returned error code is 0 (in particular, when the requested entry
    is not found). Just add a check for a null upwd to make sure we don't
    dereference a NULL pointer.
    
    Change-Id: I00e8d6c53e8228f468c984010695b798f5dcf999
    Reviewed-on: http://gerrit.openafs.org/4469
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Derrick Brashear <shadow@dementia.org>

--- src/pam/afs_auth.c
+++ src/pam/afs_auth.c
@@ -179,7 +179,7 @@ pam_sm_authenticate(pam_handle_t * pamh, int flags, int argc,
     if (i == 0)			/* getpwnam_r success */
 	upwd = &unix_pwd;
 #endif /* else AFS_HPUX110_ENV */
-    if (ignore_uid && i == 0 && upwd->pw_uid <= ignore_uid_id) {
+    if (ignore_uid && i == 0 && upwd && upwd->pw_uid <= ignore_uid_id) {
 	pam_afs_syslog(LOG_INFO, PAMAFS_IGNORINGROOT, user);
 	RET(PAM_AUTH_ERR);
     }
