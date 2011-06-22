42943aead4db2bdf9b8ec01c3917eb1c9ac9eb76
Andrew Deason <adeason@sinenomine.net>
2011-06-22 14:39:39 -0500

    afs: Use cell for md5 inode numbers
    When calculating the inode number for a file with md5 inodes, include
    the cell number in the calculation, in order to reduce collisions
    between cells.
    
    Change-Id: I4b939042dd993419f785a78e87e68cf346b56e26
    Reviewed-on: http://gerrit.openafs.org/4902
    Reviewed-by: Derrick Brashear <shadow@dementia.org>
    Tested-by: BuildBot <buildbot@rampaginggeek.com>

--- src/afs/LINUX24/osi_vnodeops.c
+++ src/afs/LINUX24/osi_vnodeops.c
@@ -305,7 +305,8 @@ afs_linux_readdir(struct file *fp, void *dirbuf, filldir_t filldir)
 	    break;
 	de = (struct DirEntry *)entry.data;
 
-	ino = afs_calc_inum (avc->f.fid.Fid.Volume, ntohl(de->fid.vnode));
+	ino = afs_calc_inum(avc->f.fid.Cell, avc->f.fid.Fid.Volume,
+	                    ntohl(de->fid.vnode));
 
 	if (de->name)
 	    len = strlen(de->name);
