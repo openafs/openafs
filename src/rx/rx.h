4aacd49270900f9a882a6265beec4b126df61d8b
Derrick Brashear <shadow@dementia.org>
2011-08-01 16:38:46 -0400

    rx: avoid nat ping until connection is attached
    drop nat pings on connections we haven't talked on yet
    
    Reviewed-on: http://gerrit.openafs.org/5130
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Jeffrey Altman <jaltman@openafs.org>
    Tested-by: Jeffrey Altman <jaltman@openafs.org>
    Reviewed-by: Derrick Brashear <shadow@dementia.org>
    (cherry picked from commit 496fb87372555f6acddd4fd88b03c94c85f48511)
    
    Change-Id: Id64a771f9cb50191a665f5ccb98ec66d991f47bb
    Reviewed-on: http://gerrit.openafs.org/5225
    Reviewed-by: Derrick Brashear <shadow@dementia.org>
    Tested-by: Derrick Brashear <shadow@dementia.org>

--- src/rx/rx.h
+++ src/rx/rx.h
@@ -450,6 +450,7 @@ struct rx_peer {
 #define RX_CONN_BUSY               32	/* connection is busy; don't delete */
 #define RX_CONN_ATTACHWAIT	   64	/* attach waiting for peer->lastReach */
 #define RX_CONN_MAKECALL_ACTIVE   128   /* a thread is actively in rx_NewCall */
+#define RX_CONN_NAT_PING          256   /* nat ping requested */
 
 /* Type of connection, client or server */
 #define	RX_CLIENT_CONNECTION	0
