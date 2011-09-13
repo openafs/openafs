1de4561529bdb92b6647a634d9f2c2714e1758e5
Marc Dionne <marc.c.dionne@gmail.com>
2011-09-13 19:32:37 -0400

    vos: print correct command name in diagnostics for changeloc
    Fix the label in the diagnostics message, it prints "addsite"
    instead of "changeloc" when an error occurs during vos changeloc
    
    Change-Id: I8c541bc1709d2c02fb58d6d15b4a66d95647d1e7
    Reviewed-on: http://gerrit.openafs.org/5414
    Reviewed-by: Jeffrey Altman <jaltman@openafs.org>
    Tested-by: Jeffrey Altman <jaltman@openafs.org>

--- src/volser/vos.c
+++ src/volser/vos.c
@@ -3449,7 +3449,7 @@ ChangeLocation(struct cmd_syndesc *as, void *arock)
     }
     code = UV_ChangeLocation(aserver, apart, avolid);
     if (code) {
-	PrintDiagnostics("addsite", code);
+	PrintDiagnostics("changeloc", code);
 	exit(1);
     }
     MapPartIdIntoName(apart, apartName);
