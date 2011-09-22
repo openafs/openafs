d87fdb50db5c306973f4f379e82d992d18244c34
Marc Dionne <marc.c.dionne@gmail.com>
2011-09-22 19:09:52 -0400

    viced: fix incorrect error message
    The error message was obviously copied from another location in the
    code (after the pr_Initialize call) and is misleading.  Adapt it for
    vl_Initialize failure.
    
    Reviewed-on: http://gerrit.openafs.org/5490
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Derrick Brashear <shadow@dementix.org>
    (cherry picked from commit a020c572d372f2e86807dc4c75ec50ff14ef2fba)
    
    Change-Id: I067ceb0e5e2281508647065f11e48d4a07033c99
    Reviewed-on: http://gerrit.openafs.org/9432
    Reviewed-by: Derrick Brashear <shadow@your-file-system.com>
    Tested-by: Andrew Deason <adeason@sinenomine.net>
    Reviewed-by: Andrew Deason <adeason@sinenomine.net>
    Reviewed-by: Stephan Wiesand <stephan.wiesand@desy.de>

--- src/viced/viced.c
+++ src/viced/viced.c
@@ -1905,7 +1905,7 @@ InitVL(void)
     code = vl_Initialize(AFSDIR_SERVER_ETC_DIRPATH);
     if (code != 0) {
 	ViceLog(0,
-		("Couldn't initialize protection library; code=%d.\n", code));
+		("Couldn't initialize volume location library; code=%d.\n", code));
 	return code;
     }
 
