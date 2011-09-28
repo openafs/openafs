5b579b83abc57d6bb9212b599aae96af9ac71832
Rod Widdowson <rdw@steadingsoftware.com>
2011-09-28 17:34:54 +0100

    Windows: Fix comment about DiskPartition64
    There is significant confusion between the partition name (/vicepX)
    and the containing volume (C:) in the windows code.  A review of
    the uses in FSYNCH found a downright wrong comment (as checked in
    the debugger).
    
    Change-Id: I9cf6e965c9efee18b608a622b83639f25e410ce5
    Reviewed-on: http://gerrit.openafs.org/5512
    Reviewed-by: Jeffrey Altman <jaltman@openafs.org>
    Tested-by: Jeffrey Altman <jaltman@openafs.org>

--- src/vol/partition.h
+++ src/vol/partition.h
@@ -88,10 +88,10 @@ struct VDiskLock {
 #endif /* AFS_DEMAND_ATTACH_FS || AFS_DEMAND_ATTACH_UTIL */
 
 
-/* For NT, the roles of "name" and "devName" are reversed. That is, "name"
- * refers to the drive letter name and "devName" refers to the /vicep style
- * or name. The reason for this is that a lot of places assume that "name"
- * is the right thing to use to access the partition. Silly of them isn't it?
+/* For NT, the roles of "name" and "devName" are no longer reversed. 
+ * That is, "name" refers to the canonical name (/vicep) style and
+ * "devName" refers to drive name.
+ *
  * The NT version of VInitPartition does the intial setup. There is an NT
  * variant for VGetPartition as well. Also, the VolPartitionInfo RPC does
  * a swap before sending the data out on the wire.
