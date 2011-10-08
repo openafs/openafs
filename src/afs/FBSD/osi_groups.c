4a1d319177ea0e58c9046c3c173bb7d2877d1bc3
Ben Kaduk <kaduk@mit.edu>
2011-10-08 17:16:26 -0400

    FBSD: deal with kernel API rename
    Upstream decided to rename the kernel functions that implement
    syscalls to have a sys_prefix (including afs3_syscall!).
    We use a couple of them, so we need to conditionalize accordingly.
    Unfortunately, __FreeBSD_version was not bumped with the change,
    so we use something close to it and hope it's close enough.
    
    Change-Id: I228805a813af70b30f94f659e4da602210a6d6df
    Reviewed-on: http://gerrit.openafs.org/5572
    Reviewed-by: Derrick Brashear <shadow@dementix.org>
    Tested-by: Derrick Brashear <shadow@dementix.org>

--- src/afs/FBSD/osi_groups.c
+++ src/afs/FBSD/osi_groups.c
@@ -48,9 +48,17 @@ Afs_xsetgroups(struct thread *td, struct setgroups_args *uap)
     AFS_GUNLOCK();
     crfree(cr);
     if (code)
+#if (__FreeBSD_version >= 900044)
+	return sys_setgroups(td, uap);	/* afs has shut down */
+#else
 	return setgroups(td, uap);	/* afs has shut down */
+#endif
 
+#if (__FreeBSD_version >= 900044)
+    code = sys_setgroups(td, uap);
+#else
     code = setgroups(td, uap);
+#endif
     /* Note that if there is a pag already in the new groups we don't
      * overwrite it with the old pag.
      */
