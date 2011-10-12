d0ca0866a1bc3b3d9f617f45ae54776ca9eef869
Simon Wilkinson <sxw@your-file-system.com>
2011-10-12 15:26:23 -0400

    ukernel: don't define rdwr env on linux
    We're not vm rdwr env in ukernel. Don't claim we are, otherwise we
    will never push chunks out to the fileserver when our cache is full
    
    Change-Id: I590482eeeb4ad72398cbeadb159b2c6822715794
    Reviewed-on: http://gerrit.openafs.org/5613
    Reviewed-by: Derrick Brashear <shadow@dementix.org>
    Tested-by: Derrick Brashear <shadow@dementix.org>
    (cherry-picked from 796de9fb43857acccbd51e2af7c8d67eea90fdc5)
    Reviewed-on: http://gerrit.openafs.org/5896
    Tested-by: Jeffrey Altman <jaltman@secure-endpoints.com>
    Reviewed-by: Jeffrey Altman <jaltman@secure-endpoints.com>

--- src/config/param.linux26.h
+++ src/config/param.linux26.h
@@ -75,7 +75,6 @@
 #define AFS_GCPAGS		0	/* if nonzero, garbage collect PAGs */
 #define AFS_HAVE_FFS		1	/* Use system's ffs. */
 #define AFS_HAVE_STATVFS	0	/* System doesn't support statvfs */
-#define AFS_VM_RDWR_ENV		1	/* read/write implemented via VM */
 
 #define	afsio_iov	uio_iov
 #define	afsio_iovcnt	uio_iovcnt
