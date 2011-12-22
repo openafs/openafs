af218dee6c517f4636a18c681bff6f1212a7d9c0
Andrew Deason <adeason@sinenomine.net>
2011-12-21 22:00:12 -0500

    afsd: Parse cacheinfo during argument parsing
    Currently we parse cacheinfo in afsd_run, when the client is
    initialized and started. Parsing cacheinfo can change
    afsd_cacheMountDir, however, which may be of interest to afsd.o users;
    in particular, libuafs exposes this via uafs_MountDir(). This means
    that if a mount dir is not explicitly specified in the libcmd
    arguments to afsd, a libuafs-using program will see the mountpoint as
    the empty string if it is queried after afsd_parse but before
    afsd_run. For afsd.fuse, this causes the cryptic error message:
    
        fuse: bad mount point `': No such file or directory
    
    since the mountpoint is the empty string if it is not specified
    explicitly on the command line.
    
    To fix this, move cacheinfo parsing to effectively near the end of
    afsd_parse, so the mountpoint is calculated in afsd_parse().
    
    Change-Id: I058f2c7c2f0cc21db21c4b1d38ff63b9e9ed1562
    Reviewed-on: http://gerrit.openafs.org/6400
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Derrick Brashear <shadow@dementix.org>

--- src/afsd/afsd.c
+++ src/afsd/afsd.c
@@ -1913,6 +1913,12 @@ mainproc(struct cmd_syndesc *as, void *arock)
 	/* -rxmaxfrags */
 	rxmaxfrags = atoi(as->parms[38].items->data);
     }
+
+    /* parse cacheinfo file if this is a diskcache */
+    if (ParseCacheInfoFile()) {
+	exit(1);
+    }
+
     return 0;
 }
 
@@ -1947,11 +1953,6 @@ afsd_run(void)
     } else {
 	if (afsd_verbose)
 	    printf("%s: My home cell is '%s'\n", rn, LclCellName);
-    }
-
-    /* parse cacheinfo file if this is a diskcache */
-    if (ParseCacheInfoFile()) {
-	exit(1);
     }
 
     if (!enable_nomount) {
