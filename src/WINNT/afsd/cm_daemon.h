95e505faead455d7e5425f271870009258f64c35
Jeffrey Altman <jaltman@your-file-system.com>
2012-01-19 01:21:02 -0500

    Windows: Redesign daemon thread queue management
    The daemon thread worker pool has some very poor properties.
    The threads spend a significant amount of time polling for
    ready to process tasks because so frequently a store/fetch data
    request is accompanied by many other requests for the same FID
    that would block.
    
    Lets try a new approach. Create one queue for each worker thread
    and assign the tasks to a thread by a hash of the FID.  This ensures
    that all tasks for a single FID are serialized and prevents multiple
    threads from attempting to perform the same task only to decide that
    the thread would be forced to block.
    
    Reviewed-on: http://gerrit.openafs.org/6575
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Jeffrey Altman <jaltman@secure-endpoints.com>
    Tested-by: Jeffrey Altman <jaltman@secure-endpoints.com>
    (cherry picked from commit 201c954a36fe6ec19f96e4c8e24e6e080c3ba55a)
    
    Change-Id: I081917ff8e5ce5179a18ed6fff9f67cc46d19e52
    Reviewed-on: http://gerrit.openafs.org/6588
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Jeffrey Altman <jaltman@secure-endpoints.com>

--- src/WINNT/afsd/cm_daemon.h
+++ src/WINNT/afsd/cm_daemon.h
@@ -18,7 +18,8 @@ extern long cm_daemonCheckCBInterval;
 extern long cm_daemonCheckLockInterval;
 extern long cm_daemonTokenCheckInterval;
 
-extern osi_rwlock_t cm_daemonLock;
+extern osi_rwlock_t *cm_daemonLockp;
+extern int cm_nDaemons;
 
 void cm_DaemonShutdown(void);
 
@@ -28,7 +29,7 @@ typedef afs_int32 (cm_bkgProc_t)(cm_scache_t *scp, afs_uint32 p1, afs_uint32 p2,
 	afs_uint32 p4, struct cm_user *up, cm_req_t *reqp);
 
 typedef struct cm_bkgRequest {
-	osi_queue_t q;
+        osi_queue_t q;
 	cm_bkgProc_t *procp;
         cm_scache_t *scp;
         afs_uint32 p1;
