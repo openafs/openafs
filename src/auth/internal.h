15cde0e40db8c99ef9af7e9f04c406ce6ea3bfeb
Michael Meffie <mmeffie@sinenomine.net>
2012-02-20 15:26:09 -0500

    auth: get local cell internal function
    Add an internal function to retrieve the local cell
    name when the auth mutex is already held. Implement
    afsconf_GetLocalCell in terms of the new internal
    function.
    
    Change-Id: I2f021a9966129e8e256b61d5219e7315b904ac0e
    Reviewed-on: http://gerrit.openafs.org/6875
    Reviewed-by: Alistair Ferguson <alistair.ferguson@mac.com>
    Reviewed-by: Derrick Brashear <shadow@dementix.org>
    Tested-by: BuildBot <buildbot@rampaginggeek.com>

--- src/auth/internal.h
+++ src/auth/internal.h
@@ -5,3 +5,4 @@ extern int _afsconf_IsClientConfigDirectory(const char *path);
 extern int _afsconf_LoadKeys(struct afsconf_dir *adir);
 extern void _afsconf_InitKeys(struct afsconf_dir *adir);
 extern void _afsconf_FreeAllKeys(struct afsconf_dir *adir);
+extern int _afsconf_GetLocalCell(struct afsconf_dir *adir, char **pname, int check);
