03ec768fa65dc5961e161517871f3035df70b643
Michael Meffie <mmeffie@sinenomine.net>
2012-03-27 00:33:26 -0400

    volscan: index offset output column
    Add index offset as a possible volscan output column.
    
    Change-Id: I1bf6e8dc3c506a2ee013536e19141ddb214879a6
    Reviewed-on: http://gerrit.openafs.org/6980
    Reviewed-by: Derrick Brashear <shadow@dementix.org>
    Tested-by: BuildBot <buildbot@rampaginggeek.com>

--- src/vol/vol-info.c
+++ src/vol/vol-info.c
@@ -81,6 +81,7 @@ typedef enum {
     c(host) \
     c(desc) \
     c(vid) \
+    c(offset) \
     c(vtype) \
     c(vname) \
     c(part) \
@@ -2442,6 +2443,9 @@ PrintColumns(struct VnodeDetails *vdp, const char *desc)
 	    break;
 	case col_vid:
 	    printf("%lu", afs_printable_uint32_lu(V_id(vdp->vp)));
+	    break;
+	case col_offset:
+	    printf("%llu", vdp->offset);
 	    break;
 	case col_vtype:
 	    printf("%s", volumeTypeShortString(V_type(vdp->vp)));
