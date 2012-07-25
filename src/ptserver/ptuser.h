d0db811939df4eda268ae2ae54a1dc0194653ed9
Garrett Wollman <wollman@csail.mit.edu>
2012-07-24 22:29:00 -0400

    pruser: AFS_NONNULL annotations for functions that deserve it
    Change-Id: I68e74affcb3235d93e684a04a56ef44798a42406
    Reviewed-on: http://gerrit.openafs.org/7861
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Derrick Brashear <shadow@dementix.org>

--- src/ptserver/ptuser.h
+++ src/ptserver/ptuser.h
@@ -16,43 +16,47 @@
 extern afs_int32 pr_Initialize(afs_int32 secLevel, const char *confDir,
  			       char *cell);
 extern int pr_End(void);
-extern int pr_CreateUser(prname name, afs_int32 *id);
+extern int pr_CreateUser(prname name, afs_int32 *id) AFS_NONNULL();
 extern int pr_CreateGroup(prname name, prname owner,
- 			  afs_int32 *id);
-extern int pr_Delete(char *name);
+ 			  afs_int32 *id) AFS_NONNULL((1,3));
+extern int pr_Delete(char *name) AFS_NONNULL();
 extern int pr_DeleteByID(afs_int32 id);
-extern int pr_AddToGroup(char *user, char *group);
-extern int pr_RemoveUserFromGroup(char *user, char *group);
-extern int pr_NameToId(namelist *names, idlist *ids);
-extern int pr_SNameToId(prname name, afs_int32 *id);
-extern int pr_IdToName(idlist *ids, namelist *names);
-extern int pr_SIdToName(afs_int32 id, prname name);
-extern int pr_GetCPS(afs_int32 id, prlist *CPS);
-extern int pr_GetCPS2(afs_int32 id, afs_uint32 host, prlist *CPS);
-extern int pr_GetHostCPS(afs_uint32 host, prlist *CPS);
-extern int pr_ListMembers(char *group, namelist *lnames);
-extern int pr_ListOwned(afs_int32 oid, namelist *lnames, afs_int32 *moreP);
-extern int pr_IDListMembers(afs_int32 gid, namelist *lnames);
-extern int pr_ListEntry(afs_int32 id, struct prcheckentry *aentry);
+extern int pr_AddToGroup(char *user, char *group) AFS_NONNULL();
+extern int pr_RemoveUserFromGroup(char *user, char *group) AFS_NONNULL();
+extern int pr_NameToId(namelist *names, idlist *ids) AFS_NONNULL();
+extern int pr_SNameToId(prname name, afs_int32 *id) AFS_NONNULL();
+extern int pr_IdToName(idlist *ids, namelist *names) AFS_NONNULL();
+extern int pr_SIdToName(afs_int32 id, prname name) AFS_NONNULL();
+extern int pr_GetCPS(afs_int32 id, prlist *CPS) AFS_NONNULL();
+extern int pr_GetCPS2(afs_int32 id, afs_uint32 host, prlist *CPS) AFS_NONNULL();
+extern int pr_GetHostCPS(afs_uint32 host, prlist *CPS) AFS_NONNULL();
+extern int pr_ListMembers(char *group, namelist *lnames) AFS_NONNULL();
+extern int pr_ListOwned(afs_int32 oid, namelist *lnames, afs_int32 *moreP)
+		       AFS_NONNULL();
+extern int pr_IDListMembers(afs_int32 gid, namelist *lnames) AFS_NONNULL();
+extern int pr_ListEntry(afs_int32 id, struct prcheckentry *aentry)
+		       AFS_NONNULL();
 extern afs_int32 pr_ListEntries(int flag, afs_int32 startindex,
  				afs_int32 *nentries,
  				struct prlistentries **entries,
- 				afs_int32 *nextstartindex);
+ 				afs_int32 *nextstartindex) AFS_NONNULL();
 extern int pr_CheckEntryByName(char *name, afs_int32 *id, char *owner,
-			       char *creator);
+			       char *creator) AFS_NONNULL();
 extern int pr_CheckEntryById(char *name, afs_int32 id, char *owner,
-			     char *creator);
+			     char *creator) AFS_NONNULL();
 extern int pr_ChangeEntry(char *oldname, char *newname, afs_int32 *newid,
-			  char *newowner);
-extern int pr_IsAMemberOf(char *uname, char *gname, afs_int32 *flag);
-extern int pr_ListMaxUserId(afs_int32 *mid);
+			  char *newowner) AFS_NONNULL((1));
+extern int pr_IsAMemberOf(char *uname, char *gname, afs_int32 *flag)
+			 AFS_NONNULL();
+extern int pr_ListMaxUserId(afs_int32 *mid) AFS_NONNULL();
 extern int pr_SetMaxUserId(afs_int32 mid);
-extern int pr_ListMaxGroupId(afs_int32 *mid);
+extern int pr_ListMaxGroupId(afs_int32 *mid) AFS_NONNULL();
 extern int pr_SetMaxGroupId(afs_int32 mid);
 extern afs_int32 pr_SetFieldsEntry(afs_int32 id, afs_int32 mask,
 				   afs_int32 flags, afs_int32 ngroups,
 				   afs_int32 nusers);
-extern int pr_ListSuperGroups(afs_int32 gid, namelist *lnames);
-extern int pr_IDListExpandedMembers(afs_int32 gid, namelist *lnames);
+extern int pr_ListSuperGroups(afs_int32 gid, namelist *lnames) AFS_NONNULL();
+extern int pr_IDListExpandedMembers(afs_int32 gid, namelist *lnames)
+				   AFS_NONNULL();
 
 #endif /* PTUSER_H */
