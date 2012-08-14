openafs/openafs

commit a0f416e3504929b304fefb5ca65e2d6a254ade2e

vlserver: Allow reading during ubik writes
Turn on the new ubik_BeginTransReadAnyWrite functionality for the
vlserver, which allows us to read data from ubik during a conflicting
ubik write lock. When writing, we now update a copy of the
application-level cache, and write back the changes during a commit.

Change-Id: Ica958af704f40e89c2062e43641d883865613802
Reviewed-on: http://gerrit.openafs.org/2106
Reviewed-by: Derrick Brashear <shadow@dementia.org>
Tested-by: Derrick Brashear <shadow@dementia.org>

src/vlserver/vlprocs.c
@@ -133,7 +133,7 @@ Init_VLdbase(struct vl_ctx *ctx,
 	    wl = 1;
 	} else if (locktype == LOCKREAD) {
 	    errorcode =
-		ubik_BeginTransReadAny(VL_dbase, UBIK_READTRANS, &ctx->trans);
+		ubik_BeginTransReadAnyWrite(VL_dbase, UBIK_READTRANS, &ctx->trans);
 	    wl = 0;
 	} else {
 	    errorcode = ubik_BeginTrans(VL_dbase, UBIK_WRITETRANS, &ctx->trans);

src/vlserver/vlutils.c
@@ -28,8 +28,12 @@
 struct vlheader xheader;
 extern int maxnservers;
 struct extentaddr extentaddr;
+extern afs_uint32 rd_HostAddress[MAXSERVERID + 1];
+extern afs_uint32 wr_HostAddress[MAXSERVERID + 1];
 struct extentaddr *rd_ex_addr[VL_MAX_ADDREXTBLKS] = { 0, 0, 0, 0 };
+struct extentaddr *wr_ex_addr[VL_MAX_ADDREXTBLKS] = { 0, 0, 0, 0 };
 struct vlheader rd_cheader;	/* kept in network byte order */
+struct vlheader wr_cheader;
 int vldbversion = 0;
 
 static int index_OK(struct vl_ctx *ctx, afs_int32 blockindex);
@@ -306,7 +310,6 @@ UpdateCache(struct ubik_trans *trans, void *rock)
     int *builddb_rock = rock;
     int builddb = *builddb_rock;
     afs_int32 error = 0, i, code, ubcode;
-    extern afs_uint32 rd_HostAddress[];
 
     /* if version changed (or first call), read the header */
     ubcode = vlread(trans, 0, (char *)&rd_cheader, sizeof(rd_cheader));
@@ -1064,13 +1067,55 @@ index_OK(struct vl_ctx *ctx, afs_int32 blockindex)
     return 1;
 }
 
+/* makes a deep copy of src_ex into dst_ex */
+static int
+vlexcpy(struct extentaddr **dst_ex, struct extentaddr **src_ex)
+{
+    int i;
+    for (i = 0; i < VL_MAX_ADDREXTBLKS; i++) {
+	if (src_ex[i]) {
+	    if (!dst_ex[i]) {
+		dst_ex[i] = malloc(VL_ADDREXTBLK_SIZE);
+	    }
+	    if (!dst_ex[i]) {
+		return VL_NOMEM;
+	    }
+	    memcpy(dst_ex[i], src_ex[i], VL_ADDREXTBLK_SIZE);
+
+	} else if (dst_ex[i]) {
+	    /* we have no src, but we have a dst... meaning, this block
+	     * has gone away */
+	    free(dst_ex[i]);
+	    dst_ex[i] = NULL;
+	}
+    }
+    return 0;
+}
+
 int
 vlsetcache(struct vl_ctx *ctx, int locktype)
 {
-    extern afs_uint32 rd_HostAddress[];
+    if (locktype == LOCKREAD) {
+	ctx->hostaddress = rd_HostAddress;
+	ctx->ex_addr = rd_ex_addr;
+	ctx->cheader = &rd_cheader;
+	return 0;
+    } else {
+	memcpy(wr_HostAddress, rd_HostAddress, sizeof(wr_HostAddress));
+	memcpy(&wr_cheader, &rd_cheader, sizeof(wr_cheader));
 
-    ctx->hostaddress = rd_HostAddress;
-    ctx->ex_addr = rd_ex_addr;
-    ctx->cheader = &rd_cheader;
-    return 0;
+	ctx->hostaddress = wr_HostAddress;
+	ctx->ex_addr = wr_ex_addr;
+	ctx->cheader = &wr_cheader;
+
+	return vlexcpy(wr_ex_addr, rd_ex_addr);
+    }
+}
+
+int
+vlsynccache(void)
+{
+    memcpy(rd_HostAddress, wr_HostAddress, sizeof(rd_HostAddress));
+    memcpy(&rd_cheader, &wr_cheader, sizeof(rd_cheader));
+    return vlexcpy(rd_ex_addr, wr_ex_addr);
 }

src/vlserver/vlserver.c
@@ -60,6 +60,7 @@ int lwps = 9;
 struct vldstats dynamic_statistics;
 struct ubik_dbase *VL_dbase;
 afs_uint32 rd_HostAddress[MAXSERVERID + 1];
+afs_uint32 wr_HostAddress[MAXSERVERID + 1];
 
 static void *CheckSignal(void*);
 int LogLevel = 0;
@@ -348,6 +349,7 @@ main(int argc, char **argv)
     ubik_SRXSecurityRock = (char *)tdir;
     ubik_CheckRXSecurityProc = afsconf_CheckAuth;
     ubik_CheckRXSecurityRock = (char *)tdir;
+    ubik_SyncWriterCacheProc = vlsynccache;
     code =
 	ubik_ServerInitByInfo(myHost, htons(AFSCONF_VLDBPORT), &info, clones,
 			      vl_dbaseName, &VL_dbase);
@@ -364,6 +366,7 @@ main(int argc, char **argv)
     rx_SetRxDeadTime(50);
 
     memset(rd_HostAddress, 0, sizeof(rd_HostAddress));
+    memset(wr_HostAddress, 0, sizeof(wr_HostAddress));
     initialize_dstats();
 
     afsconf_BuildServerSecurityObjects(tdir, 0, &securityClasses, &numClasses);

src/vlserver/vlserver_internal.h
@@ -63,4 +63,5 @@ extern afs_int32 NextEntry(struct vl_ctx *ctx, afs_int32 blockindex,
 			   struct nvlentry *tentry, afs_int32 *remaining);
 extern int FreeBlock(struct vl_ctx *ctx, afs_int32 blockindex);
 extern int vlsetcache(struct vl_ctx *ctx, int locktype);
+extern int vlsynccache(void);
 #endif

commit 355b8c73ed5adc5c182cf29561cc360ef4674f9e

vlserver: fix vldb header initialization
Avoid creating new vldb files with zeroed header data.

The code path is as follows; The call to Init_VLdbase makes several
passes. On the first pass, the header is found to be empty, and so a
write lock is obtained on the second pass.  On this second pass,
UpdateCache creates a newly initialized header and writes it to the
db.  The rd_cheader is set to the newly created header data, and the
wr_cheader is still cleared at this point.

When the transaction on the second pass ended in Init_VLdbase, the
data is committed and vlsynccache() is called.  In this call to
vlsynccache(), the cleared write header buffer (wr_cheader) is
copied over the newly initialized rd_cheader buffer.  Init_VLdbase
then returns to the caller, and if the caller writes to the db, the
header on disk is then cleared.

Instead of initializing the read header buffer when rebuilding the
db header, initialize the write header buffer. When the ubik
transaction is ended, the call to vlsynccache() updates the contents
of the read header buffer with contents of the new/rebuilt header.

This error was introduced with commits:
  a0f416e3504929b304fefb5ca65e2d6a254ade2e
  1f532d099b8b084d43dd0140890448464325b602

Change-Id: If9c178bf28c55c50311554b12ffff9404785d052
Reviewed-on: http://gerrit.openafs.org/7894
Reviewed-by: Derrick Brashear <shadow@dementix.org>
Reviewed-by: Alistair Ferguson <alistair.ferguson@mac.com>
Tested-by: BuildBot <buildbot@rampaginggeek.com>

src/vlserver/vlprocs.c
@@ -243,7 +243,12 @@ Init_VLdbase(struct vl_ctx *ctx,
 		return code;
 	} else {		/* No code */
 	    if (pass == 2) {
-		ubik_EndTrans(ctx->trans);	/* Rebuilt db. End trans, then retake original lock */
+		/* The database header was rebuilt; end the write transaction.
+		 * This will call vlsynccache() to copy the write header buffers
+		 * to the read header buffers, before calling vlsetache().
+		 * Do a third pass to re-acquire the original lock, which
+		 * may be a read lock. */
+		ubik_EndTrans(ctx->trans);
 	    } else {
 		break;		/* didn't rebuild and successful - exit */
 	    }

src/vlserver/vlutils.c
@@ -328,22 +328,24 @@ UpdateCache(struct ubik_trans *trans, void *rock)
 	    printf("Can't read VLDB header, re-initialising...\n");
 
 	    /* try to write a good header */
-	    memset(&rd_cheader, 0, sizeof(rd_cheader));
-	    rd_cheader.vital_header.vldbversion = htonl(VLDBVERSION);
-	    rd_cheader.vital_header.headersize = htonl(sizeof(rd_cheader));
+	    /* The read cache will be sync'ed to this new header
+	     * when the ubik transaction is ended by vlsynccache(). */
+	    memset(&wr_cheader, 0, sizeof(wr_cheader));
+	    wr_cheader.vital_header.vldbversion = htonl(VLDBVERSION);
+	    wr_cheader.vital_header.headersize = htonl(sizeof(wr_cheader));
 	    /* DANGER: Must get this from a master place!! */
-	    rd_cheader.vital_header.MaxVolumeId = htonl(0x20000000);
-	    rd_cheader.vital_header.eofPtr = htonl(sizeof(rd_cheader));
+	    wr_cheader.vital_header.MaxVolumeId = htonl(0x20000000);
+	    wr_cheader.vital_header.eofPtr = htonl(sizeof(wr_cheader));
 	    for (i = 0; i < MAXSERVERID + 1; i++) {
-		rd_cheader.IpMappedAddr[i] = 0;
-		rd_HostAddress[i] = 0;
+		wr_cheader.IpMappedAddr[i] = 0;
+		wr_HostAddress[i] = 0;
 	    }
-	    code = vlwrite(trans, 0, (char *)&rd_cheader, sizeof(rd_cheader));
+	    code = vlwrite(trans, 0, (char *)&wr_cheader, sizeof(wr_cheader));
 	    if (code) {
 		printf("Can't write VLDB header (error = %d)\n", code);
 		ERROR_EXIT(VL_IO);
 	    }
-	    vldbversion = ntohl(rd_cheader.vital_header.vldbversion);
+	    vldbversion = ntohl(wr_cheader.vital_header.vldbversion);
 	} else {
 	    ERROR_EXIT(VL_EMPTY);
 	}