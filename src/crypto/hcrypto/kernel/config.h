openafs/openafs

commit ab977738b1542bee2dd780285ec90ebeebdc32c1

Further rationalise our usage of assert()
This patch futher improves our usage of assert() and friends. The
intention is to bring clarity to which forms of assert are used in
which situations, and to solve the problem of assert(X), or
osi_Assert(X) being used in a situation where X has side-effects.

It introduces two new assertion macros opr_Assert() and opr_Verify(),
and clarifies the usage of osi_Assert() and assert().

*) opr_Assert is a direct equivalent of assert(), with the exception
   that its output can be redirected to a log file when used in server
   code. It is the preferred version of assert for libraries, and server
   side code. Note that whilst opr_Assert doesn't currently become a
   no-op when NDEBUG is defined, the intention is that it will do so at
   some point in the future.
*) opr_Verify(X) asserts if the value of X is false. Unlike assert()
   it will always run X, regardless of whether the value is checked or
   not. The eventual intention is that when NDEBUG is defined,
   opr_Verify(X) => X
*) osi_Assert is an assertion macro intended for use in kernel code,
   or in mixed kernel/userland code. When code is built for userspace,
   osi_Assert(X) => opr_Assert(X)
*) assert is the system's own assert macro. It should only be used
   in client code. Whilst a header (opr_assert.h) is provided to map
   assert() to opr_Assert(), its use is discouraged

Change-Id: Ie6d61305686bdc7193cc8690e6f4fbe363211faf
Reviewed-on: http://gerrit.openafs.org/5395
Tested-by: BuildBot <buildbot@rampaginggeek.com>
Reviewed-by: Jeffrey Altman <jaltman@your-file-system.com>
Reviewed-by: Derrick Brashear <shadow@dementix.org>

src/crypto/hcrypto/kernel/config.h
@@ -24,19 +24,17 @@
 
 #include <afsconfig.h>
 #include "afs/param.h"
+#include "afs/opr.h"
 
 #include "afs/stds.h"
 #include "afs/sysincludes.h"
 #include "afs/afsincludes.h"
 #include "afs/afs_prototypes.h"
 
-/* Asserting is a mess - we need the RX headers in order to get a definition
- * for osi_Assert */
 #if defined(assert)
 #undef assert
 #endif
-#define assert osi_Assert
-#include <rx/rx.h>
+#define assert opr_Assert
 
 /* hcrypto uses "static inline", which isn't supported by some of our
  * compilers */

commit dd48cd3c67cd0a6d71f6f9bea5fd34dcddeefeeb

crypto: Fix assert macro
Commit ab977738b inadvertently changed osi_Assert to opr_Assert for
kernel code, causing the kernel build to produce an unloadable
module with undefined symbols.

Change-Id: I792359974ef21db97267847dfdf6c4b17c84328a
Reviewed-on: http://gerrit.openafs.org/8039
Tested-by: BuildBot <buildbot@rampaginggeek.com>
Reviewed-by: Jeffrey Altman <jaltman@your-file-system.com>

src/crypto/hcrypto/kernel/config.h
@@ -34,7 +34,7 @@
 #if defined(assert)
 #undef assert
 #endif
-#define assert opr_Assert
+#define assert osi_Assert
 
 /* hcrypto uses "static inline", which isn't supported by some of our
  * compilers */