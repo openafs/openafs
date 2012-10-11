eca07c108cc61fc9a6da720848e1beb78833f670
Simon Wilkinson <sxw@your-file-system.com>
2012-10-11 12:34:46 +0100

    rx: Move server queue entry structure out of rx.h
    Hide the server queue management structure in its own header file,
    rather than exposing it globally in rx.h. This structure has always
    been private - applications have no business knowing about it!
    
    Change-Id: I97ac31e0e77dbe1c10b2804f33901d933a8f0627
    Reviewed-on: http://gerrit.openafs.org/8231
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Derrick Brashear <shadow@your-file-system.com>

---
+++ src/rx/rx_server.h
@@ -0,0 +1,33 @@
+/* Application server thread management structures and functions */
+
+/* A server puts itself on an idle queue for a service using an
+ * instance of the following structure.  When a call arrives, the call
+ * structure pointer is placed in "newcall", the routine to execute to
+ * service the request is placed in executeRequestProc, and the
+ * process is woken up.  The queue entry's address is used for the
+ * sleep/wakeup. If socketp is non-null, then this thread is willing
+ * to become a listener thread. A thread sets *socketp to -1 before
+ * sleeping. If *socketp is not -1 when the thread awakes, it is now
+ * the listener thread for *socketp. When socketp is non-null, tno
+ * contains the server's threadID, which is used to make decisions
+ * in GetCall.
+ */
+
+#ifdef KDUMP_RX_LOCK
+struct rx_serverQueueEntry_rx_lock {
+#else
+struct rx_serverQueueEntry {
+#endif
+    struct rx_queue queueItemHeader;
+#ifdef KDUMP_RX_LOCK
+    struct rx_call_rx_lock *newcall;
+#else
+    struct rx_call *newcall;
+#endif
+#ifdef	RX_ENABLE_LOCKS
+    afs_kmutex_t lock;
+    afs_kcondvar_t cv;
+#endif
+    int tno;
+    osi_socket *socketp;
+};
