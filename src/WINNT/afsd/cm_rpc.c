c63244e83fa0590a535646bbc782210f9d636ad6
Jeffrey Altman <jaltman@your-file-system.com>
2012-11-06 06:39:39 -0500

    Windows: call MIDL_user_allocate instead of calloc
    In the RPC service routines do not call calloc() directly.
    All memory will be deallocated by a call to MIDL_user_free()
    so use MIDL_user_allocate() to perform the allocation.
    
    Modify MIDL_user_allocate() to call calloc() instead of malloc()
    to ensure that the memory is initialized to NUL bytes.
    
    Reviewed-on: http://gerrit.openafs.org/8365
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Jeffrey Altman <jaltman@your-file-system.com>
    Tested-by: Jeffrey Altman <jaltman@your-file-system.com>
    (cherry picked from commit 8c7846f4e1dd89db4ff3b03c558d97cd13cbb205)
    
    Change-Id: I2689dce0ca19748d7656f7bfca5d1d119ca6b06e
    Reviewed-on: http://gerrit.openafs.org/8399
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Jeffrey Altman <jaltman@your-file-system.com>

--- src/WINNT/afsd/cm_rpc.c
+++ src/WINNT/afsd/cm_rpc.c
@@ -165,7 +165,7 @@ long AFSRPC_GetToken(
 
 void __RPC_FAR * __RPC_USER midl_user_allocate (size_t cBytes)
 {
-    return ((void __RPC_FAR *) malloc(cBytes));
+    return ((void __RPC_FAR *) calloc(cBytes, 1));
 }
 
 void __RPC_USER midl_user_free(void __RPC_FAR * p)
