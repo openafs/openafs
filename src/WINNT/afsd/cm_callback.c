80365b9cce72e3d3785fbcef3e44716f852da8d5
Jeffrey Altman <jaltman@your-file-system.com>
2012-11-08 00:24:14 -0500

    Windows: cm_MergeStatus now returns an error code
    cm_MergeStatus() can fail if the AFSFetchStatus InterfaceVersion
    field does not have the value 0x1 as that is the only version that
    is defined by the protocol.  The return code will be CM_ERROR_INVAL.
    cm_MergeStatus() returns 0 on success.
    
    Update all of the call sites.
    
    Reviewed-on: http://gerrit.openafs.org/8402
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Jeffrey Altman <jaltman@your-file-system.com>
    (cherry picked from commit 45f0a9ba7f139749a969e73362194b380988116d)
    
    Change-Id: I392e01b34333cc3e5ad6767b6b8c18ecc699709e
    Reviewed-on: http://gerrit.openafs.org/8415
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Jeffrey Altman <jaltman@your-file-system.com>

--- src/WINNT/afsd/cm_callback.c
+++ src/WINNT/afsd/cm_callback.c
@@ -1824,7 +1824,7 @@ long cm_GetCallback(cm_scache_t *scp, struct cm_user *userp,
             InterlockedIncrement(&scp->activeRPCs);
 
             // Fetch the status info
-            cm_MergeStatus(NULL, scp, &afsStatus, &volSync, userp, reqp, 0);
+            code = cm_MergeStatus(NULL, scp, &afsStatus, &volSync, userp, reqp, 0);
         }
         goto done;
     }
@@ -1879,7 +1879,7 @@ long cm_GetCallback(cm_scache_t *scp, struct cm_user *userp,
         if (code == 0) {
             int lostRace = cm_EndCallbackGrantingCall(scp, &cbr, &callback, &volSync, 0);
             if (!lostRace)
-                cm_MergeStatus(NULL, scp, &afsStatus, &volSync, userp, reqp, 0);
+                code = cm_MergeStatus(NULL, scp, &afsStatus, &volSync, userp, reqp, 0);
         } else {
             cm_EndCallbackGrantingCall(NULL, &cbr, NULL, NULL, 0);
             InterlockedDecrement(&scp->activeRPCs);
