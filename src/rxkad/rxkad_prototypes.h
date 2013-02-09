fe59cb9c629f8c35fb0ee412ba94cbd1ac3e5088
Chaskiel Grundman <cg2v@andrew.cmu.edu>
2013-02-09 12:01:37 -0500

    Add rxkad server hook function to decrypt more types of tokens
    Allow tokens to be encrypted with algorithms other than DES.
    The security object owner must provide an implementation
    by calling rxkad_SetAltDecryptProc.
    
    Make sure plainsiz is initialized before calling the alternate decrypt
    proc.

--- src/rxkad/rxkad_prototypes.h
+++ src/rxkad/rxkad_prototypes.h
@@ -124,6 +124,8 @@ extern afs_int32 rxkad_SetConfiguration(struct rx_securityClass *aobj,
                                         struct rx_connection *aconn, 
                                         rx_securityConfigVariables atype,
                                         void * avalue, void **aresult);
+extern int rxkad_SetAltDecryptProc(struct rx_securityClass *aobj,
+				   rxkad_alt_decrypt_func alt_decrypt);
 
 /* ticket.c */
 extern int tkt_DecodeTicket(char *asecret, afs_int32 ticketLen,
@@ -149,6 +151,7 @@ extern int tkt_DecodeTicket5(char *ticket, afs_int32 ticket_len,
 			     char *get_key_rock, int serv_kvno, char *name,
 			     char *inst, char *cell, char *session_key,
 			     afs_int32 * host, afs_int32 * start,
-			     afs_int32 * end, afs_int32 disableDotCheck);
+			     afs_int32 * end, afs_int32 disableDotCheck,
+			     rxkad_alt_decrypt_func alt_decrypt);
 
 #endif
