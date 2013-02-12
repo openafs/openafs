d1855f8e04b43c9e06cf5b8d48e51f7816cf7342
Nickolai Zeldovich <nickolai@csail.mit.edu>
2013-02-12 15:08:38 -0500

    Fix scanf buffer overflows
    Fix potential buffer overflows caused by misuse of the scanf function
    in the fileserver and ptserver.
    
    Also fix similar issues in the client side fs command and libadmin
    library.
    
    Change-Id: Ia6a46981c50537da1673507c2bc777f96e43f95a

--- src/ptserver/ptprocs.c
+++ src/ptserver/ptprocs.c
@@ -679,7 +679,7 @@ idToName(struct rx_call *call, idlist *aid, namelist *aname)
     size = aid->idlist_len;
     if (size == 0)
 	return 0;
-    if (size < 0)
+    if (size < 0 || size > INT_MAX / PR_MAXNAMELEN)
 	return PRTOOMANY;
     aname->namelist_val = (prname *) malloc(size * PR_MAXNAMELEN);
     aname->namelist_len = 0;
