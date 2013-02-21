7684dbd82a4ff4883fed40f3a84269d93fa23e4b
Simon Wilkinson <sxw@your-file-system.com>
2013-02-21 21:24:33 +0000

    bosserver: Don't check if daemon thread starts
    We don't do anything with the return code from the LWP_CreateProcess
    that starts the daemon thread, so don't bother recording it at all.
    
    Caught by clang-analyzer
    
    Change-Id: I9310bdd90236a90d71dc202c73d7e06ea58a31f8
    Reviewed-on: http://gerrit.openafs.org/9229
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Chas Williams - CONTRACTOR <chas@cmf.nrl.navy.mil>
    Reviewed-by: Derrick Brashear <shadow@your-file-system.com>

--- src/bozo/bosserver.c
+++ src/bozo/bosserver.c
@@ -1103,9 +1103,8 @@ main(int argc, char **argv, char **envp)
 	}
     }
 
-    code = LWP_CreateProcess(BozoDaemon, BOZO_LWP_STACKSIZE, /* priority */ 1,
-			     /* param */ NULL , "bozo-the-clown",
-			     &bozo_pid);
+    LWP_CreateProcess(BozoDaemon, BOZO_LWP_STACKSIZE, /* priority */ 1,
+		      /* param */ NULL , "bozo-the-clown", &bozo_pid);
 
     /* try to read the key from the config file */
     tdir = afsconf_Open(AFSDIR_SERVER_ETC_DIRPATH);
