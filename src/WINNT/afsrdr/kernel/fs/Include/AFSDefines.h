35fdcc752e52596f962c607a841ed8fceffd26b4
Jeffrey Altman <jaltman@your-file-system.com>
2013-04-03 23:33:40 -0400

    Windows: Switch to Interlocked ops for RDR Set/Clear
    Switch to using interlocked operations for the SetFlag and ClearFlag
    macros.
    
    Reviewed-on: http://gerrit.openafs.org/9722
    Reviewed-by: Peter Scott <pscott@kerneldrivers.com>
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Jeffrey Altman <jaltman@your-file-system.com>
    Tested-by: Jeffrey Altman <jaltman@your-file-system.com>
    (cherry picked from commit dd73c9c1ef98b94ee1f4e870b52e2604422595e3)
    
    Change-Id: If462505f48f6d87ae3e655566e5ad97c068f4317
    Reviewed-on: http://gerrit.openafs.org/9739
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Jeffrey Altman <jaltman@your-file-system.com>

--- src/WINNT/afsrdr/kernel/fs/Include/AFSDefines.h
+++ src/WINNT/afsrdr/kernel/fs/Include/AFSDefines.h
@@ -74,11 +74,11 @@
 #endif
 
 #ifndef SetFlag
-#define SetFlag(_F,_SF)       ((_F) |= (_SF))
+#define SetFlag(_F,_SF)       InterlockedOr(&(_F), (_SF))
 #endif
 
 #ifndef ClearFlag
-#define ClearFlag(_F,_SF)     ((_F) &= ~(_SF))
+#define ClearFlag(_F,_SF)     InterlockedAnd(&(_F), ~(_SF))
 #endif
 
 #define QuadAlign(Ptr) (                \
