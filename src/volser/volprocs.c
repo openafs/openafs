9492d2ac0ca6ff8390713229382fcc5e1adb24ce
Michael Meffie <mmeffie@sinenomine.net>
2013-05-20 11:32:04 -0400

    volser: -log restore operations
    Add vos restore to the list of operations which are logged
    when the volserver is running with the -log option. Example
    log line:
    
        admin on 172.16.50.141 is executing Restore 536870969
    
    Reviewed-on: http://gerrit.openafs.org/9926
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Mark Vitale <mvitale@sinenomine.net>
    Reviewed-by: Derrick Brashear <shadow@your-file-system.com>
    (cherry picked from commit eb7b3dac482ed0427b6e618bf60507a7a1a99769)
    
    Change-Id: If09d8d170c14cfd71c181cbdc87c6027ee570c45
    Reviewed-on: http://gerrit.openafs.org/11153
    Reviewed-by: Chas Williams - CONTRACTOR <chas@cmf.nrl.navy.mil>
    Reviewed-by: Perry Ruiter <pruiter@sinenomine.net>
    Reviewed-by: Benjamin Kaduk <kaduk@mit.edu>
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Andrew Deason <adeason@sinenomine.net>
    Reviewed-by: Stephan Wiesand <stephan.wiesand@desy.de>

--- src/volser/volprocs.c
+++ src/volser/volprocs.c
@@ -1564,6 +1564,11 @@ VolRestore(struct rx_call *acid, afs_int32 atrans, afs_int32 aflags,
 	TRELE(tt);
 	return ENOENT;
     }
+    if (DoLogging) {
+	char buffer[16];
+	Log("%s on %s is executing Restore %u\n", caller,
+	    callerAddress(acid, buffer), tt->volid);
+    }
     TSetRxCall(tt, acid, "Restore");
 
     DFlushVolume(V_parentId(tt->volume)); /* Ensure dir buffers get dropped */
