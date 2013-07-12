openafs/openafs

commit 94a8ce970d57498583e249ea61725fce1ee53a50

libafs: log server errors on hard mount retry
Save the last errors seen during a request and log those
errors if a hard-mount retry is done.

Change-Id: I65e41207c5f667c41c7f9cf459243118e5baa074
Reviewed-on: http://gerrit.openafs.org/7275
Reviewed-by: Derrick Brashear <shadow@dementix.org>
Tested-by: BuildBot <buildbot@rampaginggeek.com>

src/afs/afs_analyze.c
@@ -328,6 +328,55 @@ afs_ClearStatus(struct VenusFid *afid, int op, struct volume *avp)
 
 /*!
  * \brief
+ *      Print the last errors from the servers for the volume on
+ *      this request.
+ *
+ * \param[in] areq   The request record associated with this operation.
+ * \param[in] afid   The FID of the file involved in the action.  This argument
+ *		     may be null if none was involved.
+ *
+ * \return
+ *      None
+ *
+ * \note
+ *      This routine is called before a hard-mount retry, to display
+ *      the servers by primary address and the errors encountered.
+ */
+static void
+afs_PrintServerErrors(struct vrequest *areq, struct VenusFid *afid)
+{
+    int i;
+    struct volume *tvp;
+    struct srvAddr *sa;
+    afs_uint32 address;
+    char *sep = " (";
+    char *term = "";
+
+    if (afid) {
+	tvp = afs_FindVolume(afid, READ_LOCK);
+	if (tvp) {
+	    for (i = 0; i < AFS_MAXHOSTS; i++) {
+		if (tvp->serverHost[i]) {
+		    sa = tvp->serverHost[i]->addr;
+		    if (sa) {
+			address = ntohl(sa->sa_ip);
+			afs_warnuser("%s%d.%d.%d.%d code=%d", sep,
+				     (address >> 24), (address >> 16) & 0xff,
+				     (address >> 8) & 0xff, (address) & 0xff,
+				     areq->lasterror[i]);
+			sep = ", ";
+			term = ")";
+		    }
+		}
+	    }
+	    afs_PutVolume(tvp, READ_LOCK);
+	}
+    }
+    afs_warnuser("%s\n", term);
+}
+
+/*!
+ * \brief
  *	Analyze the outcome of an RPC operation, taking whatever support
  *	actions are necessary.
  *
@@ -487,8 +536,9 @@ afs_Analyze(struct afs_conn *aconn, struct rx_connection *rxconn,
 		    if (shouldRetry) {
 			if (warn) {
 			    afs_warnuser
-			        ("afs: hard-mount waiting for volume %u\n",
+			        ("afs: hard-mount waiting for volume %u",
 			         afid->Fid.Volume);
+			    afs_PrintServerErrors(areq, afid);
 			}
 
 			VSleep(hm_retry_int);
@@ -548,6 +598,17 @@ afs_Analyze(struct afs_conn *aconn, struct rx_connection *rxconn,
 
 	afs_PutConn(aconn, rxconn, locktype);
 	return 0;
+    }
+
+    /* Save the last code of this server on this request. */
+    tvp = afs_FindVolume(afid, READ_LOCK);
+    if (tvp) {
+	for (i = 0; i < AFS_MAXHOSTS; i++) {
+	    if (tvp->serverHost[i] == tsp) {
+		areq->lasterror[i] = acode;
+	    }
+	}
+	afs_PutVolume(tvp, READ_LOCK);
     }
 
     /* If network troubles, mark server as having bogued out again. */

src/afs/afs.h
@@ -244,6 +244,7 @@ struct vrequest {
     char tokenError;            /* a token error other than expired. */
     char idleError;             /* the server idled too long */
     char skipserver[AFS_MAXHOSTS];
+    afs_int32 lasterror[AFS_MAXHOSTS];
 };
 #define VOLMISSING 1
 #define VOLBUSY 2

commit 495d6fcbba1630b136d9c1b4256f0a71666a8a7b

libafs: initialize hard mount last errors
Initialize the values of the server last errors
introduced in commit 94a8ce970d57498583e249ea61725fce1ee53a50
to avoid logging garbage for the last error codes.

Change-Id: I0df262bad5d02231407d0dd65e1f1d32d791912f
Reviewed-on: http://gerrit.openafs.org/10082
Tested-by: BuildBot <buildbot@rampaginggeek.com>
Reviewed-by: Derrick Brashear <shadow@your-file-system.com>

src/afs/afs_analyze.c
@@ -356,7 +356,7 @@ afs_PrintServerErrors(struct vrequest *areq, struct VenusFid *afid)
 	tvp = afs_FindVolume(afid, READ_LOCK);
 	if (tvp) {
 	    for (i = 0; i < AFS_MAXHOSTS; i++) {
-		if (tvp->serverHost[i]) {
+		if (areq->lasterror[i] && tvp->serverHost[i]) {
 		    sa = tvp->serverHost[i]->addr;
 		    if (sa) {
 			address = ntohl(sa->sa_ip);

src/afs/afs_error.c
@@ -212,6 +212,7 @@ afs_CopyError(struct vrequest *afrom, struct vrequest *ato)
     afs_FinalizeReq(ato);
     while (i < AFS_MAXHOSTS) {
 	ato->skipserver[i] = afrom->skipserver[i];
+	ato->lasterror[i] = afrom->lasterror[i];
 	i++;
     }
     if (afrom->tokenError)
@@ -238,6 +239,7 @@ afs_FinalizeReq(struct vrequest *areq)
 	return;
     while (i < AFS_MAXHOSTS) {
 	areq->skipserver[i] = 0;
+	areq->lasterror[i] = 0;
 	i++;
     }
     areq->busyCount = 0;