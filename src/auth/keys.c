961ca14c2eef02fc56052ca983cd44925f9410f6
Ben Kaduk <kaduk@mit.edu>
2013-07-12 13:38:08 -0400

    Fix typo
    The file is KeyFileExt, not KeyFileEx.
    
    Change-Id: Iffd5edc8a60b1a49441822d26dc44fa252bd70e3
    Reviewed-on: http://gerrit.openafs.org/10084
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Derrick Brashear <shadow@your-file-system.com>

--- src/auth/keys.c
+++ src/auth/keys.c
@@ -300,7 +300,7 @@ _afsconf_InitKeys(struct afsconf_dir *dir)
  * in the original KeyFile, so that we can continue to be compatible with
  * utilities that directly modify that file.
  *
- * All other keys are stored in the file KeyFileEx, which has the following
+ * All other keys are stored in the file KeyFileExt, which has the following
  * format:
  *    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2
  *   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
