openafs/openafs

commit fc4ab52ec4839afb91168f846b84a05bc6df60cc

buserver-support-clones-20040311
FIXES 3355

support ubik clones in buserver as with other ubik servers

src/budb/server.c
@@ -316,6 +316,10 @@ main(argc, argv)
     time_t currentTime;
     afs_int32 code = 0;
 
+    char  clones[MAXHOSTSPERCELL];
+
+	
+	
     struct rx_service *tservice;
     struct rx_securityClass *sca[3];
 
@@ -430,7 +434,8 @@ main(argc, argv)
 
 	LogDebug(1, "Using server list from %s cell database.\n", lcell);
 
-	code = afsconf_GetCellInfo(BU_conf, lcell, 0, &cellinfo);
+	code = afsconf_GetExtendedCellInfo (BU_conf, lcell, 0, &cellinfo, 
+					    &clones); 
 	code =
 	    convert_cell_to_ubik(&cellinfo, &globalConfPtr->myHost,
 				 globalConfPtr->serverList);
@@ -465,8 +470,13 @@ main(argc, argv)
 
     rx_SetRxDeadTime(60);	/* 60 seconds inactive before timeout */
 
-    code = ubik_ServerInit(globalConfPtr->myHost, htons(AFSCONF_BUDBPORT), globalConfPtr->serverList, dbNamePtr,	/* name prefix */
-			   &BU_dbase);
+    code = ubik_ServerInitByInfo (globalConfPtr->myHost,
+				  htons(AFSCONF_BUDBPORT), 
+				  &cellinfo,
+				  &clones,              
+				  dbNamePtr,           /* name prefix */
+				  &BU_dbase);
+
     if (code) {
 	LogError(code, "Ubik init failed\n");
 	com_err(whoami, code, "Ubik init failed");

commit 6b0016dd992b3215cb17c93812e14de9e4916d25

budb: Do not use garbage cellinfo
If the -servers option is given, we never initialize cellinfo or the
clones array. So, don't give the cellinfo structure or the clones
array to ubik in that case, or we may crash or do other weird things.

This issue appears to have been introduced in commit fc4ab52e.

FIXES 131706

Change-Id: I87681c697fec7bbfd6c73a8a9a865e4309c95963
Reviewed-on: http://gerrit.openafs.org/10115
Reviewed-by: Jeffrey Altman <jaltman@your-file-system.com>
Tested-by: BuildBot <buildbot@rampaginggeek.com>
Reviewed-by: Derrick Brashear <shadow@your-file-system.com>

src/budb/server.c
@@ -357,7 +357,8 @@ main(int argc, char **argv)
 {
     char *whoami = argv[0];
     char *dbNamePtr = 0;
-    struct afsconf_cell cellinfo;
+    struct afsconf_cell cellinfo_s;
+    struct afsconf_cell *cellinfo = NULL;
     time_t currentTime;
     afs_int32 code = 0;
     afs_uint32 host = ntohl(INADDR_ANY);
@@ -394,6 +395,10 @@ main(int argc, char **argv)
     sigaction(SIGSEGV, &nsa, NULL);
     sigaction(SIGABRT, &nsa, NULL);
 #endif
+
+    memset(&cellinfo_s, 0, sizeof(cellinfo_s));
+    memset(clones, 0, sizeof(clones));
+
     osi_audit_init();
     osi_audit(BUDB_StartEvent, 0, AUD_END);
 
@@ -478,9 +483,11 @@ main(int argc, char **argv)
 	 * list from the cell's database
 	 */
 
+	cellinfo = &cellinfo_s;
+
 	LogDebug(1, "Using server list from %s cell database.\n", lcell);
 
-	code = afsconf_GetExtendedCellInfo (BU_conf, lcell, 0, &cellinfo,
+	code = afsconf_GetExtendedCellInfo (BU_conf, lcell, 0, cellinfo,
 					    clones);
 	if (code) {
 	    LogError(0, "Can't read cell information\n");
@@ -488,7 +495,7 @@ main(int argc, char **argv)
 	}
 
 	code =
-	    convert_cell_to_ubik(&cellinfo, &globalConfPtr->myHost,
+	    convert_cell_to_ubik(cellinfo, &globalConfPtr->myHost,
 				 globalConfPtr->serverList);
 	if (code)
 	    ERROR(code);
@@ -536,12 +543,19 @@ main(int argc, char **argv)
     /* Disable jumbograms */
     rx_SetNoJumbo();
 
-    code = ubik_ServerInitByInfo (globalConfPtr->myHost,
-				  htons(AFSCONF_BUDBPORT),
-				  &cellinfo,
-				  clones,
-				  dbNamePtr,           /* name prefix */
-				  &BU_dbase);
+    if (cellinfo) {
+	code = ubik_ServerInitByInfo(globalConfPtr->myHost,
+	                             htons(AFSCONF_BUDBPORT),
+	                             cellinfo,
+	                             clones,
+	                             dbNamePtr,           /* name prefix */
+	                             &BU_dbase);
+    } else {
+	code = ubik_ServerInit(globalConfPtr->myHost, htons(AFSCONF_BUDBPORT),
+	                       globalConfPtr->serverList,
+	                       dbNamePtr,  /* name prefix */
+	                       &BU_dbase);
+    }
 
     if (code) {
 	LogError(code, "Ubik init failed\n");