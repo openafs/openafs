openafs/openafs

commit 7694f536d3997768b69a635616b0cf24d71a595a

scsi_command_size became scsi_command_size_tbl
Change-Id: If91922944f90df11d5a09cd09cbb66c16296d366
Reviewed-on: http://gerrit.openafs.org/9964
Tested-by: BuildBot <buildbot@rampaginggeek.com>
Reviewed-by: Derrick Brashear <shadow@your-file-system.com>

src/afs/LINUX/osi_probe.c
@@ -56,6 +56,10 @@
 #if defined(ENABLE_LINUX_SYSCALL_PROBING)
 #include <linux/module.h> /* early to avoid printf->printk mapping */
 #include <scsi/scsi.h> /* for scsi_command_size */
+#if LINUX_VERSION_CODE > KERNEL_VERSION(2,6,26)
+/* Slightly kludgy, but too bad */
+#define scsi_command_size scsi_command_size_tbl
+#endif
 #ifndef OSI_PROBE_STANDALONE
 # include "afs/sysincludes.h"
 # include "afsincludes.h"

commit 341f2af7969842fecf352d2c42c95e56a9216546

fix linux build error in osi_probe
Fix a build error for older versions of linux, introduced by
commit 7694f536d3997768b69a635616b0cf24d71a595a
(scsi_command_size became scsi_command_size_tbl)

Fixes a build error on RHEL/CentOS 5.9; 2.6.18-348.3.1.el5.

Change-Id: I7e6f08e7f7cbba47034701e6137eb91fa567dbda
Reviewed-on: http://gerrit.openafs.org/10282
Tested-by: BuildBot <buildbot@rampaginggeek.com>
Reviewed-by: Derrick Brashear <shadow@your-file-system.com>

src/afs/LINUX/osi_probe.c
@@ -53,6 +53,7 @@
 # include "afs/param.h"
 #endif
 
+#include <linux/version.h>
 #if defined(ENABLE_LINUX_SYSCALL_PROBING)
 #include <linux/module.h> /* early to avoid printf->printk mapping */
 #include <scsi/scsi.h> /* for scsi_command_size */
@@ -64,7 +65,6 @@
 # include "afs/sysincludes.h"
 # include "afsincludes.h"
 #endif
-#include <linux/version.h>
 #include <linux/sched.h>
 #ifdef HAVE_LINUX_CONFIG_H
 # include <linux/config.h>