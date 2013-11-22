526f285306d8a676fe05b9ccd6dff6b4d3500b9c
Michael Meffie <mmeffie@sinenomine.net>
2013-11-22 15:26:34 -0500

    comerr: avoid comma operator
    Avoid unnecessary use of the comma operator in compile_et's
    command-line argument processing.
    
    Reviewed-on: http://gerrit.openafs.org/10491
    Reviewed-by: Benjamin Kaduk <kaduk@mit.edu>
    Reviewed-by: Marc Dionne <marc.c.dionne@gmail.com>
    Tested-by: Derrick Brashear <shadow@your-file-system.com>
    Reviewed-by: Derrick Brashear <shadow@your-file-system.com>
    (cherry picked from commit 15c80b1a92516a41d4aed8403db1485e97a7eff6)
    
    Change-Id: I28f7503877d670406c973520a33924480bb96ad9
    Reviewed-on: http://gerrit.openafs.org/11224
    Reviewed-by: Perry Ruiter <pruiter@sinenomine.net>
    Reviewed-by: Stephan Wiesand <stephan.wiesand@desy.de>
    Tested-by: Stephan Wiesand <stephan.wiesand@desy.de>

--- src/comerr/compile_et.c
+++ src/comerr/compile_et.c
@@ -169,8 +169,8 @@ main(int argc, char **argv)
     p = strrchr(whoami, '/');
     if (p)
 	whoami = p + 1;
-    while (argv++, --argc) {
-	char *arg = *argv;
+    while (--argc) {
+	char *arg = *++argv;
 	if (arg[0] != '-') {
 	    if (filename)
 		dup_err("filenames", filename, arg);
@@ -181,7 +181,8 @@ main(int argc, char **argv)
 		debug++;
 	    else if (check_arg(lang_args, arg)) {
 		got_language++;
-		arg = *++argv, argc--;
+		arg = *++argv;
+		argc--;
 		if (!arg)
 		    usage();
 		if (language)
@@ -208,17 +209,26 @@ main(int argc, char **argv)
 		    exit(1);
 		}
 	    } else if (strcmp(arg, "h") == 0) {
-		arg = *++argv, argc--;
+		arg = *++argv;
+		argc--;
 		if (!arg)
 		    usage();
 		got_include = arg;
 	    } else if (strcmp(arg, "p") == 0) {
-		arg = *++argv, argc--;
+		arg = *++argv;
+		argc--;
 		if (!arg)
 		    usage();
 		got_prefix = arg;
 	    } else if (strcmp(arg, "v") == 0) {
-		arg = *++argv, argc--;
+		arg = *++argv;
+		argc--;
+		if (arg == NULL) {
+		    fprintf(stderr, "%s: -v option requires an argument\n",
+			    whoami);
+		    usage();
+		    exit(1);
+		}
 		version = atoi(arg);
 		if (version != 1 && version != 2) {
 		    fprintf(stderr, "%s: unknown control argument -`%s'\n",
