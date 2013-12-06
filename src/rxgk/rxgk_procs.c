ee56e925f0c6484ac58e23ccdd1b1bf1a3760505
Ben Kaduk <kaduk@mit.edu>
2013-12-06 15:24:58 -0500

    Add rxgk boilerplate
    Just the skeleton of what needs to be there.  The actual import is split
    over multiple commits, to make the reviewer's burden more manageable.
    
    Error table, protocol description, and stubs for the security object
    routines, with header to declare them.
    
    The public header rxgk.h currently only contains a few typedefs and the
    NewSecurityObject prototypes, and includes the RPC interface and com_err
    code headers.
    
    Change-Id: I7893f78119bb4aef12112cc1e51e1ec69de326c2
    Reviewed-on: http://gerrit.openafs.org/10562
    Reviewed-by: Chas Williams - CONTRACTOR <chas@cmf.nrl.navy.mil>
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: D Brashear <shadow@your-file-system.com>

---
+++ src/rxgk/rxgk_procs.c
@@ -0,0 +1,72 @@
+/* rxgk/rxgk_procs.c - Server-side RPC procedures for RXGK */
+/*
+ * Copyright (C) 2013, 2014 by the Massachusetts Institute of Technology.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in
+ *   the documentation and/or other materials provided with the
+ *   distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * Server-side RPC procedures for RXGK.
+ */
+
+#include <afsconfig.h>
+#include <afs/param.h>
+#include <afs/stds.h>
+
+#include <rx/rx.h>
+#include <rx/rx_identity.h>
+#include <rx/rxgk.h>
+
+#include "rxgk_private.h"
+
+afs_int32
+SRXGK_GSSNegotiate(struct rx_call *z_call, RXGK_StartParams *client_start,
+		   RXGK_Data *input_token_buffer, RXGK_Data *opaque_in,
+		   RXGK_Data *output_token_buffer, RXGK_Data *opaque_out,
+		   afs_uint32 *gss_major_status, afs_uint32 *gss_minor_status,
+		   RXGK_Data *rxgk_info)
+{
+    return RXGEN_OPCODE;
+}
+
+
+afs_int32
+SRXGK_CombineTokens(struct rx_call *z_call, RXGK_Data *token0,
+		    RXGK_Data *token1, RXGK_CombineOptions *options,
+		    RXGK_Data *new_token, RXGK_TokenInfo *info)
+{
+    return RXGEN_OPCODE;
+}
+
+afs_int32
+SRXGK_AFSCombineTokens(struct rx_call *z_call, RXGK_Data *user_tok,
+		       RXGK_Data *cm_tok, RXGK_CombineOptions *options,
+		       afsUUID destination, RXGK_Data *new_token,
+		       RXGK_TokenInfo *info)
+{
+    return RXGEN_OPCODE;
+}
