d08e048e57906574d3e505a345e0935d1df2dba4
Stephan Wiesand <stephan.wiesand@desy.de>
2014-02-28 18:47:12 +0100

    bozo: remove obsolete bnode_Deactivate() prototype
    Commit 54eb2485b59550ba42569ed3a8d76211a3a35019 removed the
    implementation of bnode_Deactivate(), which had been #ifdef'd out
    for a long time, but left the prototype in place. Remove the
    obsolete declaration in bosprototypes.h as well.
    
    Reviewed-on: http://gerrit.openafs.org/10868
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Perry Ruiter <pruiter@sinenomine.net>
    Reviewed-by: Jeffrey Altman <jaltman@your-file-system.com>
    (cherry picked from commit 01a7c64e472c241798306e7f8137de28efdef37e)
    
    Change-Id: Ic1ca95ef455705a14cf68f0801d73789cc7bd4b5
    Reviewed-on: http://gerrit.openafs.org/11192
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Benjamin Kaduk <kaduk@mit.edu>
    Reviewed-by: Andrew Deason <adeason@sinenomine.net>
    Reviewed-by: Stephan Wiesand <stephan.wiesand@desy.de>

--- src/bozo/bosprototypes.h
+++ src/bozo/bosprototypes.h
@@ -41,7 +41,6 @@ int bnode_ParseLine(char *aline, struct bnode_token **alist);
 int bnode_NewProc(struct bnode *abnode, char *aexecString, char *coreName,
 		  struct bnode_proc **aproc);
 int bnode_StopProc(struct bnode_proc *aproc, int asignal);
-int bnode_Deactivate(struct bnode *abnode);
 
 /* bosserver.c */
 void bozo_Log(char *format, ... );
