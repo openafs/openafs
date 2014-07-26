2809bae58851ce9168ca8b18c94360392b8a609f
Garrett Wollman <wollman@csail.mit.edu>
2014-07-26 00:43:10 -0400

    FBSD: 10.1 is coming out soon
    The 10.1 release cycle is starting in a month, so let's get ahead of
    the curve by adding the config bits now.
    
    Change-Id: Ia17e1267dafee5c80d87a6bfc5e423982f83306d
    Reviewed-on: http://gerrit.openafs.org/11328
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Jeffrey Altman <jaltman@your-file-system.com>

--- src/config/afs_sysnames.h
+++ src/config/afs_sysnames.h
@@ -215,6 +215,7 @@
 #define SYS_NAME_ID_i386_fbsd_92        2124
 #define SYS_NAME_ID_i386_fbsd_93        2126
 #define SYS_NAME_ID_i386_fbsd_100      2130
+#define SYS_NAME_ID_i386_fbsd_101      2131
 #define SYS_NAME_ID_i386_fbsd_110      2140
 
 #define SYS_NAME_ID_ia64_linux2		2200
@@ -322,6 +323,7 @@
 #define SYS_NAME_ID_amd64_fbsd_92       3023
 #define SYS_NAME_ID_amd64_fbsd_93       3024
 #define SYS_NAME_ID_amd64_fbsd_100     3030
+#define SYS_NAME_ID_amd64_fbsd_101     3031
 #define SYS_NAME_ID_amd64_fbsd_110     3040
 
 #define SYS_NAME_ID_amd64_w2k           3400
