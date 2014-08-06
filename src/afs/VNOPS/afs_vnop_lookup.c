openafs/openafs

commit 9930567bcf9655d3f562b210b2dc4b4a99226691

libafs: reduce stack space in VNOPS
Allocate temporary vrequests to reduce the amount
of stack space used.

Change-Id: Ic14cc4f657f7c7e97ef396601bd6c8c7f91abe55
Reviewed-on: http://gerrit.openafs.org/11004
Tested-by: BuildBot <buildbot@rampaginggeek.com>
Reviewed-by: Chas Williams - CONTRACTOR <chas@cmf.nrl.navy.mil>
Reviewed-by: D Brashear <shadow@your-file-system.com>

src/afs/VNOPS/afs_vnop_lookup.c
@@ -1373,7 +1373,7 @@ afs_lookup(OSI_VC_DECL(adp), char *aname, struct vcache **avcp, afs_ucred_t *acr
 afs_lookup(OSI_VC_DECL(adp), char *aname, struct vcache **avcp, afs_ucred_t *acred)
 #endif
 {
-    struct vrequest treq;
+    struct vrequest *treq = NULL;
     char *tname = NULL;
     struct vcache *tvc = 0;
     afs_int32 code;
@@ -1393,8 +1393,8 @@ afs_lookup(OSI_VC_DECL(adp), char *aname, struct vcache **avcp, afs_ucred_t *acr
     afs_InitFakeStat(&fakestate);
 
     AFS_DISCON_LOCK();
-    
-    if ((code = afs_InitReq(&treq, acred)))
+
+    if ((code = afs_CreateReq(&treq, acred)))
 	goto done;
 
     if (afs_fakestat_enable && adp->mvstat == 1) {
@@ -1419,9 +1419,9 @@ afs_lookup(OSI_VC_DECL(adp), char *aname, struct vcache **avcp, afs_ucred_t *acr
 #endif
 
     if (tryEvalOnly)
-	code = afs_TryEvalFakeStat(&adp, &fakestate, &treq);
+	code = afs_TryEvalFakeStat(&adp, &fakestate, treq);
     else
-	code = afs_EvalFakeStat(&adp, &fakestate, &treq);
+	code = afs_EvalFakeStat(&adp, &fakestate, treq);
 
     /*printf("Code is %d\n", code);*/
     
@@ -1437,7 +1437,7 @@ afs_lookup(OSI_VC_DECL(adp), char *aname, struct vcache **avcp, afs_ucred_t *acr
     bulkcode = 0;
 
     if (!(adp->f.states & CStatd) && !afs_InReadDir(adp)) {
-	if ((code = afs_VerifyVCache2(adp, &treq))) {
+	if ((code = afs_VerifyVCache2(adp, treq))) {
 	    goto done;
 	}
     } else
@@ -1452,7 +1452,7 @@ afs_lookup(OSI_VC_DECL(adp), char *aname, struct vcache **avcp, afs_ucred_t *acr
 	}
 	/* otherwise we have the fid here, so we use it */
 	/*printf("Getting vcache\n");*/
-	tvc = afs_GetVCache(adp->mvid, &treq, NULL, NULL);
+	tvc = afs_GetVCache(adp->mvid, treq, NULL, NULL);
 	afs_Trace3(afs_iclSetp, CM_TRACE_GETVCDOTDOT, ICL_TYPE_FID, adp->mvid,
 		   ICL_TYPE_POINTER, tvc, ICL_TYPE_INT32, code);
 	*avcp = tvc;
@@ -1468,18 +1468,18 @@ afs_lookup(OSI_VC_DECL(adp), char *aname, struct vcache **avcp, afs_ucred_t *acr
     }
 
     /* now check the access */
-    if (treq.uid != adp->last_looker) {
-	if (!afs_AccessOK(adp, PRSFS_LOOKUP, &treq, CHECK_MODE_BITS)) {
+    if (treq->uid != adp->last_looker) {
+	if (!afs_AccessOK(adp, PRSFS_LOOKUP, treq, CHECK_MODE_BITS)) {
 	    *avcp = NULL;
 	    code = EACCES;
 	    goto done;
 	} else
-	    adp->last_looker = treq.uid;
+	    adp->last_looker = treq->uid;
     }
 
     /* Check for read access as well.  We need read access in order to
      * stat files, but not to stat subdirectories. */
-    if (!afs_AccessOK(adp, PRSFS_READ, &treq, CHECK_MODE_BITS))
+    if (!afs_AccessOK(adp, PRSFS_READ, treq, CHECK_MODE_BITS))
 	no_read_access = 1;
 
     /* special case lookup of ".".  Can we check for it sooner in this code,
@@ -1532,7 +1532,7 @@ afs_lookup(OSI_VC_DECL(adp), char *aname, struct vcache **avcp, afs_ucred_t *acr
 	struct VenusFid tfid;
 	afs_uint32 cellidx, volid, vnoid, uniq;
 
-	code = EvalMountData('%', aname, 0, 0, NULL, &treq, &cellidx, &volid, &vnoid, &uniq);
+	code = EvalMountData('%', aname, 0, 0, NULL, treq, &cellidx, &volid, &vnoid, &uniq);
 	if (code)
 	    goto done;
 	/* If a vnode was returned, it's not a real mount point */
@@ -1548,7 +1548,7 @@ afs_lookup(OSI_VC_DECL(adp), char *aname, struct vcache **avcp, afs_ucred_t *acr
 	    tfid.Fid.Vnode = VNUM_FROM_TYPEID(VN_TYPE_MOUNT, cellidx << 2);
 	    tfid.Fid.Unique = volid;
 	}
-	*avcp = tvc = afs_GetVCache(&tfid, &treq, NULL, NULL);
+	*avcp = tvc = afs_GetVCache(&tfid, treq, NULL, NULL);
 	code = (tvc ? 0 : ENOENT);
 	hit = 1;
 	goto done;
@@ -1564,14 +1564,14 @@ afs_lookup(OSI_VC_DECL(adp), char *aname, struct vcache **avcp, afs_ucred_t *acr
 	struct VenusFid tfid;
 
 	afs_GetDynrootMountFid(&tfid);
-	*avcp = tvc = afs_GetVCache(&tfid, &treq, NULL, NULL);
+	*avcp = tvc = afs_GetVCache(&tfid, treq, NULL, NULL);
 	code = 0;
 	hit = 1;
 	goto done;
     }
 #endif
 
-    Check_AtSys(adp, aname, &sysState, &treq);
+    Check_AtSys(adp, aname, &sysState, treq);
     tname = sysState.name;
 
     /* 1st Check_AtSys and lookup by tname is required here, for now,
@@ -1616,7 +1616,7 @@ afs_lookup(OSI_VC_DECL(adp), char *aname, struct vcache **avcp, afs_ucred_t *acr
 	if (afs_InReadDir(adp))
 	    tdc = adp->dcreaddir;
 	else
-	    tdc = afs_GetDCache(adp, (afs_size_t) 0, &treq,
+	    tdc = afs_GetDCache(adp, (afs_size_t) 0, treq,
 				&dirOffset, &dirLen, 1);
 	if (!tdc) {
 	    *avcp = NULL;	/* redundant, but harmless */
@@ -1681,7 +1681,7 @@ afs_lookup(OSI_VC_DECL(adp), char *aname, struct vcache **avcp, afs_ucred_t *acr
 				 &dirCookie);
 
 	/* If the first lookup doesn't succeed, maybe it's got @sys in the name */
-	while (code == ENOENT && Next_AtSys(adp, &treq, &sysState))
+	while (code == ENOENT && Next_AtSys(adp, treq, &sysState))
 	    code =
 		afs_dir_LookupOffset(tdc, sysState.name, &tfid.Fid,
 				     &dirCookie);
@@ -1750,7 +1750,7 @@ afs_lookup(OSI_VC_DECL(adp), char *aname, struct vcache **avcp, afs_ucred_t *acr
 	    } while (tvc && retry);
 
 	    if (!tvc || !(tvc->f.states & CStatd))
-		bulkcode = afs_DoBulkStat(adp, dirCookie, &treq);
+		bulkcode = afs_DoBulkStat(adp, dirCookie, treq);
 	    else
 		bulkcode = 0;
 
@@ -1774,10 +1774,10 @@ afs_lookup(OSI_VC_DECL(adp), char *aname, struct vcache **avcp, afs_ucred_t *acr
 	if (!tvc) {
 	    afs_int32 cached = 0;
 	    if (!tfid.Fid.Unique && (adp->f.states & CForeign)) {
-		tvc = afs_LookupVCache(&tfid, &treq, &cached, adp, tname);
+		tvc = afs_LookupVCache(&tfid, treq, &cached, adp, tname);
 	    }
 	    if (!tvc && !bulkcode) {	/* lookup failed or wasn't called */
-		tvc = afs_GetVCache(&tfid, &treq, &cached, NULL);
+		tvc = afs_GetVCache(&tfid, treq, &cached, NULL);
 	    }
 	}			/* if !tvc */
     }				/* sub-block just to reduce stack usage */
@@ -1793,7 +1793,7 @@ afs_lookup(OSI_VC_DECL(adp), char *aname, struct vcache **avcp, afs_ucred_t *acr
 	    ObtainSharedLock(&tvc->lock, 680);
 	    if (!tvc->linkData) {
 		UpgradeSToWLock(&tvc->lock, 681);
-		code = afs_HandleLink(tvc, &treq);
+		code = afs_HandleLink(tvc, treq);
 		ConvertWToRLock(&tvc->lock);
 	    } else {
 		ConvertSToRLock(&tvc->lock);
@@ -1815,7 +1815,7 @@ afs_lookup(OSI_VC_DECL(adp), char *aname, struct vcache **avcp, afs_ucred_t *acr
 		struct volume *tvolp;
 
 		ObtainWriteLock(&tvc->lock, 133);
-		code = EvalMountPoint(tvc, adp, &tvolp, &treq);
+		code = EvalMountPoint(tvc, adp, &tvolp, treq);
 		ReleaseWriteLock(&tvc->lock);
 
 		if (code) {
@@ -1837,9 +1837,9 @@ afs_lookup(OSI_VC_DECL(adp), char *aname, struct vcache **avcp, afs_ucred_t *acr
 		    if (tvolp && (tvolp->states & VForeign)) {
 			/* XXXX tvolp has ref cnt on but not locked! XXX */
 			tvc =
-			    afs_GetRootVCache(tvc->mvid, &treq, NULL, tvolp);
+			    afs_GetRootVCache(tvc->mvid, treq, NULL, tvolp);
 		    } else {
-			tvc = afs_GetVCache(tvc->mvid, &treq, NULL, NULL);
+			tvc = afs_GetVCache(tvc->mvid, treq, NULL, NULL);
 		    }
 		    afs_PutVCache(uvc);	/* we're done with it */
 
@@ -1886,7 +1886,7 @@ afs_lookup(OSI_VC_DECL(adp), char *aname, struct vcache **avcp, afs_ucred_t *acr
 	if (!AFS_IS_DISCONNECTED) {
 	    if (pass == 0) {
 	        struct volume *tv;
-	        tv = afs_GetVolume(&adp->f.fid, &treq, READ_LOCK);
+	        tv = afs_GetVolume(&adp->f.fid, treq, READ_LOCK);
 	        if (tv) {
 		    if (tv->states & VRO) {
 		        pass = 1;	/* try this *once* */
@@ -1929,7 +1929,7 @@ afs_lookup(OSI_VC_DECL(adp), char *aname, struct vcache **avcp, afs_ucred_t *acr
 	    } else {
 #ifdef AFS_LINUX20_ENV
 		/* So Linux inode cache is up to date. */
-		code = afs_VerifyVCache(tvc, &treq);
+		code = afs_VerifyVCache(tvc, treq);
 #else
 		afs_PutFakeStat(&fakestate);
 		AFS_DISCON_UNLOCK();
@@ -1943,7 +1943,7 @@ afs_lookup(OSI_VC_DECL(adp), char *aname, struct vcache **avcp, afs_ucred_t *acr
     if (bulkcode)
 	code = bulkcode;
 
-    code = afs_CheckCode(code, &treq, 19);
+    code = afs_CheckCode(code, treq, 19);
     if (code) {
 	/* If there is an error, make sure *avcp is null.
 	 * Alphas panic otherwise - defect 10719.
@@ -1952,6 +1952,7 @@ afs_lookup(OSI_VC_DECL(adp), char *aname, struct vcache **avcp, afs_ucred_t *acr
     }
 
     afs_PutFakeStat(&fakestate);
+    afs_DestroyReq(treq);
     AFS_DISCON_UNLOCK();
     return code;
 }

src/afs/VNOPS/afs_vnop_access.c
@@ -198,7 +198,7 @@ afs_access(OSI_VC_DECL(avc), afs_int32 amode,
 #endif
 {
     afs_int32 code;
-    struct vrequest treq;
+    struct vrequest *treq = NULL;
     struct afs_fakestat_state fakestate;
     OSI_VC_CONVERT(avc);
 
@@ -206,35 +206,40 @@ afs_access(OSI_VC_DECL(avc), afs_int32 amode,
     afs_Trace3(afs_iclSetp, CM_TRACE_ACCESS, ICL_TYPE_POINTER, avc,
 	       ICL_TYPE_INT32, amode, ICL_TYPE_OFFSET,
 	       ICL_HANDLE_OFFSET(avc->f.m.Length));
+
     afs_InitFakeStat(&fakestate);
-    if ((code = afs_InitReq(&treq, acred)))
+    if ((code = afs_CreateReq(&treq, acred))) {
 	return code;
+    }
 
     AFS_DISCON_LOCK();
 
     if (afs_fakestat_enable && avc->mvstat == 1) {
-	code = afs_TryEvalFakeStat(&avc, &fakestate, &treq);
+	code = afs_TryEvalFakeStat(&avc, &fakestate, treq);
         if (code == 0 && avc->mvstat == 1) {
 	    afs_PutFakeStat(&fakestate);
 	    AFS_DISCON_UNLOCK();
+	    afs_DestroyReq(treq);
 	    return 0;
         }
     } else {
-	code = afs_EvalFakeStat(&avc, &fakestate, &treq);
+	code = afs_EvalFakeStat(&avc, &fakestate, treq);
     }
 
     if (code) {
 	afs_PutFakeStat(&fakestate);
 	AFS_DISCON_UNLOCK();
+	afs_DestroyReq(treq);
 	return code;
     }
 
     if (vType(avc) != VDIR || !afs_InReadDir(avc)) {
-	code = afs_VerifyVCache(avc, &treq);
+	code = afs_VerifyVCache(avc, treq);
 	if (code) {
 	    afs_PutFakeStat(&fakestate);
 	    AFS_DISCON_UNLOCK();
-	    code = afs_CheckCode(code, &treq, 16);
+	    code = afs_CheckCode(code, treq, 16);
+	    afs_DestroyReq(treq);
 	    return code;
 	}
     }
@@ -243,6 +248,7 @@ afs_access(OSI_VC_DECL(avc), afs_int32 amode,
     if ((amode & VWRITE) && (avc->f.states & CRO)) {
 	afs_PutFakeStat(&fakestate);
 	AFS_DISCON_UNLOCK();
+	afs_DestroyReq(treq);
 	return EROFS;
     }
     
@@ -251,6 +257,7 @@ afs_access(OSI_VC_DECL(avc), afs_int32 amode,
         afs_PutFakeStat(&fakestate);
 	AFS_DISCON_UNLOCK();
 	/* printf("Network is down in afs_vnop_access\n"); */
+	afs_DestroyReq(treq);
         return ENETDOWN;
     }
     
@@ -258,41 +265,41 @@ afs_access(OSI_VC_DECL(avc), afs_int32 amode,
     if (avc->f.states & CForeign) {
 	/* In the dfs xlator the EXEC bit is mapped to LOOKUP */
 	if (amode & VEXEC)
-	    code = afs_AccessOK(avc, PRSFS_LOOKUP, &treq, CHECK_MODE_BITS);
+	    code = afs_AccessOK(avc, PRSFS_LOOKUP, treq, CHECK_MODE_BITS);
 	if (code && (amode & VWRITE)) {
-	    code = afs_AccessOK(avc, PRSFS_WRITE, &treq, CHECK_MODE_BITS);
+	    code = afs_AccessOK(avc, PRSFS_WRITE, treq, CHECK_MODE_BITS);
 	    if (code && (vType(avc) == VDIR)) {
 		if (code)
 		    code =
-			afs_AccessOK(avc, PRSFS_INSERT, &treq,
+			afs_AccessOK(avc, PRSFS_INSERT, treq,
 				     CHECK_MODE_BITS);
 		if (!code)
 		    code =
-			afs_AccessOK(avc, PRSFS_DELETE, &treq,
+			afs_AccessOK(avc, PRSFS_DELETE, treq,
 				     CHECK_MODE_BITS);
 	    }
 	}
 	if (code && (amode & VREAD))
-	    code = afs_AccessOK(avc, PRSFS_READ, &treq, CHECK_MODE_BITS);
+	    code = afs_AccessOK(avc, PRSFS_READ, treq, CHECK_MODE_BITS);
     } else {
 	if (vType(avc) == VDIR) {
 	    if (amode & VEXEC)
 		code =
-		    afs_AccessOK(avc, PRSFS_LOOKUP, &treq, CHECK_MODE_BITS);
+		    afs_AccessOK(avc, PRSFS_LOOKUP, treq, CHECK_MODE_BITS);
 	    if (code && (amode & VWRITE)) {
 		code =
-		    afs_AccessOK(avc, PRSFS_INSERT, &treq, CHECK_MODE_BITS);
+		    afs_AccessOK(avc, PRSFS_INSERT, treq, CHECK_MODE_BITS);
 		if (!code)
 		    code =
-			afs_AccessOK(avc, PRSFS_DELETE, &treq,
+			afs_AccessOK(avc, PRSFS_DELETE, treq,
 				     CHECK_MODE_BITS);
 	    }
 	    if (code && (amode & VREAD))
 		code =
-		    afs_AccessOK(avc, PRSFS_LOOKUP, &treq, CHECK_MODE_BITS);
+		    afs_AccessOK(avc, PRSFS_LOOKUP, treq, CHECK_MODE_BITS);
 	} else {
 	    if (amode & VEXEC) {
-		code = afs_AccessOK(avc, PRSFS_READ, &treq, CHECK_MODE_BITS);
+		code = afs_AccessOK(avc, PRSFS_READ, treq, CHECK_MODE_BITS);
 		if (code) {
 			if ((avc->f.m.Mode & 0100) == 0)
 			    code = 0;
@@ -300,7 +307,7 @@ afs_access(OSI_VC_DECL(avc), afs_int32 amode,
 		    code = 1;
 	    }
 	    if (code && (amode & VWRITE)) {
-		code = afs_AccessOK(avc, PRSFS_WRITE, &treq, CHECK_MODE_BITS);
+		code = afs_AccessOK(avc, PRSFS_WRITE, treq, CHECK_MODE_BITS);
 
 		/* The above call fails when the NFS translator tries to copy
 		 ** a file with r--r--r-- permissions into a directory which
@@ -312,11 +319,11 @@ afs_access(OSI_VC_DECL(avc), afs_int32 amode,
 		if (!code && AFS_NFSXLATORREQ(acred)
 		    && avc->f.m.Owner == ANONYMOUSID)
 		    code =
-			afs_AccessOK(avc, PRSFS_WRITE, &treq,
+			afs_AccessOK(avc, PRSFS_WRITE, treq,
 				     DONT_CHECK_MODE_BITS);
 	    }
 	    if (code && (amode & VREAD))
-		code = afs_AccessOK(avc, PRSFS_READ, &treq, CHECK_MODE_BITS);
+		code = afs_AccessOK(avc, PRSFS_READ, treq, CHECK_MODE_BITS);
 	}
     }
     afs_PutFakeStat(&fakestate);
@@ -324,9 +331,11 @@ afs_access(OSI_VC_DECL(avc), afs_int32 amode,
     AFS_DISCON_UNLOCK();
     
     if (code) {
+	afs_DestroyReq(treq);
 	return 0;		/* if access is ok */
     } else {
-	code = afs_CheckCode(EACCES, &treq, 17);	/* failure code */
+	code = afs_CheckCode(EACCES, treq, 17);	/* failure code */
+	afs_DestroyReq(treq);
 	return code;
     }
 }
@@ -341,18 +350,21 @@ afs_getRights(OSI_VC_DECL(avc), afs_int32 arights,
 	      afs_ucred_t *acred)
 {
     afs_int32 code;
-    struct vrequest treq;
+    struct vrequest *treq = NULL;
     OSI_VC_CONVERT(avc);
 
-    if ((code = afs_InitReq(&treq, acred)))
+    if ((code = afs_CreateReq(&treq, acred)))
 	return code;
 
-    code = afs_VerifyVCache(avc, &treq);
+    code = afs_VerifyVCache(avc, treq);
     if (code) {
-	code = afs_CheckCode(code, &treq, 18);
+	code = afs_CheckCode(code, treq, 18);
+	afs_DestroyReq(treq);
 	return code;
     }
 
-    return afs_GetAccessBits(avc, arights, &treq);
+    code = afs_GetAccessBits(avc, arights, treq);
+    afs_DestroyReq(treq);
+    return code;
 }
 #endif /* defined(UKERNEL) */

src/afs/VNOPS/afs_vnop_attrs.c
@@ -194,7 +194,7 @@ afs_getattr(OSI_VC_DECL(avc), struct vattr *attrs, afs_ucred_t *acred)
 #endif
 {
     afs_int32 code;
-    struct vrequest treq;
+    struct vrequest *treq = NULL;
     struct unixuser *au;
     int inited = 0;
     OSI_VC_CONVERT(avc);
@@ -205,19 +205,23 @@ afs_getattr(OSI_VC_DECL(avc), struct vattr *attrs, afs_ucred_t *acred)
 
     if (afs_fakestat_enable && avc->mvstat == 1) {
 	struct afs_fakestat_state fakestat;
+	struct vrequest *ureq = NULL;
 
-	code = afs_InitReq(&treq, acred);
-	if (code)
+	code = afs_CreateReq(&ureq, acred);
+	if (code) {
 	    return code;
+	}
 	afs_InitFakeStat(&fakestat);
-	code = afs_TryEvalFakeStat(&avc, &fakestat, &treq);
+	code = afs_TryEvalFakeStat(&avc, &fakestat, ureq);
 	if (code) {
 	    afs_PutFakeStat(&fakestat);
+	    afs_DestroyReq(ureq);
 	    return code;
 	}
 
 	code = afs_CopyOutAttrs(avc, attrs);
 	afs_PutFakeStat(&fakestat);
+	afs_DestroyReq(ureq);
 	return code;
     }
 #if defined(AFS_SUN5_ENV)
@@ -248,8 +252,8 @@ afs_getattr(OSI_VC_DECL(avc), struct vattr *attrs, afs_ucred_t *acred)
     }
 
     if (!(avc->f.states & CStatd)) {
-	if (!(code = afs_InitReq(&treq, acred))) {
-	    code = afs_VerifyVCache2(avc, &treq);
+	if (!(code = afs_CreateReq(&treq, acred))) {
+	    code = afs_VerifyVCache2(avc, treq);
 	    inited = 1;
 	}
     } else
@@ -270,19 +274,21 @@ afs_getattr(OSI_VC_DECL(avc), struct vattr *attrs, afs_ucred_t *acred)
 
 	if (afs_nfsexporter) {
 	    if (!inited) {
-		if ((code = afs_InitReq(&treq, acred)))
+		if ((code = afs_CreateReq(&treq, acred))) {
 		    return code;
+		}
 		inited = 1;
 	    }
 	    if (AFS_NFSXLATORREQ(acred)) {
 		if ((vType(avc) != VDIR)
-		    && !afs_AccessOK(avc, PRSFS_READ, &treq,
+		    && !afs_AccessOK(avc, PRSFS_READ, treq,
 				     CHECK_MODE_BITS |
 				     CMB_ALLOW_EXEC_AS_READ)) {
+		    afs_DestroyReq(treq);
 		    return EACCES;
 		}
 	    }
-	    if ((au = afs_FindUser(treq.uid, -1, READ_LOCK))) {
+	    if ((au = afs_FindUser(treq->uid, -1, READ_LOCK))) {
 		struct afs_exporter *exporter = au->exporter;
 
 		if (exporter && !(afs_nfsexporter->exp_states & EXP_UNIXMODE)) {
@@ -345,9 +351,12 @@ afs_getattr(OSI_VC_DECL(avc), struct vattr *attrs, afs_ucred_t *acred)
 
     AFS_DISCON_UNLOCK();
 
-    if (!code)
+    if (!code) {
+	afs_DestroyReq(treq);
 	return 0;
-    code = afs_CheckCode(code, &treq, 14);
+    }
+    code = afs_CheckCode(code, treq, 14);
+    afs_DestroyReq(treq);
     return code;
 }
 
@@ -459,7 +468,7 @@ afs_setattr(OSI_VC_DECL(avc), struct vattr *attrs,
 	    afs_ucred_t *acred)
 #endif
 {
-    struct vrequest treq;
+    struct vrequest *treq = NULL;
     struct AFSStoreStatus astat;
     afs_int32 code;
 #if defined(AFS_FBSD_ENV) || defined(AFS_DFBSD_ENV)
@@ -480,13 +489,13 @@ afs_setattr(OSI_VC_DECL(avc), struct vattr *attrs,
 	       ICL_HANDLE_OFFSET(attrs->va_size), ICL_TYPE_OFFSET,
 	       ICL_HANDLE_OFFSET(avc->f.m.Length));
 #endif
-    if ((code = afs_InitReq(&treq, acred)))
+    if ((code = afs_CreateReq(&treq, acred)))
 	return code;
 
     AFS_DISCON_LOCK();
 
     afs_InitFakeStat(&fakestate);
-    code = afs_EvalFakeStat(&avc, &fakestate, &treq);
+    code = afs_EvalFakeStat(&avc, &fakestate, treq);
     if (code)
 	goto done;
 
@@ -518,7 +527,7 @@ afs_setattr(OSI_VC_DECL(avc), struct vattr *attrs,
 #else
     if (attrs->va_size != ~0) {
 #endif
-	if (!afs_AccessOK(avc, PRSFS_WRITE, &treq, DONT_CHECK_MODE_BITS)) {
+	if (!afs_AccessOK(avc, PRSFS_WRITE, treq, DONT_CHECK_MODE_BITS)) {
 	    code = EACCES;
 	    goto done;
 	}
@@ -563,9 +572,9 @@ afs_setattr(OSI_VC_DECL(avc), struct vattr *attrs,
         if (AFS_IS_DISCONNECTED && tsize >=avc->f.m.Length) {
 	    /* If we're growing the file, and we're disconnected, we need
  	     * to make the relevant dcache chunks appear ourselves. */
-	    code = afs_ExtendSegments(avc, tsize, &treq);
+	    code = afs_ExtendSegments(avc, tsize, treq);
 	} else {
-	    code = afs_TruncateAllSegments(avc, tsize, &treq, acred);
+	    code = afs_TruncateAllSegments(avc, tsize, treq, acred);
 	}
 #ifdef AFS_LINUX26_ENV
 	/* We must update the Linux kernel's idea of file size as soon as
@@ -591,7 +600,7 @@ afs_setattr(OSI_VC_DECL(avc), struct vattr *attrs,
 		/* Store files now if not disconnected. */
 		/* XXX: AFS_IS_DISCON_RW handled. */
 		if (!AFS_IS_DISCONNECTED) {
-			code = afs_StoreAllSegments(avc, &treq, AFS_ASYNC);
+			code = afs_StoreAllSegments(avc, treq, AFS_ASYNC);
 			if (!code)
 		    		avc->f.states &= ~CDirty;
 		}
@@ -607,7 +616,7 @@ afs_setattr(OSI_VC_DECL(avc), struct vattr *attrs,
     if (!AFS_IS_DISCONNECTED) {
         if (code == 0) {
 	    ObtainSharedLock(&avc->lock, 16);	/* lock entry */
-	    code = afs_WriteVCache(avc, &astat, &treq);	/* send request */
+	    code = afs_WriteVCache(avc, &astat, treq);	/* send request */
 	    ReleaseSharedLock(&avc->lock);	/* release lock */
         }
         if (code) {
@@ -641,6 +650,7 @@ afs_setattr(OSI_VC_DECL(avc), struct vattr *attrs,
     afs_PutFakeStat(&fakestate);
 
     AFS_DISCON_UNLOCK();
-    code = afs_CheckCode(code, &treq, 15);
+    code = afs_CheckCode(code, treq, 15);
+    afs_DestroyReq(treq);
     return code;
 }

src/afs/VNOPS/afs_vnop_create.c
@@ -41,7 +41,7 @@ afs_create(OSI_VC_DECL(adp), char *aname, struct vattr *attrs,
 #endif				/* AFS_SGI64_ENV */
 {
     afs_int32 origCBs, origZaps, finalZaps;
-    struct vrequest treq;
+    struct vrequest *treq = NULL;
     afs_int32 code;
     struct afs_conn *tc;
     struct VenusFid newFid;
@@ -65,7 +65,7 @@ afs_create(OSI_VC_DECL(adp), char *aname, struct vattr *attrs,
     OutFidStatus = osi_AllocSmallSpace(AFS_SMALLOCSIZ);
     OutDirStatus = osi_AllocSmallSpace(AFS_SMALLOCSIZ);
 
-    if ((code = afs_InitReq(&treq, acred)))
+    if ((code = afs_CreateReq(&treq, acred)))
 	goto done2;
 
     afs_Trace3(afs_iclSetp, CM_TRACE_CREATE, ICL_TYPE_POINTER, adp,
@@ -108,11 +108,11 @@ afs_create(OSI_VC_DECL(adp), char *aname, struct vattr *attrs,
     }
     AFS_DISCON_LOCK();
 
-    code = afs_EvalFakeStat(&adp, &fakestate, &treq);
+    code = afs_EvalFakeStat(&adp, &fakestate, treq);
     if (code)
 	goto done;
   tagain:
-    code = afs_VerifyVCache(adp, &treq);
+    code = afs_VerifyVCache(adp, treq);
     if (code)
 	goto done;
 
@@ -129,7 +129,7 @@ afs_create(OSI_VC_DECL(adp), char *aname, struct vattr *attrs,
         goto done;
     }
 
-    tdc = afs_GetDCache(adp, (afs_size_t) 0, &treq, &offset, &len, 1);
+    tdc = afs_GetDCache(adp, (afs_size_t) 0, treq, &offset, &len, 1);
     ObtainWriteLock(&adp->lock, 135);
     if (tdc)
 	ObtainSharedLock(&tdc->lock, 630);
@@ -168,10 +168,10 @@ afs_create(OSI_VC_DECL(adp), char *aname, struct vattr *attrs,
 	    newFid.Fid.Volume = adp->f.fid.Fid.Volume;
 	    tvc = NULL;
 	    if (newFid.Fid.Unique == 0) {
-		tvc = afs_LookupVCache(&newFid, &treq, NULL, adp, aname);
+		tvc = afs_LookupVCache(&newFid, treq, NULL, adp, aname);
 	    }
 	    if (!tvc)		/* lookup failed or wasn't called */
-		tvc = afs_GetVCache(&newFid, &treq, NULL, NULL);
+		tvc = afs_GetVCache(&newFid, treq, NULL, NULL);
 
 	    if (tvc) {
 		/* if the thing exists, we need the right access to open it.
@@ -184,7 +184,7 @@ afs_create(OSI_VC_DECL(adp), char *aname, struct vattr *attrs,
 		 * has mode -w-w-w, which is wrong.
 		 */
 		if ((amode & VREAD)
-		    && !afs_AccessOK(tvc, PRSFS_READ, &treq, CHECK_MODE_BITS)) {
+		    && !afs_AccessOK(tvc, PRSFS_READ, treq, CHECK_MODE_BITS)) {
 		    afs_PutVCache(tvc);
 		    code = EACCES;
 		    goto done;
@@ -202,7 +202,7 @@ afs_create(OSI_VC_DECL(adp), char *aname, struct vattr *attrs,
 		    tvc->f.parent.unique = adp->f.fid.Fid.Unique;
 		    /* need write mode for these guys */
 		    if (!afs_AccessOK
-			(tvc, PRSFS_WRITE, &treq, CHECK_MODE_BITS)) {
+			(tvc, PRSFS_WRITE, treq, CHECK_MODE_BITS)) {
 			afs_PutVCache(tvc);
 			code = EACCES;
 			goto done;
@@ -301,7 +301,7 @@ afs_create(OSI_VC_DECL(adp), char *aname, struct vattr *attrs,
 
     	InStatus.UnixModeBits = attrs->va_mode & 0xffff;	/* only care about protection bits */
     	do {
-	    tc = afs_Conn(&adp->f.fid, &treq, SHARED_LOCK, &rxconn);
+	    tc = afs_Conn(&adp->f.fid, treq, SHARED_LOCK, &rxconn);
 	    if (tc) {
 	    	hostp = tc->parent->srvr->server; /* remember for callback processing */
 	    	now = osi_Time();
@@ -318,7 +318,7 @@ afs_create(OSI_VC_DECL(adp), char *aname, struct vattr *attrs,
 	    } else
 	    	code = -1;
     	} while (afs_Analyze
-	         (tc, rxconn, code, &adp->f.fid, &treq, AFS_STATS_FS_RPCIDX_CREATEFILE,
+	         (tc, rxconn, code, &adp->f.fid, treq, AFS_STATS_FS_RPCIDX_CREATEFILE,
 	          SHARED_LOCK, NULL));
 
 	if ((code == EEXIST || code == UAEEXIST) &&
@@ -455,9 +455,9 @@ afs_create(OSI_VC_DECL(adp), char *aname, struct vattr *attrs,
 	    ReleaseWriteLock(&afs_xcbhash);
 	    if (AFS_IS_DISCON_RW) {
 		afs_DisconAddDirty(tvc, VDisconCreate, 0);
-		afs_GenDisconStatus(adp, tvc, &newFid, attrs, &treq, VREG);
+		afs_GenDisconStatus(adp, tvc, &newFid, attrs, treq, VREG);
 	    } else {
-		afs_ProcessFS(tvc, OutFidStatus, &treq);
+		afs_ProcessFS(tvc, OutFidStatus, treq);
 	    }
 
 	    tvc->f.parent.vnode = adp->f.fid.Fid.Vnode;
@@ -498,7 +498,8 @@ afs_create(OSI_VC_DECL(adp), char *aname, struct vattr *attrs,
     }
 
     afs_PutFakeStat(&fakestate);
-    code = afs_CheckCode(code, &treq, 20);
+    code = afs_CheckCode(code, treq, 20);
+    afs_DestroyReq(treq);
 
   done2:
     osi_FreeSmallSpace(OutFidStatus);

commit 51c97beb3f3bb68704d33e126561b8c2866ddab3

libafs: fix vrequest leak in afs_lookup
Fix vrequest leak introduced in commit
9930567bcf9655d3f562b210b2dc4b4a99226691.

Thanks to Andrew Deason for finding this error.

Change-Id: I8fc1391ab43f33c5a8208ff58d1d0641292cf63e
Reviewed-on: http://gerrit.openafs.org/11337
Tested-by: BuildBot <buildbot@rampaginggeek.com>
Reviewed-by: Benjamin Kaduk <kaduk@mit.edu>
Reviewed-by: Chas Williams - CONTRACTOR <chas@cmf.nrl.navy.mil>
Reviewed-by: Andrew Deason <adeason@sinenomine.net>
Reviewed-by: Jeffrey Altman <jaltman@your-file-system.com>

src/afs/VNOPS/afs_vnop_lookup.c
@@ -1932,6 +1932,7 @@ afs_lookup(OSI_VC_DECL(adp), char *aname, struct vcache **avcp, afs_ucred_t *acr
 		code = afs_VerifyVCache(tvc, treq);
 #else
 		afs_PutFakeStat(&fakestate);
+		afs_DestroyReq(treq);
 		AFS_DISCON_UNLOCK();
 		return 0;	/* can't have been any errors if hit and !code */
 #endif