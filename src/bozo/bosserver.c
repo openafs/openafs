openafs/openafs

commit 720363fa9bf7cfbebdc485104b74ca6bac1895f6

Fix unchecked return values
This change fixes numerous places where the return values of various
system calls and standard library routines are not checked.  In
particular, this fixes occurrances called out when building on Ubuntu
12.10, with gcc 4.7.2 and eglibc 2.15-0ubuntu20.1, when the possible
failure is one we actually do (or should) care about.  This change
does not consider calls where the failure is one we deliberately
choose to ignore.

Change-Id: Id526f5dd7ee48be2604b77a3f00ea1e51b08c21d
Reviewed-on: http://gerrit.openafs.org/9979
Tested-by: BuildBot <buildbot@rampaginggeek.com>
Reviewed-by: Jeffrey Altman <jaltman@your-file-system.com>

src/bozo/bosserver.c
@@ -253,32 +253,46 @@ CreateDirs(const char *coredir)
 	(!strncmp
 	 (AFSDIR_USR_DIRPATH, AFSDIR_SERVER_BIN_DIRPATH,
 	  strlen(AFSDIR_USR_DIRPATH)))) {
-	MakeDir(AFSDIR_USR_DIRPATH);
+	if (MakeDir(AFSDIR_USR_DIRPATH))
+	    return errno;
     }
     if (!strncmp
 	(AFSDIR_SERVER_AFS_DIRPATH, AFSDIR_SERVER_BIN_DIRPATH,
 	 strlen(AFSDIR_SERVER_AFS_DIRPATH))) {
-	MakeDir(AFSDIR_SERVER_AFS_DIRPATH);
+	if (MakeDir(AFSDIR_SERVER_AFS_DIRPATH))
+	    return errno;
     }
-    MakeDir(AFSDIR_SERVER_BIN_DIRPATH);
-    MakeDir(AFSDIR_SERVER_ETC_DIRPATH);
-    MakeDir(AFSDIR_SERVER_LOCAL_DIRPATH);
-    MakeDir(AFSDIR_SERVER_DB_DIRPATH);
-    MakeDir(AFSDIR_SERVER_LOGS_DIRPATH);
+    if (MakeDir(AFSDIR_SERVER_BIN_DIRPATH))
+	return errno;
+    if (MakeDir(AFSDIR_SERVER_ETC_DIRPATH))
+	return errno;
+    if (MakeDir(AFSDIR_SERVER_LOCAL_DIRPATH))
+	return errno;
+    if (MakeDir(AFSDIR_SERVER_DB_DIRPATH))
+	return errno;
+    if (MakeDir(AFSDIR_SERVER_LOGS_DIRPATH))
+	return errno;
 #ifndef AFS_NT40_ENV
     if (!strncmp
 	(AFSDIR_CLIENT_VICE_DIRPATH, AFSDIR_CLIENT_ETC_DIRPATH,
 	 strlen(AFSDIR_CLIENT_VICE_DIRPATH))) {
-	MakeDir(AFSDIR_CLIENT_VICE_DIRPATH);
+	if (MakeDir(AFSDIR_CLIENT_VICE_DIRPATH))
+	    return errno;
     }
-    MakeDir(AFSDIR_CLIENT_ETC_DIRPATH);
+    if (MakeDir(AFSDIR_CLIENT_ETC_DIRPATH))
+	return errno;
 
-    symlink(AFSDIR_SERVER_THISCELL_FILEPATH, AFSDIR_CLIENT_THISCELL_FILEPATH);
-    symlink(AFSDIR_SERVER_CELLSERVDB_FILEPATH,
-	    AFSDIR_CLIENT_CELLSERVDB_FILEPATH);
+    if (symlink(AFSDIR_SERVER_THISCELL_FILEPATH,
+	    AFSDIR_CLIENT_THISCELL_FILEPATH))
+	return errno;
+    if (symlink(AFSDIR_SERVER_CELLSERVDB_FILEPATH,
+	    AFSDIR_CLIENT_CELLSERVDB_FILEPATH))
+	return errno;
 #endif /* AFS_NT40_ENV */
-    if (coredir)
-	MakeDir(coredir);
+    if (coredir) {
+	if (MakeDir(coredir))
+	    return errno;
+    }
     return 0;
 }
 
@@ -975,21 +989,32 @@ main(int argc, char **argv, char **envp)
      */
 
 #ifndef AFS_NT40_ENV
-    if (!nofork)
-	daemon(1, 0);
+    if (!nofork) {
+	if (daemon(1, 0))
+	    printf("bosserver: warning - daemon() returned code %d\n", errno);
+    }
 #endif /* ! AFS_NT40_ENV */
 
     /* create useful dirs */
-    CreateDirs(DoCore);
+    i = CreateDirs(DoCore);
+    if (i) {
+	printf("bosserver: could not set up directories, code %d\n", i);
+	exit(1);
+    }
 
     /* Write current state of directory permissions to log file */
     DirAccessOK();
 
     /* chdir to AFS log directory */
     if (DoCore)
-	chdir(DoCore);
+	i = chdir(DoCore);
     else
-	chdir(AFSDIR_SERVER_LOGS_DIRPATH);
+	i = chdir(AFSDIR_SERVER_LOGS_DIRPATH);
+    if (i) {
+	printf("bosserver: could not change to %s, code %d\n",
+	       DoCore ? DoCore : AFSDIR_SERVER_LOGS_DIRPATH, errno);
+	exit(1);
+    }
 
     /* try to read the key from the config file */
     tdir = afsconf_Open(AFSDIR_SERVER_ETC_DIRPATH);

src/auth/cellconfig.c
@@ -453,7 +453,7 @@ afsconf_Open(const char *adir)
 	    /* The "AFSCONF" environment (or contents of "/.AFSCONF") will be typically set to something like "/afs/<cell>/common/etc" where, by convention, the default files for "ThisCell" and "CellServDB" will reside; note that a major drawback is that a given afs client on that cell may NOT contain the same contents... */
 	    char *home_dir;
 	    afsconf_FILE *fp;
-	    size_t len;
+	    size_t len = 0;
 	    int r;
 
 	    if (!(home_dir = getenv("HOME"))) {
@@ -462,8 +462,6 @@ afsconf_Open(const char *adir)
 		if (fp == 0)
 		    goto fail;
 
-		fgets(afs_confdir, 128, fp);
-		fclose(fp);
 	    } else {
 		char *pathname = NULL;
 
@@ -480,10 +478,10 @@ afsconf_Open(const char *adir)
 		    if (fp == 0)
 			goto fail;
 		}
-		fgets(afs_confdir, 128, fp);
-		fclose(fp);
 	    }
-	    len = strlen(afs_confdir);
+	    if (fgets(afs_confdir, 128, fp) != NULL)
+		len = strlen(afs_confdir);
+	    fclose(fp);
 	    if (len == 0)
 		goto fail;
 

src/budb/db_text.c
@@ -457,22 +457,28 @@ saveTextToFile(struct ubik_trans *ut, struct textBlock *tbPtr)
     afs_int32 blockAddr;
     struct block block;
     char filename[128];
-    afs_int32 size, chunkSize;
+    afs_int32 size, totalSize, chunkSize;
     int fid;
 
     sprintf(filename, "%s/%s", gettmpdir(), "dbg_XXXXXX");
 
     fid = mkstemp(filename);
-    size = ntohl(tbPtr->size);
+    totalSize = size = ntohl(tbPtr->size);
     blockAddr = ntohl(tbPtr->textAddr);
     while (size) {
 	chunkSize = min(BLOCK_DATA_SIZE, size);
 	dbread(ut, blockAddr, (char *)&block, sizeof(block));
-	write(fid, &block.a[0], chunkSize);
+	if (write(fid, &block.a[0], chunkSize) < 0)
+	    break;
 	blockAddr = ntohl(block.h.next);
 	size -= chunkSize;
     }
     close(fid);
-    printf("wrote debug file %s\n", filename);
+    if (size) {
+	printf("Wrote partial debug file (%ld bytes out of %ld)\n",
+	       (long)(totalSize - size), (long)totalSize);
+    } else {
+	printf("wrote debug file %s\n", filename);
+    }
 }
 

src/kauth/kkids.c
@@ -387,8 +387,10 @@ init_child(char *myname)
      * reads from the latter, the child reads from the former, and
      * writes to the latter.
      */
-    pipe(pipe1);
-    pipe(pipe2);
+    if (pipe(pipe1) || pipe(pipe2)) {
+	using_child = 0;
+	return 0;
+    }
 
     /* fork a child */
     pid = fork();
@@ -429,7 +431,8 @@ password_bad(char *pw)
     if (using_child) {
 	fprintf(childin, "%s\n", pw);
 	fflush(childin);
-	fscanf(childout, "%d", &rc);
+	if (fscanf(childout, "%d", &rc) < 1)
+	    rc = -1;
     }
 
     return (rc);

src/lwp/lwp.c
@@ -979,11 +979,15 @@ Overflow_Complain(void)
     currenttime = time(0);
     timeStamp = ctime(&currenttime);
     timeStamp[24] = 0;
-    write(2, timeStamp, strlen(timeStamp));
+    if (write(2, timeStamp, strlen(timeStamp)) < 0)
+	return;
 
-    write(2, msg1, strlen(msg1));
-    write(2, lwp_cpptr->name, strlen(lwp_cpptr->name));
-    write(2, msg2, strlen(msg2));
+    if (write(2, msg1, strlen(msg1)) < 0)
+	return;
+    if (write(2, lwp_cpptr->name, strlen(lwp_cpptr->name)) < 0)
+	return;
+    if (write(2, msg2, strlen(msg2)) < 0)
+	return;
 }
 
 static void

src/ptserver/pt_util.c
@@ -394,7 +394,11 @@ static int
 display_entry(int offset)
 {
     lseek(dbase_fd, offset + HDRSIZE, L_SET);
-    read(dbase_fd, &pre, sizeof(struct prentry));
+    if (read(dbase_fd, &pre, sizeof(struct prentry)) < 0) {
+	fprintf(stderr, "pt_util: error reading entry %d: %s\n",
+		offset, strerror(errno));
+	exit(1);
+    }
 
     fix_pre(&pre);
 
@@ -493,7 +497,11 @@ display_group(int id)
 	offset = pre.next;
 	while (offset) {
 	    lseek(dbase_fd, offset + HDRSIZE, L_SET);
-	    read(dbase_fd, &prco, sizeof(struct contentry));
+	    if (read(dbase_fd, &prco, sizeof(struct contentry)) < 0) {
+		fprintf(stderr, "pt_util: read i/o error: %s\n",
+			strerror(errno));
+		exit(1);
+	    }
 	    prco.next = ntohl(prco.next);
 	    for (i = 0; i < COSIZE; i++) {
 		prco.entries[i] = ntohl(prco.entries[i]);

src/ptserver/ptubik.c
@@ -31,6 +31,7 @@ ubik_BeginTrans(struct ubik_dbase *dbase, afs_int32 transMode,
 {
     static int init = 0;
     struct ubik_hdr thdr;
+    ssize_t count;
 
     if (!init) {
 	memset(&thdr, 0, sizeof(thdr));
@@ -38,9 +39,15 @@ ubik_BeginTrans(struct ubik_dbase *dbase, afs_int32 transMode,
 	thdr.version.counter = htonl(0);
 	thdr.magic = htonl(UBIK_MAGIC);
 	thdr.size = htons(HDRSIZE);
-	lseek(dbase_fd, 0, 0);
-	write(dbase_fd, &thdr, sizeof(thdr));
-	fsync(dbase_fd);
+	if (lseek(dbase_fd, 0, 0) == (off_t)-1)
+	    return errno;
+	count = write(dbase_fd, &thdr, sizeof(thdr));
+	if (count < 0)
+	    return errno;
+	else if (count != sizeof(thdr))
+	    return UIOERROR;
+	if (fsync(dbase_fd))
+	    return errno;
 	init = 1;
     }
     return (0);

src/sys/rmtsysc.c
@@ -57,7 +57,7 @@ GetAfsServerAddr(char *syscall)
     if (!(afs_server = getenv("AFSSERVER"))) {
 	char *home_dir;
 	FILE *fp;
-	int len;
+	int len = 0;
 
 	if (!(home_dir = getenv("HOME"))) {
 	    /* Our last chance is the "/.AFSSERVER" file */
@@ -65,8 +65,6 @@ GetAfsServerAddr(char *syscall)
 	    if (fp == 0) {
 		return 0;
 	    }
-	    fgets(server_name, 128, fp);
-	    fclose(fp);
 	} else {
 	    char *pathname;
 
@@ -83,10 +81,10 @@ GetAfsServerAddr(char *syscall)
 		    return 0;
 		}
 	    }
-	    fgets(server_name, 128, fp);
-	    fclose(fp);
 	}
-	len = strlen(server_name);
+	if (fgets(server_name, 128, fp) != NULL)
+	    len = strlen(server_name);
+	fclose(fp);
 	if (len == 0) {
 	    return 0;
 	}

commit 381c7afbe60a0e6d53e8cc1bca2de920574b3383

bozo: do not exit when the client config already exists
The bosserver creates symlinks for the client CSDB and ThisCell config
files during initialization.  Avoid exiting if the client CSDB or
ThisCell configuration already exists, otherwise the bosserver cannot be
restarted with bos restart.

This fixes an error introduced with commit
720363fa9bf7cfbebdc485104b74ca6bac1895f6, Fix unchecked return values.

Change-Id: Ie6ecf126d1ed663f161c26da2a8c4d568369d99d
Reviewed-on: http://gerrit.openafs.org/11684
Tested-by: BuildBot <buildbot@rampaginggeek.com>
Reviewed-by: Perry Ruiter <pruiter@sinenomine.net>
Reviewed-by: Benjamin Kaduk <kaduk@mit.edu>

src/bozo/bosserver.c
@@ -283,11 +283,17 @@ CreateDirs(const char *coredir)
 	return errno;
 
     if (symlink(AFSDIR_SERVER_THISCELL_FILEPATH,
-	    AFSDIR_CLIENT_THISCELL_FILEPATH))
-	return errno;
+	    AFSDIR_CLIENT_THISCELL_FILEPATH)) {
+	if (errno != EEXIST) {
+	    return errno;
+	}
+    }
     if (symlink(AFSDIR_SERVER_CELLSERVDB_FILEPATH,
-	    AFSDIR_CLIENT_CELLSERVDB_FILEPATH))
-	return errno;
+	    AFSDIR_CLIENT_CELLSERVDB_FILEPATH)) {
+	if (errno != EEXIST) {
+	    return errno;
+	}
+    }
 #endif /* AFS_NT40_ENV */
     if (coredir) {
 	if (MakeDir(coredir))