4f7550dcaf9375046514cdd97cea0f667e955e9f
Andrew Deason <adeason@sinenomine.net>
2015-03-07 17:27:47 -0600

    Add support for arm64_linux26
    Add support for the arm64/aarch64 architecture on Linux 2.6+. The
    param header file is mostly combined from arm and amd64.
    
    Note that the code for syscall interception has not been updated for
    arm64, so this will not build on arm64 without support for kernel
    keyrings. This also does not define any AFS syscall number, since no
    number in the Linux arm64 syscall table is "free" for us to use, as
    far as I am aware.
    
    Adapted from initial patches from Micheal Waltz <mwaltz@qualcomm.com>.
    
    Change-Id: I1ee239ded17d8fea3b91b70405215aa1b3f7a6e9
    Reviewed-on: https://gerrit.openafs.org/11940
    Reviewed-by: Benjamin Kaduk <kaduk@mit.edu>
    Tested-by: BuildBot <buildbot@rampaginggeek.com>

--- src/afs/sysincludes.h
+++ src/afs/sysincludes.h
@@ -122,6 +122,7 @@
 # include <linux/kdev_t.h>
 # include <linux/ioctl.h>
 # if defined(AFS_LINUX26_ENV)
+#  include <linux/compat.h>
 #  include <linux/backing-dev.h>
 #  include <linux/pagemap.h>
 #  include <linux/namei.h>
