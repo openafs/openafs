dda47aab6179b6940aa994a0cd7b88a4b0942fe6
Benjamin Kaduk <kaduk@mit.edu>
2016-07-04 20:13:31 -0500

    Add sysname IDs for FreeBSD 10.2 and 10.3
    While here, de-conflict the numbers for 10.0/10.1 and 7.2/7.3
    
    Change-Id: I87697587359a26258298f4710c7232bea417f807
    Reviewed-on: https://gerrit.openafs.org/12321
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Mark Vitale <mvitale@sinenomine.net>
    Reviewed-by: Benjamin Kaduk <kaduk@mit.edu>

--- src/config/afs_sysnames.h
+++ src/config/afs_sysnames.h
@@ -221,8 +221,10 @@
 #define SYS_NAME_ID_i386_fbsd_91        2122
 #define SYS_NAME_ID_i386_fbsd_92        2124
 #define SYS_NAME_ID_i386_fbsd_93        2126
-#define SYS_NAME_ID_i386_fbsd_100      2130
-#define SYS_NAME_ID_i386_fbsd_101      2131
+#define SYS_NAME_ID_i386_fbsd_100      2150
+#define SYS_NAME_ID_i386_fbsd_101      2151
+#define SYS_NAME_ID_i386_fbsd_102      2152
+#define SYS_NAME_ID_i386_fbsd_103      2153
 #define SYS_NAME_ID_i386_fbsd_110      2140
 
 #define SYS_NAME_ID_ia64_linux2		2200
@@ -329,8 +331,10 @@
 #define SYS_NAME_ID_amd64_fbsd_91       3022
 #define SYS_NAME_ID_amd64_fbsd_92       3023
 #define SYS_NAME_ID_amd64_fbsd_93       3024
-#define SYS_NAME_ID_amd64_fbsd_100     3030
-#define SYS_NAME_ID_amd64_fbsd_101     3031
+#define SYS_NAME_ID_amd64_fbsd_100     3050
+#define SYS_NAME_ID_amd64_fbsd_101     3051
+#define SYS_NAME_ID_amd64_fbsd_102     3052
+#define SYS_NAME_ID_amd64_fbsd_103     3053
 #define SYS_NAME_ID_amd64_fbsd_110     3040
 
 #define SYS_NAME_ID_amd64_w2k           3400
