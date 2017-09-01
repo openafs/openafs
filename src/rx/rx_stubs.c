20b0f5b4d0b55e79e55442978c297663a5e18b76
Benjamin Kaduk <kaduk@mit.edu>
2017-09-01 17:45:10 -0500

    Add rxgk_GetServerInfo stub
    Provide a stub function that libafsrpc can export when rxgk support
    is disabled.  (It always returns failure, of course.)
    
    Change-Id: Id9f816d25c1a8f56995ec185ae83db0924de0010
    Reviewed-on: https://gerrit.openafs.org/12721
    Reviewed-by: Michael Meffie <mmeffie@sinenomine.net>
    Reviewed-by: Benjamin Kaduk <kaduk@mit.edu>
    Tested-by: BuildBot <buildbot@rampaginggeek.com>

---
+++ src/rx/rx_stubs.c
@@ -0,0 +1,59 @@
+/* rx/rx_stubs.c - Stub functions for things not really implemented. */
+/*
+ * Copyright (C) 2017 by Benjamin Kaduk.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in
+ *   the documentation and/or other materials provided with the
+ *   distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * @file
+ * Stub functions that don't have anyplace better to live.
+ * For example, libafsrpc is going to export an rxgk_GetServerInfo
+ * routine whether or not rxgk support is enabled at configure time.
+ * But src/rxgk is not compiled at all in that case, so we need a home
+ * for the stub function that will be exported in that case.
+ */
+
+#include <afsconfig.h>
+#include <afs/param.h>
+#include <afs/stds.h>
+
+#ifndef ENABLE_RXGK
+
+#include <rx/rx.h>
+#include <rx/rxgk.h>
+#include <rx/rx_identity.h>
+
+#include <errno.h>
+
+afs_int32
+rxgk_GetServerInfo(struct rx_connection *conn, RXGK_Level *level,
+		   rxgkTime *expiry, struct rx_identity **identity)
+{
+    return EINVAL;
+}
+#endif	/* !ENABLE_RXGK */
