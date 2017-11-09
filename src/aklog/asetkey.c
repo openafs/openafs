438bc2c4caf96485e12eda9e7591fc4bd887ebad
Andrew Deason <adeason@sinenomine.net>
2017-11-09 12:47:57 -0600

    asetkey: Add new 'delete' command variants
    The current 'delete' command from asetkey only lets the user delete
    old-style rxkad keys. Add a couple of new variants to allow specifying
    the key type and subtype, so the user can delete specific key types
    and enctypes if they want.
    
    Reviewed-on: https://gerrit.openafs.org/12767
    Reviewed-by: Benjamin Kaduk <kaduk@mit.edu>
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    (cherry picked from commit 5120409cc998284f2fb0467c2f88030976140341)
    
    Change-Id: I8c762839b50f4faf5e583fb5c510bf2ff9dd2259
    Reviewed-on: https://gerrit.openafs.org/14293
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Cheyenne Wills <cwills@sinenomine.net>
    Reviewed-by: Stephan Wiesand <stephan.wiesand@desy.de>

--- src/aklog/asetkey.c
+++ src/aklog/asetkey.c
@@ -239,18 +239,52 @@ addKey(struct afsconf_dir *dir, int argc, char **argv) {
 static void
 deleteKey(struct afsconf_dir *dir, int argc, char **argv)
 {
+    int type;
+    int subtype;
     int kvno;
     int code;
 
-    if (argc != 3) {
+    switch (argc) {
+    case 3:
+	kvno = atoi(argv[2]);
+	code = afsconf_DeleteKey(dir, kvno);
+	if (code) {
+	    afs_com_err(argv[0], code, "while deleting key %d", kvno);
+	    exit(1);
+	}
+	printf("Deleted rxkad key %d\n", kvno);
+	break;
+
+    case 4:
+	type = stringToType(argv[2]);
+	kvno = atoi(argv[3]);
+	code = afsconf_DeleteKeyByType(dir, type, kvno);
+	if (code) {
+	    afs_com_err(argv[0], code, "while deleting key (type %d kvno %d)",
+			type, kvno);
+	    exit(1);
+	}
+	printf("Deleted key (type %d kvno %d)\n", type, kvno);
+	break;
+
+    case 5:
+	type = stringToType(argv[2]);
+	kvno = atoi(argv[3]);
+	subtype = atoi(argv[4]);
+	code = afsconf_DeleteKeyBySubType(dir, type, kvno, subtype);
+	if (code) {
+	    afs_com_err(argv[0], code, "while deleting key (type %d kvno %d subtype %d)\n",
+			type, kvno, subtype);
+	    exit(1);
+	}
+	printf("Deleted key (type %d kvno %d subtype %d)\n", type, kvno, subtype);
+	break;
+
+    default:
 	fprintf(stderr, "%s delete: usage is '%s delete <kvno>\n",
 		argv[0], argv[0]);
-	exit(1);
-    }
-    kvno = atoi(argv[2]);
-    code = afsconf_DeleteKey(dir, kvno);
-    if (code) {
-	afs_com_err(argv[0], code, "while deleting key %d", kvno);
+	fprintf(stderr, "\tOR\n\t%s delete <type> <kvno>\n", argv[0]);
+	fprintf(stderr, "\tOR\n\t%s delete <type> <kvno> <subtype>\n", argv[0]);
 	exit(1);
     }
 }
@@ -316,6 +350,8 @@ main(int argc, char *argv[])
 	        argv[0]);
 	fprintf(stderr, "\t\tEx: %s add 0 \"80b6a7cd7a9dadb6\"\n", argv[0]);
 	fprintf(stderr, "\t%s delete <kvno>\n", argv[0]);
+	fprintf(stderr, "\t%s delete <type> <kvno>\n", argv[0]);
+	fprintf(stderr, "\t%s delete <type> <kvno> <subtype>\n", argv[0]);
 	fprintf(stderr, "\t%s list\n", argv[0]);
 	exit(1);
     }
