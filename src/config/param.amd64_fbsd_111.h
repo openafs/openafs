openafs/openafs

commit 88dc4d93f5ef080da8f56fac453f095e6c79d4a0

Add param.h files for recent FreeBSD
Add files for FreeBSD 10.4, 11.1, and 12.0 (12-CURRENT), for i386 and amd64.

Change-Id: I904f576914bb965a659750e6302f011acf66ba81
Reviewed-on: https://gerrit.openafs.org/12863
Tested-by: BuildBot <buildbot@rampaginggeek.com>
Reviewed-by: Stephan Wiesand <stephan.wiesand@desy.de>
Reviewed-by: Benjamin Kaduk <kaduk@mit.edu>

src/config/param.amd64_fbsd_111.h
@@ -1,0 +1,88 @@
+#ifndef	AFS_PARAM_H
+#define	AFS_PARAM_H
+
+/* Machine / Operating system information */
+#define SYS_NAME	"amd64_fbsd_111"
+#define SYS_NAME_ID	SYS_NAME_ID_amd64_fbsd_111
+
+#define AFS_64BITPOINTER_ENV 1
+
+#ifndef UKERNEL
+/* This section for kernel libafs compiles only */
+
+#define AFS_64BITUSERPOINTER_ENV 1
+
+#define AFS_FBSD50_ENV 1
+#define AFS_FBSD51_ENV 1
+#define AFS_FBSD52_ENV 1
+#define AFS_FBSD53_ENV 1
+#define AFS_FBSD60_ENV 1
+#define AFS_FBSD61_ENV 1
+#define AFS_FBSD62_ENV 1
+#define AFS_FBSD70_ENV 1
+#define AFS_FBSD71_ENV 1
+#define AFS_FBSD72_ENV 1
+#define AFS_FBSD73_ENV 1
+#define AFS_FBSD80_ENV 1
+#define AFS_FBSD81_ENV 1
+#define AFS_FBSD82_ENV 1
+#define AFS_FBSD83_ENV 1
+#define AFS_FBSD84_ENV 1
+#define AFS_FBSD90_ENV 1
+#define AFS_FBSD91_ENV 1
+#define AFS_FBSD92_ENV 1
+#define AFS_FBSD93_ENV 1
+#define AFS_FBSD100_ENV 1
+#define AFS_FBSD110_ENV 1
+#define AFS_FBSD111_ENV 1
+
+#define AFS_X86_FBSD50_ENV 1
+#define AFS_X86_FBSD60_ENV 1 /* added at 70--ie, some changes should port <-- */
+#define AFS_X86_FBSD62_ENV 1
+#define AFS_X86_FBSD70_ENV 1
+#define AFS_X86_FBSD71_ENV 1
+#define AFS_X86_FBSD72_ENV 1
+#define AFS_X86_FBSD73_ENV 1
+#define AFS_X86_FBSD80_ENV 1
+#define AFS_X86_FBSD81_ENV 1
+#define AFS_X86_FBSD82_ENV 1
+#define AFS_X86_FBSD83_ENV 1
+#define AFS_X86_FBSD84_ENV 1
+#define AFS_X86_FBSD90_ENV 1
+#define AFS_X86_FBSD91_ENV 1
+#define AFS_X86_FBSD92_ENV 1
+#define AFS_X86_FBSD93_ENV 1
+#define AFS_X86_FBSD100_ENV 1
+#define AFS_X86_FBSD110_ENV 1
+#define AFS_X86_FBSD111_ENV 1
+
+#else /* !defined(UKERNEL) */
+
+/* This section for user space compiles only */
+
+#define AFS_USR_FBSD50_ENV 1
+#define AFS_USR_FBSD51_ENV 1
+#define AFS_USR_FBSD52_ENV 1
+#define AFS_USR_FBSD53_ENV 1
+#define AFS_USR_FBSD60_ENV 1
+#define AFS_USR_FBSD61_ENV 1
+#define AFS_USR_FBSD70_ENV 1
+#define AFS_USR_FBSD71_ENV 1
+#define AFS_USR_FBSD72_ENV 1
+#define AFS_USR_FBSD73_ENV 1
+#define AFS_USR_FBSD80_ENV 1
+#define AFS_USR_FBSD81_ENV 1
+#define AFS_USR_FBSD82_ENV 1
+#define AFS_USR_FBSD83_ENV 1
+#define AFS_USR_FBSD84_ENV 1
+#define AFS_USR_FBSD90_ENV 1
+#define AFS_USR_FBSD91_ENV 1
+#define AFS_USR_FBSD92_ENV 1
+#define AFS_USR_FBSD93_ENV 1
+#define AFS_USR_FBSD100_ENV 1
+#define AFS_USR_FBSD110_ENV 1
+#define AFS_USR_FBSD111_ENV 1
+
+#endif /* !defined(UKERNEL) */
+
+#endif /* AFS_PARAM_H */

src/config/param.amd64_fbsd_120.h
@@ -1,0 +1,91 @@
+#ifndef	AFS_PARAM_H
+#define	AFS_PARAM_H
+
+/* Machine / Operating system information */
+#define SYS_NAME	"amd64_fbsd_120"
+#define SYS_NAME_ID	SYS_NAME_ID_amd64_fbsd_120
+
+#define AFS_64BITPOINTER_ENV 1
+
+#ifndef UKERNEL
+/* This section for kernel libafs compiles only */
+
+#define AFS_64BITUSERPOINTER_ENV 1
+
+#define AFS_FBSD50_ENV 1
+#define AFS_FBSD51_ENV 1
+#define AFS_FBSD52_ENV 1
+#define AFS_FBSD53_ENV 1
+#define AFS_FBSD60_ENV 1
+#define AFS_FBSD61_ENV 1
+#define AFS_FBSD62_ENV 1
+#define AFS_FBSD70_ENV 1
+#define AFS_FBSD71_ENV 1
+#define AFS_FBSD72_ENV 1
+#define AFS_FBSD73_ENV 1
+#define AFS_FBSD80_ENV 1
+#define AFS_FBSD81_ENV 1
+#define AFS_FBSD82_ENV 1
+#define AFS_FBSD83_ENV 1
+#define AFS_FBSD84_ENV 1
+#define AFS_FBSD90_ENV 1
+#define AFS_FBSD91_ENV 1
+#define AFS_FBSD92_ENV 1
+#define AFS_FBSD93_ENV 1
+#define AFS_FBSD100_ENV 1
+#define AFS_FBSD110_ENV 1
+#define AFS_FBSD111_ENV 1
+#define AFS_FBSD120_ENV 1
+
+#define AFS_X86_FBSD50_ENV 1
+#define AFS_X86_FBSD60_ENV 1 /* added at 70--ie, some changes should port <-- */
+#define AFS_X86_FBSD62_ENV 1
+#define AFS_X86_FBSD70_ENV 1
+#define AFS_X86_FBSD71_ENV 1
+#define AFS_X86_FBSD72_ENV 1
+#define AFS_X86_FBSD73_ENV 1
+#define AFS_X86_FBSD80_ENV 1
+#define AFS_X86_FBSD81_ENV 1
+#define AFS_X86_FBSD82_ENV 1
+#define AFS_X86_FBSD83_ENV 1
+#define AFS_X86_FBSD84_ENV 1
+#define AFS_X86_FBSD90_ENV 1
+#define AFS_X86_FBSD91_ENV 1
+#define AFS_X86_FBSD92_ENV 1
+#define AFS_X86_FBSD93_ENV 1
+#define AFS_X86_FBSD100_ENV 1
+#define AFS_X86_FBSD110_ENV 1
+#define AFS_X86_FBSD111_ENV 1
+#define AFS_X86_FBSD120_ENV 1
+
+#else /* !defined(UKERNEL) */
+
+/* This section for user space compiles only */
+
+#define AFS_USR_FBSD50_ENV 1
+#define AFS_USR_FBSD51_ENV 1
+#define AFS_USR_FBSD52_ENV 1
+#define AFS_USR_FBSD53_ENV 1
+#define AFS_USR_FBSD60_ENV 1
+#define AFS_USR_FBSD61_ENV 1
+#define AFS_USR_FBSD70_ENV 1
+#define AFS_USR_FBSD71_ENV 1
+#define AFS_USR_FBSD72_ENV 1
+#define AFS_USR_FBSD73_ENV 1
+#define AFS_USR_FBSD80_ENV 1
+#define AFS_USR_FBSD81_ENV 1
+#define AFS_USR_FBSD82_ENV 1
+#define AFS_USR_FBSD83_ENV 1
+#define AFS_USR_FBSD84_ENV 1
+#define AFS_USR_FBSD90_ENV 1
+#define AFS_USR_FBSD91_ENV 1
+#define AFS_USR_FBSD92_ENV 1
+#define AFS_USR_FBSD93_ENV 1
+#define AFS_USR_FBSD100_ENV 1
+#define AFS_USR_FBSD110_ENV 1
+#define AFS_USR_FBSD111_ENV 1
+#define AFS_USR_FBSD120_ENV 1
+
+#endif /* !defined(UKERNEL) */
+
+#endif /* AFS_PARAM_H */

src/config/param.i386_fbsd_111.h
@@ -1,0 +1,95 @@
+#ifndef	AFS_PARAM_H
+#define	AFS_PARAM_H
+
+/* Machine / Operating system information */
+#define SYS_NAME	"i386_fbsd_111"
+#define SYS_NAME_ID	SYS_NAME_ID_i386_fbsd_111
+
+#define AFS_FAKEOPEN_ENV 1	/* call afs_FakeOpen as if !AFS_VM_RDWR */
+
+#ifndef UKERNEL
+/* This section for kernel libafs compiles only */
+
+#define AFS_FBSD50_ENV 1
+#define AFS_FBSD51_ENV 1
+#define AFS_FBSD52_ENV 1
+#define AFS_FBSD53_ENV 1
+#define AFS_FBSD60_ENV 1
+#define AFS_FBSD61_ENV 1
+#define AFS_FBSD62_ENV 1
+#define AFS_FBSD70_ENV 1
+#define AFS_FBSD71_ENV 1
+#define AFS_FBSD72_ENV 1
+#define AFS_FBSD73_ENV 1
+#define AFS_FBSD74_ENV 1
+#define AFS_FBSD80_ENV 1
+#define AFS_FBSD81_ENV 1
+#define AFS_FBSD82_ENV 1
+#define AFS_FBSD83_ENV 1
+#define AFS_FBSD84_ENV 1
+#define AFS_FBSD90_ENV 1
+#define AFS_FBSD91_ENV 1
+#define AFS_FBSD92_ENV 1
+#define AFS_FBSD93_ENV 1
+#define AFS_FBSD100_ENV 1
+#define AFS_FBSD101_ENV 1
+#define AFS_FBSD110_ENV 1
+#define AFS_FBSD111_ENV 1
+
+#define AFS_X86_FBSD50_ENV 1
+#define AFS_X86_FBSD51_ENV 1
+#define AFS_X86_FBSD52_ENV 1
+#define AFS_X86_FBSD53_ENV 1
+#define AFS_X86_FBSD60_ENV 1
+#define AFS_X86_FBSD61_ENV 1
+#define AFS_X86_FBSD62_ENV 1
+#define AFS_X86_FBSD70_ENV 1
+#define AFS_X86_FBSD71_ENV 1
+#define AFS_X86_FBSD72_ENV 1
+#define AFS_X86_FBSD74_ENV 1
+#define AFS_X86_FBSD80_ENV 1
+#define AFS_X86_FBSD81_ENV 1
+#define AFS_X86_FBSD82_ENV 1
+#define AFS_X86_FBSD83_ENV 1
+#define AFS_X86_FBSD84_ENV 1
+#define AFS_X86_FBSD90_ENV 1
+#define AFS_X86_FBSD91_ENV 1
+#define AFS_X86_FBSD92_ENV 1
+#define AFS_X86_FBSD93_ENV 1
+#define AFS_X86_FBSD100_ENV 1
+#define AFS_X86_FBSD101_ENV 1
+#define AFS_X86_FBSD110_ENV 1
+#define AFS_X86_FBSD111_ENV 1
+
+#else /* !defined(UKERNEL) */
+
+/* This section for user space compiles only */
+
+#define AFS_USR_FBSD50_ENV 1
+#define AFS_USR_FBSD51_ENV 1
+#define AFS_USR_FBSD52_ENV 1
+#define AFS_USR_FBSD53_ENV 1
+#define AFS_USR_FBSD60_ENV 1
+#define AFS_USR_FBSD61_ENV 1
+#define AFS_USR_FBSD62_ENV 1
+#define AFS_USR_FBSD70_ENV 1
+#define AFS_USR_FBSD71_ENV 1
+#define AFS_USR_FBSD72_ENV 1
+#define AFS_USR_FBSD73_ENV 1
+#define AFS_USR_FBSD80_ENV 1
+#define AFS_USR_FBSD81_ENV 1
+#define AFS_USR_FBSD82_ENV 1
+#define AFS_USR_FBSD83_ENV 1
+#define AFS_USR_FBSD84_ENV 1
+#define AFS_USR_FBSD90_ENV 1
+#define AFS_USR_FBSD91_ENV 1
+#define AFS_USR_FBSD92_ENV 1
+#define AFS_USR_FBSD93_ENV 1
+#define AFS_USR_FBSD100_ENV 1
+#define AFS_USR_FBSD101_ENV 1
+#define AFS_USR_FBSD110_ENV 1
+#define AFS_USR_FBSD111_ENV 1
+
+#endif /* !defined(UKERNEL) */
+
+#endif /* AFS_PARAM_H */

src/config/param.i386_fbsd_120.h
@@ -1,0 +1,98 @@
+#ifndef	AFS_PARAM_H
+#define	AFS_PARAM_H
+
+/* Machine / Operating system information */
+#define SYS_NAME	"i386_fbsd_120"
+#define SYS_NAME_ID	SYS_NAME_ID_i386_fbsd_120
+
+#define AFS_FAKEOPEN_ENV 1	/* call afs_FakeOpen as if !AFS_VM_RDWR */
+
+#ifndef UKERNEL
+/* This section for kernel libafs compiles only */
+
+#define AFS_FBSD50_ENV 1
+#define AFS_FBSD51_ENV 1
+#define AFS_FBSD52_ENV 1
+#define AFS_FBSD53_ENV 1
+#define AFS_FBSD60_ENV 1
+#define AFS_FBSD61_ENV 1
+#define AFS_FBSD62_ENV 1
+#define AFS_FBSD70_ENV 1
+#define AFS_FBSD71_ENV 1
+#define AFS_FBSD72_ENV 1
+#define AFS_FBSD73_ENV 1
+#define AFS_FBSD74_ENV 1
+#define AFS_FBSD80_ENV 1
+#define AFS_FBSD81_ENV 1
+#define AFS_FBSD82_ENV 1
+#define AFS_FBSD83_ENV 1
+#define AFS_FBSD84_ENV 1
+#define AFS_FBSD90_ENV 1
+#define AFS_FBSD91_ENV 1
+#define AFS_FBSD92_ENV 1
+#define AFS_FBSD93_ENV 1
+#define AFS_FBSD100_ENV 1
+#define AFS_FBSD101_ENV 1
+#define AFS_FBSD110_ENV 1
+#define AFS_FBSD111_ENV 1
+#define AFS_FBSD120_ENV 1
+
+#define AFS_X86_FBSD50_ENV 1
+#define AFS_X86_FBSD51_ENV 1
+#define AFS_X86_FBSD52_ENV 1
+#define AFS_X86_FBSD53_ENV 1
+#define AFS_X86_FBSD60_ENV 1
+#define AFS_X86_FBSD61_ENV 1
+#define AFS_X86_FBSD62_ENV 1
+#define AFS_X86_FBSD70_ENV 1
+#define AFS_X86_FBSD71_ENV 1
+#define AFS_X86_FBSD72_ENV 1
+#define AFS_X86_FBSD74_ENV 1
+#define AFS_X86_FBSD80_ENV 1
+#define AFS_X86_FBSD81_ENV 1
+#define AFS_X86_FBSD82_ENV 1
+#define AFS_X86_FBSD83_ENV 1
+#define AFS_X86_FBSD84_ENV 1
+#define AFS_X86_FBSD90_ENV 1
+#define AFS_X86_FBSD91_ENV 1
+#define AFS_X86_FBSD92_ENV 1
+#define AFS_X86_FBSD93_ENV 1
+#define AFS_X86_FBSD100_ENV 1
+#define AFS_X86_FBSD101_ENV 1
+#define AFS_X86_FBSD110_ENV 1
+#define AFS_X86_FBSD111_ENV 1
+#define AFS_X86_FBSD120_ENV 1
+
+#else /* !defined(UKERNEL) */
+
+/* This section for user space compiles only */
+
+#define AFS_USR_FBSD50_ENV 1
+#define AFS_USR_FBSD51_ENV 1
+#define AFS_USR_FBSD52_ENV 1
+#define AFS_USR_FBSD53_ENV 1
+#define AFS_USR_FBSD60_ENV 1
+#define AFS_USR_FBSD61_ENV 1
+#define AFS_USR_FBSD62_ENV 1
+#define AFS_USR_FBSD70_ENV 1
+#define AFS_USR_FBSD71_ENV 1
+#define AFS_USR_FBSD72_ENV 1
+#define AFS_USR_FBSD73_ENV 1
+#define AFS_USR_FBSD80_ENV 1
+#define AFS_USR_FBSD81_ENV 1
+#define AFS_USR_FBSD82_ENV 1
+#define AFS_USR_FBSD83_ENV 1
+#define AFS_USR_FBSD84_ENV 1
+#define AFS_USR_FBSD90_ENV 1
+#define AFS_USR_FBSD91_ENV 1
+#define AFS_USR_FBSD92_ENV 1
+#define AFS_USR_FBSD93_ENV 1
+#define AFS_USR_FBSD100_ENV 1
+#define AFS_USR_FBSD101_ENV 1
+#define AFS_USR_FBSD110_ENV 1
+#define AFS_USR_FBSD111_ENV 1
+#define AFS_USR_FBSD120_ENV 1
+
+#endif /* !defined(UKERNEL) */
+
+#endif /* AFS_PARAM_H */

src/config/param.amd64_fbsd_104.h
@@ -1,0 +1,94 @@
+#ifndef	AFS_PARAM_H
+#define	AFS_PARAM_H
+
+/* Machine / Operating system information */
+#define SYS_NAME	"amd64_fbsd_104"
+#define SYS_NAME_ID	SYS_NAME_ID_amd64_fbsd_104
+
+#define AFS_64BITPOINTER_ENV 1
+
+#ifndef UKERNEL
+/* This section for kernel libafs compiles only */
+
+#define AFS_64BITUSERPOINTER_ENV 1
+
+#define AFS_FBSD50_ENV 1
+#define AFS_FBSD51_ENV 1
+#define AFS_FBSD52_ENV 1
+#define AFS_FBSD53_ENV 1
+#define AFS_FBSD60_ENV 1
+#define AFS_FBSD61_ENV 1
+#define AFS_FBSD62_ENV 1
+#define AFS_FBSD70_ENV 1
+#define AFS_FBSD71_ENV 1
+#define AFS_FBSD72_ENV 1
+#define AFS_FBSD73_ENV 1
+#define AFS_FBSD80_ENV 1
+#define AFS_FBSD81_ENV 1
+#define AFS_FBSD82_ENV 1
+#define AFS_FBSD83_ENV 1
+#define AFS_FBSD84_ENV 1
+#define AFS_FBSD90_ENV 1
+#define AFS_FBSD91_ENV 1
+#define AFS_FBSD92_ENV 1
+#define AFS_FBSD93_ENV 1
+#define AFS_FBSD100_ENV 1
+#define AFS_FBSD101_ENV 1
+#define AFS_FBSD102_ENV 1
+#define AFS_FBSD103_ENV 1
+#define AFS_FBSD104_ENV 1
+
+#define AFS_X86_FBSD50_ENV 1
+#define AFS_X86_FBSD60_ENV 1 /* added at 70--ie, some changes should port <-- */
+#define AFS_X86_FBSD62_ENV 1
+#define AFS_X86_FBSD70_ENV 1
+#define AFS_X86_FBSD71_ENV 1
+#define AFS_X86_FBSD72_ENV 1
+#define AFS_X86_FBSD73_ENV 1
+#define AFS_X86_FBSD80_ENV 1
+#define AFS_X86_FBSD81_ENV 1
+#define AFS_X86_FBSD82_ENV 1
+#define AFS_X86_FBSD83_ENV 1
+#define AFS_X86_FBSD84_ENV 1
+#define AFS_X86_FBSD90_ENV 1
+#define AFS_X86_FBSD91_ENV 1
+#define AFS_X86_FBSD92_ENV 1
+#define AFS_X86_FBSD93_ENV 1
+#define AFS_X86_FBSD100_ENV 1
+#define AFS_X86_FBSD101_ENV 1
+#define AFS_X86_FBSD102_ENV 1
+#define AFS_X86_FBSD103_ENV 1
+#define AFS_X86_FBSD104_ENV 1
+
+#else /* !defined(UKERNEL) */
+
+/* This section for user space compiles only */
+
+#define AFS_USR_FBSD50_ENV 1
+#define AFS_USR_FBSD51_ENV 1
+#define AFS_USR_FBSD52_ENV 1
+#define AFS_USR_FBSD53_ENV 1
+#define AFS_USR_FBSD60_ENV 1
+#define AFS_USR_FBSD61_ENV 1
+#define AFS_USR_FBSD70_ENV 1
+#define AFS_USR_FBSD71_ENV 1
+#define AFS_USR_FBSD72_ENV 1
+#define AFS_USR_FBSD73_ENV 1
+#define AFS_USR_FBSD80_ENV 1
+#define AFS_USR_FBSD81_ENV 1
+#define AFS_USR_FBSD82_ENV 1
+#define AFS_USR_FBSD83_ENV 1
+#define AFS_USR_FBSD84_ENV 1
+#define AFS_USR_FBSD90_ENV 1
+#define AFS_USR_FBSD91_ENV 1
+#define AFS_USR_FBSD92_ENV 1
+#define AFS_USR_FBSD93_ENV 1
+#define AFS_USR_FBSD100_ENV 1
+#define AFS_USR_FBSD101_ENV 1
+#define AFS_USR_FBSD102_ENV 1
+#define AFS_USR_FBSD103_ENV 1
+#define AFS_USR_FBSD104_ENV 1
+
+#endif /* !defined(UKERNEL) */
+
+#endif /* AFS_PARAM_H */

src/config/param.i386_fbsd_104.h
@@ -1,0 +1,98 @@
+#ifndef	AFS_PARAM_H
+#define	AFS_PARAM_H
+
+/* Machine / Operating system information */
+#define SYS_NAME	"i386_fbsd_104"
+#define SYS_NAME_ID	SYS_NAME_ID_i386_fbsd_104
+
+#define AFS_FAKEOPEN_ENV 1	/* call afs_FakeOpen as if !AFS_VM_RDWR */
+
+#ifndef UKERNEL
+/* This section for kernel libafs compiles only */
+
+#define AFS_FBSD50_ENV 1
+#define AFS_FBSD51_ENV 1
+#define AFS_FBSD52_ENV 1
+#define AFS_FBSD53_ENV 1
+#define AFS_FBSD60_ENV 1
+#define AFS_FBSD61_ENV 1
+#define AFS_FBSD62_ENV 1
+#define AFS_FBSD70_ENV 1
+#define AFS_FBSD71_ENV 1
+#define AFS_FBSD72_ENV 1
+#define AFS_FBSD73_ENV 1
+#define AFS_FBSD74_ENV 1
+#define AFS_FBSD80_ENV 1
+#define AFS_FBSD81_ENV 1
+#define AFS_FBSD82_ENV 1
+#define AFS_FBSD83_ENV 1
+#define AFS_FBSD84_ENV 1
+#define AFS_FBSD90_ENV 1
+#define AFS_FBSD91_ENV 1
+#define AFS_FBSD92_ENV 1
+#define AFS_FBSD93_ENV 1
+#define AFS_FBSD100_ENV 1
+#define AFS_FBSD101_ENV 1
+#define AFS_FBSD102_ENV 1
+#define AFS_FBSD103_ENV 1
+#define AFS_FBSD104_ENV 1
+
+#define AFS_X86_FBSD50_ENV 1
+#define AFS_X86_FBSD51_ENV 1
+#define AFS_X86_FBSD52_ENV 1
+#define AFS_X86_FBSD53_ENV 1
+#define AFS_X86_FBSD60_ENV 1
+#define AFS_X86_FBSD61_ENV 1
+#define AFS_X86_FBSD62_ENV 1
+#define AFS_X86_FBSD70_ENV 1
+#define AFS_X86_FBSD71_ENV 1
+#define AFS_X86_FBSD72_ENV 1
+#define AFS_X86_FBSD74_ENV 1
+#define AFS_X86_FBSD80_ENV 1
+#define AFS_X86_FBSD81_ENV 1
+#define AFS_X86_FBSD82_ENV 1
+#define AFS_X86_FBSD83_ENV 1
+#define AFS_X86_FBSD84_ENV 1
+#define AFS_X86_FBSD90_ENV 1
+#define AFS_X86_FBSD91_ENV 1
+#define AFS_X86_FBSD92_ENV 1
+#define AFS_X86_FBSD93_ENV 1
+#define AFS_X86_FBSD100_ENV 1
+#define AFS_X86_FBSD101_ENV 1
+#define AFS_X86_FBSD102_ENV 1
+#define AFS_X86_FBSD103_ENV 1
+#define AFS_X86_FBSD104_ENV 1
+
+#else /* !defined(UKERNEL) */
+
+/* This section for user space compiles only */
+
+#define AFS_USR_FBSD50_ENV 1
+#define AFS_USR_FBSD51_ENV 1
+#define AFS_USR_FBSD52_ENV 1
+#define AFS_USR_FBSD53_ENV 1
+#define AFS_USR_FBSD60_ENV 1
+#define AFS_USR_FBSD61_ENV 1
+#define AFS_USR_FBSD62_ENV 1
+#define AFS_USR_FBSD70_ENV 1
+#define AFS_USR_FBSD71_ENV 1
+#define AFS_USR_FBSD72_ENV 1
+#define AFS_USR_FBSD73_ENV 1
+#define AFS_USR_FBSD80_ENV 1
+#define AFS_USR_FBSD81_ENV 1
+#define AFS_USR_FBSD82_ENV 1
+#define AFS_USR_FBSD83_ENV 1
+#define AFS_USR_FBSD84_ENV 1
+#define AFS_USR_FBSD90_ENV 1
+#define AFS_USR_FBSD91_ENV 1
+#define AFS_USR_FBSD92_ENV 1
+#define AFS_USR_FBSD93_ENV 1
+#define AFS_USR_FBSD100_ENV 1
+#define AFS_USR_FBSD101_ENV 1
+#define AFS_USR_FBSD102_ENV 1
+#define AFS_USR_FBSD103_ENV 1
+#define AFS_USR_FBSD104_ENV 1
+
+#endif /* !defined(UKERNEL) */
+
+#endif /* AFS_PARAM_H */

commit 154512831966d12c1e32e6271d4ab1440a25b96e

FBSD: param.h consistency
Commit 88dc4d93f5ef080da8f56fac453f095e6c79d4a0 ("Add param.h
files for recent FreeBSD") introduced an inconsistency between
the i386 and amd64 param.h files for 11.1 and 12.0 regarding
the *_FBSD101_ENV #defines.

Citing Benjamin Kaduk: "Traditionally we have the param.h for
a FreeBSD N.0 release include the (N-1).Y values that existed
at the time of the N.0 release, and freeze that set of (N-1).Y
values for the lifetime of FreeBSD N.x, if that makes sense."

Given that FreeBSD 11.0 was released shortly after 10.3, and
12.0 is not yet released, consistently #define
*_FBSD10{1..3}_ENV for 11.1 and *_FBSD10{1..4}_ENV for 12.0

Change-Id: Ibb7e6c4caaab7aa97b32eeec7aa0bbe998bb57f7
Reviewed-on: https://gerrit.openafs.org/12990
Tested-by: BuildBot <buildbot@rampaginggeek.com>
Reviewed-by: Benjamin Kaduk <kaduk@mit.edu>

src/config/param.amd64_fbsd_111.h
@@ -33,6 +33,9 @@
 #define AFS_FBSD92_ENV 1
 #define AFS_FBSD93_ENV 1
 #define AFS_FBSD100_ENV 1
+#define AFS_FBSD101_ENV 1
+#define AFS_FBSD102_ENV 1
+#define AFS_FBSD103_ENV 1
 #define AFS_FBSD110_ENV 1
 #define AFS_FBSD111_ENV 1
 
@@ -53,6 +56,9 @@
 #define AFS_X86_FBSD92_ENV 1
 #define AFS_X86_FBSD93_ENV 1
 #define AFS_X86_FBSD100_ENV 1
+#define AFS_X86_FBSD101_ENV 1
+#define AFS_X86_FBSD102_ENV 1
+#define AFS_X86_FBSD103_ENV 1
 #define AFS_X86_FBSD110_ENV 1
 #define AFS_X86_FBSD111_ENV 1
 
@@ -80,6 +86,9 @@
 #define AFS_USR_FBSD92_ENV 1
 #define AFS_USR_FBSD93_ENV 1
 #define AFS_USR_FBSD100_ENV 1
+#define AFS_USR_FBSD101_ENV 1
+#define AFS_USR_FBSD102_ENV 1
+#define AFS_USR_FBSD103_ENV 1
 #define AFS_USR_FBSD110_ENV 1
 #define AFS_USR_FBSD111_ENV 1
 

src/config/param.amd64_fbsd_120.h
@@ -33,6 +33,10 @@
 #define AFS_FBSD92_ENV 1
 #define AFS_FBSD93_ENV 1
 #define AFS_FBSD100_ENV 1
+#define AFS_FBSD101_ENV 1
+#define AFS_FBSD102_ENV 1
+#define AFS_FBSD103_ENV 1
+#define AFS_FBSD104_ENV 1
 #define AFS_FBSD110_ENV 1
 #define AFS_FBSD111_ENV 1
 #define AFS_FBSD120_ENV 1
@@ -54,6 +58,10 @@
 #define AFS_X86_FBSD92_ENV 1
 #define AFS_X86_FBSD93_ENV 1
 #define AFS_X86_FBSD100_ENV 1
+#define AFS_X86_FBSD101_ENV 1
+#define AFS_X86_FBSD102_ENV 1
+#define AFS_X86_FBSD103_ENV 1
+#define AFS_X86_FBSD104_ENV 1
 #define AFS_X86_FBSD110_ENV 1
 #define AFS_X86_FBSD111_ENV 1
 #define AFS_X86_FBSD120_ENV 1
@@ -82,6 +90,10 @@
 #define AFS_USR_FBSD92_ENV 1
 #define AFS_USR_FBSD93_ENV 1
 #define AFS_USR_FBSD100_ENV 1
+#define AFS_USR_FBSD101_ENV 1
+#define AFS_USR_FBSD102_ENV 1
+#define AFS_USR_FBSD103_ENV 1
+#define AFS_USR_FBSD104_ENV 1
 #define AFS_USR_FBSD110_ENV 1
 #define AFS_USR_FBSD111_ENV 1
 #define AFS_USR_FBSD120_ENV 1

src/config/param.i386_fbsd_111.h
@@ -33,6 +33,8 @@
 #define AFS_FBSD93_ENV 1
 #define AFS_FBSD100_ENV 1
 #define AFS_FBSD101_ENV 1
+#define AFS_FBSD102_ENV 1
+#define AFS_FBSD103_ENV 1
 #define AFS_FBSD110_ENV 1
 #define AFS_FBSD111_ENV 1
 
@@ -58,6 +60,8 @@
 #define AFS_X86_FBSD93_ENV 1
 #define AFS_X86_FBSD100_ENV 1
 #define AFS_X86_FBSD101_ENV 1
+#define AFS_X86_FBSD102_ENV 1
+#define AFS_X86_FBSD103_ENV 1
 #define AFS_X86_FBSD110_ENV 1
 #define AFS_X86_FBSD111_ENV 1
 
@@ -87,6 +91,8 @@
 #define AFS_USR_FBSD93_ENV 1
 #define AFS_USR_FBSD100_ENV 1
 #define AFS_USR_FBSD101_ENV 1
+#define AFS_USR_FBSD102_ENV 1
+#define AFS_USR_FBSD103_ENV 1
 #define AFS_USR_FBSD110_ENV 1
 #define AFS_USR_FBSD111_ENV 1
 

src/config/param.i386_fbsd_120.h
@@ -33,6 +33,9 @@
 #define AFS_FBSD93_ENV 1
 #define AFS_FBSD100_ENV 1
 #define AFS_FBSD101_ENV 1
+#define AFS_FBSD102_ENV 1
+#define AFS_FBSD103_ENV 1
+#define AFS_FBSD104_ENV 1
 #define AFS_FBSD110_ENV 1
 #define AFS_FBSD111_ENV 1
 #define AFS_FBSD120_ENV 1
@@ -59,6 +62,9 @@
 #define AFS_X86_FBSD93_ENV 1
 #define AFS_X86_FBSD100_ENV 1
 #define AFS_X86_FBSD101_ENV 1
+#define AFS_X86_FBSD102_ENV 1
+#define AFS_X86_FBSD103_ENV 1
+#define AFS_X86_FBSD104_ENV 1
 #define AFS_X86_FBSD110_ENV 1
 #define AFS_X86_FBSD111_ENV 1
 #define AFS_X86_FBSD120_ENV 1
@@ -89,6 +95,9 @@
 #define AFS_USR_FBSD93_ENV 1
 #define AFS_USR_FBSD100_ENV 1
 #define AFS_USR_FBSD101_ENV 1
+#define AFS_USR_FBSD102_ENV 1
+#define AFS_USR_FBSD103_ENV 1
+#define AFS_USR_FBSD104_ENV 1
 #define AFS_USR_FBSD110_ENV 1
 #define AFS_USR_FBSD111_ENV 1
 #define AFS_USR_FBSD120_ENV 1