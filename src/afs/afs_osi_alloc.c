ad1fe5e1a825a3b3f88c04fd84613e4105206443
Andrew Deason <adeason@dson.org>
2019-07-14 22:53:39 -0500

    FBSD: Remove unnecessary explicit osi_fbsd_alloc
    AFS_KALLOC is already defined to be osi_fbsd_alloc on FBSD, so this
    extra #ifdef here is completely unnecessary. Remove it.
    
    Do the same for AFS_KFREE/osi_fbsd_free.
    
    Change-Id: I3e42ec433a732402cc9de9ba9c035774ec29c2a5
    Reviewed-on: https://gerrit.openafs.org/13708
    Reviewed-by: Benjamin Kaduk <kaduk@mit.edu>
    Tested-by: BuildBot <buildbot@rampaginggeek.com>

--- src/afs/afs_osi_alloc.c
+++ src/afs/afs_osi_alloc.c
@@ -52,8 +52,6 @@ afs_osi_Alloc(size_t size)
     AFS_STATS(afs_stats_cmperf.OutStandingMemUsage += size);
 #ifdef AFS_LINUX20_ENV
     return osi_linux_alloc(size, 1);
-#elif defined(AFS_FBSD_ENV)
-    return osi_fbsd_alloc(size, 1);
 #else
     return AFS_KALLOC(size);
 #endif
@@ -70,8 +68,6 @@ afs_osi_Free(void *x, size_t asize)
     AFS_STATS(afs_stats_cmperf.OutStandingMemUsage -= asize);
 #if defined(AFS_LINUX20_ENV)
     osi_linux_free(x);
-#elif defined(AFS_FBSD_ENV)
-    osi_fbsd_free(x);
 #else
     AFS_KFREE(x, asize);
 #endif
