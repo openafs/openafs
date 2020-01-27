6c54bc9e121b923ec5fdd60ee510171987e55017
Mark Vitale <mvitale@sinenomine.net>
2020-01-27 12:26:41 -0500

    uss: more gcc9 truncation warning appeasement
    uss_procs_PickADir needs a larger buffer to avoid a truncation warning.
    While here, replace some magic numbers with existing symbols.
    
    Change-Id: If981dddfa50bdbc8c4730cf8038429f071b1d5be
    Reviewed-on: https://gerrit.openafs.org/14049
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Cheyenne Wills <cwills@sinenomine.net>
    Reviewed-by: Benjamin Kaduk <kaduk@mit.edu>

--- src/uss/uss_procs.c
+++ src/uss/uss_procs.c
@@ -659,12 +659,12 @@ afs_int32
 uss_procs_PickADir(char *path, char *cp)
 {				/*uss_procs_PickADir */
 
-    char cd[300];		/*Current  directory for search */
+    char cd[uss_DirPoolLen];		/*Current  directory for search */
 
     int i, count, MinIndex = 0, mina = 10000;
     struct dirent *dp;
     DIR *dirp;
-    char dirname[301];
+    char dirname[uss_DirPoolLen*2];
 
     if (uss_NumGroups == 0) {
 	fprintf(stderr, "%s: No choice yet given to replace $AUTO\n",
