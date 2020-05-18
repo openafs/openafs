37187ba29454cba5a79eebff6251d6ff145907cf
Andrew Deason <adeason@sinenomine.net>
2020-05-18 12:09:38 -0500

    auth: Close fd on SetExtendedCellInfo write error
    Currently, and since OpenAFS 1.0, if write() fails here, we leak the
    file descriptor. A write() failure should be very unlikely, but close
    the fd to make sure we avoid the leak.
    
    Reviewed-on: https://gerrit.openafs.org/14213
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Benjamin Kaduk <kaduk@mit.edu>
    (cherry picked from commit c81579dc7b0c0ac6bc34f63384d705a4445c2bbd)
    
    Change-Id: I4dd96cca2fd9c01390fb508ab12d507ab1a56c8b
    Reviewed-on: https://gerrit.openafs.org/14461
    Reviewed-by: Benjamin Kaduk <kaduk@mit.edu>
    Reviewed-by: Michael Meffie <mmeffie@sinenomine.net>
    Reviewed-by: Cheyenne Wills <cwills@sinenomine.net>
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Stephan Wiesand <stephan.wiesand@desy.de>

--- src/auth/writeconfig.c
+++ src/auth/writeconfig.c
@@ -123,6 +123,7 @@ afsconf_SetExtendedCellInfo(struct afsconf_dir *adir,
     i = (int)strlen(acellInfo->name);
     code = write(fd, acellInfo->name, i);
     if (code != i) {
+	close(fd);
 	UNLOCK_GLOBAL_MUTEX;
 	return AFSCONF_FAILURE;
     }
