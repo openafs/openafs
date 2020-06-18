7c9fb4455745ed0015d4a6311bd4a7770efbf40d
Mark Vitale <mvitale@sinenomine.net>
2020-06-18 13:43:35 -0400

    LINUX: replace BUG() call with osi_Panic() in osi_linux_free
    If osi_linux_free fails, it printf's an error message, then calls BUG().
    This is the sole open-coded call to BUG() in OpenAFS; all other calls
    to BUG() are indirect via osi_Panic().
    
    For consistency, eliminate this direct BUG() call by replacing the
    printf and BUG() with an equivalent osi_Panic().  This also ensures that
    the error messsage is logged as critical, and prefixed with "openafs:".
    
    Change-Id: Id319dffa859308528a66991bbbc522ca49552d51
    Reviewed-on: https://gerrit.openafs.org/14250
    Reviewed-by: Benjamin Kaduk <kaduk@mit.edu>
    Tested-by: Benjamin Kaduk <kaduk@mit.edu>

--- src/afs/LINUX/osi_alloc.c
+++ src/afs/LINUX/osi_alloc.c
@@ -305,8 +305,7 @@ osi_linux_free(void *addr)
 	afs_atomlist_put(al_mem_pool, lmp);	/* return osi_linux_mem struct to pool */
 	afs_linux_cur_allocs--;
     } else {
-	printf("osi_linux_free: failed to remove chunk from hashtable\n");
-	BUG();
+	osi_Panic("osi_linux_free: failed to remove chunk from hashtable\n");
     }
 
     mutex_unlock(&afs_linux_alloc_sem);
