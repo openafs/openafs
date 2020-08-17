b65b33da4e65e2c981ce264ef4fa4e3e4c94b732
Michael Meffie <mmeffie@sinenomine.net>
2020-08-17 15:44:55 -0400

    vldb_check: Check for volume lock inconsistencies
    Verify the a lock timestamp is set if, and only if, a lock volume
    operation flag is also set.
    
    When running vldb_check with the -fix option, fix the inconsistent
    entries by setting the lock timestamp to the current time if a lock flag
    is set, or by setting the VLOP_DELETE flag if the lock timestamp is set
    but no lock flags are set. (The VLOP_DELETE flag is the flag set by the
    'vos lock command, and is shown in vos output as "delete/misc".)
    
    Volume lock fields can be put into an inconsistent state, at least, by
    interupted vos rename operations, due to bugs in vos rename. When the
    volume lock timestamp and lock flags are in this inconsistent state, the
    volume is locked, but that is not indicated by 'vos listvldb'. The
    volume can be unlocked by issuing 'vos unlock'.
    
    Reviewed-on: https://gerrit.openafs.org/14307
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Andrew Deason <adeason@sinenomine.net>
    Reviewed-by: Cheyenne Wills <cwills@sinenomine.net>
    Reviewed-by: Michael Meffie <mmeffie@sinenomine.net>
    Reviewed-by: Benjamin Kaduk <kaduk@mit.edu>
    (cherry picked from commit 4c33820525af510a8a937289005e39d5b6683b19)
    
    Change-Id: Ia894139145d92948b2af43bd115792556131cd5a
    Reviewed-on: https://gerrit.openafs.org/14450
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Michael Meffie <mmeffie@sinenomine.net>
    Reviewed-by: Cheyenne Wills <cwills@sinenomine.net>
    Reviewed-by: Stephan Wiesand <stephan.wiesand@desy.de>

--- src/vlserver/vldb_check.c
+++ src/vlserver/vldb_check.c
@@ -1465,6 +1465,14 @@ WorkerBee(struct cmd_syndesc *as, void *arock)
 		log_error(VLDB_CHECK_ERROR,"address %u (offset 0x%0x): Record not in a BK chain (type 0x%0x)\n",
 		       record[i].addr, OFFSET(record[i].addr), record[i].type);
 	    }
+	    if ((vlentry.LockTimestamp == 0 && (vlentry.flags & VLOP_ALLOPERS) != 0) ||
+	       (vlentry.LockTimestamp != 0 && (vlentry.flags & VLOP_ALLOPERS) == 0)) {
+		log_error(VLDB_CHECK_ERROR,
+		    "address %u (offset 0x%0x): Lock inconsistency in volume '%s'; timestamp %d, lock flags 0x%0x\n",
+		    record[i].addr, OFFSET(record[i].addr), vlentry.name,
+		    vlentry.LockTimestamp, (vlentry.flags & VLOP_ALLOPERS));
+	    }
+
 	    if (fix) {
 		afs_uint32 oldhash, newhash;
 		char oldNameBuffer[10 + VL_MAXNAMELEN];
@@ -1564,6 +1572,21 @@ WorkerBee(struct cmd_syndesc *as, void *arock)
 		    vlentry.nextIdHash[j] = header.VolidHash[j][hash];
 		    header.VolidHash[j][hash] = record[i].addr;
 		}
+
+		/*
+		 * Fix lock timestamp and flag inconsistencies.
+		 */
+		if (vlentry.LockTimestamp == 0 && (vlentry.flags & VLOP_ALLOPERS) != 0) {
+		    quiet_println("FIX: Record %u: Inconsistent lock in volume %s; setting timestamp.\n",
+				  record[i].addr, vlentry.name);
+		    vlentry.LockTimestamp = time(NULL);
+		}
+		if (vlentry.LockTimestamp != 0 && (vlentry.flags & VLOP_ALLOPERS) == 0) {
+		    quiet_println("FIX: Record %u: Inconsistent lock in volume %s; setting misc lock flag.\n",
+				  record[i].addr, vlentry.name);
+		    vlentry.flags |= VLOP_DELETE; /* Shown by vos as "delete/misc". */
+		}
+
 		writeentry(record[i].addr, &vlentry);
 	    }
 	}
