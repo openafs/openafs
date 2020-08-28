712ef18ac1902fabc540883827b1925769e0a050
Michael Meffie <mmeffie@sinenomine.net>
2020-08-28 11:23:00 -0400

    bozo: Add KeyFileExt and rxkad.keytab to access rights check
    When the KeyFileExt and rxkad.keytab were added to OpenAFS, they were
    not added to the bosserver's access rights check. Add these files to the
    bosserver access checks, with the same access rights needed for the
    original KeyFile.
    
    Also, add the full path for KeyFileExt to the dirpath package (not just
    the filename), which was not done when the KeyFileExt was introduced.
    This is needed to perform the access checks.
    
    Reviewed-on: https://gerrit.openafs.org/14329
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Andrew Deason <adeason@sinenomine.net>
    Reviewed-by: Benjamin Kaduk <kaduk@mit.edu>
    (cherry picked from commit a6b14ea90259fbc4ead62f5f4288e435801db81e)
    
    Change-Id: I94d9027f9455ee8a8f307864ffa49debaf579e71
    Reviewed-on: https://gerrit.openafs.org/14419
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Michael Meffie <mmeffie@sinenomine.net>
    Reviewed-by: Andrew Deason <adeason@sinenomine.net>
    Reviewed-by: Stephan Wiesand <stephan.wiesand@desy.de>

--- src/bozo/bosoprocs.c
+++ src/bozo/bosoprocs.c
@@ -1266,8 +1266,10 @@ struct bozo_bosEntryStats bozo_bosEntryStats[] = {
     {NULL, 1, 1, 0700, 07},	/* AFSDIR_SERVER_DB_DIRPATH     */
     {NULL, 1, 1, 0700, 07},	/* AFSDIR_SERVER_LOCAL_DIRPATH  */
     {NULL, 0, 1, 0600, 07},	/* AFSDIR_SERVER_KEY_FILEPATH   */
-    {NULL, 0, 1, 0600, 03}
-};				/* AFSDIR_SERVER_ULIST_FILEPATH */
+    {NULL, 0, 1, 0600, 03},	/* AFSDIR_SERVER_ULIST_FILEPATH */
+    {NULL, 0, 1, 0600, 07},	/* AFSDIR_SERVER_RXKAD_KEYTAB_FILEPATH   */
+    {NULL, 0, 1, 0600, 07}	/* AFSDIR_SERVER_EXT_KEY_FILEPATH   */
+};
 int bozo_nbosEntryStats =
     sizeof(bozo_bosEntryStats) / sizeof(bozo_bosEntryStats[0]);
 
@@ -1290,6 +1292,8 @@ initBosEntryStats(void)
     bozo_bosEntryStats[6].path = AFSDIR_SERVER_LOCAL_DIRPATH;
     bozo_bosEntryStats[7].path = AFSDIR_SERVER_KEY_FILEPATH;
     bozo_bosEntryStats[8].path = AFSDIR_SERVER_ULIST_FILEPATH;
+    bozo_bosEntryStats[9].path = AFSDIR_SERVER_RXKAD_KEYTAB_FILEPATH;
+    bozo_bosEntryStats[10].path = AFSDIR_SERVER_EXT_KEY_FILEPATH;
 
     return 0;
 }
