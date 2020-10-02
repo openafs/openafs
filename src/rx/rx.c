29f30ed16c6db544f020142c11c4296227eebf99
Jeffrey Altman <jaltman@auristor.com>
2020-10-02 19:51:06 -0400

    rx: compare RX_ACK_TYPE_ACK as a bit-field
    The rx_ackPacket.acks array (the SACK table) consists of up to 255
    octets.  Each octet stores either the value zero (RX_ACK_TYPE_NACK)
    or one (RX_ACK_TYPE_ACK).  Effectively only bit-zero of each octet
    is used.
    
    The rx_ackPacket.acks array cannot be enlarged but one possible
    method of encoding the ACK/NACK state for packets when the
    window size is greater than 255 is to use bits 1-7 of each
    octet.
    
    This change alters the test for ACK vs NACK to be a bit comparison
    instead of a equality comparison.  This change permits RX to be
    compatible with any future use of bits 1-7.
    
    No peer that treats the SACK table as bytes can ever send more
    than 255 packets regardless of the advertised receive window.
    Therefore, existing peers will never receive a SACK table with
    more than 255 packets worth of bits.
    
    Reviewed-on: https://gerrit.openafs.org/14465
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Benjamin Kaduk <kaduk@mit.edu>
    (cherry picked from commit 7739d1e967177686e91a6ea86c73b404515a1c0d)
    
    Change-Id: I59c098414f309bac283017823dff6d491d895db8
    Reviewed-on: https://gerrit.openafs.org/14740
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Cheyenne Wills <cwills@sinenomine.net>
    Reviewed-by: Benjamin Kaduk <kaduk@mit.edu>
    Reviewed-by: Stephan Wiesand <stephan.wiesand@desy.de>

--- src/rx/rx.c
+++ src/rx/rx.c
@@ -4374,7 +4374,8 @@ rxi_ReceiveAckPacket(struct rx_call *call, struct rx_packet *np,
 	    int offset;
 
 	    for (offset = 0; offset < nAcks && len < sizeof(msg); offset++)
-		msg[len++] = (ap->acks[offset] == RX_ACK_TYPE_NACK ? '-' : '*');
+		msg[len++] = ((ap->acks[offset] & RX_ACK_TYPE_ACK) != 0 ? '*'
+									: '-');
 	}
 	msg[len++]='\n';
 	msg[len] = '\0';
@@ -4390,7 +4391,7 @@ rxi_ReceiveAckPacket(struct rx_call *call, struct rx_packet *np,
 	if (nAcks) {
 	    int offset;
 	    for (offset = 0; offset < nAcks; offset++)
-		putc(ap->acks[offset] == RX_ACK_TYPE_NACK ? '-' : '*',
+		putc((ap->acks[offset] & RX_ACK_TYPE_ACK) != 0 ? '*' : '-',
 		     rx_Log);
 	}
 	putc('\n', rx_Log);
@@ -4506,7 +4507,7 @@ rxi_ReceiveAckPacket(struct rx_call *call, struct rx_packet *np,
 	 * be downgraded when the server has discarded a packet it
 	 * soacked previously, or when an ack packet is received
 	 * out of sequence. */
-	if (ap->acks[tp->header.seq - first] == RX_ACK_TYPE_ACK) {
+	if ((ap->acks[tp->header.seq - first] & RX_ACK_TYPE_ACK) != 0) {
 	    if (!(tp->flags & RX_PKTFLAG_ACKED)) {
 		newAckCount++;
 		tp->flags |= RX_PKTFLAG_ACKED;
@@ -5754,7 +5755,8 @@ rxi_SendAck(struct rx_call *call,
 	    int offset;
 
 	    for (offset = 0; offset < ap->nAcks && len < sizeof(msg); offset++)
-		msg[len++] = (ap->acks[offset] == RX_ACK_TYPE_NACK ? '-' : '*');
+		msg[len++] = ((ap->acks[offset] & RX_ACK_TYPE_ACK) != 0 ? '*'
+									: '-');
 	}
 	msg[len++]='\n';
 	msg[len] = '\0';
@@ -5767,7 +5769,7 @@ rxi_SendAck(struct rx_call *call,
 		(unsigned int)p->header.seq, ntohl(ap->firstPacket));
 	if (ap->nAcks) {
 	    for (offset = 0; offset < ap->nAcks; offset++)
-		putc(ap->acks[offset] == RX_ACK_TYPE_NACK ? '-' : '*',
+		putc((ap->acks[offset] & RX_ACK_TYPE_ACK) != 0 ? '*' : '-',
 		     rx_Log);
 	}
 	putc('\n', rx_Log);
