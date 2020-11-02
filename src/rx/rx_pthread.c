a6173b39a874d7cce3a826d7b4dce58ee6a48983
Andrew Deason <adeason@sinenomine.net>
2020-11-02 13:16:41 -0600

    rx: Save errno in pthread rxi_Sendmsg
    Currently, our pthread version of rxi_Sendmsg uses 'errno' in some
    logic if sendmsg fails, but we do so after calling functions that
    might alter errno (e.g. fflush).
    
    To make sure we get the correct errno value, save the value of errno
    right after sendmsg returns an error. Reorganize this function a bit
    to help make the logic easier to follow.
    
    Reviewed-on: https://gerrit.openafs.org/14423
    Reviewed-by: Cheyenne Wills <cwills@sinenomine.net>
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Benjamin Kaduk <kaduk@mit.edu>
    (cherry picked from commit eff7fa4b2eb9a3001dc18dca157ccbd5f19f89b6)
    
    Change-Id: Ie761bf8fbf930718d933fdc2d0ad6961b2660607
    Reviewed-on: https://gerrit.openafs.org/14502
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Andrew Deason <adeason@sinenomine.net>
    Reviewed-by: Cheyenne Wills <cwills@sinenomine.net>
    Reviewed-by: Michael Meffie <mmeffie@sinenomine.net>
    Reviewed-by: Stephan Wiesand <stephan.wiesand@desy.de>

--- src/rx/rx_pthread.c
+++ src/rx/rx_pthread.c
@@ -409,37 +409,37 @@ rxi_Recvmsg(osi_socket socket, struct msghdr *msg_p, int flags)
 int
 rxi_Sendmsg(osi_socket socket, struct msghdr *msg_p, int flags)
 {
-    int ret;
-    ret = sendmsg(socket, msg_p, flags);
+    int err;
+    if (sendmsg(socket, msg_p, flags) >= 0) {
+	return 0;
+    }
+
+#ifdef AFS_NT40_ENV
+    err = WSAGetLastError();
+#else
+    err = errno;
+#endif
 
 #ifdef AFS_RXERRQ_ENV
-    if (ret < 0) {
-	while (rxi_HandleSocketError(socket) > 0)
-	    ;
-	return ret;
-    }
+    while (rxi_HandleSocketError(socket) > 0)
+	;
 #else
 # ifdef AFS_LINUX22_ENV
     /* linux unfortunately returns ECONNREFUSED if the target port
      * is no longer in use */
     /* and EAGAIN if a UDP checksum is incorrect */
-    if (ret == -1 && errno != ECONNREFUSED && errno != EAGAIN) {
-# else
-    if (ret == -1) {
-# endif
-	dpf(("rxi_sendmsg failed, error %d\n", errno));
-	fflush(stdout);
-# ifndef AFS_NT40_ENV
-        if (errno > 0)
-          return -errno;
-# else
-            if (WSAGetLastError() > 0)
-              return -WSAGetLastError();
-# endif
-	return -1;
+    if (err == ECONNREFUSED || err == EAGAIN) {
+	return 0;
     }
+# endif
+    dpf(("rxi_sendmsg failed, error %d\n", errno));
+    fflush(stdout);
 #endif /* !AFS_RXERRQ_ENV */
-    return 0;
+
+    if (err > 0) {
+	return -err;
+    }
+    return -1;
 }
 
 struct rx_ts_info_t * rx_ts_info_init(void) {
