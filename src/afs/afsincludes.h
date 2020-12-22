031ebf43a8d4db79ee1aa9aff571094354c548b1
Cheyenne Wills <cwills@sinenomine.net>
2020-12-22 11:06:42 -0700

    afs: Cleanup afsincludes.h indentation
    Clean up the indentation of preprocessor statements
    
    Remove commented out code.
    
    Change-Id: I37fec6f15a8972651ef05aa00580a2628e0a1a46
    Reviewed-on: https://gerrit.openafs.org/14471
    Reviewed-by: Andrew Deason <adeason@sinenomine.net>
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Benjamin Kaduk <kaduk@mit.edu>

--- src/afs/afsincludes.h
+++ src/afs/afsincludes.h
@@ -13,52 +13,50 @@
 #include <afsconfig.h>
 
 #ifdef UKERNEL
-#include <UKERNEL/afsincludes.h>
+# include <UKERNEL/afsincludes.h>
 #else
 
-
 /* AFS based headers */
-#include "afs/stds.h"
-#ifdef	AFS_AIX_ENV
-#include "osi_vfs.h"
-#elif defined(AFS_HPUX_ENV) || defined(AFS_NBSD_ENV)
-#include "osi_vfs.h"
-#endif
-#if defined(AFS_SGI_ENV) || defined(AFS_LINUX20_ENV)
-#include "osi_vfs.h"
-#endif
-#include "rx/rx.h"
-#include "afs/afs_osi.h"
-#include "afs/lock.h"
-#include "afs/volerrors.h"
-#include "afs/voldefs.h"
-#ifdef AFS_LINUX20_ENV
-#ifdef TRUE
-#undef TRUE
-#undef FALSE
-#endif
-#endif
-#ifdef AFS_LINUX20_ENV
-#undef __NFDBITS
-#undef __FDMASK
-#endif
-#include "afsint.h"
-#include "afs/exporter.h"
-#include "vldbint.h"
-#include "afs/afs.h"
-#include "afs/afs_chunkops.h"
-/*#include "afs/auxinode.h"	-- used only in afs_istuff.c */
-#include "rx/rxkad.h"
-#include "afs/prs_fs.h"
-#include "afs/dir.h"
-#include "afs/afs_axscache.h"
-#include "afs/icl.h"
-#include "afs/afs_stats.h"
-#include "afs/afs_prototypes.h"
-#include "afs/discon.h"
-#if defined(AFS_LINUX20_ENV) || defined(AFS_DARWIN_ENV) || defined(AFS_FBSD_ENV)
-#include "osi_machdep.h"
-#endif
+# include "afs/stds.h"
+# ifdef	AFS_AIX_ENV
+#  include "osi_vfs.h"
+# elif defined(AFS_HPUX_ENV) || defined(AFS_NBSD_ENV)
+#  include "osi_vfs.h"
+# endif
+# if defined(AFS_SGI_ENV) || defined(AFS_LINUX20_ENV)
+#  include "osi_vfs.h"
+# endif
+# include "rx/rx.h"
+# include "afs/afs_osi.h"
+# include "afs/lock.h"
+# include "afs/volerrors.h"
+# include "afs/voldefs.h"
+# ifdef AFS_LINUX20_ENV
+#  ifdef TRUE
+#   undef TRUE
+#   undef FALSE
+#  endif
+# endif
+# ifdef AFS_LINUX20_ENV
+#  undef __NFDBITS
+#  undef __FDMASK
+# endif
+# include "afsint.h"
+# include "afs/exporter.h"
+# include "vldbint.h"
+# include "afs/afs.h"
+# include "afs/afs_chunkops.h"
+# include "rx/rxkad.h"
+# include "afs/prs_fs.h"
+# include "afs/dir.h"
+# include "afs/afs_axscache.h"
+# include "afs/icl.h"
+# include "afs/afs_stats.h"
+# include "afs/afs_prototypes.h"
+# include "afs/discon.h"
+# if defined(AFS_LINUX20_ENV) || defined(AFS_DARWIN_ENV) || defined(AFS_FBSD_ENV)
+#  include "osi_machdep.h"
+# endif
 
 #endif
 
