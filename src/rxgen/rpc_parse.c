9b2c27fc6d299cb0b23eca3e8cb25b7e0c41b11a
Andrew Deason <adeason@sinenomine.net>
2021-12-07 15:43:35 -0600

    rxgen: Declare generated PKG_TranslateOpCode()
    Ever since commit 5c77a6ca (rxgen: add *TranslateOpcode functions to
    XDR code), rxgen generates a PKG_TranslateOpCode function in the
    relevant .xdr.c source, which can translate an RPC opcode into the RPC
    name. But we never declare this function in a header file, making it
    impossible to use without compiler warnings (unless the caller
    declares the function itself).
    
    To make it possible to actually use this function, declare this
    function in the generated header file.
    
    Change-Id: I013349d28ab1cac8e8aac2ddf6b2cecb64bdcc51
    Reviewed-on: https://gerrit.openafs.org/14871
    Reviewed-by: Cheyenne Wills <cwills@sinenomine.net>
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Mark Vitale <mvitale@sinenomine.net>
    Reviewed-by: Benjamin Kaduk <kaduk@mit.edu>

--- src/rxgen/rpc_parse.c
+++ src/rxgen/rpc_parse.c
@@ -2113,6 +2113,8 @@ h_ProcMainBody_setup(void)
 {
     f_print(fout,"\nextern int %s%sExecuteRequest(struct rx_call *);\n",
 	    prefix, PackagePrefix[PackageIndex]);
+    f_print(fout,"extern char * %s%sTranslateOpCode(int op);\n", prefix,
+	    PackagePrefix[PackageIndex]);
 }
 
 static void
@@ -2124,6 +2126,8 @@ h_HeadofOldStyleProc_setup(void)
     f_print(fout,"\nextern int %s%sExecuteRequest(struct rx_call *);\n",
 	    prefix, pprefix);
     f_print(fout,"\nextern int %sOpCodeIndex(int op);\n", PackagePrefix[PackageIndex]);
+    f_print(fout, "extern char * %s%sTranslateOpCode(int op);\n",
+	    prefix, pprefix);
 }
 
 void
