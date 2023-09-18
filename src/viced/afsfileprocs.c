19f8f35949cf8fc7e6b91afc0d29deb0d4db4d99
Andrew Deason <adeason@sinenomine.net>
2023-09-18 16:13:57 -0500

    OPENAFS-SA-2024-002: viced: Refuse ACLs without '\0' in SRXAFS_StoreACL
    CVE-2024-10396
    
    Currently, the fileserver treats the ACL given in RXAFS_StoreACL as a
    string, even though it is technically an AFSOpaque and could be not
    NUL-terminated.
    
    We give the ACL opaque/string to acl_Internalize_pr() to parse, which
    will run off the end of the allocated buffer if the given ACL does not
    contain a '\0' character. Usually this will result in a parse error
    since we'll encounter garbage, but if the partially-garbage ACL
    happens to parse successfully, some uninitialized data could make it
    into the stored ACL.
    
    In addition, if the given ACL is an opaque of length 0, we'll still
    give the opaque pointer to acl_Internalize_pr(). In this case, the
    pointer will point to &memZero, which happens to contain a NUL byte,
    and so is treated like an empty string (which is not a valid ACL). But
    the fact that this causes no problems is somewhat a coincidence, and
    so should also be avoided.
    
    To avoid both of these situations, just check if the given ACL string
    contains a NUL byte. If it doesn't, or if it has length 0, refuse to
    look at it and abort the call with EINVAL.
    
    FIXES 135445
    
    Reviewed-on: https://gerrit.openafs.org/15908
    Reviewed-by: Benjamin Kaduk <kaduk@mit.edu>
    Tested-by: Benjamin Kaduk <kaduk@mit.edu>
    (cherry picked from commit e15decb318797f1d471588dc669c3e3b26f1b8b3)
    
    Reviewed-on: https://gerrit.openafs.org/15929
    Reviewed-by: Benjamin Kaduk <kaduk@mit.edu>
    Tested-by: Benjamin Kaduk <kaduk@mit.edu>
    (cherry picked from commit f74f960a18f559e683d6a1f5104e43c3ca93ecb8)
    
    Change-Id: I451b0c7b1e983c464dc05ed6bf7ef603bd7ad3ac

--- src/viced/afsfileprocs.c
+++ src/viced/afsfileprocs.c
@@ -3462,6 +3462,36 @@ SRXAFS_StoreData64(struct rx_call * acall, struct AFSFid * Fid,
     return code;
 }
 
+/**
+ * Check if the given ACL blob is okay to use.
+ *
+ * If the given ACL is 0-length, or doesn't contain a NUL byte, return an error
+ * and refuse the process the given ACL. The ACL must contain a NUL byte,
+ * otherwise ACL-processing code may run off the end of the buffer and process
+ * uninitialized memory.
+ *
+ * If there is any data beyond the NUL byte, just ignore it and return success.
+ * We won't look at any post-NUL data, but theoretically clients could send
+ * such an ACL to us, since historically it's been allowed.
+ *
+ * @param[in] AccessList    The ACL blob to check
+ *
+ * @returns errno error code to abort the call with
+ * @retval 0 ACL is okay to use
+ */
+static afs_int32
+check_acl(struct AFSOpaque *AccessList)
+{
+    if (AccessList->AFSOpaque_len == 0) {
+	return EINVAL;
+    }
+    if (memchr(AccessList->AFSOpaque_val, '\0',
+	       AccessList->AFSOpaque_len) == NULL) {
+	return EINVAL;
+    }
+    return 0;
+}
+
 afs_int32
 SRXAFS_StoreACL(struct rx_call * acall, struct AFSFid * Fid,
 		struct AFSOpaque * AccessList,
@@ -3495,6 +3525,11 @@ SRXAFS_StoreACL(struct rx_call * acall, struct AFSFid * Fid,
 #endif /* FS_STATS_DETAILED */
     if ((errorCode = CallPreamble(acall, ACTIVECALL, Fid, &tcon, &thost)))
 	goto Bad_StoreACL;
+
+    errorCode = check_acl(AccessList);
+    if (errorCode != 0) {
+	goto Bad_StoreACL;
+    }
 
     /* Get ptr to client data for user Id for logging */
     t_client = (struct client *)rx_GetSpecific(tcon, rxcon_client_key);
