7b4802efaf29ef76969c8b931a31e93dd4fbb000
Cheyenne Wills <cwills@sinenomine.net>
2024-03-11 09:05:33 -0600

    Linux 6.8: Use roken's strlcpy() in kernel module
    The Linux 6.8 commit 'string: Remove strlcpy()' (d26270061a) removed the
    the strlcpy function from the Linux kernel.  The replacement function,
    strscpy(), cannot be used as a drop-in replacement as its currently a
    Linux kernel specific function and there are differences in the returned
    value.
    
    We can use roken's strlcpy() (provided in roken/strlcpy.c).
    
    Create a configure test to that defines its own strlcpy() to test
    if the kernel doesn't provide one itself. Note, we need to use a
    different function signature for strlcpy() from what the kernel might
    have otherwise the test build succeeds when the kernel does provide a
    strlcpy().
    
    Update the OpenAFS kernel specific roken.h to define the prototype for
    strlcpy when it's not present in the Linux kernel.  We need to match the
    defines used in the 'real' roken.h so the roken/strlcpy.c can build
    properly. Add defines for ROKEN_LIB_FUNCTION, ROKEN_LIB_CALL and
    ROKEN_LIB_VARIABLE to the kernel roken.h
    
    Update Linux's osi_machdep.h to include roken.h so the strlcpy protoype
    is available.
    
    Update the Linux MakefileProto to include the strcpy-kernel object
    when building the kernel module.
    
    Reviewed-on: https://gerrit.openafs.org/15646
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Benjamin Kaduk <kaduk@mit.edu>
    (cherry picked from commit 30b18c165752e6d0ce7b6daa6a90453f5e5e6d17)
    
    Change-Id: I8013623e8f735d15bb7d4ac84ed0867f12b77783
    Reviewed-on: https://gerrit.openafs.org/15706
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Andrew Deason <adeason@sinenomine.net>
    Reviewed-by: Michael Meffie <mmeffie@sinenomine.net>
    Reviewed-by: Benjamin Kaduk <kaduk@mit.edu>

--- src/crypto/hcrypto/kernel/roken.h
+++ src/crypto/hcrypto/kernel/roken.h
@@ -11,6 +11,35 @@
 # error "This header is for kernel code only"
 #endif
 
+/*
+ * The following function annotations are not needed when building for kernel
+ * space
+ */
+#define ROKEN_LIB_FUNCTION
+#define ROKEN_LIB_CALL
+#define ROKEN_LIB_VARIABLE
+
+/*
+ * Our HAVE_STRLCPY from autoconf refers to whether strlcpy() is available in
+ * userspace. Whether it's available in the kernel is another question, so
+ * override HAVE_STRLCPY here. Usually it is available (only a few cases lack
+ * it), so turn it on by default, and turn it off for a few cases below.
+ */
+#undef HAVE_STRLCPY
+#define HAVE_STRLCPY 1
+
+#ifdef AFS_AIX_ENV
+# undef HAVE_STRLCPY
+#elif defined(AFS_LINUX_ENV) && defined(HAVE_LINUX_NO_STRLCPY)
+# undef HAVE_STRLCPY
+#endif
+
+/* strlcpy.c */
+#if defined (AFS_LINUX_ENV) && !defined(HAVE_STRLCPY)
+# define strlcpy rk_strlcpy
+ROKEN_LIB_FUNCTION size_t ROKEN_LIB_CALL strlcpy (char *, const char *, size_t);
+#endif
+
 /* ct.c */
 int ct_memcmp(const void *p1, const void *p2, size_t len);
 
