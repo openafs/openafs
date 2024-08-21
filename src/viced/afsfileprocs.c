bb01d76a2095baa65880bdc5d504e7a198958265
Andrew Deason <adeason@sinenomine.net>
2024-08-21 00:41:49 -0500

    OPENAFS-SA-2024-002: viced: Avoid unchecked ACL in StoreACL audit log
    CVE-2024-10396
    
    Currently in SRXAFS_StoreACL, if CallPreamble() or check_acl() fail, we
    will jump to Bad_StoreACL, which will pass the ACL string from the
    client to osi_auditU. Since check_acl() hasn't yet checked if the given
    ACL contains a NUL byte, the ACL may be an unterminated string. If
    auditing is enabled, this can cause garbage to be logged to the audit
    log, or cause the fileserver to crash.
    
    To avoid this, set 'rawACL' to NULL at first, only setting it to the
    actual ACL string after check_acl() has succeeded. This ensures that all
    code accessing 'rawACL' is guaranteed to be using a terminated string.
    
    This may mean that we pass a NULL AUD_ACL to osi_auditU. Our auditing
    code explicitly checks for and handles handles NULL strings, so this is
    fine.
    
    FIXES 135445
    
    Reviewed-on: https://gerrit.openafs.org/15913
    Reviewed-by: Benjamin Kaduk <kaduk@mit.edu>
    Tested-by: Benjamin Kaduk <kaduk@mit.edu>
    (cherry picked from commit c9eae1e8b26144063e5d1db23d47ee82c4b9ef3a)
    
    Change-Id: Ieda6f910d875c4b5179011e5e93e5694d3f4ce47
    Reviewed-on: https://gerrit.openafs.org/15934
    Reviewed-by: Benjamin Kaduk <kaduk@mit.edu>
    Tested-by: Benjamin Kaduk <kaduk@mit.edu>

--- src/viced/afsfileprocs.c
+++ src/viced/afsfileprocs.c
@@ -3114,7 +3114,7 @@ SRXAFS_StoreACL(struct rx_call * acall, struct AFSFid * Fid,
     struct client *t_client = NULL;	/* tmp ptr to client data */
     struct in_addr logHostAddr;	/* host ip holder for inet_ntoa */
     struct fsstats fsstats;
-    char *rawACL = uncheckedACL->AFSOpaque_val;
+    char *rawACL = NULL;
 
     fsstats_StartOp(&fsstats, FS_STATS_RPCIDX_STOREACL);
 
@@ -3125,6 +3125,7 @@ SRXAFS_StoreACL(struct rx_call * acall, struct AFSFid * Fid,
     if (errorCode != 0) {
 	goto Bad_StoreACL;
     }
+    rawACL = uncheckedACL->AFSOpaque_val;
 
     /* Get ptr to client data for user Id for logging */
     t_client = (struct client *)rx_GetSpecific(tcon, rxcon_client_key);
