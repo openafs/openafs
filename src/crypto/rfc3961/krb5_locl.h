openafs/openafs

commit de46edef693d9fc7ab530eb17286edc999c12f03

Remove lowercase min/max macro definitions
A prior commit:
    "Replace lowercase min/max macros with opr_min/max"
       (Change-Id: Ib7c91b4e59aaf76ab77f293016496d6179c23c88)
replaced the uses of the min and max macros with opr_min and opr_max.

As a cleanup, remove the definitions for the min() and max() macros.

As noted in the above commit, there were several locations that left
the min/max macros alone:
     src/WINNT
     src/external/heimdal

Change-Id: I263d91b04a2aed14bf9e1ebca1616629f36e6b23
Reviewed-on: https://gerrit.openafs.org/15843
Reviewed-by: Cheyenne Wills <cwills@sinenomine.net>
Tested-by: BuildBot <buildbot@rampaginggeek.com>
Reviewed-by: Andrew Deason <adeason@sinenomine.net>
Reviewed-by: Michael Meffie <mmeffie@sinenomine.net>

src/crypto/rfc3961/krb5_locl.h
@@ -66,10 +66,6 @@
 
 #define ALLOC(X, N) (X) = calloc((N), sizeof(*(X)))
 
-#ifndef max
-#define max(a,b) (((a)>(b))?(a):(b))
-#endif
-
 #ifndef O_BINARY
 #define O_BINARY 0
 #endif

src/afs/AIX/osi_vfs.h
@@ -14,7 +14,6 @@
 #define	VSGID	S_ISGID
 #define	VSVTX	S_ISVTX
 
-#define min(a, b)	(((a) < (b)) ? (a) : (b))
 #define	bcopyin	copyin		/* Aux has identical bcopyin */
 
 #define BLKDEV_IOSIZE	BSIZE

src/butc/afsxbsa.h
@@ -808,14 +808,6 @@ extern char           traceStr2[];
 #define dsFree(a)     free(a)
 
 extern void StrUpper(char *s);
-/* Min() and Max() macros */
-#ifndef min
-#define min(a,b)  (((a) < (b)) ? (a) : (b))
-#endif
-
-#ifndef max
-#define max(a,b)  (((a) > (b)) ? (a) : (b))
-#endif
 
 /* Combined file name w/ line number */
 #define TrFL __FILE__, __LINE__

src/config/stds.h
@@ -176,14 +176,6 @@ typedef struct afs_hyper_t {	/* unsigned 64 bit integers */
 
 #define hadd(a,b) (hadd32(a,(b).low), (a).high += (b).high)
 
-#if !defined(KERNEL) || defined(UKERNEL)
-#ifndef AFS_NT40_ENV
-#define max(a, b)               ((a) < (b) ? (b) : (a))
-#define min(a, b)               ((a) > (b) ? (b) : (a))
-#endif
-/*#define abs(x)                  ((x) >= 0 ? (x) : -(x))*/
-#endif
-
 /* minumum length of string to pass to int_to_base64 */
 typedef char b64_string_t[8];
 #ifndef AFS_NT40_ENV

src/rxkad/rxkad_client.c
@@ -45,10 +45,6 @@
 #include "private_data.h"
 #define XPRT_RXKAD_CLIENT
 
-#ifndef max
-#define	max(a,b)    ((a) < (b)? (b) : (a))
-#endif /* max */
-
 static struct rx_securityOps rxkad_client_ops = {
     rxkad_Close,
     rxkad_NewConnection,	/* every new connection */

src/tests/create-files.c
@@ -46,11 +46,6 @@
 
 #include <err.h>
 
-#undef max
-#define max(a,b) ((a) > (b) ? (a) : (b))
-#undef min
-#define min(a,b) ((a) < (b) ? (a) : (b))
-
 static int
 creat_files(int count, long startsize)
 {

src/util/ktime.h
@@ -62,5 +62,4 @@ struct ktime {
 
 afs_int32 ktime_InterpretDate(struct ktime_date *akdate);
 
-#define min(a,b) (((a) < (b)) ? (a) : (b))
 #endif /* __INCL_KTIME_ */

commit 21491d9a822433c42b031b173bf0015a3122d084

Restore max() macro for Heimdal
Commit de46edef693d (Remove lowercase min/max macro definitions)
inadvertently removed a definition of max() that is required by
_krb5_n_fold() (src/external/heimdal/krb5/n-fold.c) if no system
definition of max() is available.  For example, on Solaris this results
in a kernel module with an undefined external symbol 'max', which
prevents the kernel module from loading:

  genunix: [ID 819705 kern.notice] /kernel/drv/amd64/afs: undefined symbol
  genunix: [ID 826211 kern.notice]  'max'
  genunix: [ID 472681 kern.notice] WARNING: mod_load: cannot load module 'afs'

Restore the required max() macro definition.

Change-Id: I6bca2bb2b90d7cdbe90a3b769997cdc153f59f6c
Reviewed-on: https://gerrit.openafs.org/15874
Tested-by: BuildBot <buildbot@rampaginggeek.com>
Reviewed-by: Cheyenne Wills <cwills@sinenomine.net>
Reviewed-by: Andrew Deason <adeason@sinenomine.net>

src/crypto/rfc3961/krb5_locl.h
@@ -66,6 +66,10 @@
 
 #define ALLOC(X, N) (X) = calloc((N), sizeof(*(X)))
 
+#ifndef max
+#define max(a,b) (((a)>(b))?(a):(b))
+#endif
+
 #ifndef O_BINARY
 #define O_BINARY 0
 #endif