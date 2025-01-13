134b6711716c54ce495b393d19c3bd099d784239
Andrew Deason <adeason@sinenomine.net>
2025-01-13 16:29:42 -0600

    LINUX: Clean up param.linux26.h
    Indent preprocessor directives. Remove unnecessary #endif comments for
    very short #if blocks. Shorten #endif comments to our conventional style
    (e.g., '!UKERNEL' instead of '!defined(UKERNEL)'). Reduce some
    unnecessary extra indentation.
    
    Change-Id: I8f714a05b0ef876036dffbfd7e2f54844e793740
    Reviewed-on: https://gerrit.openafs.org/16084
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Cheyenne Wills <cwills@sinenomine.net>
    Reviewed-by: Michael Meffie <mmeffie@sinenomine.net>

--- src/config/param.linux26.h
+++ src/config/param.linux26.h
@@ -13,78 +13,78 @@
 #ifndef UKERNEL
 
 /* This section for kernel libafs compiles only */
-#include <linux/version.h>
+# include <linux/version.h>
 
-#define AFS_LINUX_ENV		1
+# define AFS_LINUX_ENV		1
 
-#define AFS_64BIT_IOPS_ENV	1
-#define AFS_NAMEI_ENV		1	/* User space interface to file system */
-#define AFS_64BIT_CLIENT	1
-#undef  AFS_NONFSTRANS
-#define AFS_NONFSTRANS		1
-#define AFS_USERSPACE_IP_ADDR	1
-#define RXK_LISTENER_ENV	1
-#define AFS_GCPAGS		1	/* Set to Userdisabled, allow sysctl to override */
-#define AFS_PAG_ONEGROUP_ENV	1
-#define AFS_HAVE_FFS		1	/* Use system's ffs */
-#define AFS_HAVE_STATVFS	0	/* System doesn't support statvfs */
-#define AFS_VM_RDWR_ENV		1	/* read/write implemented via VM */
-#define AFS_USE_GETTIMEOFDAY	1	/* use gettimeofday to implement rx clock */
-#define AFS_MAXVCOUNT_ENV       1
-#define AFS_NEW_BKG             1
+# define AFS_64BIT_IOPS_ENV	1
+# define AFS_NAMEI_ENV		1 /* User space interface to file system */
+# define AFS_64BIT_CLIENT	1
+# undef  AFS_NONFSTRANS
+# define AFS_NONFSTRANS		1
+# define AFS_USERSPACE_IP_ADDR	1
+# define RXK_LISTENER_ENV	1
+# define AFS_GCPAGS		1 /* Set to Userdisabled, allow sysctl to override */
+# define AFS_PAG_ONEGROUP_ENV	1
+# define AFS_HAVE_FFS		1 /* Use system's ffs */
+# define AFS_HAVE_STATVFS	0 /* System doesn't support statvfs */
+# define AFS_VM_RDWR_ENV	1 /* read/write implemented via VM */
+# define AFS_USE_GETTIMEOFDAY	1 /* use gettimeofday to implement rx clock */
+# define AFS_MAXVCOUNT_ENV	1
+# define AFS_NEW_BKG		1
 
-#define AFS_PRIVATE_OSI_ALLOCSPACES	1
+# define AFS_PRIVATE_OSI_ALLOCSPACES 1
 
-#if defined(__KERNEL__)
-#define AFS_GLOBAL_SUNLOCK
-#endif /* __KERNEL__ */
+# if defined(__KERNEL__)
+#  define AFS_GLOBAL_SUNLOCK
+# endif
 
-#ifndef KERNEL
-#define __USE_LARGEFILE64 1
-#if !defined off64_t
-#define off64_t __off64_t
-#endif
-#endif
+# ifndef KERNEL
+#  define __USE_LARGEFILE64 1
+#  if !defined off64_t
+#   define off64_t __off64_t
+#  endif
+# endif /* !KERNEL */
 
-#else /* !defined(UKERNEL) */
+#else /* !UKERNEL */
 
 /* This section for user space compiles only */
 
-#define AFS_USR_LINUX_ENV	1
+# define AFS_USR_LINUX_ENV	1
 
-#define AFS_ENV			1
-#define AFS_64BIT_CLIENT	1
-#undef  AFS_NONFSTRANS
-#define AFS_NONFSTRANS 		1
-#define AFS_64BIT_IOPS_ENV	1
-#define AFS_NAMEI_ENV		1	/* User space interface to file system */
-#define AFS_USERSPACE_IP_ADDR 	1
-#define RXK_LISTENER_ENV 	1
-#define AFS_GCPAGS		0	/* if nonzero, garbage collect PAGs */
-#define AFS_HAVE_FFS		1	/* Use system's ffs. */
-#define AFS_HAVE_STATVFS	0	/* System doesn't support statvfs */
+# define AFS_ENV		1
+# define AFS_64BIT_CLIENT	1
+# undef  AFS_NONFSTRANS
+# define AFS_NONFSTRANS 	1
+# define AFS_64BIT_IOPS_ENV	1
+# define AFS_NAMEI_ENV		1 /* User space interface to file system */
+# define AFS_USERSPACE_IP_ADDR 	1
+# define RXK_LISTENER_ENV 	1
+# define AFS_GCPAGS		0 /* if nonzero, garbage collect PAGs */
+# define AFS_HAVE_FFS		1 /* Use system's ffs. */
+# define AFS_HAVE_STATVFS	0 /* System doesn't support statvfs */
 
-#define	afsio_iov	uio_iov
-#define	afsio_iovcnt	uio_iovcnt
-#define	afsio_offset	uio_offset
-#define	afsio_seg	uio_segflg
-#define	afsio_fmode	uio_fmode
-#define	afsio_resid	uio_resid
-#define	AFS_UIOSYS	1
-#define	AFS_UIOUSER	UIO_USERSPACE
-#define	AFS_CLBYTES	MCLBYTES
-#define	AFS_MINCHANGE	2
-#define	VATTR_NULL	usr_vattr_null
+# define afsio_iov	uio_iov
+# define afsio_iovcnt	uio_iovcnt
+# define afsio_offset	uio_offset
+# define afsio_seg	uio_segflg
+# define afsio_fmode	uio_fmode
+# define afsio_resid	uio_resid
+# define AFS_UIOSYS	1
+# define AFS_UIOUSER	UIO_USERSPACE
+# define AFS_CLBYTES	MCLBYTES
+# define AFS_MINCHANGE	2
+# define VATTR_NULL	usr_vattr_null
 
-#define AFS_DIRENT
-#ifndef CMSERVERPREF
-#define CMSERVERPREF
-#endif
+# define AFS_DIRENT
+# ifndef CMSERVERPREF
+#  define CMSERVERPREF
+# endif
 
-#endif /* !defined(UKERNEL) */
+#endif /* !UKERNEL */
 
 #if defined(UKERNEL) || !defined(KERNEL)
-#include <features.h>
+# include <features.h>
 #endif
 
 #if defined(HAVE_LINUX_ERRQUEUE_H) && defined(HAVE_SETSOCKOPT_IP_RECVERR) && !defined(UKERNEL)
@@ -95,9 +95,9 @@
 #endif
 
 #ifdef __GLIBC__
-#if (__GLIBC__ > 2) || (__GLIBC__ == 2 && __GLIBC_MINOR__ > 3)
-#define USE_UCONTEXT
-#endif
+# if (__GLIBC__ > 2) || (__GLIBC__ == 2 && __GLIBC_MINOR__ > 3)
+#  define USE_UCONTEXT
+# endif
 #endif
 
 #include <afs/afs_sysnames.h>
