openafs/openafs

commit 8a040cfd848410b75b4e5ac5498f00f073932598

viced: remove static local realms
Use the new auth function to do the local realm match
check instead of static local realms lists.

Override the krb.conf file with the -realms command line
option.

Change-Id: Ic364e61b03385fbc9496ac4af3877a1fdee3a3a5
Reviewed-on: http://gerrit.openafs.org/6878
Tested-by: BuildBot <buildbot@rampaginggeek.com>
Reviewed-by: Derrick Brashear <shadow@dementix.org>

src/viced/host.c
@@ -2181,8 +2181,6 @@ h_GetHost_r(struct rx_connection *tcon)
 
 
 static char localcellname[PR_MAXNAMELEN + 1];
-char local_realms[AFS_NUM_LREALMS][AFS_REALM_SZ];
-int  num_lrealms = -1;
 
 /* not reentrant */
 void
@@ -2190,75 +2188,22 @@ h_InitHostPackage(void)
 {
     memset(&nulluuid, 0, sizeof(afsUUID));
     afsconf_GetLocalCell(confDir, localcellname, PR_MAXNAMELEN);
-    if (num_lrealms == -1) {
-	int i;
-	for (i=0; i<AFS_NUM_LREALMS; i++) {
-	    if (afs_krb_get_lrealm(local_realms[i], i) != 0 /*KSUCCESS*/)
-		break;
-	}
-
-	if (i == 0) {
-	    ViceLog(0,
-		    ("afs_krb_get_lrealm failed, using %s.\n",
-		     localcellname));
-	    strncpy(local_realms[0], localcellname, AFS_REALM_SZ);
-	    num_lrealms = i =1;
-	} else {
-	    num_lrealms = i;
-	}
-
-	/* initialize the rest of the local realms to nullstring for debugging */
-	for (; i<AFS_NUM_LREALMS; i++)
-	    local_realms[i][0] = '\0';
-    }
     rxcon_ident_key = rx_KeyCreate((rx_destructor_t) free);
     rxcon_client_key = rx_KeyCreate((rx_destructor_t) 0);
     MUTEX_INIT(&host_glock_mutex, "host glock", MUTEX_DEFAULT, 0);
 }
 
 static int
-MapName_r(char *aname, char *acell, afs_int32 * aval)
+MapName_r(char *uname, afs_int32 * aval)
 {
     namelist lnames;
     idlist lids;
     afs_int32 code;
-    afs_int32 anamelen, cnamelen;
-    int foreign = 0;
-    char *tname;
-
-    anamelen = strlen(aname);
-    if (anamelen >= PR_MAXNAMELEN)
-	return -1;		/* bad name -- caller interprets this as anonymous, but retries later */
 
     lnames.namelist_len = 1;
-    lnames.namelist_val = (prname *) aname;	/* don't malloc in the common case */
+    lnames.namelist_val = (prname *) uname;
     lids.idlist_len = 0;
     lids.idlist_val = NULL;
-
-    cnamelen = strlen(acell);
-    if (cnamelen) {
-	if (afs_is_foreign_ticket_name(aname, NULL, acell, localcellname)) {
-	    ViceLog(2,
-		    ("MapName: cell is foreign.  cell=%s, localcell=%s, localrealms={%s,%s,%s,%s}\n",
-		    acell, localcellname, local_realms[0],local_realms[1],local_realms[2],local_realms[3]));
-	    if ((anamelen + cnamelen + 1) >= PR_MAXNAMELEN) {
-		ViceLog(2,
-			("MapName: Name too long, using AnonymousID for %s@%s\n",
-			 aname, acell));
-		*aval = AnonymousID;
-		return 0;
-	    }
-	    foreign = 1;	/* attempt cross-cell authentication */
-	    tname = (char *)malloc(PR_MAXNAMELEN);
-	    if (!tname) {
-		ViceLogThenPanic(0, ("Failed malloc in MapName_r\n"));
-	    }
-	    strcpy(tname, aname);
-	    tname[anamelen] = '@';
-	    strcpy(tname + anamelen + 1, acell);
-	    lnames.namelist_val = (prname *) tname;
-	}
-    }
 
     H_UNLOCK;
     code = hpr_NameToId(&lnames, &lids);
@@ -2278,10 +2223,6 @@ MapName_r(char *aname, char *acell, afs_int32 * aval)
 		     lnames.namelist_val[0]));
 	    code = -1;
 	}
-    }
-
-    if (foreign) {
-	free(lnames.namelist_val);	/* We allocated this above, so we must free it now. */
     }
     return code;
 }
@@ -2319,6 +2260,34 @@ h_ID2Client(afs_int32 vid)
 
     H_UNLOCK;
     return NULL;
+}
+
+static int
+format_vname(char *vname, int usize, const char *tname, const char *tinst,
+	     const char *tcell, afs_int32 islocal)
+{
+    int len;
+
+    len = strlcpy(vname, tname, usize);
+    if (len >= usize)
+	return -1;
+    if (tinst[0]) {
+	len = strlcat(vname, ".", usize);
+	if (len >= usize)
+	    return -1;
+	len = strlcat(vname, tinst, usize);
+	if (len >= usize)
+	    return -1;
+    }
+    if (tcell[0] && !islocal) {
+	len = strlcat(vname, "@", usize);
+	if (len >= usize)
+	    return -1;
+	len = strlcat(vname, tcell, usize);
+	if (len >= usize)
+	    return -1;
+    }
+    return 0;
 }
 
 /*
@@ -2386,6 +2355,7 @@ h_FindClient_r(struct rx_connection *tcon)
 	expTime = 0x7fffffff;
     } else if (authClass == 2) {
 	afs_int32 kvno;
+	afs_int32 islocal;
 
 	/* kerberos ticket */
 	code = rxkad_GetServerInfo(tcon, /*level */ 0, (afs_uint32 *)&expTime,
@@ -2395,29 +2365,34 @@ h_FindClient_r(struct rx_connection *tcon)
 	    viceid = AnonymousID;
 	    expTime = 0x7fffffff;
 	} else {
-	    int ilen = strlen(tinst);
 	    ViceLog(5,
 		    ("FindClient: rxkad conn: name=%s,inst=%s,cell=%s,exp=%d,kvno=%d\n",
 		     tname, tinst, tcell, expTime, kvno));
-	    strncpy(uname, tname, sizeof(uname));
-	    if (ilen) {
-		if (strlen(uname) + 1 + ilen >= sizeof(uname)) {
-		    code = -1;
-		    goto bad_name;
-		}
-		strcat(uname, ".");
-		strcat(uname, tinst);
-	    }
-	    /* translate the name to a vice id */
-	    code = MapName_r(uname, tcell, &viceid);
+	    code = afsconf_IsLocalRealmMatch(confDir, &islocal, tname, tinst, tcell);
 	    if (code) {
-	      bad_name:
-		ViceLog(1,
-			("failed to map name=%s, cell=%s -> code=%d\n", uname,
-			 tcell, code));
-		fail = 1;
+		ViceLog(0, ("FindClient: local realm check failed; code=%d", code));
 		viceid = AnonymousID;
 		expTime = 0x7fffffff;
+	    }
+	    if (!code) {
+		code = format_vname(uname, sizeof(uname), tname, tinst, tcell, islocal);
+		if (code) {
+		    ViceLog(0, ("FindClient: uname truncated."));
+		    viceid = AnonymousID;
+		    expTime = 0x7fffffff;
+		}
+	    }
+	    if (!code) {
+		/* translate the name to a vice id */
+		code = MapName_r(uname, &viceid);
+		if (code) {
+		    ViceLog(1,
+			    ("failed to map name=%s -> code=%d\n", uname,
+			     code));
+		    fail = 1;
+		    viceid = AnonymousID;
+		    expTime = 0x7fffffff;
+		}
 	    }
 	}
     } else {

src/viced/viced.c
@@ -1362,8 +1362,6 @@ ParseArgs(int argc, char *argv[])
     /* rxkad options */
     cmd_OptionAsFlag(opts, OPT_dotted, &rxkadDisableDotCheck);
     if (cmd_OptionAsList(opts, OPT_realm, &optlist) == 0) {
-	extern char local_realms[AFS_NUM_LREALMS][AFS_REALM_SZ];
-	extern int num_lrealms;
 
 	for (; optlist != NULL; optlist=optlist->next) {
 	    if (strlen(optlist->data) >= AFS_REALM_SZ) {
@@ -1371,16 +1369,7 @@ ParseArgs(int argc, char *argv[])
 		       "characters.\n", AFS_REALM_SZ);
 		return -1;
 	    }
-
-	    if (num_lrealms == -1)
-		num_lrealms = 0;
-	    if (num_lrealms >= AFS_NUM_LREALMS) {
-		printf("a maximum of %d -realm arguments can be "
-		       "specified.\n", AFS_NUM_LREALMS);
-	        return -1;
-	    }
-
-	    strncpy(local_realms[num_lrealms++], optlist->data, AFS_REALM_SZ);
+	    afsconf_SetLocalRealm(optlist->data); /* overrides krb.conf file, if one */
 	}
     }
 

commit c57eddb1f24eba22902fe7726c54baedef642e06

viced: Give an actual 'prname' to hpr_NameToId
In MapName_r(), we cast 'uname' (a char*) into a prname* when
populating a 'namelist' to give to hpr_NameToId(). A 'prname' is just
an rxgen-derived typedef for char[PR_MAXNAMELEN], which looks similar
to a char*, but is a fixed-size array.

MapName_r() passes the given 'namelist' to hpr_NameToId(), which calls
ubik_PR_NameToID() -> PR_NameToID() -> xdr_namelist() -> xdr_array()
-> xdr_prname() -> xdr_vector(). That will send exactly PR_MAXNAMELEN
characters of the given name to the wire, even if the string is only a
few characters long. If the given string is significantly shorter than
PR_MAXNAMELEN characters, then we may send uninitialized data from the
stack on the wire.

The ptserver should ignore any data after the trailing NUL byte in the
string, but the data will still be exposed on the wire, possibly
allowing others to see the uninitialized stack data.

To fix this, copy the given string into a local blanked 'prname', and
use that instead, avoiding the cast.

Also fix the 'ptclient' program, which uses a proper 'prname' array,
but doesn't initialize all of the memory, causing a similar problem.
Just change a malloc() to calloc() to fix it.

All other callers of PR_NameToID pass a properly-initialized 'prname'
(or an array of them), or use strncpy() to copy the given string,
which fills the given array with NUL bytes. However, change several
callers to memset() or calloc() the relevant memory, to try to avoid
similar mistakes in the future (for example, if someone changes a
strncpy() call to strlcpy()).

The issue in the fileserver calling hpr_NameToId() was introduced in
commit 8a040cfd848410b75b4e5ac5498f00f073932598 (viced: remove static
local realms), included in OpenAFS 1.8.0, since the given string was
no longer NUL-filled by strncpy().

FIXES 135444

Change-Id: Ia1dfc56d0ca8e4b57264150761f576d2621f0c21
Reviewed-on: https://gerrit.openafs.org/16147
Reviewed-by: Andrew Deason <adeason@sinenomine.net>

src/WINNT/afsd/cm_ioctl.c
@@ -2518,7 +2518,7 @@ cm_UsernameToId(char *uname, cm_ucell_t * ucellp, afs_uint32* uid)
     lids.idlist_len = 0;
     lids.idlist_val = 0;
     lnames.namelist_len = 1;
-    lnames.namelist_val = (prname *) malloc(PR_MAXNAMELEN);
+    lnames.namelist_val = calloc(1, PR_MAXNAMELEN);
     strncpy(lnames.namelist_val[0], uname, PR_MAXNAMELEN);
     lnames.namelist_val[0][PR_MAXNAMELEN-1] = '\0';
     for ( p=lnames.namelist_val[0], r=NULL; *p; p++ ) {

src/libadmin/pts/afs_ptsAdmin.c
@@ -191,6 +191,8 @@ TranslateTwoNames(const afs_cell_handle_p c_handle, const char *id1,
     namelist names;
     char tmp_array[2 * PTS_MAX_NAME_LEN];
 
+    memset(tmp_array, 0, sizeof(tmp_array));
+
     /*
      * Copy the group and user names in order to translate them
      */
@@ -270,6 +272,8 @@ TranslateOneName(const afs_cell_handle_p c_handle, const char *ptsName,
     namelist names[1];
     char tmp_array[PTS_MAX_NAME_LEN];
     idlist ids;
+
+    memset(tmp_array, 0, sizeof(tmp_array));
 
     /*
      * Copy the name in order to translate it

src/ptserver/ptclient.c
@@ -342,7 +342,7 @@ main(int argc, char **argv)
 	    lid.idlist_val = 0;
 	    lid.idlist_len = 0;
 	} else if (!strcmp(op, "ntoi")) {
-	    lnames.namelist_val = malloc(PR_MAXLIST * PR_MAXNAMELEN);
+	    lnames.namelist_val = calloc(PR_MAXLIST, PR_MAXNAMELEN);
 	    lnames.namelist_len = 0;
 	    foo = line;
 	    skip(&foo);

src/ptserver/pts.c
@@ -441,14 +441,14 @@ GetNameOrId(struct cmd_syndesc *as, struct idlist *lids,
     lids->idlist_len = n;
     ids.idlist_val = malloc(n * sizeof(afs_int32));
     ids.idlist_len = n;
-    names.namelist_val = malloc(n * PR_MAXNAMELEN);
+    names.namelist_val = calloc(n, PR_MAXNAMELEN);
     names.namelist_len = n;
     if (lnames) {
 	lnames->namelist_val = malloc(n * PR_MAXNAMELEN);
 	lnames->namelist_len = 0;
     }
     for (i = as->parms[0].items; i; i = i->next) {
-	tnames.namelist_val = malloc(PR_MAXNAMELEN);
+	tnames.namelist_val = calloc(1, PR_MAXNAMELEN);
 	strncpy(tnames.namelist_val[0], i->data, PR_MAXNAMELEN);
 	tnames.namelist_len = 1;
 	tids.idlist_len = 0;

src/ptserver/ptuser.c
@@ -465,7 +465,7 @@ pr_AddToGroup(prname user, prname group)
     if (code)
 	return code;
     lnames.namelist_len = 2;
-    lnames.namelist_val = malloc(2 * PR_MAXNAMELEN);
+    lnames.namelist_val = calloc(2, PR_MAXNAMELEN);
     strncpy(lnames.namelist_val[0], user, PR_MAXNAMELEN);
     strncpy(lnames.namelist_val[1], group, PR_MAXNAMELEN);
     lids.idlist_val = 0;
@@ -508,7 +508,7 @@ pr_RemoveUserFromGroup(prname user, prname group)
     if (code)
 	return code;
     lnames.namelist_len = 2;
-    lnames.namelist_val = malloc(2 * PR_MAXNAMELEN);
+    lnames.namelist_val = calloc(2, PR_MAXNAMELEN);
     strncpy(lnames.namelist_val[0], user, PR_MAXNAMELEN);
     strncpy(lnames.namelist_val[1], group, PR_MAXNAMELEN);
     lids.idlist_val = 0;
@@ -570,7 +570,7 @@ pr_SNameToId(prname name, afs_int32 *id)
     lids.idlist_len = 0;
     lids.idlist_val = 0;
     lnames.namelist_len = 1;
-    lnames.namelist_val = malloc(PR_MAXNAMELEN);
+    lnames.namelist_val = calloc(1, PR_MAXNAMELEN);
     stolower(name);
     strncpy(lnames.namelist_val[0], name, PR_MAXNAMELEN);
     code = ubik_PR_NameToID(pruclient, 0, &lnames, &lids);

src/viced/host.c
@@ -2431,9 +2431,17 @@ MapName_r(char *uname, afs_int32 * aval)
     namelist lnames;
     idlist lids;
     afs_int32 code;
+    prname namebuf;
+
+    memset(&namebuf, 0, sizeof(namebuf));
+
+    if (strlcpy(namebuf, uname, sizeof(namebuf)) >= sizeof(namebuf)) {
+	ViceLog(0, ("MapName: name %s is too long\n", uname));
+	return -1;
+    }
 
     lnames.namelist_len = 1;
-    lnames.namelist_val = (prname *) uname;
+    lnames.namelist_val = &namebuf;
     lids.idlist_len = 0;
     lids.idlist_val = NULL;
 

src/libacl/test/acltest.c
@@ -235,7 +235,7 @@ main()
 		continue;
 	    }
 	    names.namelist_len = 1;
-	    names.namelist_val = malloc(strlen(name) + 1);
+	    names.namelist_val = calloc(1, PR_MAXNAMELEN);
 	    strncpy(names.namelist_val, name, PR_MAXNAMELEN);
 	    code = pr_NameToId(&names, &ids);
 	    if (code) {