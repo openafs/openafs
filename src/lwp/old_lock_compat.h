52eb39bb1cf7fac0d12210c64c93ecdc7b88652c
Andrew Deason <adeason@sinenomine.net>
2025-10-20 16:30:34 -0500

    lwp: Remove compat header /usr/include/lock.h
    Change I85b85ed4e0425e08bfee873f23698a4bf65dc0e4 (lwp: Install compat
    header /usr/include/lock.h) installed a lock.h header for compatibility
    with old software that may be using lock.h instead of the newer
    afs/afs_lock.h. This should not be necessary anymore, so remove the
    compat header.
    
    This reverts change I85b85ed4e0425e08bfee873f23698a4bf65dc0e4.
    
    Change-Id: I4a684056d84987701b3bcd636b45d940de754c12
    Reviewed-on: https://gerrit.openafs.org/16563
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Cheyenne Wills <cwills@sinenomine.net>
    Reviewed-by: Michael Meffie <mmeffie@sinenomine.net>

--- src/lwp/old_lock_compat.h
+++
@@ -1,7 +0,0 @@
-/*
- * This is a compatibility header for <afs/afs_lock.h> in OpenAFS. OpenAFS used
- * to include a header <lock.h>, but it was renamed to <afs/afs_lock.h>. This
- * header exists just to allow older source code built against <lock.h> to
- * still compile, and should be removed when possible.
- */
-#include <afs/afs_lock.h>
