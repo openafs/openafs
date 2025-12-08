6461017a87ced8f24441234abf8e5c99eeab5325
Michael Meffie <mmeffie@sinenomine.net>
2025-12-08 18:29:18 -0500

    build: Remove `which` dependency for configure generation
    The build scripts used to generate the `configure` script currently
    depend on the `which` command.  In minimal Linux environments (such as
    containers), the `which` command may not be installed.
    
    Instead of running `which`, update regen.sh to check for the libtoolize
    and aclocal commands by running them with --version to check if they are
    available.  Update the git-version command to run git with the 'git
    version' command to check if it is available.
    
    This allows us to run regen.sh on a minimal system that has git,
    autoconf, and libtool install, but is missing the `which` command.
    
    Change-Id: Idfc9de46e77be3767512bae6a0a2e8b6b7d8d9bf
    Reviewed-on: https://gerrit.openafs.org/16645
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Cheyenne Wills <cwills@sinenomine.net>
    Reviewed-by: Michael Meffie <mmeffie@sinenomine.net>

--- regen.sh
+++ regen.sh
@@ -3,9 +3,9 @@
 echo "Updating configuration..."
 
 echo "Running libtoolize"
-if which libtoolize > /dev/null 2>&1; then
+if libtoolize --version > /dev/null 2>&1; then
     libtoolize -c -f -i
-elif which glibtoolize > /dev/null 2>&1; then
+elif glibtoolize --version > /dev/null 2>&1; then
     glibtoolize -c -f -i
 else
   echo "No libtoolize found on your system (looked for libtoolize & glibtoolize)"
@@ -17,9 +17,9 @@ M4_INCS="$M4_INCS -I src/external/rra-c-util/m4"
 M4_INCS="$M4_INCS -I src/external/autoconf-archive/m4"
 
 echo "Running aclocal"
-if which aclocal > /dev/null 2>&1; then
+if aclocal --version > /dev/null 2>&1; then
   aclocal $M4_INCS
-elif which aclocal-1.10 > /dev/null 2>&1; then
+elif aclocal-1.10 --version > /dev/null 2>&1; then
   aclocal-1.10 $M4_INCS
 else
   echo "No aclocal found on your system (looked for aclocal & aclocal-1.10)"
