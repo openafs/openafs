11849e96820eca64d91742a8c521614e1e99d9fa
Andrew Deason <adeason@sinenomine.net>
2026-02-20 11:57:49 -0600

    LINUX: Re-dirty folio on writepages recursion
    Commit b2c039541d (linux: convert aops->writepage to writepages)
    introduced afs_linux_writefolio_cb(), and returned early when
    afs_linux_begin_writeback() detects recursion and returns
    AOP_WRITEPAGE_ACTIVATE. This follows similar existing logic in
    afs_linux_writepage(), but in this code path where write_cache_pages()
    calls afs_linux_writefolio_cb(), there is not much special handling for
    the AOP_WRITEPAGE_ACTIVATE return code (write_cache_pages() just unlocks
    the page).
    
    In afs_linux_writefolio_cb(), when we see AOP_WRITEPAGE_ACTIVATE and
    return, the page has not actually been written out and we never called
    folio_start_writeback() on the page. write_cache_pages() calls our
    afs_linux_writefolio_cb() after calling folio_clear_dirty_for_io(), so
    the dirty flag has been cleared on the page at this point. (Via:
    write_cache_pages -> writeback_iter -> writeback_get_folio ->
    folio_prepare_writeback -> folio_clear_dirty_for_io.) And
    folio_clear_dirty_for_io() clearly states in its comments that we are
    supposed to call either folio_start_writeback() or folio_mark_dirty()
    afterwards, in order to ensure coherence between the page's dirty flag
    and the page's dirty xarray tag.
    
    Also in this AOP_WRITEPAGE_ACTIVATE code path, we never folio_put() our
    extra folio_get()'d reference from earlier in this function, since we
    don't go through our 'goto done' destructor.
    
    So, to make sure the page is properly still marked as dirty in the
    AOP_WRITEPAGE_ACTIVATE case, call folio_mark_dirty() on it. Also release
    the extra reference with folio_put().
    
    Also return a normal success, instead of returning the special value
    AOP_WRITEPAGE_ACTIVATE. Our caller, write_cache_pages(), just unlocks
    the page and ignores the error, so returning this special value doesn't
    do anything special here, and just perpetuates a weird inconsistent
    calling convention.
    
    Reviewed-on: https://gerrit.openafs.org/16696
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Benjamin Kaduk <kaduk@mit.edu>
    Reviewed-by: Cheyenne Wills <cwills@sinenomine.net>
    (cherry picked from commit 291a26a23bcf18b09f0c571d793273cc5e909123)
    
    Change-Id: I8d2890070f9f87bfb51454f35a1a35fdb2b4edff
    Reviewed-on: https://gerrit.openafs.org/16702
    Tested-by: BuildBot <buildbot@rampaginggeek.com>
    Reviewed-by: Marcio Brito Barbosa <mbarbosa@sinenomine.net>
    Reviewed-by: Michael Meffie <mmeffie@sinenomine.net>
    Reviewed-by: Mark Vitale <mvitale@sinenomine.net>
    Reviewed-by: Andrew Deason <adeason@sinenomine.net>
    Reviewed-by: Benjamin Kaduk <kaduk@mit.edu>

--- src/afs/LINUX/osi_vnodeops.c
+++ src/afs/LINUX/osi_vnodeops.c
@@ -3569,9 +3569,12 @@ afs_linux_writefolio_cb(struct folio *folio, struct writeback_control *wbc, void
     }
     code = afs_linux_begin_writeback(vcp, &credp);
     if (code == AOP_WRITEPAGE_ACTIVATE) {
-	/* WRITEPAGE_ACTIVATE is the only return value that permits us
-	 * to return with the folio still locked */
-	return code;
+	/*
+	 * afs_linux_begin_writeback() detected a recursive writeback, so bail
+	 * out. We didn't actually write out the folio, so mark it as dirty
+	 * again.
+	 */
+	goto redirty;
     }
 
     folio_start_writeback(folio);
@@ -3612,6 +3615,12 @@ afs_linux_writefolio_cb(struct folio *folio, struct writeback_control *wbc, void
     }
 
     return code;
+
+ redirty:
+    folio_mark_dirty(folio);
+    folio_unlock(folio);
+    folio_put(folio);
+    return 0;
 }
 
 static int
