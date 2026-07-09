openafs/openafs

commit aa496684774b2e6ac8691067bd09f9e6ab5c8506

afs: Add basic storage of rxgk tokens
Add the types and code necessary for storing rxgk tokens in libafs
(that is, cell-wide dbserver rxgk tokens). This allows userspace
callers to be able to store rxgk tokens via ktc_SetTokenEx(), and
retrieve them via ktc_GetTokenEx(), ktc_ListTokensEx(), etc. The cache
manager does not yet make use of any rxgk tokens; they can only be used
by userspace applications with this commit.

Also add rxgk support to the 'tokens' program, so we can see any rxgk
tokens present in the kernel.

Change-Id: I71189f306c6cacf193de9e81aa1649f7a06db951
Co-authored-by: Andrew Deason <adeason@sinenomine.net>
Reviewed-on: https://gerrit.openafs.org/16829
Reviewed-by: Marcio Brito Barbosa <mbarbosa@sinenomine.net>
Reviewed-by: Cheyenne Wills <cwills@sinenomine.net>
Reviewed-by: Mark Vitale <mvitale@sinenomine.net>
Reviewed-by: Andrew Deason <adeason@sinenomine.net>
Reviewed-by: Michael Meffie <mmeffie@sinenomine.net>
Tested-by: BuildBot <buildbot@rampaginggeek.com>

src/afs/afs_tokens.c
@@ -1,5 +1,7 @@
 /*
  * Copyright (c) 2010 Your Filesystem Inc. All rights reserved.
+ * Copyright (c) 2014 the Massachusetts Institute of Technology.
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -27,6 +29,8 @@
 #include "afs/sysincludes.h"
 #include "afsincludes.h"
 #include "token.h"
+#include <rx/rxgk.h>
+#include <opr/time.h>
 
 /* A jar for storing tokens in */
 
@@ -86,6 +90,10 @@ afs_FreeFirstToken(struct tokenJar **tokenPtr)
 	    afs_osi_Free(token->content.rxkad.ticket,
 			 token->content.rxkad.ticketLen);
 	}
+	break;
+      case RX_SECIDX_GK:
+	rx_opaque_zeroFreeContents(&token->content.rxgk.token);
+	rx_opaque_zeroFreeContents(&token->content.rxgk.clearToken.k0);
 	break;
       default:
 	break;
@@ -163,6 +171,11 @@ afs_IsTokenExpired(struct tokenJar *token, afs_int32 now)
 	if (token->content.rxkad.clearToken.EndTimestamp < now - NOTOKTIMEOUT)
 	    return 1;
 	break;
+      case RX_SECIDX_GK:
+	if (opr_time64_toSecs(token->content.rxgk.clearToken.expiration) < now) {
+	    return 1;
+	}
+	break;
       default:
 	return 0;
     }
@@ -192,6 +205,9 @@ afs_IsTokenUsable(struct tokenJar *token, afs_int32 now)
 
     switch (token->type) {
       case RX_SECIDX_KAD:
+	/* We assume that all non-expired rxkad tokens are usable by us */
+	return 1;
+      case RX_SECIDX_GK:
 	/* We assume that all non-expired rxkad tokens are usable by us */
 	return 1;
       default :
@@ -373,6 +389,117 @@ rxkad_extractTokenForPioctl(struct tokenJar *token,
     return 0;
 }
 
+#ifdef AFS_RXGK_ENV
+/*!
+ * Add an rxgk token to the token jar
+ *
+ * @param[inout] tokens
+ * 	A pointer to the address of the jar to add the token to
+ * @param[inout] token
+ * 	An rx_opaque containing the opaque rxgk token. Zeroed before returning,
+ * 	since the memory is donated to the new token.
+ * @param[inout] clearToken
+ * 	The cleartext token information, including token master key. Zeroed
+ * 	before returning, since the memory in clearToken.k0 is donated to the
+ * 	new token.
+ */
+static void
+afs_AddRxgkToken(struct tokenJar **tokens, struct rx_opaque *token,
+		 struct ClearTokenRXGK *clearToken)
+{
+    union tokenUnion *tokenU;
+    struct rxgkToken *rxgk;
+
+    tokenU = afs_AddToken(tokens, RX_SECIDX_GK);
+    rxgk = &tokenU->rxgk;
+
+    rxgk->token = *token;
+    /* 'token' is donated; make sure the caller doesn't free it. */
+    memset(token, 0, sizeof(*token));
+
+    rxgk->clearToken = *clearToken;
+
+    /*
+     * clearToken.k0 is donated from the incoming clearToken; make sure the
+     * caller doesn't free it.
+     */
+    memset(clearToken, 0, sizeof(*clearToken));
+}
+
+static int
+afs_AddRxgkTokenFromPioctl(struct tokenJar **tokens,
+			   struct ktc_tokenUnion *pioctlToken)
+{
+    struct token_rxgk *rxgkPioctl;
+    struct rx_opaque token;
+    struct ClearTokenRXGK clear;
+    int code;
+
+    memset(&clear, 0, sizeof(clear));
+    memset(&token, 0, sizeof(token));
+
+    rxgkPioctl = &pioctlToken->ktc_tokenUnion_u.at_gk;
+
+    clear.ViceId = rxgkPioctl->gk_viceid;
+    clear.enctype = rxgkPioctl->gk_enctype;
+    clear.level = rxgkPioctl->gk_level;
+    clear.lifetime = rxgkPioctl->gk_lifetime;
+    clear.bytelife = rxgkPioctl->gk_bytelife;
+    clear.expiration = rxgkPioctl->gk_expiration;
+    code = rx_opaque_populate(&clear.k0, rxgkPioctl->gk_k0.gk_k0_val,
+			      rxgkPioctl->gk_k0.gk_k0_len);
+    if (code != 0) {
+	goto done;
+    }
+
+    code = rx_opaque_populate(&token, rxgkPioctl->gk_token.gk_token_val,
+			      rxgkPioctl->gk_token.gk_token_len);
+    if (code != 0) {
+	goto done;
+    }
+
+    afs_AddRxgkToken(tokens, &token, &clear);
+
+ done:
+    rx_opaque_zeroFreeContents(&token);
+    rx_opaque_zeroFreeContents(&clear.k0);
+    return code;
+}
+
+static int
+rxgk_extractTokenForPioctl(struct tokenJar *token,
+			   struct ktc_tokenUnion *pioctlToken)
+{
+    struct token_rxgk *rxgkPioctl;
+    struct rxgkToken *rxgk;
+
+    rxgkPioctl = &pioctlToken->ktc_tokenUnion_u.at_gk;
+    rxgk = &token->content.rxgk;
+    rxgkPioctl->gk_viceid = rxgk->clearToken.ViceId;
+    rxgkPioctl->gk_enctype = rxgk->clearToken.enctype;
+    rxgkPioctl->gk_level = rxgk->clearToken.level;
+    rxgkPioctl->gk_lifetime = rxgk->clearToken.lifetime;
+    rxgkPioctl->gk_bytelife = rxgk->clearToken.bytelife;
+    rxgkPioctl->gk_expiration = rxgk->clearToken.expiration;
+    rxgkPioctl->gk_token.gk_token_val = xdr_alloc(rxgk->token.len);
+    if (rxgkPioctl->gk_token.gk_token_val == NULL) {
+	return ENOMEM;
+    }
+    memcpy(rxgkPioctl->gk_token.gk_token_val, rxgk->token.val,
+	   rxgk->token.len);
+    rxgkPioctl->gk_token.gk_token_len = rxgk->token.len;
+
+    rxgkPioctl->gk_k0.gk_k0_val = xdr_alloc(rxgk->clearToken.k0.len);
+    if (rxgkPioctl->gk_k0.gk_k0_val == NULL) {
+	return ENOMEM;
+    }
+    memcpy(rxgkPioctl->gk_k0.gk_k0_val, rxgk->clearToken.k0.val,
+	   rxgk->clearToken.k0.len);
+    rxgkPioctl->gk_k0.gk_k0_len = rxgk->clearToken.k0.len;
+    return 0;
+}
+#endif /* AFS_RXGK_ENV */
+
 /*!
  * Add a token to a token jar based on the input from a new-style
  * SetToken pioctl
@@ -394,6 +521,10 @@ afs_AddTokenFromPioctl(struct tokenJar **tokens,
     switch (pioctlToken->at_type) {
       case RX_SECIDX_KAD:
 	return afs_AddRxkadTokenFromPioctl(tokens, pioctlToken);
+#ifdef AFS_RXGK_ENV
+      case RX_SECIDX_GK:
+	return afs_AddRxgkTokenFromPioctl(tokens, pioctlToken);
+#endif
     }
 
     return EINVAL;
@@ -419,6 +550,11 @@ extractPioctlToken(struct tokenJar *token,
       case RX_SECIDX_KAD:
 	code = rxkad_extractTokenForPioctl(token, pioctlToken);
 	break;
+#ifdef AFS_RXGK_ENV
+      case RX_SECIDX_GK:
+	code = rxgk_extractTokenForPioctl(token, pioctlToken);
+	break;
+#endif
       default:
 	code = EINVAL;;
     }

src/afs/AIX/osi_misc.c
@@ -48,6 +48,7 @@
 #include "vldbint.h"
 #include "afs/lock.h"
 #include "afs/exporter.h"
+#include "afsincludes.h"
 #include "afs/afs.h"
 #include "afs/afs_stats.h"
 

src/afs/AIX/osi_vnodeops.c
@@ -42,6 +42,7 @@
 #include "vldbint.h"
 #include "afs/lock.h"
 #include "afs/exporter.h"
+#include "afsincludes.h"
 #include "afs/afs.h"
 #include "afs/afs_chunkops.h"
 #include "afs/afs_stats.h"
@@ -49,7 +50,6 @@
 #include "afs/icl.h"
 #include "afs/prs_fs.h"
 #include "h/flock.h"
-#include "afsincludes.h"
 
 static int afs_vm_rdwr(struct vnode *vp, struct uio *uiop, enum uio_rw rw,
 		       int ioflag, struct ucred *credp);

src/afs/SOLARIS/osi_kstat.c
@@ -36,12 +36,12 @@
 #include <rx/rx.h>
 
 #include "afs/sysincludes.h"
+#include "afs/afsincludes.h"
 #include "afsint.h"
 #include "vldbint.h"
 #include "afs/lock.h"
 #include "afs/afs_stats.h"
 #include "afs/afs.h"
-#include "afs/afsincludes.h"
 #include "afs/afs_prototypes.h"
 #include "rx/rx_atomic.h"
 #include "rx/rx_stats.h"

src/afs/UKERNEL/afsincludes.h
@@ -15,6 +15,7 @@
 #include "roken.h"
 #include "afs/opr.h"
 #include "rx/rx.h"
+#include "rx/rx_opaque.h"
 #include "afs/afs_osi.h"
 #include "afs/lock.h"
 #include "afs/volerrors.h"

src/afs/afs.h
@@ -207,6 +207,16 @@ struct ClearToken {
     afs_int32 EndTimestamp;
 };
 
+struct ClearTokenRXGK {
+    afs_int64 ViceId;
+    afs_int32 enctype;
+    afs_int32 level;
+    afs_uint32 lifetime;
+    afs_uint32 bytelife;
+    struct afs_time64 expiration;
+    struct rx_opaque k0;
+};
+
 struct VenusFid {
     afs_int32 Cell;		/* better sun packing if at end of structure */
     struct AFSFid Fid;
@@ -397,8 +407,14 @@ struct rxkadToken {
     struct ClearToken clearToken;
 };
 
+struct rxgkToken {
+    struct rx_opaque token;
+    struct ClearTokenRXGK clearToken;
+};
+
 union tokenUnion {
     struct rxkadToken rxkad;
+    struct rxgkToken rxgk;
 };
 
 struct tokenJar {

src/afs/afsincludes.h
@@ -29,6 +29,7 @@
 #  include "osi_vfs.h"
 # endif
 # include "rx/rx.h"
+# include "rx/rx_opaque.h"
 # include "afs/afs_osi.h"
 # include "afs/lock.h"
 # include "afs/volerrors.h"

src/auth/token.xg
@@ -7,6 +7,7 @@
 /* this file is in the public domain.  Do what thou wilt. */
 
 const AFSTOKEN_RK_TIX_MAX = 12000; 	/* Matches entry in rxkad.h */
+const AFSTOKEN_GK_TOK_MAX = 1048576;	/* Matches RXGK_MAXDATA in rxgk_int.xg */
 
 struct token_rxkad {
     afs_int32 rk_viceid;
@@ -18,11 +19,25 @@ struct token_rxkad {
     opaque rk_ticket<AFSTOKEN_RK_TIX_MAX>;
 };
 
+struct token_rxgk {
+    afs_int64 gk_viceid;	/* 64-bits to be future-proof. */
+    afs_int32 gk_enctype;
+    afs_int32 gk_level;		/* rxgk_int.xg has this as an enum. */
+    afs_uint32 gk_lifetime;
+    afs_uint32 gk_bytelife;
+    struct afs_time64 gk_expiration;
+    opaque gk_token<AFSTOKEN_GK_TOK_MAX>;
+    opaque gk_k0<AFSTOKEN_GK_TOK_MAX>;
+};
+
 const AFSTOKEN_UNION_NOAUTH = 0;
 const AFSTOKEN_UNION_KAD = 2;
+const AFSTOKEN_UNION_GK = 4;
 union ktc_tokenUnion switch (afs_int32 at_type) {
     case AFSTOKEN_UNION_KAD:
 	token_rxkad at_kad;
+    case AFSTOKEN_UNION_GK:
+	token_rxgk at_gk;
 };
 
 const AFSTOKEN_LENGTH_MAX = 16384;

commit 5be96bae8038917573eacf0dfd9680a9ce0553fb

afs: Fix typo in comment in afs_tokens.c
Introduced in commit aa49668477 (afs: Add basic storage of rxgk tokens).

Change-Id: Ie61d7956be86f6233b95f83d257892a18cc19684
Reviewed-on: https://gerrit.openafs.org/16867
Tested-by: BuildBot <buildbot@rampaginggeek.com>
Reviewed-by: Cheyenne Wills <cwills@sinenomine.net>
Reviewed-by: Andrew Deason <adeason@sinenomine.net>

src/afs/afs_tokens.c
@@ -208,7 +208,7 @@ afs_IsTokenUsable(struct tokenJar *token, afs_int32 now)
 	/* We assume that all non-expired rxkad tokens are usable by us */
 	return 1;
       case RX_SECIDX_GK:
-	/* We assume that all non-expired rxkad tokens are usable by us */
+	/* We assume that all non-expired rxgk tokens are usable by us */
 	return 1;
       default :
 	return 0;